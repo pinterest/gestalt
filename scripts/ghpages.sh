0ed4b4f202bc0ddeaa472c0c976d723777b0df50
Chris Lloyd <christopher.lloyd@gmail.com>
2018-02-17 08:57:28 -0800

    Fixes repo url in docs build script (#4)

--- scripts/ghpages.sh
+++ scripts/ghpages.sh
@@ -10,14 +10,14 @@ set -e
 # Prints out commands about to be executed
 set -x
 
-repo=${1:-pinterest}
+repo=${1:-https://github.com/pinterest/gestalt.git}
 
 git checkout -b tmp-deploy
 npm run build_docs
 git add -f docs
 git commit -m "Deployed to Github Pages"
-git push git@github.com:${repo}/gestalt.git :gh-pages
-git subtree push --prefix docs git@github.com:${repo}/gestalt.git gh-pages
+git push ${repo} :gh-pages
+git subtree push --prefix docs ${repo} gh-pages
 git checkout master
 git reset --hard HEAD
 git branch -D tmp-deploy
