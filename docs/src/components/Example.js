81c51c089657ce8eb75d61bd039e36e29c413ab8
Chris Lloyd <chrislloyd@pinterest.com>
2018-02-21 13:45:52 -0800

    more bad merges

--- docs/src/components/Example.js
+++ docs/src/components/Example.js
@@ -1,12 +1,8 @@
 // @flow
 import React from 'react';
-<<<<<<< Updated upstream
-import Box from '../../../src/Box/Box';
-import Container from '../../../src/Container/Container';
-import Text from '../../../src/Text/Text';
-=======
 import {
   Box,
+  Container,
   Text,
   Link,
   Icon,
@@ -15,9 +11,8 @@ import {
   Mask,
   Heading,
 } from 'gestalt';
->>>>>>> Stashed changes
-import Checkerboard from './Checkerboard';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+import Checkerboard from './Checkerboard';
 
 type Props = {|
   defaultCode: string,
@@ -25,7 +20,6 @@ type Props = {|
   stacked: boolean,
 |};
 
-<<<<<<< Updated upstream
 export default ({ defaultCode, scope, stacked = false }: Props) => (
   <LiveProvider
     code={defaultCode.trim()}
@@ -56,46 +50,12 @@ export default ({ defaultCode, scope, stacked = false }: Props) => (
             <Box paddingY={2}>
               <Text size="sm" color="gray">
                 Editor
-=======
-export default function Example({ defaultCode, scope }: Props) {
-  return (
-    <LiveProvider
-      code={defaultCode.trim()}
-      scope={{
-        Box,
-        Link,
-        Text,
-        Icon,
-        Checkbox,
-        Column,
-        Mask,
-        Heading,
-        ...scope,
-      }}
-    >
-      <Box>
-        <Box
-          display="flex"
-          direction="column"
-          mdDirection="row"
-          alignItems="stretch"
-          marginLeft={-2}
-          marginRight={-2}
-        >
-          <Box xs={{ column: 12 }} md={{ column: 6 }}>
-            <Box paddingX={2}>
-              <Box paddingY={2}>
-                <Text size="sm" color="gray">
-                  Editor
-                </Text>
-              </Box>
-              <Text>
-                <LiveEditor />
->>>>>>> Stashed changes
               </Text>
             </Box>
+            <Text>
+              <LiveEditor />
+            </Text>
           </Box>
-<<<<<<< Updated upstream
         </Box>
 
         <Box column={12} mdColumn={stacked ? 12 : 6}>
@@ -111,41 +71,18 @@ export default function Example({ defaultCode, scope }: Props) {
                 Preview
               </Text>
             </Box>
-=======
->>>>>>> Stashed changes
 
-          <Box xs={{ column: 12 }} md={{ column: 6 }}>
-            <Box
-              paddingX={2}
-              display="flex"
-              direction="column"
-              alignItems="stretch"
-              height="100%"
-            >
-              <Box paddingY={2}>
-                <Text size="sm" color="gray">
-                  Preview
-                </Text>
+            <Box flex="grow" position="relative">
+              <Box position="absolute" top bottom left right>
+                <Checkerboard />
               </Box>
-
-              <Box flex="grow" position="relative">
-                <Box position="absolute" top bottom left right>
-                  <Checkerboard />
-                </Box>
-                <Box position="relative" padding={4}>
-                  <LivePreview />
-                </Box>
+              <Box position="relative" padding={4}>
+                <LivePreview />
               </Box>
             </Box>
           </Box>
         </Box>
-        <Box padding={2}>
-          <Text color="watermelon" __dangerouslyIncreaseLineHeight>
-            <LiveError />
-          </Text>
-        </Box>
       </Box>
-<<<<<<< Updated upstream
       <Box padding={2}>
         <Text color="watermelon" leading="tall">
           <LiveError />
@@ -154,8 +91,3 @@ export default function Example({ defaultCode, scope }: Props) {
     </Box>
   </LiveProvider>
 );
-=======
-    </LiveProvider>
-  );
-}
->>>>>>> Stashed changes
