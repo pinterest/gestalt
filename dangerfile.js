5e191ad3efffaf618f3aac7be8e8e566fc0b27d6
Chris Lloyd <chrislloyd@pinterest.com>
2018-02-23 12:13:11 -0800

    update changelog

--- dangerfile.js
+++ dangerfile.js
@@ -1,7 +1,7 @@
 // @flow
 import { danger, warn } from 'danger';
 
-if (!danger.git.modified_files.includes('changelog.md')) {
+if (!danger.git.modified_files.includes('CHANGELOG.md')) {
   warn(
     'Please add a [changelog.md](https://github.com/pinterest/gestalt/blob/master/CHANGELOG.md) entry for your changes.'
   );
