923220454e5099ad185d85d26d0685d537e4ae12
Chris Lloyd <christopher.lloyd@gmail.com>
2018-02-28 15:23:10 -0800

    fix lint

--- test/node.webpack.config.js
+++ test/node.webpack.config.js
@@ -1,5 +1,4 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
   output: {
--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,5 +1,3 @@
-const webpack = require('webpack');
-
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: ['./views/main.js'],
