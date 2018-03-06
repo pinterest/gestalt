ba1a14890ef292ad4528b92c63fb517dc9fb9547
Christian Vuerings <cvuerings@pinterest.com>
2018-03-06 08:56:13 -0800

    Docs: fix github source link

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,7 @@
 * Flyout: Update the docs with correct flowtypes (#37)
 * Internal: Removes [corkboard](https://yarnpkg.com/en/package/corkboard) from the docs (#41)
 * Internal: User prettier for markdown and css (#45)
+* Docs: fix github source link (#50)
 
 </details>
 
--- docs/src/components/PageHeader.js
+++ docs/src/components/PageHeader.js
@@ -10,7 +10,7 @@ type Props = {|
 
 const githubSourceUrl = (component: string): string => {
   const file = component.replace(/\s+/g, '');
-  return `https://github.com/pinterest/gestalt/blob/master/src/${file}/${file}.js`;
+  return `https://github.com/pinterest/gestalt/blob/master/packages/gestalt/src/${file}/${file}.js`;
 };
 
 export default function PageHeader({ name, description = '' }: Props) {
