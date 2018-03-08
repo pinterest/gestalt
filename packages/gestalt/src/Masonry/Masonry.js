72e5f373803b18c9c814e9f33a047563b72cb994
Chris Lloyd <chrislloyd@pinterest.com>
2018-03-07 16:10:09 -0800

    Guard against Infinity values in Masonry

--- packages/gestalt/src/Masonry/Masonry.js
+++ packages/gestalt/src/Masonry/Masonry.js
@@ -65,10 +65,7 @@ const RESIZE_DEBOUNCE = 300;
 // The amount of extra buffer space for populating visible items.
 const VIRTUAL_BUFFER_FACTOR = 0.7;
 
-export default class ExperimentalMasonry<T> extends React.Component<
-  Props<T>,
-  State
-> {
+export default class Masonry<T> extends React.Component<Props<T>, State> {
   static createMeasurementStore() {
     return new MeasurementStore();
   }
@@ -394,8 +391,8 @@ export default class ExperimentalMasonry<T> extends React.Component<
           left: 0,
           transform: `translateX(${left}px) translateY(${top}px)`,
           WebkitTransform: `translateX(${left}px) translateY(${top}px)`,
-          width,
-          height,
+          width: width >= Infinity ? undefined : width,
+          height: height >= Infinity ? undefined : height,
           ...(virtualize || isVisible
             ? {}
             : { display: 'none', transition: 'none' }),
@@ -470,7 +467,9 @@ export default class ExperimentalMasonry<T> extends React.Component<
                 left: 0,
                 transform: 'translateX(0px) translateY(0px)',
                 WebkitTransform: 'translateX(0px) translateY(0px)',
-                width: flexible ? undefined : columnWidth, // we can't set a width for server rendered flexible items
+                width: flexible
+                  ? undefined
+                  : columnWidth >= Infinity ? undefined : columnWidth, // we can't set a width for server rendered flexible items
               }}
               ref={el => {
                 if (el && !flexible) {
