93e39595990b500c61d7f073cb661645e401d06a
Chong <cyao@pinterest.com>
2018-04-17 15:35:12 -0700

    Add color of mediumGray

--- docs/src/Heading.doc.js
+++ docs/src/Heading.doc.js
@@ -31,7 +31,7 @@ card(
       },
       {
         name: 'color',
-        type: `"blue" | "darkGray" | "eggplant" | "gray" | "green" | "lightGray" | "maroon" | "midnight" | "navy" | "olive" | "orange" | "orchid" | "pine" | "purple" | "red" | "watermelon" | "white"`,
+        type: `"blue" | "darkGray" | "eggplant" | "gray" | "green" | "lightGray" | "maroon" | "midnight" | "navy" | "olive" | "orange" | "orchid" | "pine" | "purple" | "red" | "watermelon" | "white" | "mediumGray"`,
         defaultValue: 'darkGray',
       },
       {
