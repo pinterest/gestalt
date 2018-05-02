8f0d837d508576915fb144411aa715b3ac0bd09f
Peter Farejowicz <pfarejowicz@pinterest.com>
2018-05-02 14:43:19 -0700

    Tidy up the video docs

---
+++ packages/gestalt/src/Video/VideoScrubber.js
@@ -0,0 +1,52 @@
+// @flow
+
+import * as React from 'react';
+import Box from '../Box/Box';
+
+type Props = {|
+  currentTime: number,
+  duration: number,
+|};
+
+export default class VideoScrubber extends React.PureComponent<Props> {
+  render() {
+    const { currentTime, duration } = this.props;
+    const scrubberWidth = `${Math.floor(currentTime * 10000 / duration) /
+      100}%`;
+    return (
+      <Box position="relative" display="flex" flex="grow" alignItems="center">
+        <Box
+          left
+          right
+          position="absolute"
+          color="lightGray"
+          shape="rounded"
+          height={4}
+        />
+        <Box
+          left
+          position="absolute"
+          color="white"
+          shape="rounded"
+          height={4}
+          width={scrubberWidth}
+        />
+        <Box
+          position="absolute"
+          shape="rounded"
+          height={4}
+          dangerouslySetInlineStyle={{ __style: { left: scrubberWidth } }}
+        >
+          <Box
+            shape="circle"
+            width={16}
+            height={16}
+            color="white"
+            marginLeft={-2}
+            dangerouslySetInlineStyle={{ __style: { marginTop: -6 } }}
+          />
+        </Box>
+      </Box>
+    );
+  }
+}
