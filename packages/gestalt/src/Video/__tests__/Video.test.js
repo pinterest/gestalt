06d0fcf5970350164bc62365e41d456ba3b962e2
Peter Farejowicz <pfarejowicz@pinterest.com>
2018-05-03 15:45:51 -0700

    Update snapshots

--- packages/gestalt/src/Video/__tests__/Video.test.js
+++ packages/gestalt/src/Video/__tests__/Video.test.js
@@ -28,6 +28,7 @@ test('Video with callbacks', () => {
     <Video
       src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
       onDurationChange={() => {}}
+      onFullScreenChange={() => {}}
       onPlay={() => {}}
       onPause={() => {}}
       onTimeUpdate={() => {}}
