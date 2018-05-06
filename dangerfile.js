2e7a9acabb749a559fec3e2a4dc81d3ee9805a08
Chris Lloyd <chrislloyd@pinterest.com>
2018-05-06 14:31:06 -0700

    Add the yarn dangerfile plugin

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,11 @@
 // @flow
 import { danger, warn } from 'danger';
+import yarn from 'danger-plugin-yarn';
 
 if (!danger.git.modified_files.includes('CHANGELOG.md')) {
   warn(
     'Please add a [changelog.md](https://github.com/pinterest/gestalt/blob/master/CHANGELOG.md) entry for your changes.'
   );
 }
+
+yarn();
