8bf53faaece85b9de1c4dd552c4bd529ccfe2c73
Peter Farejowicz <pfarejowicz@pinterest.com>
2018-05-07 14:58:27 -0700

    Convert timeToString tests to snapshot tests and remove export of util

--- packages/gestalt/src/Video/__tests__/VideoControls.test.js
+++ packages/gestalt/src/Video/__tests__/VideoControls.test.js
@@ -1,51 +1,119 @@
 /* eslint-env jest */
 import React from 'react';
 import { create } from 'react-test-renderer';
-import VideoControls, { timeToString } from '../VideoControls';
+import VideoControls from '../VideoControls';
 
-describe('VideoControls', () => {
-  it('Video controls show correct icons', () => {
-    const tree = create(
-      <VideoControls
-        accessibilityMaximizeLabel="Maximize"
-        accessibilityMinimizeLabel="Minimize"
-        accessibilityMuteLabel="Mute"
-        accessibilityPauseLabel="Pause"
-        accessibilityPlayLabel="Play"
-        accessibilityUnmuteLabel="Unmute"
-        currentTime={0}
-        duration={10}
-        fullscreen={false}
-        muted={false}
-        paused={false}
-        seek={() => {}}
-        toggleFullscreen={() => {}}
-        toggleMute={() => {}}
-        togglePlay={() => {}}
-      />
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+test('VideoControls for single digit seconds', () => {
+  const tree = create(
+    <VideoControls
+      accessibilityMaximizeLabel="Maximize"
+      accessibilityMinimizeLabel="Minimize"
+      accessibilityMuteLabel="Mute"
+      accessibilityPauseLabel="Pause"
+      accessibilityPlayLabel="Play"
+      accessibilityUnmuteLabel="Unmute"
+      currentTime={5}
+      duration={5}
+      fullscreen={false}
+      muted={false}
+      paused={false}
+      seek={() => {}}
+      toggleFullscreen={() => {}}
+      toggleMute={() => {}}
+      togglePlay={() => {}}
+    />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
-describe('timeToString', () => {
-  it('timeToString for single digit seconds', () => {
-    expect(timeToString(5)).toBe('00:05');
-  });
+test('VideoControls for double digit seconds', () => {
+  const tree = create(
+    <VideoControls
+      accessibilityMaximizeLabel="Maximize"
+      accessibilityMinimizeLabel="Minimize"
+      accessibilityMuteLabel="Mute"
+      accessibilityPauseLabel="Pause"
+      accessibilityPlayLabel="Play"
+      accessibilityUnmuteLabel="Unmute"
+      currentTime={15}
+      duration={15}
+      fullscreen={false}
+      muted={false}
+      paused={false}
+      seek={() => {}}
+      toggleFullscreen={() => {}}
+      toggleMute={() => {}}
+      togglePlay={() => {}}
+    />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-  it('timeToString for double digit seconds', () => {
-    expect(timeToString(15)).toBe('00:15');
-  });
+test('VideoControls for single digit minutes', () => {
+  const tree = create(
+    <VideoControls
+      accessibilityMaximizeLabel="Maximize"
+      accessibilityMinimizeLabel="Minimize"
+      accessibilityMuteLabel="Mute"
+      accessibilityPauseLabel="Pause"
+      accessibilityPlayLabel="Play"
+      accessibilityUnmuteLabel="Unmute"
+      currentTime={65}
+      duration={65}
+      fullscreen={false}
+      muted={false}
+      paused={false}
+      seek={() => {}}
+      toggleFullscreen={() => {}}
+      toggleMute={() => {}}
+      togglePlay={() => {}}
+    />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-  it('timeToString for single digit minutes', () => {
-    expect(timeToString(65)).toBe('01:05');
-  });
+test('VideoControls for double digit minutes', () => {
+  const tree = create(
+    <VideoControls
+      accessibilityMaximizeLabel="Maximize"
+      accessibilityMinimizeLabel="Minimize"
+      accessibilityMuteLabel="Mute"
+      accessibilityPauseLabel="Pause"
+      accessibilityPlayLabel="Play"
+      accessibilityUnmuteLabel="Unmute"
+      currentTime={905}
+      duration={905}
+      fullscreen={false}
+      muted={false}
+      paused={false}
+      seek={() => {}}
+      toggleFullscreen={() => {}}
+      toggleMute={() => {}}
+      togglePlay={() => {}}
+    />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-  it('timeToString for double digit minutes', () => {
-    expect(timeToString(905)).toBe('15:05');
-  });
-
-  it('timeToString rounds for partial seconds', () => {
-    expect(timeToString(67.3)).toBe('01:07');
-  });
+test('VideoControls rounds for partial seconds', () => {
+  const tree = create(
+    <VideoControls
+      accessibilityMaximizeLabel="Maximize"
+      accessibilityMinimizeLabel="Minimize"
+      accessibilityMuteLabel="Mute"
+      accessibilityPauseLabel="Pause"
+      accessibilityPlayLabel="Play"
+      accessibilityUnmuteLabel="Unmute"
+      currentTime={67.3}
+      duration={67.3}
+      fullscreen={false}
+      muted={false}
+      paused={false}
+      seek={() => {}}
+      toggleFullscreen={() => {}}
+      toggleMute={() => {}}
+      togglePlay={() => {}}
+    />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
