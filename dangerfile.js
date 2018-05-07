a0b3e62afdc49c9484a7bc824046e9d340a139b0
Yen-Wei Liu <yenweiliu@pinterest.com>
2018-05-07 00:51:25 -0700

    fix pipeline Add api key buildkite-agent maybe?? shrug mount tmpfs and store stats there use temp sha apparently we have to pass buildnumber into artifact download buildkite artifact downloads using the exact path

--- dangerfile.js
+++ dangerfile.js
@@ -11,31 +11,44 @@ if (!danger.git.modified_files.includes('CHANGELOG.md')) {
 }
 
 // Bundle Size Alerting
-const BUILDKITE_API_BASE_URL = `https://api.buildkite.com/v3/organizations/${env.BUILDKITE_ORGANIZATION_SLUG}/pipelines/${env.BUILDKITE_PIPELINE_SLUG}`;
+const organization = process.env.BUILDKITE_ORGANIZATION_SLUG || 'pinterest';
+const pipeline = process.env.BUILDKITE_PIPELINE_SLUG || 'gestalt';
+const apiKey = process.env.BUILDKITE_API_TOKEN || '';
+const currentBuild = process.env.BUILDKITE_BUILD_ID || '';
+const BUILDKITE_API_BASE_URL = `https://api.buildkite.com/v2/organizations/${organization}/pipelines/${pipeline}`;
 
 const getBuildNumber = async (sha: string) => {
   const response = await fetch(
-    `${BUILDKITE_API_BASE_URL}/builds?commit=${sha}`
+    `${BUILDKITE_API_BASE_URL}/builds?commit=${sha}&access_token=${apiKey}`
   );
-  const responseJson = await response.toJson();
+  const responseJson = await response.json();
+  console.log(responseJson);
   return responseJson[0].id;
 };
 
-const baseCommit = danger.github.pr.base.sha;
+// const baseCommit = danger.github.pr.base.sha;
+const baseCommit = '4e633a80312863c8bbd6859b54e27913702d6328';
 // const currentCommit = danger.github.pr.head.sha;
-const currentStats = JSON.parse(
-  readFileSync('./packages/gestalt/dist/stats.json', 'utf8')
-);
 // Yen - fetching via API because i didn't see a way to get the build number for a commit via buildkite-agent
 console.log(`Fetching stats for base commit: ${baseCommit}`);
 getBuildNumber(baseCommit).then(buildNumber => {
-  console.log(`Downloading stats from build: ${buildNumber}`);
+  console.log('Creating temp directories');
+  execSync('mkdir -p ./tmp/previous');
+  execSync('mkdir -p ./tmp/current');
+  console.log('Downloading stats from current build');
   execSync(
-    `buildkite-agent artifact download stats.json tmp/previous-stats.json --build ${buildNumber}`
+    `buildkite-agent artifact download dist/stats.json tmp/current/ --build ${currentBuild}`
   );
-  const previousStats = JSON.parse(
-    readFileSync('./tmp/previous-stats.json', 'utf-8')
+  const currentStats = JSON.parse(
+    readFileSync('./tmp/current/dist/stats.json', 'utf8')
   );
   console.log(currentStats);
+  console.log(`Downloading stats from build: ${buildNumber}`);
+  execSync(
+    `buildkite-agent artifact download dist/stats.json tmp/previous/ --build ${buildNumber}`
+  );
+  const previousStats = JSON.parse(
+    readFileSync('./tmp/previous/dist/stats.json', 'utf-8')
+  );
   console.log(previousStats);
 });
