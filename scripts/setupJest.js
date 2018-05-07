7225da22c1ae6542394ddb837e48aaefb15d921e
Peter Farejowicz <pfarejowicz@pinterest.com>
2018-05-07 12:33:37 -0700

    Update jest setup script to add jsdom

--- scripts/setupJest.js
+++ scripts/setupJest.js
@@ -1,4 +1,29 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { JSDOM } from 'jsdom';
+
+// JSDOM setup from https://github.com/airbnb/enzyme/blob/master/docs/guides/jsdom.md
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .reduce(
+      (result, prop) => ({
+        ...result,
+        [prop]: Object.getOwnPropertyDescriptor(src, prop),
+      }),
+      {}
+    );
+  Object.defineProperties(target, props);
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+copyProps(window, global);
 
 Enzyme.configure({ adapter: new Adapter() });
