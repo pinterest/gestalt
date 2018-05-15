0bcb4a98ee4a65340b152a21c0068dfd14919d22
Chris Lloyd <chrislloyd@pinterest.com>
2018-05-15 13:54:38 -0700

    More scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-yarn test --env jsdom
-yarn test --env node
-yarn codecov --commit $BUILDKITE_COMMIT --branch $BUILDKITE_BRANCH --build $BUILDKITE_BUILD_ID
+yarn test --env jsdom && \
+yarn test --env node && \
+yarn codecov --commit "$BUILDKITE_COMMIT" \
+             --branch "$BUILDKITE_BRANCH" \
+             --build "$BUILDKITE_BUILD_ID"
