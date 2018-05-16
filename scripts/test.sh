e23b18bec8312d3024100595aa9618165e8c96d4
Chris Lloyd <chrislloyd@pinterest.com>
2018-05-15 17:28:02 -0700

    fix scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-yarn test --env jsdom && \
-yarn test --env node && \
-yarn codecov --commit "$BUILDKITE_COMMIT" \
-             --branch "$BUILDKITE_BRANCH" \
-             --build "$BUILDKITE_BUILD_ID"
+yarn test --env jsdom && yarn test --env node && yarn codecov
