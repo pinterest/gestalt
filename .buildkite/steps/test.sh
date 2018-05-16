72808e4c0cdb29e71c08d8206ebf9d1ff278034d
Chris Lloyd <chrislloyd@pinterest.com>
2018-05-15 19:26:21 -0700

    Tweaks

--- .buildkite/steps/test.sh
+++ .buildkite/steps/test.sh
@@ -3,10 +3,8 @@ set -euo pipefail
 
 echo "+++ :jest: Running Jest"
 yarn test --coverage
-
 echo "👌 Looks good to me!"
 
-echo "--- :codecov: Sending coverage report"
+echo "--- :codecov: Sending coverage report to CodeCov"
 yarn codecov
-
 echo "📑 Done!"
