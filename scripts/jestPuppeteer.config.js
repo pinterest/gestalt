48ab44cead9eaba78b7db228bb66256cba64fd0f
Christian Vuerings <cvuerings@pinterest.com>
2018-05-16 11:37:41 -0700

    Use correct puppeteer args

--- scripts/jestPuppeteer.config.js
+++ scripts/jestPuppeteer.config.js
@@ -2,4 +2,12 @@ module.exports = {
   launch: {
     headless: true,
   },
+  args: [
+    // Required for Docker version of Puppeteer
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    // This will write shared memory files into /tmp instead of /dev/shm,
+    // because Docker’s default for /dev/shm is 64MB
+    '--disable-dev-shm-usage',
+  ],
 };
