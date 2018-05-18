8360eaa5bd3229d5dcdcb750456f7c81ebc85722
Chris Lloyd <chrislloyd@pinterest.com>
2018-05-17 20:21:10 -0700

    fix bad merge

--- packages/gestalt/src/Masonry/__integration__/handleUpdateDuringInsertions.integration.js
+++ packages/gestalt/src/Masonry/__integration__/handleUpdateDuringInsertions.integration.js
@@ -3,16 +3,11 @@ import selectors from './lib/selectors';
 
 describe('Masonry > Update during insertions', () => {
   it('Should not throw an error when Masonry is updated during insertions', async () => {
-<<<<<<< HEAD:packages/gestalt/src/Masonry/__integration__/handleUpdateDuringInsertions.integration.js
     await page.setViewport({
       width: 800,
       height: 800,
     });
     await page.goto('http://localhost:3000/Masonry');
-=======
-    ghost.close();
-    await ghost.open('http://localhost:3000/Masonry');
->>>>>>> Update integration test ports:packages/gestalt/src/Masonry/__integration__/handleUpdateDuringInsertions.ghost.js
 
     const initialErrors = await page.evaluate(() => window.ERROR_COUNT);
     assert.equal(initialErrors, 0);
