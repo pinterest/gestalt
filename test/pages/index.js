2f7e536ce7d7ed0c6e934dc2c00acebf84242438
Chris Lloyd <chrislloyd@pinterest.com>
2018-05-17 20:32:35 -0700

    Appease eslint

--- test/pages/index.js
+++ test/pages/index.js
@@ -10,14 +10,16 @@ const pages = [
   'A11y',
 ];
 
-export default () => (
-  <ul>
-    {pages.map(page => (
-      <li key={page}>
-        <Link href={{ pathname: `/${page}` }}>
-          <a href={`/${page}`}>{page}</a>
-        </Link>
-      </li>
-    ))}
-  </ul>
-);
+export default function IndexPage() {
+  return (
+    <ul>
+      {pages.map(page => (
+        <li key={page}>
+          <Link href={{ pathname: `/${page}` }}>
+            <a href={`/${page}`}>{page}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
