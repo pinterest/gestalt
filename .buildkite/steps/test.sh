00781032cc7508d1a8ffe00eaa8710cc4d3f1384
Chris Lloyd <chrislloyd@pinterest.com>
2018-05-21 13:05:32 -0700

    Cleanup test pipelines

--- .buildkite/steps/test.sh
+++ .buildkite/steps/test.sh
@@ -5,6 +5,16 @@ echo "+++ :jest: Running Jest"
 yarn test --coverage
 echo "👌 Looks good to me!"
 
+echo "--- :hammer_and_wrench: Building for integration tests"
+yarn build
+(cd test && yarn build)
+
+echo "+++ :jest: Running Integration Jest"
+yarn concurrently --kill-others --names "SERVER,JEST" \
+  "(cd test && yarn start)" \
+  "./run_integration_tests.sh"
+echo "👌 Looks good to me!"
+
 echo "--- :codecov: Sending coverage report to CodeCov"
 yarn codecov
 echo "📑 Done!"
