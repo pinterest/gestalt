887b011b7336c776fc83c62fdce0620b28708279
Chris Lloyd <chrislloyd@pinterest.com>
2018-05-21 10:52:54 -0700

    Get A11y tests passing

--- test/pages/A11y.js
+++ test/pages/A11y.js
@@ -1,8 +1,21 @@
 // @flow
 import * as React from 'react';
-import A11YCheck from '../containers/A11YCheck';
-import Container from '../components/Container';
+import { Icon } from 'gestalt';
 
-export default function A11y() {
-  return <Container Component={A11YCheck} />;
+export default function A11Y() {
+  return (
+    <div>
+      {Icon.icons.map((iconName, idx) => (
+        <div key={`${idx}-${iconName}`}>
+          <h2>{iconName}</h2>
+          <Icon
+            icon={iconName}
+            accessibilityLabel={iconName.replace(/-/g, ' ')}
+            size={21}
+            color="gray"
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
--- test/pages/_document.js
+++ test/pages/_document.js
@@ -3,6 +3,11 @@ import * as React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <html lang="en">
