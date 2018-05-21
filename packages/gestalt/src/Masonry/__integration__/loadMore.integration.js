bf6f8454d4a644f8a73852d4f1f3c787400ccca4
Chris Lloyd <chrislloyd@pinterest.com>
2018-05-21 10:51:35 -0700

    loadMore tests passing

--- packages/gestalt/src/Masonry/__integration__/loadMore.integration.js
+++ packages/gestalt/src/Masonry/__integration__/loadMore.integration.js
@@ -1,4 +1,3 @@
-import assert from 'assert';
 import selectors from './lib/selectors';
 
 const PIN_INSERTION_TIME = 500;
@@ -12,29 +11,22 @@ describe('Masonry > Scrolls', () => {
     });
     await page.goto('http://localhost:3000/Masonry?deferMount=1&manualFetch=1');
 
-    const serverItems = await page.$$(selectors.staticItem);
-
     // Hard-coded value for initial pins in server.js
     const initialServerItemCount = 20;
-    assert.equal(serverItems.length, initialServerItemCount);
-
-    await page.evaluate(() => {
-      window.dispatchEvent(new CustomEvent('trigger-mount'));
-    });
 
     // We should fetch more items on render to fill the viewport.
     await page.waitFor(PIN_INSERTION_TIME);
     await page.evaluate(() => window.NEXT_FETCH());
     const gridItems = await page.$$(selectors.gridItem);
     const afterLoadItemCount = gridItems.length;
-    assert.ok(afterLoadItemCount > initialServerItemCount);
+    expect(afterLoadItemCount).toBeGreaterThan(initialServerItemCount);
     await page.waitFor(PIN_INSERTION_TIME);
     await page.evaluate(() => window.NEXT_FETCH());
 
     const initialFetchCount = await page.evaluate(
       () => window.TEST_FETCH_COUNTS
     );
-    assert.ok(initialFetchCount >= 1);
+    expect(initialFetchCount).toBeGreaterThanOrEqual(1);
 
     // Scroll a few times to triggle multiple scrolls.
     await page.evaluate(() => window.scrollTo(0, window.scrollMaxY));
@@ -46,9 +38,9 @@ describe('Masonry > Scrolls', () => {
     await page.evaluate(() => window.NEXT_FETCH());
 
     const newFetchCount = await page.evaluate(() => window.TEST_FETCH_COUNTS);
-    assert.ok(newFetchCount >= initialFetchCount + 1);
+    expect(newFetchCount).toBeGreaterThanOrEqual(initialFetchCount + 1);
 
     const gridItemsAfter = await page.$$(selectors.gridItem);
-    assert.ok(gridItemsAfter.length > afterLoadItemCount);
+    expect(gridItemsAfter.length).toBeGreaterThan(afterLoadItemCount);
   });
 });
