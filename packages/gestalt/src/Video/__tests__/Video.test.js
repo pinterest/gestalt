3eef333720275491637a42efaa1dec00a95594eb
Peter Farejowicz <pfarejowicz@pinterest.com>
2018-05-21 18:21:52 -0700

    One more test for src length changing

--- packages/gestalt/src/Video/__tests__/Video.test.js
+++ packages/gestalt/src/Video/__tests__/Video.test.js
@@ -68,19 +68,6 @@ test('Video with callbacks', () => {
 });
 
 describe('Video loading', () => {
-  it('Loads when string src changes to new string src', () => {
-    const wrapper = shallow(
-      <Video
-        aspectRatio={1}
-        captions="https://media.w3.org/2010/05/sintel/captions.vtt"
-        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-      />
-    );
-    const spy = jest.spyOn(wrapper.instance(), 'load');
-    wrapper.setProps({ src: 'http://media.w3.org/2010/05/bunny/movie.mp4' });
-    expect(spy).toHaveBeenCalled();
-  });
-
   it('Does not load when string src does not change', () => {
     const wrapper = shallow(
       <Video
@@ -92,6 +79,37 @@ describe('Video loading', () => {
     const spy = jest.spyOn(wrapper.instance(), 'load');
     wrapper.setProps({ volume: 0 });
     expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('Does not load when array src does not change', () => {
+    const wrapper = shallow(
+      <Video
+        aspectRatio={1}
+        captions="https://media.w3.org/2010/05/sintel/captions.vtt"
+        src={[
+          {
+            type: 'video/mp4',
+            src: 'http://media.w3.org/2010/05/bunny/movie.mp4',
+          },
+        ]}
+      />
+    );
+    const spy = jest.spyOn(wrapper.instance(), 'load');
+    wrapper.setProps({ volume: 0 });
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('Loads when string src changes to new string src', () => {
+    const wrapper = shallow(
+      <Video
+        aspectRatio={1}
+        captions="https://media.w3.org/2010/05/sintel/captions.vtt"
+        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+      />
+    );
+    const spy = jest.spyOn(wrapper.instance(), 'load');
+    wrapper.setProps({ src: 'http://media.w3.org/2010/05/bunny/movie.mp4' });
+    expect(spy).toHaveBeenCalled();
   });
 
   it('Loads when string src changes to new array src', () => {
@@ -134,24 +152,6 @@ describe('Video loading', () => {
     expect(spy).toHaveBeenCalled();
   });
 
-  it('Does not load when array src does not change', () => {
-    const wrapper = shallow(
-      <Video
-        aspectRatio={1}
-        captions="https://media.w3.org/2010/05/sintel/captions.vtt"
-        src={[
-          {
-            type: 'video/mp4',
-            src: 'http://media.w3.org/2010/05/bunny/movie.mp4',
-          },
-        ]}
-      />
-    );
-    const spy = jest.spyOn(wrapper.instance(), 'load');
-    wrapper.setProps({ volume: 0 });
-    expect(spy).not.toHaveBeenCalled();
-  });
-
   it('Loads when array src changes to new array src', () => {
     const wrapper = shallow(
       <Video
@@ -171,6 +171,35 @@ describe('Video loading', () => {
         {
           type: 'video/mp4',
           src: 'http://media.w3.org/2010/05/bunny/movie.mp4',
+        },
+      ],
+    });
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('Loads when array src changes to new length array src', () => {
+    const wrapper = shallow(
+      <Video
+        aspectRatio={1}
+        captions="https://media.w3.org/2010/05/sintel/captions.vtt"
+        src={[
+          {
+            type: 'video/mp4',
+            src: 'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
+          },
+        ]}
+      />
+    );
+    const spy = jest.spyOn(wrapper.instance(), 'load');
+    wrapper.setProps({
+      src: [
+        {
+          type: 'video/mp4',
+          src: 'https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4',
+        },
+        {
+          type: 'video/ogg',
+          src: 'https://archive.org/download/ElephantsDream/ed_hd.ogv',
         },
       ],
     });
