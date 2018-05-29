603f69d42115579c6a19a913b618cc9950819795
Chris Lloyd <chrislloyd@pinterest.com>
2018-05-29 13:37:35 -0700

    Fix restoring scroll behavior

--- packages/gestalt/src/behaviors/NoScrollBehavior.js
+++ packages/gestalt/src/behaviors/NoScrollBehavior.js
@@ -6,20 +6,25 @@ type Props = {|
 |};
 
 export default class NoScrollBehavior extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+    this.prevOverflow = null;
+  }
+
   componentDidMount() {
-    if (typeof window !== 'undefined' && window.document) {
+    if (typeof window !== 'undefined') {
       this.prevOverflow = window.document.body.style.overflow;
       window.document.body.style.overflow = 'hidden';
     }
   }
 
   componentWillUnmount() {
-    if (this.prevOverflow) {
+    if (typeof window !== 'undefined') {
       window.document.body.style.overflow = this.prevOverflow;
     }
   }
 
-  prevOverflow: ?string;
+  prevOverflow: string | null;
 
   render() {
     return this.props.children;
