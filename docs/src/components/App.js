d63e95dafa1b6aa9615ab5181534d79aa352947f
Peter Farejowicz <pfarejowicz@pinterest.com>
2018-05-29 18:29:11 -0700

    Update SelectList to work with async rendering

--- docs/src/components/App.js
+++ docs/src/components/App.js
@@ -12,19 +12,21 @@ export default function App(props: Props) {
   const { children } = props;
   return (
     <Box minHeight="100vh">
-      <Header />
+      <React.StrictMode>
+        <Header />
 
-      <Box mdDisplay="flex" direction="row">
-        <Column span={12} mdSpan={2}>
-          <Navigation />
-        </Column>
-        <Divider />
-        <Column span={12} mdSpan={8}>
-          <Box padding={4} mdPadding={6} lgPadding={8}>
-            {children}
-          </Box>
-        </Column>
-      </Box>
+        <Box mdDisplay="flex" direction="row">
+          <Column span={12} mdSpan={2}>
+            <Navigation />
+          </Column>
+          <Divider />
+          <Column span={12} mdSpan={8}>
+            <Box padding={4} mdPadding={6} lgPadding={8}>
+              {children}
+            </Box>
+          </Column>
+        </Box>
+      </React.StrictMode>
     </Box>
   );
 }
