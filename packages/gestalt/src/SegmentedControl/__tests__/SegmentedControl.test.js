cb146efe664e267af7fcf78ca9f5ecb781701e91
J.C. Zhong <jianchengzhong@pinterest.com>
2018-06-01 09:57:38 -0700

    Update unit test to use snapshot test

--- packages/gestalt/src/SegmentedControl/__tests__/SegmentedControl.test.js
+++ packages/gestalt/src/SegmentedControl/__tests__/SegmentedControl.test.js
@@ -1,36 +1,15 @@
 // @flow
 import React from 'react';
 import { create } from 'react-test-renderer';
-import { shallow } from 'enzyme';
 import SegmentedControl from '../SegmentedControl';
-import Icon from '../../Icon/Icon';
-import Text from '../../Text/Text';
 
 test('SegmentedControl renders', () => {
   const tree = create(
     <SegmentedControl
-      items={['News', 'You', 'Messages']}
+      items={['News', 'You', 'Messages', <div key="dummy" />]}
       selectedItemIndex={0}
       onChange={() => {}}
     />
   ).toJSON();
   expect(tree).toMatchSnapshot();
-});
-
-test('SegmentedControl renders component items', () => {
-  const wrapper = shallow(
-    <SegmentedControl
-      items={[
-        'News',
-        'You',
-        'Messages',
-        <Icon key="icon1" accessibilityLabel="" icon="pin" color="red" />,
-        <Icon key="icon2" accessibilityLabel="" icon="pin" color="red" />,
-      ]}
-      selectedItemIndex={0}
-      onChange={() => {}}
-    />
-  );
-  expect(wrapper.find(Text)).toHaveLength(3);
-  expect(wrapper.find(Icon)).toHaveLength(2);
 });
