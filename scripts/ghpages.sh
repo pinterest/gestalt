8511e2a4a148c5d9babe91aa287fafbb35223d41
Peter Farejowicz <pfarejowicz@pinterest.com>
2018-06-27 10:09:37 -0700

    Fix release script

--- scripts/ghpages.sh
+++ scripts/ghpages.sh
@@ -15,7 +15,7 @@ repo=${1:-https://github.com/pinterest/gestalt.git}
 git checkout -b tmp-deploy
 (cd docs && NODE_ENV=production yarn build --output-public-path '/gestalt')
 git add -f docs/build
-git commit -m "Deployed to Github Pages"
+git commit -m "Deployed to Github Pages" --no-verify
 git subtree split --prefix docs/build -b tmp-gh-pages
 git push -f ${repo} tmp-gh-pages:gh-pages
 git checkout master
