e48203e8042732fae84c5a4b1542b8cafb8e7652
Mason Jennings <mjennings@pinterest.com>
2018-07-16 14:48:46 -0700

    TextArea: Add hasError prop
    Add hasError prop to `TextArea` (identical to `TextField`)

--- packages/gestalt/src/TextArea.test.js
+++ packages/gestalt/src/TextArea.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { create } from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import TextArea from './TextArea.js';
+import Flyout from './Flyout.js';
 
 describe('TextArea', () => {
   it('Renders a Flyout if an error message is passed in', () => {
@@ -11,12 +12,12 @@ describe('TextArea', () => {
     );
     wrapper.instance().setState({ errorIsOpen: true });
     wrapper.simulate('focus');
-    expect(wrapper.find('Flyout')).toHaveLength(1);
+    expect(wrapper.find(Flyout)).toHaveLength(1);
   });
 
   it('Does not render a Flyout when errorMessage is null', () => {
     const wrapper = shallow(<TextArea id="test" onChange={jest.fn()} />);
-    expect(wrapper.find('Flyout')).toHaveLength(0);
+    expect(wrapper.find(Flyout)).toHaveLength(0);
   });
 
   it('TextArea normal', () => {
@@ -44,6 +45,20 @@ describe('TextArea', () => {
     expect(tree).toMatchSnapshot();
   });
 
+  it('TextArea with hasError', () => {
+    const wrapper = shallow(
+      <TextArea
+        hasError
+        id="test"
+        onChange={jest.fn()}
+        onFocus={jest.fn()}
+        onBlur={jest.fn()}
+      />
+    );
+    expect(wrapper.find(Flyout)).toHaveLength(0);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
   it('TextArea with rows', () => {
     const tree = create(
       <TextArea id="test" onChange={jest.fn()} rows={5} />
@@ -60,10 +75,10 @@ describe('TextArea', () => {
         onBlur={jest.fn()}
       />
     );
-    expect(tree.find('Flyout')).toHaveLength(0);
+    expect(tree.find(Flyout)).toHaveLength(0);
     tree.setProps({
       errorMessage: 'error message',
     });
-    expect(tree.find('Flyout')).toHaveLength(1);
+    expect(tree.find(Flyout)).toHaveLength(1);
   });
 });
