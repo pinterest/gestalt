d0aa3e2dcc5ca98429a8ed5b5f8f2c1c91d8d82c
Chris Lloyd <chrislloyd@pinterest.com>
2018-09-03 18:36:56 -0700

    another attempt

--- scripts/jestPuppeteer.config.js
+++ scripts/jestPuppeteer.config.js
@@ -1,15 +1,11 @@
 module.exports = {
   launch: {
     headless: true,
-    ...(process.env.CI ? { executablePath: 'google-chrome-unstable' } : {}),
     args: process.env.CI
       ? [
           // Required for Docker version of Puppeteer
           '--no-sandbox',
           '--disable-setuid-sandbox',
-          // This will write shared memory files into /tmp instead of /dev/shm,
-          // because Docker’s default for /dev/shm is 64MB
-          '--disable-dev-shm-usage',
         ]
       : [],
   },
