96c69c90a02e46a59fa8aa02606ebbb1cf007229
Brent Traut <btraut@pinterest.com>
2018-10-26 00:04:13 -0700

    Fixed flakey integration test

--- packages/gestalt/src/__integration__/optimizedRemount.integration.js
+++ packages/gestalt/src/__integration__/optimizedRemount.integration.js
@@ -1,4 +1,3 @@
-import assert from 'assert';
 import selectors from './lib/selectors.js';
 
 describe('Masonry > External cache', () => {
@@ -9,11 +8,16 @@ describe('Masonry > External cache', () => {
       'http://localhost:3001/MasonryInfinite?virtualize=1&externalCache=1',
     ],
   ])('should only mount visible items on remount - %s', async (name, url) => {
+    expect.assertions(3);
+
     await page.setViewport({
       width: 800,
       height: 800,
     });
     await page.goto(url);
+
+    // Wait for Masonry multi-stage rendering.
+    await page.waitFor(1000);
 
     const initialMountCount = await page.evaluate(
       () => window.ITEM_MOUNT_COUNT
@@ -35,9 +39,12 @@ describe('Masonry > External cache', () => {
       )
     );
 
+    // Wait for Masonry multi-stage rendering.
+    await page.waitFor(1000);
+
     // mount count should be increased
     let updatedMountCount = await page.evaluate(() => window.ITEM_MOUNT_COUNT);
-    assert.ok(updatedMountCount > initialMountCount);
+    expect(updatedMountCount).toBeGreaterThan(initialMountCount);
 
     // unmount/remount the grid
     const toggleMountTrigger = await page.$(selectors.toggleMount);
@@ -45,8 +52,11 @@ describe('Masonry > External cache', () => {
 
     // wait for grid to be unmounted
     updatedMountCount = await page.evaluate(() => window.ITEM_MOUNT_COUNT);
-    assert.equal(updatedMountCount, 0);
+    expect(updatedMountCount).toBe(0);
     await toggleMountTrigger.click();
+
+    // Wait for Masonry multi-stage rendering.
+    await page.waitFor(1000);
 
     // wait for grid to be remounted
     const updatedCount = await page.evaluate(() => window.ITEM_MOUNT_COUNT);
