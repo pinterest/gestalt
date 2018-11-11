9546625b3f3874bf991bcfb99e8bf776477b279b
Chris Lloyd <christopher.lloyd@gmail.com>
2018-11-11 13:14:02 -0800

    use special config on buildkite

--- scripts/jestPuppeteer.config.js
+++ scripts/jestPuppeteer.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-  launch: {
-    headless: true,
-  },
+  launch: process.env.BUILDKITE
+    ? {
+        headless: true,
+        executablePath: 'google-chrome-unstable',
+        args: [
+          // Required for Docker version of Puppeteer	    launchTimeout: 30000,
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          // This will write shared memory files into /tmp instead of /dev/shm,
+          // because Docker’s default for /dev/shm is 64MB
+          '--disable-dev-shm-usage',
+        ],
+      }
+    : {
+        headless: true,
+      },
   server: {
     command: './scripts/integration_server.sh',
     port: 3001,
