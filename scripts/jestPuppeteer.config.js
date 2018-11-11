aa4a0543b6ba0fb529956bd9ef68b98dd1dc795a
Chris Lloyd <christopher.lloyd@gmail.com>
2018-11-11 12:50:07 -0800

    Don't do anything fancy

--- scripts/jestPuppeteer.config.js
+++ scripts/jestPuppeteer.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   launch: {
     headless: true,
-    args: process.env.CI
-      ? [
-          // Required for Docker version of Puppeteer
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-        ]
-      : [],
   },
 };
