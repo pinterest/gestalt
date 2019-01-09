54ef1373dd5fdc86cd0e71fef0e9ccd0c6553c7a
Brent Traut <btraut@pinterest.com>
2019-01-09 15:24:23 -0800

    Refactored ClassnameBuilder a bit. Added more comments.

--- packages/gestalt/lib/classnameBuilder.js
+++ packages/gestalt/lib/classnameBuilder.js
@@ -1,5 +1,22 @@
+// ClassnameBuilder is a hack used to help us minify Gestalt CSS classnames down
+// to two characters each for prod builds.
+//
+// The issue with such an aggressive minification approach is that it leaves a
+// very small hash value size and collisions often occur. Gestalt previously
+// solved this by assigning each classname an incrementing number and hashing on
+// that number, but this resulted in lots of name/hash changes whenever
+// classnames were added or removed.
+//
+// To solve issues of hash collision and also thrashing on changes,
+// ClassnameBuilder uses a bit of a hack: an on-disk cache holds all classnames
+// and corresponding hashes and those names/hashes will remain reserved for as
+// long as the classname lives in the code. This means that all existing
+// classnames will remain the same when others are added or removed.
+// Unfortunately, it requires all Gestalt developers to share a cache, and thus
+// the cache must be checked into the repo and committed whenever it changes. As
+// such, this cache should be treated the same as the yarn.lock file.
+
 import fs from 'fs';
-import { hashStrToInt, encodeInt } from './crypto.js';
 
 const CLASSNAMES_CACHE_PATH = './classnames-cache.json';
 
@@ -58,7 +75,11 @@ class ClassnameBuilder {
     let incrementor = 0;
 
     do {
-      minified = encodeInt(hashStrToInt(classname) + incrementor, 2, 2);
+      minified = ClassnameBuilder.encodeInt(
+        ClassnameBuilder.hashStrToInt(classname) + incrementor,
+        2,
+        2
+      );
       incrementor += 1;
     } while (this.hashes[minified]);
 
@@ -69,6 +90,38 @@ class ClassnameBuilder {
     this.saveClassnamesCache();
 
     return minified;
+  }
+
+  // Hash any string to an int value.
+  // https://gist.github.com/hyamamoto/fd435505d29ebfa3d9716fd2be8d42f0
+  static hashStrToInt(s) {
+    let h = 0;
+
+    for (let i = 0; i < s.length; i += 1) {
+      // eslint-disable-next-line no-bitwise
+      h = (Math.imul(31, h) + s.charCodeAt(i)) | 0;
+    }
+
+    return h;
+  }
+
+  // Encode a number to a string using 6-bit characters (a-z, A-Z, 0-9, -, and _).
+  // Conditionally zero-pad and/or truncate to max length.
+  static encodeInt(int, minLength, maxLength) {
+    let remainder = int;
+    let result = '';
+
+    /* eslint-disable no-bitwise */
+    while (remainder > 0 || result.length < minLength) {
+      result =
+        '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_'[
+          remainder & 63
+        ] + result;
+      remainder >>>= 6;
+    }
+    /* eslint-enable no-bitwise */
+
+    return result.substr(-maxLength);
   }
 }
 
