32344495aca9cc1b600841b55d1e6a24321005e5
Jack Hsu <jackhsu@pinterest.com>
2019-02-16 08:44:29 -0800

    [dep] flow 0.84.0

--- packages/gestalt/src/Layer.js
+++ packages/gestalt/src/Layer.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { createPortal } from 'react-dom';
 
 type Props = {|
-  children?: React.Node,
+  children: React.Node,
 |};
 type State = {|
   mounted: boolean,
