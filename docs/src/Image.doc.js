8321619283b855e7c4bf3c1476c4bacc84c29c83
Felipe Vargas <github@fvgs.ai>
2019-02-19 21:40:24 -0500

    Improve Image description (#481)

--- docs/src/Image.doc.js
+++ docs/src/Image.doc.js
@@ -17,9 +17,9 @@ card(
   <PageHeader
     name="Image"
     description={`
-This component the workhorse of Pinterest. If you define Pinterest to be all
+This component is the workhorse of Pinterest. If you define Pinterest to be all
 about collecting ideas, then images is how we choose to represent those ideas.
-In response, we've added a few extra super-powers to the regular Image tag to
+In response, we've added a few extra superpowers to the regular <img> tag to
 make it even more awesome.
 `}
   />
