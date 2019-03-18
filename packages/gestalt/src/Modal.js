1c07af3e152f653337c4044a249a8d2f450eab93
Peter Farejowicz <pfarejowicz@gmail.com>
2019-03-18 13:52:00 -0700

    Make Modal heading an overloaded prop to accept react elements (#500)
    * Make Modal heading an overloaded prop to accept react elements
    
    * Switch to dual use prop flow type

--- packages/gestalt/src/Modal.js
+++ packages/gestalt/src/Modal.js
@@ -15,7 +15,7 @@ type Props = {|
   accessibilityModalLabel: string,
   children?: React.Node,
   footer?: React.Node,
-  heading: string,
+  heading: string | React.Node,
   onDismiss: () => void,
   role?: 'alertdialog' | 'dialog',
   size?: 'sm' | 'md' | 'lg' | number,
@@ -29,12 +29,51 @@ const SIZE_WIDTH_MAP = {
 
 const ESCAPE_KEY_CODE = 27;
 
-const Backdrop = ({ children }: { children?: React.Node }) => (
-  <React.Fragment>
-    <div className={styles.Backdrop} />
-    {children}
-  </React.Fragment>
-);
+function Backdrop({ children }: { children?: React.Node }) {
+  return (
+    <React.Fragment>
+      <div className={styles.Backdrop} />
+      {children}
+    </React.Fragment>
+  );
+}
+
+function Header({
+  heading,
+  role,
+}: {
+  heading: string | React.Node,
+  role?: 'alertdialog' | 'dialog',
+}) {
+  if (typeof heading !== 'string') {
+    return heading;
+  }
+
+  if (role === 'dialog') {
+    return (
+      <Box
+        dangerouslySetInlineStyle={{
+          __style: { paddingLeft: 50, paddingRight: 50 },
+        }}
+        display="flex"
+        justifyContent="center"
+        paddingY={5}
+      >
+        <Heading size="xs" accessibilityLevel={1}>
+          {heading}
+        </Heading>
+      </Box>
+    );
+  }
+
+  return (
+    <Box display="flex" padding={4}>
+      <Heading size="sm" accessibilityLevel={1}>
+        {heading}
+      </Heading>
+    </Box>
+  );
+}
 
 export default class Modal extends React.Component<Props> {
   static propTypes = {
@@ -42,7 +81,7 @@ export default class Modal extends React.Component<Props> {
     accessibilityModalLabel: PropTypes.string.isRequired,
     children: PropTypes.node,
     footer: PropTypes.node,
-    heading: PropTypes.string.isRequired,
+    heading: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
     onDismiss: PropTypes.func,
     role: PropTypes.oneOf(['alertdialog', 'dialog']),
     size: PropTypes.oneOfType([
@@ -105,36 +144,19 @@ export default class Modal extends React.Component<Props> {
                     width="100%"
                   >
                     <Box fit>
-                      {role === 'dialog' ? (
-                        <Box
-                          dangerouslySetInlineStyle={{
-                            __style: { paddingLeft: 50, paddingRight: 50 },
-                          }}
-                          display="flex"
-                          justifyContent="center"
-                          paddingY={5}
-                        >
-                          <Heading size="xs" accessibilityLevel={1}>
-                            {heading}
-                          </Heading>
-                        </Box>
-                      ) : (
-                        <Box display="flex" padding={4}>
-                          <Heading size="sm" accessibilityLevel={1}>
-                            {heading}
-                          </Heading>
-                        </Box>
-                      )}
+                      <Header heading={heading} role={role} />
                       {role === 'dialog' && (
-                        <Box padding={2} position="absolute" top right>
-                          <IconButton
-                            accessibilityLabel={accessibilityCloseLabel}
-                            icon="cancel"
-                            onClick={this.handleCloseClick}
-                          />
-                        </Box>
+                        <React.Fragment>
+                          <Box padding={2} position="absolute" top right>
+                            <IconButton
+                              accessibilityLabel={accessibilityCloseLabel}
+                              icon="cancel"
+                              onClick={this.handleCloseClick}
+                            />
+                          </Box>
+                          <Divider />
+                        </React.Fragment>
                       )}
-                      {role === 'dialog' && <Divider />}
                     </Box>
                     <Box flex="grow" overflow="auto" position="relative">
                       {children}
