bbc9f0bd3d92aa603f44c2bbf5deb063393f96a1
Peter Farejowicz <pfarejowicz@pinterest.com>
2019-07-31 09:45:55 -0700

    Bring gestalt to parity with PDS icons

---
+++ scripts/icons.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+to_camel_case() {
+    echo $1 | perl -pe 's/(-)./uc($&)/ge;s/-//g'
+}
+
+echo "// @flow"
+
+for icon in $(find ~/Downloads/web | sort)
+do
+    if [[ $icon =~ (([0-9a-zA-Z-]+)\.svg) ]]; then
+        echo "import $(to_camel_case ${BASH_REMATCH[2]}) from './${BASH_REMATCH[1]}';"
+    fi
+done
+
+echo "export default {"
+
+for icon in $(find ~/Downloads/web | sort)
+do
+    if [[ $icon =~ (([0-9a-zA-Z-]+)\.svg) ]]; then
+        echo "  '${BASH_REMATCH[2]}': $(to_camel_case ${BASH_REMATCH[2]}),"
+    fi
+done
+
+echo "};"
