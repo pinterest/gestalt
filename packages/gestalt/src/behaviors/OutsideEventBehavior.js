5482612230a6a7075489e276446de977d5218b75
emilyalwayss <emilynchen@gmail.com>
2020-03-30 08:42:56 -0700

    Modal: Update OutsideEventBehavior to work well with Portals (#777)
    * Text: removed __dangerouslyIncreaseLineHeight (#773)
    
    * removed __dangerouslyIncreaseLineHeight
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    Co-authored-by: Christian Vuerings <cvuerings@pinterest.com>
    
    * SelectList: Refactor placeholder for better React support (#759)
    
    * Refactor SelectList placeholder for better React support
    
    * updated changelog
    
    * Version bump: 1.27.0 (#775)
    
    * Update OutsideEventBehavior to work well with Portals
    
    * update snapshots
    
    * Text: removed __dangerouslyIncreaseLineHeight (#773)
    
    * removed __dangerouslyIncreaseLineHeight
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    Co-authored-by: Christian Vuerings <cvuerings@pinterest.com>
    
    * Text: removed __dangerouslyIncreaseLineHeight (#773)
    
    * removed __dangerouslyIncreaseLineHeight
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    Co-authored-by: Christian Vuerings <cvuerings@pinterest.com>
    
    * SelectList: Refactor placeholder for better React support (#759)
    
    * Refactor SelectList placeholder for better React support
    
    * updated changelog
    
    * SelectList: Refactor placeholder for better React support (#759)
    
    * Refactor SelectList placeholder for better React support
    
    * updated changelog
    
    * Version bump: 1.27.0 (#775)
    
    * Version bump: 1.27.0 (#775)
    
    * Update OutsideEventBehavior to work well with Portals
    
    * fix flow error and handle falsy onClick handler
    
    * improve doc/naming and fix flyout bug
    
    * Text: removed __dangerouslyIncreaseLineHeight (#773)
    
    * removed __dangerouslyIncreaseLineHeight
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    Co-authored-by: Christian Vuerings <cvuerings@pinterest.com>
    
    * SelectList: Refactor placeholder for better React support (#759)
    
    * Refactor SelectList placeholder for better React support
    
    * updated changelog
    
    * Version bump: 1.27.0 (#775)
    
    * Text: removed __dangerouslyIncreaseLineHeight (#773)
    
    * removed __dangerouslyIncreaseLineHeight
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    Co-authored-by: Christian Vuerings <cvuerings@pinterest.com>
    
    * SelectList: Refactor placeholder for better React support (#759)
    
    * Refactor SelectList placeholder for better React support
    
    * updated changelog
    
    * Version bump: 1.27.0 (#775)
    
    * Text: removed __dangerouslyIncreaseLineHeight (#773)
    
    * removed __dangerouslyIncreaseLineHeight
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    Co-authored-by: Christian Vuerings <cvuerings@pinterest.com>
    
    * SelectList: Refactor placeholder for better React support (#759)
    
    * Refactor SelectList placeholder for better React support
    
    * updated changelog
    
    * Version bump: 1.27.0 (#775)
    
    * fix rebase..
    
    * revert version change
    
    * update shapshot
    
    Co-authored-by: Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
    Co-authored-by: Christian Vuerings <cvuerings@pinterest.com>
    Co-authored-by: Peter Zed <rootvar@users.noreply.github.com>
    Co-authored-by: Christian Vuerings <vueringschristian@gmail.com>
    Co-authored-by: Emily Chen <emilychen@pinterest.com>

--- packages/gestalt/src/behaviors/OutsideEventBehavior.js
+++ packages/gestalt/src/behaviors/OutsideEventBehavior.js
@@ -1,39 +1,35 @@
 // @flow
-import * as React from 'react';
-import { findDOMNode } from 'react-dom';
+import type { Element } from 'react';
+import React, { useEffect } from 'react';
 
 type Props = {|
-  children: React.Node,
+  children: Element<*> | Array<Element<*>>,
   onClick?: (event: MouseEvent) => void,
 |};
 
-export default class OutsideEventBehavior extends React.Component<Props> {
-  componentDidMount() {
-    document.addEventListener('click', this.handleClickEvent, {
-      capture: true,
-    });
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('click', this.handleClickEvent, {
-      capture: true,
-    });
-  }
-
-  handleClickEvent = (event: MouseEvent) => {
-    // eslint-disable-next-line react/no-find-dom-node
-    const el = findDOMNode(this);
-    if (
-      !this.props.onClick ||
-      !el ||
-      (event.target instanceof Node && el.contains(event.target))
-    ) {
+const OutsideEventBehavior = ({ children, onClick }: Props) => {
+  let isClickedInside = false;
+  const handleDocumentClickEvent = (event: MouseEvent) => {
+    if (isClickedInside) {
+      isClickedInside = false;
       return;
     }
-    this.props.onClick(event);
+    if (onClick) {
+      onClick(event);
+    }
   };
 
-  render() {
-    return this.props.children;
-  }
-}
+  useEffect(() => {
+    document.addEventListener('click', handleDocumentClickEvent);
+    return () => {
+      document.removeEventListener('click', handleDocumentClickEvent);
+    };
+  });
+
+  const handleClick = () => {
+    isClickedInside = true;
+  };
+  return <div onClickCapture={handleClick}>{children}</div>;
+};
+
+export default OutsideEventBehavior;
