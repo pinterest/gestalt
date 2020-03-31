ec6d8733a5208f691dac06e2100f82861dcefede
Christian Vuerings <vueringschristian@gmail.com>
2020-03-31 07:24:51 -0700

    Revert "Modal: Update OutsideEventBehavior to work well with Portals (#777)" (#788)
    This reverts commit 5482612230a6a7075489e276446de977d5218b75.

--- packages/gestalt/src/behaviors/OutsideEventBehavior.js
+++ packages/gestalt/src/behaviors/OutsideEventBehavior.js
@@ -1,35 +1,39 @@
 // @flow
-import type { Element } from 'react';
-import React, { useEffect } from 'react';
+import * as React from 'react';
+import { findDOMNode } from 'react-dom';
 
 type Props = {|
-  children: Element<*> | Array<Element<*>>,
+  children: React.Node,
   onClick?: (event: MouseEvent) => void,
 |};
 
-const OutsideEventBehavior = ({ children, onClick }: Props) => {
-  let isClickedInside = false;
-  const handleDocumentClickEvent = (event: MouseEvent) => {
-    if (isClickedInside) {
-      isClickedInside = false;
+export default class OutsideEventBehavior extends React.Component<Props> {
+  componentDidMount() {
+    document.addEventListener('click', this.handleClickEvent, {
+      capture: true,
+    });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClickEvent, {
+      capture: true,
+    });
+  }
+
+  handleClickEvent = (event: MouseEvent) => {
+    // eslint-disable-next-line react/no-find-dom-node
+    const el = findDOMNode(this);
+    if (
+      !this.props.onClick ||
+      !el ||
+      (event.target instanceof Node && el.contains(event.target))
+    ) {
       return;
     }
-    if (onClick) {
-      onClick(event);
-    }
+    this.props.onClick(event);
   };
 
-  useEffect(() => {
-    document.addEventListener('click', handleDocumentClickEvent);
-    return () => {
-      document.removeEventListener('click', handleDocumentClickEvent);
-    };
-  });
-
-  const handleClick = () => {
-    isClickedInside = true;
-  };
-  return <div onClickCapture={handleClick}>{children}</div>;
-};
-
-export default OutsideEventBehavior;
+  render() {
+    return this.props.children;
+  }
+}
