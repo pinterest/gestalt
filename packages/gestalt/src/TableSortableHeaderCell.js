e2cd5d222e08d0c765bfc759c494af64a36b4054
Payal <payal@pinterest.com>
2020-05-26 15:09:05 -0700

    Table: Update sortable header cells

--- packages/gestalt/src/TableSortableHeaderCell.js
+++ packages/gestalt/src/TableSortableHeaderCell.js
@@ -1,26 +1,23 @@
 // @flow strict
 import * as React from 'react';
-import classnames from 'classnames';
 import Box from './Box.js';
 import Icon from './Icon.js';
-import styles from './Table.css';
+import TableHeaderCell from './TableHeaderCell.js';
+import Touchable from './Touchable.js';
 
 type Props = {|
   children: React.Node,
   colSpan?: number,
   onSortChange: ({
     event:
-      | SyntheticMouseEvent<HTMLTableCellElement>
-      | SyntheticKeyboardEvent<HTMLTableCellElement>,
+      | SyntheticMouseEvent<HTMLDivElement>
+      | SyntheticKeyboardEvent<HTMLDivElement>,
   }) => void,
   rowSpan?: number,
   scope?: 'col' | 'colgroup' | 'row' | 'rowgroup',
   sortOrder: 'asc' | 'desc',
   status: 'active' | 'inactive',
 |};
-
-const SPACE_CHAR_CODE = 32;
-const ENTER_CHAR_CODE = 13;
 
 export default function TableSortableHeaderCell(props: Props) {
   const {
@@ -36,58 +33,38 @@ export default function TableSortableHeaderCell(props: Props) {
   const [isFocused, setFocused] = React.useState(false);
   const [isHovered, setHovered] = React.useState(false);
 
-  const handleKeyPress = (
-    event: SyntheticKeyboardEvent<HTMLTableCellElement>
-  ) => {
-    if (
-      event.charCode === SPACE_CHAR_CODE ||
-      event.charCode === ENTER_CHAR_CODE
-    ) {
-      // Prevent the default action to stop scrolling when space is pressed
-      event.preventDefault();
-      onSortChange({ event });
-    }
-  };
-
   const shouldShowIcon = status === 'active' || isHovered || isFocused;
   const visibility = shouldShowIcon ? 'visible' : 'hidden';
 
-  const classes = classnames(styles.th, styles.pointer, styles.accessibleStyle);
-
   return (
-    <th
-      className={classes}
-      scope={scope || 'col'}
-      colSpan={colSpan}
-      rowSpan={rowSpan}
-      onClick={event => onSortChange({ event })}
-      onKeyPress={handleKeyPress}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-      onFocus={() => setFocused(true)}
-      onBlur={() => setFocused(false)}
-      tabIndex="0"
-      role="button"
-    >
-      <Box display="flex">
-        {children}
-        <Box
-          marginLeft={2}
-          dangerouslySetInlineStyle={{
-            __style: { visibility },
-          }}
-        >
-          <Icon
-            accessibilityLabel=""
-            icon={
-              status === 'active' && sortOrder === 'asc'
-                ? 'sort-ascending'
-                : 'sort-descending'
-            }
-            color={status === 'active' ? 'darkGray' : 'gray'}
-          />
+    <TableHeaderCell colSpan={colSpan} rowSpan={rowSpan} scope={scope}>
+      <Touchable
+        onTouch={onSortChange}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+      >
+        <Box display="flex">
+          {children}
+          <Box
+            marginLeft={2}
+            dangerouslySetInlineStyle={{
+              __style: { visibility },
+            }}
+          >
+            <Icon
+              accessibilityLabel=""
+              icon={
+                status === 'active' && sortOrder === 'asc'
+                  ? 'sort-ascending'
+                  : 'sort-descending'
+              }
+              color={status === 'active' ? 'darkGray' : 'gray'}
+            />
+          </Box>
         </Box>
-      </Box>
-    </th>
+      </Touchable>
+    </TableHeaderCell>
   );
 }
