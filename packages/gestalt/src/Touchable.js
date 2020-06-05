7ebf08cb7087e79fcdd67cb928c2e0ee57e30804
Jack Hsu <jackhsu@pinterest.com>
2020-06-05 13:13:36 -0700

    Convert TapArea to function component

--- packages/gestalt/src/Touchable.js
+++ packages/gestalt/src/Touchable.js
@@ -73,126 +73,110 @@ const getRoundingStyle = (r: Rounding): Style => {
   return identity();
 };
 
-export default class Touchable extends React.Component<Props> {
-  static propTypes = {
-    accessibilityControls: PropTypes.string,
-    accessibilityExpanded: PropTypes.bool,
-    accessibilityHaspopup: PropTypes.bool,
-    accessibilityLabel: PropTypes.string,
-    children: PropTypes.node,
-    disabled: PropTypes.bool,
-    fullHeight: PropTypes.bool,
-    fullWidth: PropTypes.bool,
-    mouseCursor: PropTypes.oneOf([
-      'copy',
-      'grab',
-      'grabbing',
-      'move',
-      'noDrop',
-      'pointer',
-      'zoomIn',
-      'zoomOut',
-    ]),
-    onBlur: PropTypes.func,
-    onFocus: PropTypes.func,
-    onTouch: PropTypes.func,
-    onMouseEnter: PropTypes.func,
-    onMouseLeave: PropTypes.func,
-    rounding: RoundingPropType,
-  };
-
-  handleKeyPress = (event: SyntheticKeyboardEvent<HTMLDivElement>) => {
-    const { disabled, onTouch } = this.props;
-    // Check to see if space or enter were pressed
-    if (
-      !disabled &&
-      onTouch &&
-      (event.charCode === SPACE_CHAR_CODE || event.charCode === ENTER_CHAR_CODE)
-    ) {
-      // Prevent the default action to stop scrolling when space is pressed
-      event.preventDefault();
-      onTouch({ event });
+export default function Touchable({
+  accessibilityControls,
+  accessibilityExpanded,
+  accessibilityHaspopup,
+  accessibilityLabel,
+  children,
+  disabled = false,
+  fullWidth = true,
+  fullHeight,
+  mouseCursor = 'pointer',
+  onBlur,
+  onFocus,
+  onMouseEnter,
+  onMouseLeave,
+  onTouch,
+  rounding = 0,
+}: Props) {
+  const classes = classnames(
+    styles.touchable,
+    toProps(getRoundingStyle(rounding)).className,
+    {
+      [styles.fullHeight]: fullHeight,
+      [styles.fullWidth]: fullWidth,
+      [styles[mouseCursor]]: !disabled,
     }
-  };
+  );
 
-  handleBlur = (event: SyntheticFocusEvent<HTMLDivElement>) => {
-    const { disabled, onBlur } = this.props;
-    if (!disabled && onBlur) {
-      onBlur({ event });
-    }
-  };
-
-  handleFocus = (event: SyntheticFocusEvent<HTMLDivElement>) => {
-    const { disabled, onFocus } = this.props;
-    if (!disabled && onFocus) {
-      onFocus({ event });
-    }
-  };
-
-  handleMouseEnter = (event: SyntheticMouseEvent<HTMLDivElement>) => {
-    const { disabled, onMouseEnter } = this.props;
-    if (!disabled && onMouseEnter) {
-      onMouseEnter({ event });
-    }
-  };
-
-  handleMouseLeave = (event: SyntheticMouseEvent<HTMLDivElement>) => {
-    const { disabled, onMouseLeave } = this.props;
-    if (!disabled && onMouseLeave) {
-      onMouseLeave({ event });
-    }
-  };
-
-  handleClick = (event: SyntheticMouseEvent<HTMLDivElement>) => {
-    const { disabled, onTouch } = this.props;
-    if (!disabled && onTouch) {
-      onTouch({ event });
-    }
-  };
-
-  render() {
-    const {
-      accessibilityControls,
-      accessibilityExpanded,
-      accessibilityHaspopup,
-      accessibilityLabel,
-      children,
-      disabled = false,
-      fullWidth = true,
-      fullHeight,
-      mouseCursor = 'pointer',
-      rounding = 0,
-    } = this.props;
-
-    const classes = classnames(
-      styles.touchable,
-      toProps(getRoundingStyle(rounding)).className,
-      {
-        [styles.fullHeight]: fullHeight,
-        [styles.fullWidth]: fullWidth,
-        [styles[mouseCursor]]: !disabled,
-      }
-    );
-
-    return (
-      <div
-        aria-controls={accessibilityControls}
-        aria-disabled={disabled}
-        aria-expanded={accessibilityExpanded}
-        aria-haspopup={accessibilityHaspopup}
-        aria-label={accessibilityLabel}
-        className={classes}
-        onClick={this.handleClick}
-        onBlur={this.handleBlur}
-        onFocus={this.handleFocus}
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-        onKeyPress={this.handleKeyPress}
-        role="button"
-        tabIndex={disabled ? null : '0'}
-      >
-        {children}
-      </div>
-    );
-  }
+  return (
+    <div
+      aria-controls={accessibilityControls}
+      aria-disabled={disabled}
+      aria-expanded={accessibilityExpanded}
+      aria-haspopup={accessibilityHaspopup}
+      aria-label={accessibilityLabel}
+      className={classes}
+      onClick={event => {
+        if (!disabled && onTouch) {
+          onTouch({ event });
+        }
+      }}
+      onBlur={event => {
+        if (!disabled && onBlur) {
+          onBlur({ event });
+        }
+      }}
+      onFocus={event => {
+        if (!disabled && onFocus) {
+          onFocus({ event });
+        }
+      }}
+      onMouseEnter={event => {
+        if (!disabled && onMouseEnter) {
+          onMouseEnter({ event });
+        }
+      }}
+      onMouseLeave={event => {
+        if (!disabled && onMouseLeave) {
+          onMouseLeave({ event });
+        }
+      }}
+      onKeyPress={event => {
+        // Check to see if space or enter were pressed
+        if (
+          !disabled &&
+          onTouch &&
+          (event.charCode === SPACE_CHAR_CODE ||
+            event.charCode === ENTER_CHAR_CODE)
+        ) {
+          // Prevent the default action to stop scrolling when space is pressed
+          event.preventDefault();
+          onTouch({ event });
+        }
+      }}
+      role="button"
+      tabIndex={disabled ? null : '0'}
+    >
+      {children}
+    </div>
+  );
 }
+
+Touchable.propTypes = {
+  accessibilityControls: PropTypes.string,
+  accessibilityExpanded: PropTypes.bool,
+  accessibilityHaspopup: PropTypes.bool,
+  accessibilityLabel: PropTypes.string,
+  children: PropTypes.node,
+  disabled: PropTypes.bool,
+  fullHeight: PropTypes.bool,
+  fullWidth: PropTypes.bool,
+  mouseCursor: PropTypes.oneOf([
+    'copy',
+    'grab',
+    'grabbing',
+    'move',
+    'noDrop',
+    'pointer',
+    'zoomIn',
+    'zoomOut',
+  ]),
+  onBlur: PropTypes.func,
+  onFocus: PropTypes.func,
+  onTouch: PropTypes.func,
+  onMouseEnter: PropTypes.func,
+  onMouseLeave: PropTypes.func,
+  rounding: RoundingPropType,
+};
