03be3c32dbdff9ac927159c33ef459f8428d8805
Kessy Similien <ksimilien@pinterest.com>
2020-06-22 11:09:34 -0700

    Remove log

--- packages/gestalt/src/TapArea.js
+++ packages/gestalt/src/TapArea.js
@@ -94,8 +94,6 @@ function TapArea({
       className={className}
       onContextMenu={event => event.preventDefault()}
       onClick={event => {
-        console.log('handleOnClick');
-
         if (!disabled && onTap) {
           onTap({ event });
         }
