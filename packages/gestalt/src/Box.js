7693edfcae890a0029806f44b4cdd2d8b725dbc0
Chris Lloyd <chrislloyd@pinterest.com>
2020-07-07 22:15:16 -0700

    appease eslint gods

--- packages/gestalt/src/Box.js
+++ packages/gestalt/src/Box.js
@@ -135,7 +135,6 @@ type ResponsiveProps = {|
 
 type Rounding = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 'circle' | 'pill';
 
-// eslint-disable-next-line flowtype/require-exact-type
 type PropType = {
   children?: React.Node,
   dangerouslySetInlineStyle?: {|
