22b972997597a1c0c31567842ee6d6e53c95bd15
Roman Chen <zhuoranchen@pinterest.com>
2020-07-09 02:28:38 -0700

    Icon: Add protected icon

--- packages/gestalt/src/icons/index.js
+++ packages/gestalt/src/icons/index.js
@@ -115,6 +115,7 @@ import pin from './pin.svg';
 import pinHide from './pin-hide.svg';
 import pinterest from './pinterest.svg';
 import play from './play.svg';
+import protected from './protected.svg';
 import questionMark from './question-mark.svg';
 import refresh from './refresh.svg';
 import remove from './remove.svg';
@@ -285,6 +286,7 @@ export default {
   'pin-hide': pinHide,
   pinterest,
   play,
+  protected,
   refresh,
   'question-mark': questionMark,
   remove,
