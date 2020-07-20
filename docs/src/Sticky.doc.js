2ec90ab968ea4af01df074fcee882142b4c7e0a4
Alberto Carreras Carrasco <acarreras@pinterest.com>
2020-07-20 16:15:00 -0700

    Docs: Refactored Sticky example to use new Box's zIndex prop

--- docs/src/Sticky.doc.js
+++ docs/src/Sticky.doc.js
@@ -54,33 +54,50 @@ card(
   <Example
     name="Example: Sticky top"
     defaultCode={`
-<Box color="white" height={200} overflow="scroll">
-  <Box height={500} marginTop={10}>
-    <Box>
-      <Sticky top={0}>
-        <Box alignItems="center" color="lightGray" display="flex" height={40}>
-          <Text>This should stick</Text>
+function Example() {
+  const BOX_ZINDEX = new FixedZIndex(1);
+  const STICKY_ZINDEX = new CompositeZIndex([BOX_ZINDEX]);
+
+  return (
+    <Box color="white" height={200} overflow="scroll">
+      <Box height={500} marginTop={10}>
+        <Box>
+          <Sticky top={0}>
+            <Box
+              alignItems="center"
+              color="lightGray"
+              display="flex"
+              height={40}>
+                <Text>This should stick</Text>
+            </Box>
+          </Sticky>
+          <Box marginTop={10} position="relative">
+            <Text>Scroll</Text>
+            <Text>Keep scrolling</Text>
+            <Text>Scroll more</Text>
+          </Box>
         </Box>
-      </Sticky>
-      <Box marginTop={10} position="relative">
-        <Text>Scroll</Text>
-        <Text>Keep scrolling</Text>
-        <Text>Scroll more</Text>
+        <Box>
+          <Sticky top={0} zIndex={STICKY_ZINDEX}>
+            <Box
+              alignItems="center"
+              color="lightGray"
+              display="flex"
+              height={40}
+              position="relative"
+              zIndex={BOX_ZINDEX}>
+                <Text>This should also stick</Text>
+            </Box>
+          </Sticky>
+          <Box marginTop={10}>
+            <Text>Still scrolling</Text>
+            <Text>Tadaaaaa</Text>
+          </Box>
+        </Box>
       </Box>
     </Box>
-    <Box>
-      <Sticky top={0} dangerouslySetZIndex={{ __zIndex: 3 }}>
-      <Box alignItems="center" color="lightGray" display="flex" height={40} position="relative" dangerouslySetInlineStyle={{ __style: { zIndex: 2 } }}>
-          <Text>This should also stick</Text>
-        </Box>
-      </Sticky>
-      <Box marginTop={10}>
-        <Text>Still scrolling</Text>
-        <Text>Tadaaaaa</Text>
-      </Box>
-    </Box>
-  </Box>
-</Box>
+  )
+}
 `}
   />
 );
