7518f463db51abe45e26890dd45e069c219013d6
Christian Vuerings <vueringschristian@gmail.com>
2020-07-27 12:18:59 -0700

    Docs Sidebar: remove collapsing + update heading / selected styles (#1078)
    * Remove collapsing
    * Update heading style
    * Update selected style
    
    We also decided that
    * `CompositeZIndex` and `FixedZIndex` will live under `Layout`
    * `useColorScheme` will live under `Setup`
    
    ## Before
    
    ![image](https://user-images.githubusercontent.com/127199/88576175-df658480-cff9-11ea-914b-56b5b54822c8.png)
    
    
    ## After
    
    ![image](https://user-images.githubusercontent.com/127199/88576212-ec827380-cff9-11ea-8275-78be58de2690.png)
    
    
    Note: this will break our search functionality for < 24 hours since the URLs will change and the crawler runs every 24 hours.

---
+++ docs/src/components/SidebarSection.js
@@ -0,0 +1,26 @@
+// @flow strict
+import React from 'react';
+import { Box, Row, Text } from 'gestalt';
+import { type sidebarIndexType } from './sidebarIndex.js';
+import NavLink from './NavLink.js';
+
+export default function CollapsibleSection({ section }: sidebarIndexType) {
+  return (
+    <>
+      <Box padding={2} marginTop={4} role="list">
+        <Row justifyContent="between">
+          <Text size="sm">{section.sectionName}</Text>
+        </Row>
+      </Box>
+      {section.pages.map((component, i) => (
+        <Box key={i}>
+          <NavLink to={`/${section.sectionPathname}/${component}`}>
+            <Box padding={2} role="listitem">
+              {component}
+            </Box>
+          </NavLink>
+        </Box>
+      ))}
+    </>
+  );
+}
--- docs/src/components/sidebarIndex.js
+++ docs/src/components/sidebarIndex.js
@@ -3,106 +3,93 @@
 export type sidebarIndexType = Array<{|
   sectionPathname: string,
   displayName: string,
-  pages?: Array<string>,
-  subsections?: Array<{| [string]: Array<string> |}>,
+  pages: Array<string>,
 |}>;
-
-const componentSubSectionPages = {
-  displayDate: ['Avatar', 'AvatarPair', 'Badge', 'GroupAvatar', 'Table'],
-  feedBack: ['Modal', 'Pulsar', 'Spinner', 'Toast', 'Tooltip'],
-  forms: [
-    'Button',
-    'Checkbox',
-    'DatePicker',
-    'Flyout',
-    'IconButton',
-    'Label',
-    'Pog',
-    'RadioButton',
-    'SearchField',
-    'SelectList',
-    'Switch',
-    'TapArea',
-    'TextArea',
-    'TextField',
-    'Tooltip',
-    'Typeahead',
-  ],
-  foundation: ['Heading', 'Icon', 'Text'],
-  layout: [
-    'Box',
-    'Card',
-    'Collage',
-    'Column',
-    'Container',
-    'Divider',
-    'Layer',
-    'Masonry',
-    'Row',
-    'Stack',
-    'Sticky',
-  ],
-  media: ['Image', 'Letterbox', 'Mask', 'Video'],
-  navigation: ['Link', 'SegmentedControl', 'Tabs'],
-  setup: ['GestaltProvider'],
-};
 
 // sidebarIndex is the source of truth for the sidebar documentation menu.
 // sidebarIndex establishes the sidebar hierarchical menu order:
 // section 1
-//    >>> subsection 1
-//           >>> page 1
-//    >>> subsection 2
-//           >>> page 2
-//           >>> page 3
-// [Documentation] pages (p.e. 'Spinner', 'Installation') get grouped in subsections (p.e. 'Feedback', 'Forms'),
-// subsection get grouped in top level sections (p.e. 'getting-started', 'components').
-// Any new section/subsectionName/page must be added to sidebarIndex to be displayed.
+//    >>> page 1
+// section 2
+//    >>> page 2
+//    >>> page 3
+// Any new section/page must be added to sidebarIndex to be displayed.
 const sidebarIndex: sidebarIndexType = [
   {
     sectionPathname: 'getting-started',
     sectionName: 'Getting Started',
     pages: ['Installation'],
   },
+  {
+    sectionPathname: 'foundation',
+    sectionName: 'Foundation',
+    pages: ['Heading', 'Icon', 'Text'],
+  },
 
   {
-    sectionPathname: 'components',
-    sectionName: 'Components',
-    subsections: [
-      {
-        subsectionName: 'Foundation',
-        pages: componentSubSectionPages.foundation,
-      },
-      {
-        subsectionName: 'Setup',
-        pages: componentSubSectionPages.setup,
-      },
-      {
-        subsectionName: 'Data Display',
-        pages: componentSubSectionPages.displayDate,
-      },
-      {
-        subsectionName: 'Feedback',
-        pages: componentSubSectionPages.feedBack,
-      },
+    sectionName: 'Setup',
+    sectionPathname: 'setup',
+    pages: ['GestaltProvider'],
+  },
+  {
+    sectionName: 'Data Display',
+    sectionPathname: 'data-display',
+    pages: ['Avatar', 'AvatarPair', 'Badge', 'GroupAvatar', 'Table'],
+  },
+  {
+    sectionName: 'Feedback',
+    sectionPathname: 'feedback',
+    pages: ['Modal', 'Pulsar', 'Spinner', 'Toast', 'Tooltip'],
+  },
 
-      {
-        subsectionName: 'Forms',
-        pages: componentSubSectionPages.forms,
-      },
-      {
-        subsectionName: 'Layout',
-        pages: componentSubSectionPages.layout,
-      },
-      {
-        subsectionName: 'Media',
-        pages: componentSubSectionPages.media,
-      },
-      {
-        subsectionName: 'Navigation',
-        pages: componentSubSectionPages.navigation,
-      },
+  {
+    sectionName: 'Forms',
+    sectionPathname: 'forms',
+    pages: [
+      'Button',
+      'Checkbox',
+      'DatePicker',
+      'Flyout',
+      'IconButton',
+      'Label',
+      'Pog',
+      'RadioButton',
+      'SearchField',
+      'SelectList',
+      'Switch',
+      'TapArea',
+      'TextArea',
+      'TextField',
+      'Tooltip',
+      'Typeahead',
     ],
+  },
+  {
+    sectionName: 'Layout',
+    sectionPathname: 'layout',
+    pages: [
+      'Box',
+      'Card',
+      'Collage',
+      'Column',
+      'Container',
+      'Divider',
+      'Layer',
+      'Masonry',
+      'Row',
+      'Stack',
+      'Sticky',
+    ],
+  },
+  {
+    sectionName: 'Media',
+    sectionPathname: 'media',
+    pages: ['Image', 'Letterbox', 'Mask', 'Video'],
+  },
+  {
+    sectionName: 'Navigation',
+    sectionPathname: 'navigation',
+    pages: ['Link', 'SegmentedControl', 'Tabs'],
   },
 ];
 
