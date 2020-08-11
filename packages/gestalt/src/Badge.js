537b5398c58cfb5b2d19cb66ca1f511c7393b333
Jenny Steele <jsteele@pinterest.com>
2020-08-10 18:01:04 -0700

    Badge and Callout updates

--- packages/gestalt/src/Badge.js
+++ packages/gestalt/src/Badge.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import styles from './Badge.css';
 import colors from './Colors.css';
+import { useColorScheme } from './contexts/ColorScheme.js';
 
 type Props = {|
   position?: 'middle' | 'top',
@@ -12,8 +13,11 @@ type Props = {|
 
 export default function Badge(props: Props): Node {
   const { position = 'middle', text } = props;
+  const { name: colorSchemeName } = useColorScheme();
 
-  const cs = cx(styles.Badge, styles[position], colors.blueBg);
+  const cs = cx(styles.Badge, styles[position], colors.blueBg, {
+    [colors.darkGray]: colorSchemeName === 'darkMode',
+  });
 
   return <span className={cs}>{text}</span>;
 }
