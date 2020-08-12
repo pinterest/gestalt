6b2aef6116faea44a0e6dd9aea4d2d6f536b0b66
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2020-08-12 14:14:16 -0700

    Datepicker: standardized date format & placeholder date format across… (#1135)
    … locale
    
    - `date format` is used to format the displayed date and as default placeholder following [datepicker best practices]( https://uxplanet.org/date-picker-design-best-practices-41bd522f10a5).
    - Locale format provided by `date-fns` was inconsistent across locales. 
    - We added a small library to provide a consistent date format across locales.
    
    ### Before
    ![image](https://user-images.githubusercontent.com/10593890/89683327-33e7ea00-d8ad-11ea-9316-569053712f3b.png)
    ![image](https://user-images.githubusercontent.com/10593890/89683392-4cf09b00-d8ad-11ea-9f3e-7c5c35baed4d.png)
    
    ### After
    ![image](https://user-images.githubusercontent.com/10593890/89683349-3a766180-d8ad-11ea-84f4-d9a59e0b989d.png)
    ![image](https://user-images.githubusercontent.com/10593890/89683403-5548d600-d8ad-11ea-9728-b0b57005036b.png)
    
    <!--
    What is the purpose of this PR?
    
    * What is the context surrounding this PR? Include links if possible.
    * What kind of feedback do you want?
    * Have you [formatted the PR title](https://github.com/pinterest/gestalt/#releasing)? `ComponentName: Description`
    -->
    
    ## Test Plan
    
    <!--
    How can reviewers verify this is good to merge?
    
    * Is it tested?
    * Is it accessible?
    * Is it documented?
    * Have you involved other stakeholders (such as a Pinterest Designer)?
    -->

--- packages/gestalt-datepicker/src/DatePicker.js
+++ packages/gestalt-datepicker/src/DatePicker.js
@@ -14,6 +14,7 @@ import { Icon, Box, Label, Text } from 'gestalt';
 import PropTypes from 'prop-types';
 import DatePickerTextField from './DatePickerTextField.js';
 import styles from './DatePicker.css';
+import dateFormat from './dateFormat.js';
 import { LocaleDataPropTypes, type LocaleData } from './LocaleDataTypes.js';
 
 type Props = {|
@@ -75,7 +76,7 @@ const DatePickerWithForwardRef: React$AbstractComponent<
   // in the month and we need to keep the flyout pointed at the input correctly
   const [selected, setSelected] = useState<?Date>(dateValue);
   const [, setMonth] = useState<?number>();
-  const [dateFormat, setDateFormat] = useState<?string>();
+  const [format, setFormat] = useState<?string>();
   const [updatedLocale, setUpdatedLocale] = useState<?string>();
   const [initRangeHighlight, setInitRangeHighlight] = useState<?Date>();
 
@@ -89,9 +90,7 @@ const DatePickerWithForwardRef: React$AbstractComponent<
     if (localeData && localeData.code) {
       registerLocale(localeData.code, localeData);
       setUpdatedLocale(localeData.code);
-      setDateFormat(
-        localeData.formatLong && localeData.formatLong.date({ width: 'short' })
-      );
+      setFormat(dateFormat[localeData.code || 'en-US']);
     }
   }, [localeData]);
 
@@ -125,7 +124,7 @@ const DatePickerWithForwardRef: React$AbstractComponent<
       <ReactDatePicker
         calendarClassName={classnames(styles['react-datepicker'])}
         customInput={<DatePickerTextField id={id} />}
-        dateFormat={dateFormat}
+        dateFormat={format}
         dayClassName={() => classnames(styles['react-datepicker__days'])}
         disabled={disabled}
         endDate={rangeEndDate}
@@ -156,7 +155,7 @@ const DatePickerWithForwardRef: React$AbstractComponent<
         }}
         onKeyDown={event => updateNextRef(event.key === 'Enter')}
         onMonthChange={(newMonth: Date) => setMonth(newMonth.getMonth())}
-        placeholderText={placeholder || dateFormat || 'mm/dd/yyyy'}
+        placeholderText={placeholder || format?.toUpperCase() || 'MM/DD/YYYY'}
         popperClassName={classnames(
           styles['react-datepicker-popper'],
           styles[`react-datepicker-popper-${popperPlacement[idealDirection]}`]
