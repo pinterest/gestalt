c77076e32a8d5a9b9e1707f18ad04d99b51f1915
Alberto Carreras Carrasco <acarreras@pinterest.com>
2020-08-12 12:50:04 -0700

    Callout: added codemod

---
+++ packages/gestalt-codemods/11.15.0-11.16.0/__testfixtures__/callout-rename-description-message.input.js
@@ -0,0 +1,20 @@
+// @flow strict
+import React from 'react';
+import { Box, Callout, Callout as Renamed } from 'gestalt';
+
+export default function TestBox() {
+  return (
+    <Box>
+      <Callout
+        type="error"
+        iconAccessibilityLabel="Error icon"
+        description="This action can't be undone."
+      />
+      <Renamed
+        type="error"
+        iconAccessibilityLabel="Error icon"
+        description="This action can't be undone."
+      />
+    </Box>
+  );
+}
---
+++ packages/gestalt-codemods/11.15.0-11.16.0/__tests__/callout-rename-description-message.test.js
@@ -0,0 +1,21 @@
+import { defineTest } from 'jscodeshift/dist/testUtils.js';
+
+jest.mock('../callout-rename-description-message', () => {
+  return Object.assign(
+    jest.requireActual('../callout-rename-description-message'),
+    {
+      parser: 'flow',
+    }
+  );
+});
+
+describe('callout-rename-description-message', () => {
+  ['callout-rename-description-message'].forEach(test => {
+    defineTest(
+      __dirname,
+      'callout-rename-description-message',
+      { quote: 'single' },
+      test
+    );
+  });
+});
