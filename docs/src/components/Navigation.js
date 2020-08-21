b61b9ede2d4d7dde9a450d4496a8467de97dbaef
Christian Vuerings <vueringschristian@gmail.com>
2020-08-21 10:56:32 -0700

    Docs: add sidebar alphabetization toggle (#1154)
    * Docs: Navigation- Add an option to list components alphabetically
    
    * fix rendering navigation
    
    * Docs: add sidebar alphabetization toggle
    
    * Address comments
    
    Co-authored-by: Mason Jennings <mjennings@pinterest.com>

--- docs/src/components/Navigation.js
+++ docs/src/components/Navigation.js
@@ -2,26 +2,50 @@
 import React from 'react';
 import { Box } from 'gestalt';
 import SidebarSection from './SidebarSection.js';
+import SidebarSectionLink from './SidebarSectionLink.js';
 import sidebarIndex from './sidebarIndex.js';
 import { useSidebarContext } from './sidebarContext.js';
 
+function getAlphabetizedComponents() {
+  return Array.from(
+    new Set(
+      sidebarIndex
+        .map(section => section.pages)
+        .flat()
+        .sort()
+    )
+  );
+}
+
 export default function Navigation() {
-  const { isSidebarOpen } = useSidebarContext();
+  const { sidebarOrganisedBy, isSidebarOpen } = useSidebarContext();
+
+  const navList = (
+    <>
+      {sidebarOrganisedBy === 'categorized' ? (
+        sidebarIndex.map(section => (
+          <SidebarSection section={section} key={section.sectionName} />
+        ))
+      ) : (
+        <Box marginTop={4}>
+          {getAlphabetizedComponents().map((componentName, i) => (
+            <SidebarSectionLink key={i} componentName={componentName} />
+          ))}
+        </Box>
+      )}
+    </>
+  );
 
   return (
     <>
       {isSidebarOpen && (
         <Box display="block" mdDisplay="none" padding={4}>
-          {sidebarIndex.map(section => (
-            <SidebarSection section={section} key={section.sectionName} />
-          ))}
+          {navList}
         </Box>
       )}
 
       <Box display="none" mdDisplay="block" padding={4}>
-        {sidebarIndex.map(section => (
-          <SidebarSection section={section} key={section.sectionName} />
-        ))}
+        {navList}
       </Box>
     </>
   );
