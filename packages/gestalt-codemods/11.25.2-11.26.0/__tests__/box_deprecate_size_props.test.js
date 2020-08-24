1ee5ee745bacdcb8a1487f243adebc5c1a8761e0
Alberto Carreras Carrasco <acarreras@pinterest.com>
2020-08-24 10:24:11 -0700

    Box: removed deprecated xs, sm, md, lg props + codemod helper

---
+++ packages/gestalt-codemods/11.25.2-11.26.0/__tests__/box_deprecate_size_props.test.js
@@ -0,0 +1,18 @@
+import { defineTest } from 'jscodeshift/dist/testUtils.js';
+
+jest.mock('../box_deprecate_size_props', () => {
+  return Object.assign(jest.requireActual('../box_deprecate_size_props'), {
+    parser: 'flow',
+  });
+});
+
+describe('box_deprecate_size_props', () => {
+  ['box_deprecate_size_props'].forEach(test => {
+    defineTest(
+      __dirname,
+      'box_deprecate_size_props',
+      { quote: 'single' },
+      test
+    );
+  });
+});
