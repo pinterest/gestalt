f00434a03f279b6cf2397beee853c1e3dffb1626
Alberto Carreras Carrasco <acarreras@pinterest.com>
2020-08-24 10:54:59 -0700

    Column: removed deprecated xs, sm, md, lg props + codemod helper

--- packages/gestalt/src/Column.flowtest.js
+++ packages/gestalt/src/Column.flowtest.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Column from './Column.js';
 
-const Valid = <Column xs={1}>Hello world</Column>;
+const Valid = <Column span={1}>Hello world</Column>;
 
-// $FlowExpectedError[incompatible-type]
-const NonExistingProp = <Column nonexisting={33} />;
+// $FlowExpectedError[prop-missing]
+const NonExistingProp = <Column span={1} nonexisting={33} />;
