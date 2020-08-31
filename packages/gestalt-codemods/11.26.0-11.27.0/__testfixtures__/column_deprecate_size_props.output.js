a7858d761eaaf10948c5631d6f94a4a109ea4f9f
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2020-08-31 14:44:56 -0700

    Column: removed deprecated xs, sm, md, lg props + codemod helper (#1165)
    Column: removed deprecated xs, sm, md, lg props + codemod helper
    
    ## FROM
    ```
    <Column xs={12}>
          <RenamedColumn md={12} />
     </Column>
    ```
     
    ## TO
    ```
    <Column span={12}>
          <RenamedColumn mdSpan={12} />
     </Column>
    ```
    
    Codemod helper, run
    `yarn codemod --parser=flow -t=packages/gestalt-codemods/11.26.0-11.27.0/column_deprecate_size_props.js relative/path/to/your/code
    `
    
    <!--
    What is the purpose of this PR?
    
    * What is the context surrounding this PR? Include links if possible.
    * What kind of feedback do you want?
    * Have you [formatted the PR title](https://github.com/pinterest/gestalt/#releasing)? `ComponentName: Description`
    -->
    
    ## Test Plan
    
    <!--
    How can reviewers verify this is good to merge?
    
    * Is it tested?
    * Is it accessible?
    * Is it documented?
    * Have you involved other stakeholders (such as a Pinterest Designer)?
    -->

---
+++ packages/gestalt-codemods/11.26.0-11.27.0/__testfixtures__/column_deprecate_size_props.output.js
@@ -0,0 +1,12 @@
+// @flow strict
+import React from 'react';
+import { Column, Column as RenamedColumn } from 'gestalt';
+
+export default function TestBox() {
+  const a = true
+  return (
+    <Column span={12}>
+      <RenamedColumn span={a ? 12 : 6} mdSpan={12} />
+    </Column>
+  );
+}
--- packages/gestalt/src/Column.js
+++ packages/gestalt/src/Column.js
@@ -2,39 +2,21 @@
 import React, { type Node } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import styles from './ColumnColumn.css';
+import styles from './Column.css';
 
-type DeprecatedColumns = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 type Columns = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
-type ColumnProps =
-  | {|
-      children?: Node,
-      span: Columns,
-      smSpan?: Columns,
-      mdSpan?: Columns,
-      lgSpan?: Columns,
-    |}
-  | {|
-      children?: Node,
-      xs?: DeprecatedColumns,
-      sm?: DeprecatedColumns,
-      md?: DeprecatedColumns,
-      lg?: DeprecatedColumns,
-    |};
+type ColumnProps = {|
+  children?: Node,
+  span: Columns,
+  smSpan?: Columns,
+  mdSpan?: Columns,
+  lgSpan?: Columns,
+|};
 
 export default function Column(props: ColumnProps): Node {
   const { children } = props;
   const cs = classnames(
-    (props.xs !== undefined ||
-      props.sm !== undefined ||
-      props.md !== undefined ||
-      props.lg !== undefined) &&
-      styles.deprecatedColumn,
-    props.xs && styles[`xsCol${props.xs}`],
-    props.sm && styles[`smCol${props.sm}`],
-    props.md && styles[`mdCol${props.md}`],
-    props.lg && styles[`lgCol${props.lg}`],
     props.span != null && styles[`xsCol${props.span}`],
     props.smSpan != null && styles[`smCol${props.smSpan}`],
     props.mdSpan != null && styles[`mdCol${props.mdSpan}`],
@@ -45,10 +27,6 @@ export default function Column(props: ColumnProps): Node {
 
 Column.propTypes = {
   children: PropTypes.node,
-  xs: PropTypes.number,
-  sm: PropTypes.number,
-  md: PropTypes.number,
-  lg: PropTypes.number,
   span: PropTypes.number,
   smSpan: PropTypes.number,
   mdSpan: PropTypes.number,
