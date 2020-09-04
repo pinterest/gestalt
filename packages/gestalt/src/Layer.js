b408cd55ba4b2758e2a7cb34e099c259056df059
Christian Vuerings <cvuerings@pinterest.com>
2020-09-04 09:08:27 -0700

    Layer: convert to function component

--- packages/gestalt/src/Layer.js
+++ packages/gestalt/src/Layer.js
@@ -1,54 +1,36 @@
 // @flow strict
-import { Component, type Portal, type Node } from 'react';
+import { useRef, useState, type Portal, type Node, useEffect } from 'react';
 // flowlint-next-line untyped-import:off
 import { createPortal } from 'react-dom';
 
-type Props = {|
+export default function Layer({
+  children,
+}: {|
   children: Node,
-|};
+|}): Portal | null {
+  const [mounted, setMounted] = useState(false);
+  const element = useRef<?HTMLDivElement>(null);
 
-type State = {|
-  mounted: boolean,
-|};
+  useEffect(() => {
+    setMounted(true);
 
-export default class Layer extends Component<Props, State> {
-  state: State = {
-    mounted: false,
-  };
-
-  el: HTMLDivElement;
-
-  constructor(props: Props) {
-    super(props);
     if (typeof document !== 'undefined' && document.createElement) {
-      this.el = document.createElement('div');
-    } else {
-      // eslint-disable-next-line no-console
-      console.warn(
-        'Using Layer without document present. Children will not be rendered.'
-      );
+      element.current = document.createElement('div');
     }
-  }
 
-  componentDidMount() {
-    if (typeof document !== 'undefined' && document.body) {
-      document.body.appendChild(this.el);
-      this.setState({ mounted: true });
+    if (typeof document !== 'undefined' && document.body && element.current) {
+      document.body.appendChild(element.current);
     }
-  }
 
-  componentWillUnmount() {
-    if (document.body) {
-      document.body.removeChild(this.el);
-    }
-  }
+    return () => {
+      if (typeof document !== 'undefined' && document.body && element.current) {
+        document.body.removeChild(element.current);
+      }
+    };
+  }, []);
 
-  render(): Portal | null {
-    const { children } = this.props;
-    const { mounted } = this.state;
-    if (!mounted) {
-      return null;
-    }
-    return createPortal(children, this.el);
+  if (!mounted || !element.current) {
+    return null;
   }
+  return createPortal(children, element.current);
 }
