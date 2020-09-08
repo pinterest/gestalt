40b3976348336a6eb7a863cd7bddfc850eb8d2fe
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2020-09-08 13:54:56 -0700

    Button: deprecated textColor prop values (#1188)
    - Deprecated `textColor` prop color values used for `white` & `transparent` background buttons. White/Transparent backgrounds default to `darkGray`.
    - New color value `color = 'transparentWhiteText'` for transparent background buttons with white text 
    ![image](https://user-images.githubusercontent.com/10593890/92288216-0e0c3000-eec1-11ea-912c-1df4bbbd09a3.png)
    
    
    ## Before
    ![image](https://user-images.githubusercontent.com/10593890/92183015-076ab380-ee02-11ea-8e37-ef9f02219193.png)
    ![image](https://user-images.githubusercontent.com/10593890/92183030-0cc7fe00-ee02-11ea-9e3d-f2f1254e44d7.png)
    
    ## After
    ![image](https://user-images.githubusercontent.com/10593890/92183046-12254880-ee02-11ea-8294-0d4c7700bc31.png)
    ![image](https://user-images.githubusercontent.com/10593890/92183054-17829300-ee02-11ea-9ced-502cd2ba1fc8.png)
    
    
    # Codemod
    
    ## From
    ```
           <Box>
          <Button text="Medium-sized button" inline textColor="red"/>
          <Button text="Medium-sized button" inline color="transparent"/>
          <Button text="Medium-sized button" inline textColor="white" color="transparent" />
          <Button text="Medium-sized button" inline textColor="white" color="red"/>
          <Renamed text="Medium-sized button" inline textColor="red"/>
        </Box>
    ```
    ## To
    ```
        <Box>
          <Button text="Medium-sized button" inline />
          <Button text="Medium-sized button" inline color="transparent"/>
          <Button text="Medium-sized button" inline color="transparentWhiteText" />
          <Button text="Medium-sized button" inline color="red" />
          <Renamed text="Medium-sized button" inline />
        </Box>
    ```
    
    Codemod helper, run
    yarn codemod --parser=flow -t=packages/gestalt-codemods/12.6.0-12.7.0/button-remove-textColor-prop.js relative/path/to/your/code
    
    
    <!--
    What is the purpose of this PR?
    
    * What is the context surrounding this PR? Include links if possible.
    * What kind of feedback do you want?
    * Have you [formatted the PR title](https://github.com/pinterest/gestalt/#releasing)? `ComponentName: Description`
    -->
    
    ## Test Plan
    
    <!--
    How can reviewers verify this is good to merge?
    
    * Is it tested?
    * Is it accessible?
    * Is it documented?
    * Have you involved other stakeholders (such as a Pinterest Designer)?
    -->

---
+++ packages/gestalt-codemods/12.6.0-12.7.0/__testfixtures__/button-remove-textColor-prop.input.js
@@ -0,0 +1,15 @@
+// @flow strict
+import React from 'react';
+import { Box, Button, Button as Renamed } from 'gestalt';
+
+export default function TestBox() {
+  return (
+    <Box>
+      <Button text="Medium-sized button" inline textColor="red"/>
+      <Button text="Medium-sized button" inline color="transparent"/>
+      <Button text="Medium-sized button" inline textColor="white" color="transparent" />
+      <Button text="Medium-sized button" inline textColor="white" color="red"/>
+      <Renamed text="Medium-sized button" inline textColor="red"/>
+    </Box>
+  );
+}
---
+++ packages/gestalt-codemods/12.6.0-12.7.0/__testfixtures__/button-remove-textColor-prop.output.js
@@ -0,0 +1,15 @@
+// @flow strict
+import React from 'react';
+import { Box, Button, Button as Renamed } from 'gestalt';
+
+export default function TestBox() {
+  return (
+    <Box>
+      <Button text="Medium-sized button" inline />
+      <Button text="Medium-sized button" inline color="transparent"/>
+      <Button text="Medium-sized button" inline color="transparentWhiteText" />
+      <Button text="Medium-sized button" inline color="red" />
+      <Renamed text="Medium-sized button" inline />
+    </Box>
+  );
+}
