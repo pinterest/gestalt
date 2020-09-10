491dc5c783f27c8a6ebcaff2784ee1d0e0b40db4
Christian Vuerings <cvuerings@pinterest.com>
2020-09-10 14:36:11 -0700

    Table.Header: fix stacking context on sticky header

--- docs/src/Table.doc.js
+++ docs/src/Table.doc.js
@@ -196,9 +196,12 @@ card(
     id="stickyHeader"
     name="Example: Sticky header"
     defaultCode={`
-<Table maxHeight={110}>
+<Table maxHeight={200}>
   <Table.Header sticky>
     <Table.Row>
+      <Table.HeaderCell>
+        <Text weight="bold">Image</Text>
+      </Table.HeaderCell>
       <Table.HeaderCell>
         <Text weight="bold">Name</Text>
       </Table.HeaderCell>
@@ -209,14 +212,50 @@ card(
   </Table.Header>
   <Table.Body>
     <Table.Row>
+      <Table.Cell>
+        <Box width={50}>
+          <Mask rounding="circle">
+            <Image
+              alt="Luna"
+              src="https://i.ibb.co/QY9qR7h/luna.png"
+              naturalHeight={50}
+              naturalWidth={50}
+            />
+          </Mask>
+        </Box>
+      </Table.Cell>
       <Table.Cell><Text>Luna Lovegood</Text></Table.Cell>
       <Table.Cell><Text>Ravenclaw</Text></Table.Cell>
     </Table.Row>
     <Table.Row>
+      <Table.Cell>
+        <Box width={50}>
+          <Mask rounding="circle">
+            <Image
+              alt="Draco"
+              src="https://i.ibb.co/Hzcfxjt/draco.png"
+              naturalHeight={50}
+              naturalWidth={50}
+            />
+          </Mask>
+        </Box>
+      </Table.Cell>
       <Table.Cell><Text>Draco Malfoy</Text></Table.Cell>
       <Table.Cell><Text>Slytherin</Text></Table.Cell>
     </Table.Row>
     <Table.Row>
+      <Table.Cell>
+        <Box width={50}>
+          <Mask rounding="circle">
+            <Image
+              alt="Neville"
+              src="https://i.ibb.co/JvY9DKK/neville.png"
+              naturalHeight={50}
+              naturalWidth={50}
+            />
+          </Mask>
+        </Box>
+      </Table.Cell>
       <Table.Cell><Text>Neville Longbottom</Text></Table.Cell>
       <Table.Cell><Text>Gryffindor</Text></Table.Cell>
     </Table.Row>
