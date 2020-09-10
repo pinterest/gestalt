a4f88e31c76eea42333b39d2cf6d2539bde9c23a
Jenny Steele <jsteele@pinterest.com>
2020-09-10 08:25:09 -0700

    Internal: BREAKING only build css file with css vars (#1201)
    After experimenting in the main repo with the gestalt-future.css file that has css vars in it, we've determined this is safe to ship. I'm marking this as a breaking change because any app using gestalt that needs to support older browsers such as IE11 will need to use a polyfill. I've already added this one to our app's build for legacy browsers: https://github.com/nuxodin/ie11CustomProperties
    
    Possible improvement: We still have `:root` declarations spread across a few css files. This is nice from a code organization perspective (border width vars defined in Borders.css, color vars defined in Colors.css for example) but makes less sense in the output css file. Perhaps we could combine the declarations into one Variables.css file? Some vars like the ones specific to the datepicker could be scoped to a specific class rather than globally as well. This PR is safe to ship, so I could make these changes in a follow up.
    
    I also noticed markdown syntax highlighting wasn't working when I added documentation for the polyfill. This fixes that too.
    
    ## Test Plan
    The docs are using the new css file. I've confirmed the docs look as expected and I can see the css vars in the built css file. Switching to dark mode works correctly indicating the css vars are being overwritten by the color scheme provider properly.

--- docs/src/index.js
+++ docs/src/index.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { render } from 'react-dom';
-import 'gestalt/dist/gestalt-future.css';
-import 'gestalt-datepicker/dist/gestalt-datepicker-future.css';
+import 'gestalt/dist/gestalt.css';
+import 'gestalt-datepicker/dist/gestalt-datepicker.css';
 import App from './components/App.js';
 import CardPage from './components/CardPage.js';
 import routes from './components/routes.js';
