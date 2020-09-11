f2a2186d5807d46a288853e5b1f0cf4f6b24cc8a
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2020-09-11 10:25:20 -0700

    Callout: implement Link Button on primary/secondary links (#1207)
    - Replaced links with link-role Buttons
    - Implemented accessibility labels for primary/secondary links
    - Updated Docs API
    
    ## Before
    ![Kapture 2020-09-09 at 18 32 16](https://user-images.githubusercontent.com/10593890/92671360-e525d980-f2ca-11ea-95bc-35b69cce57c0.gif)
    
    ## After
    ![Kapture 2020-09-09 at 18 34 54](https://user-images.githubusercontent.com/10593890/92671474-4188f900-f2cb-11ea-80bd-efb43475ce3a.gif)
    
    <!--
    What is the purpose of this PR?
    
    * What is the context surrounding this PR? Include links if possible.
    * What kind of feedback do you want?
    * Have you [formatted the PR title](https://github.com/pinterest/gestalt/#releasing)? `ComponentName: Description`
    -->
    
    ## Test Plan
    
    <!--
    How can reviewers verify this is good to merge?
    
    * Is it tested?
    * Is it accessible?
    * Is it documented?
    * Have you involved other stakeholders (such as a Pinterest Designer)?
    -->

--- docs/src/Callout.doc.js
+++ docs/src/Callout.doc.js
@@ -10,8 +10,7 @@ const card = c => cards.push(c);
 card(
   <PageHeader
     name="Callout"
-    description="
-A callout is a banner that displays a short message that either is helpful information to a task on the page or requires user attention."
+    description="Callouts are banners that display short messages that are either helpful information to a task on the page or require user attention."
   />
 );
 
@@ -22,34 +21,88 @@ card(
         name: 'message',
         type: 'string',
         required: true,
+        defaultValue: null,
+        description: [
+          'Text to render inside the callout to convey detailed information to the user. The message text has a fixed size.',
+        ],
+        href: '',
       },
       {
         name: 'dismissButton',
         type: '{| accessibilityLabel: string, onDismiss: () => void, |}',
+        required: false,
+        defaultValue: null,
+        description: [
+          'Callback fired when the dismiss button is clicked (pressed and released) with a mouse or keyboard.',
+          'Supply a short, descriptive label for screen-readers to provide sufficient context about the dismiss button action. IconButtons do not render text for screen readers to read requiring an accessibility label.',
+          'Accessibility: `accessibilityLabel` populates aria-label.',
+        ],
+        href: '',
       },
       {
         name: 'iconAccessibilityLabel',
         type: 'string',
         required: true,
+        defaultValue: null,
+        description: [
+          'Supply a short, descriptive label for screen-readers to provide sufficient context about the callout intent.',
+          'The callout component displays `error`, `info`, or `warning` icons which visually indicate the intent of the component. `iconAccessibilityLabel` allows screen readers to communicate that intent.',
+          'Accessibility: It populates aria-label.',
+        ],
+        href: '',
       },
       {
         name: 'primaryLink',
         type:
-          '{| href: string, label: string, onClick?: ({ event: SyntheticMouseEvent<HTMLAnchorElement> | SyntheticKeyboardEvent<HTMLAnchorElement> }) => void |}',
+          '{| accessibilityLabel?: string , href: string, label: string, onClick?: ({ event: SyntheticMouseEvent<HTMLAnchorElement> | SyntheticKeyboardEvent<HTMLAnchorElement | SyntheticMouseEvent<HTMLButtonElement> | SyntheticKeyboardEvent<HTMLButtonElement> }) => void |}',
+        required: false,
+        defaultValue: null,
+        description: [
+          'Link-role button to render inside the callout as the main call-to-action to the user. The label text has a fixed size.',
+          '- label: Text to render inside the button to convey the function and purpose of the button. The button text has a fixed size.',
+          '- accessibilityLabel: Supply a short, descriptive label for screen-readers to replace button texts that do not provide sufficient context about the button component behavior. Texts like `Click Here,` `Follow,` or `Read More` can be confusing when a screen reader reads them out of context. In those cases, we must pass an alternative text to replace the button text.',
+          '- onClick: Callback fired when the button component is clicked (pressed and released) with a mouse or keyboard.',
+          'Accessibility: `accessibilityLabel` populates aria-label. Screen readers read the `accessibilityLabel` prop, if present, instead of the button `text`.',
+        ],
+        href: '',
       },
       {
         name: 'secondaryLink',
         type:
-          '{| href: string, label: string, onClick?: ({ event: SyntheticMouseEvent<HTMLAnchorElement> | SyntheticKeyboardEvent<HTMLAnchorElement> }) => void |}',
+          '{| accessibilityLabel?: string , href: string, label: string, onClick?: ({ event: SyntheticMouseEvent<HTMLAnchorElement> | SyntheticKeyboardEvent<HTMLAnchorElement | SyntheticMouseEvent<HTMLButtonElement> | SyntheticKeyboardEvent<HTMLButtonElement> }) => void |}',
+        required: false,
+        defaultValue: null,
+        description: [
+          'Link-role button to render inside the callout as a secondary call-to-action to the user.',
+          '- label: Text to render inside the button to convey the function and purpose of the button. The button text has a fixed size.',
+          '- accessibilityLabel: Supply a short, descriptive label for screen-readers to replace button texts that do not provide sufficient context about the button component behavior. Texts like `Click Here,` `Follow,` or `Read More` can be confusing when a screen reader reads them out of context. In those cases, we must pass an alternative text to replace the button text.',
+          '- onClick: Callback fired when the button component is clicked (pressed and released) with a mouse or keyboard.',
+          'Accessibility: `accessibilityLabel` populates aria-label. Screen readers read the `accessibilityLabel` prop, if present, instead of the button `text`.',
+        ],
+        href: '',
       },
       {
         name: 'type',
         type: `"error" | "info" | "warning"`,
         required: true,
+        defaultValue: null,
+        description: [
+          'Select a type of callout:',
+          '-`error`: To provide error messaging to the user.',
+          '-`info`: To provide general information to the user.',
+          '-`warning`: To provide a warn message to the user.',
+        ],
+        href: '',
       },
       {
         name: 'title',
         type: 'string',
+        required: false,
+        defaultValue: null,
+        description: [
+          'Heading to render inside the callout above the message to convey the callout topic to the user. The message text has a fixed size.',
+        ],
+        href: '',
       },
     ]}
   />
