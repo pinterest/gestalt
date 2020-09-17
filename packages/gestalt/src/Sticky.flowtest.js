215eab3e6881fa87bcd4a577a63edb6e1f5a70ea
Christian Vuerings <vueringschristian@gmail.com>
2020-09-17 13:53:35 -0700

    Sticky: remove deprecated dangerouslySetZIndex (#1224)

---
+++ packages/gestalt/src/Sticky.flowtest.js
@@ -0,0 +1,29 @@
+// @flow strict
+import React from 'react';
+import Sticky from './Sticky.js';
+import { FixedZIndex } from './zIndex.js';
+
+const ValidDefaultStickyType = <Sticky top={0}>Content</Sticky>;
+
+const ValidZIndex = (
+  <Sticky top={0} zIndex={new FixedZIndex(1)}>
+    Content
+  </Sticky>
+);
+
+const InValidZIndex = (
+  // $FlowExpectedError[incompatible-type]
+  <Sticky top={0} zIndex={1}>
+    Content
+  </Sticky>
+);
+
+// $FlowExpectedError[incompatible-type]
+const MissingProp = <Sticky />;
+
+const IncompatibleLegacyZIndexProp = (
+  // $FlowExpectedError[incompatible-type]
+  <Sticky top={0} dangerouslySetZIndex={1}>
+    Content
+  </Sticky>
+);
