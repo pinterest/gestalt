d64f94d29f7c732d0231d7402d820753d07e77c8
Christian Vuerings <vueringschristian@gmail.com>
2020-09-28 13:27:15 -0700

    Docs: add copy code button (#1235)
    Add a copy code button to each code example
    
    ![image](https://user-images.githubusercontent.com/127199/94458466-d6647100-016a-11eb-8045-f0e698066128.png)
    
    
    ## Test Plan
    
    - Copy code button shows next to each code example
    - Clicking the button does copy the code

--- docs/src/components/ExampleCode.js
+++ docs/src/components/ExampleCode.js
@@ -3,6 +3,16 @@ import React, { useEffect, useRef, useState, type Node } from 'react';
 import { Box, Button, IconButton, Stack, Text, Tooltip } from 'gestalt';
 import { LiveEditor } from 'react-live';
 import handleCodeSandbox from './handleCodeSandbox.js';
+import clipboardCopy from './clipboardCopy.js';
+
+async function copyCode({ code }: {| code: string |}) {
+  try {
+    await clipboardCopy(code);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
+}
 
 export default function ExampleCode({
   code,
@@ -46,7 +56,7 @@ export default function ExampleCode({
                 <LiveEditor padding={16} />
               </label>
             </Box>
-            <Box padding={2}>
+            <Box padding={2} display="flex" direction="column">
               <Tooltip inline text="Open in CodeSandbox">
                 <IconButton
                   dangerouslySetSvgPath={{
@@ -57,6 +67,20 @@ export default function ExampleCode({
                   iconColor="white"
                   onClick={() => {
                     handleCodeSandbox({ code, title: name });
+                  }}
+                />
+              </Tooltip>
+
+              <Tooltip inline text="Copy Code">
+                <IconButton
+                  dangerouslySetSvgPath={{
+                    __path:
+                      'M22.696 5.13c.348 0 .652.13.913.392.26.26.391.565.391.913v16.217c0 .348-.13.652-.391.913s-.566.391-.913.391H9.87c-.348 0-.653-.13-.913-.39a1.251 1.251 0 01-.392-.914v-3.826h-7.26c-.348 0-.653-.13-.914-.391A1.251 1.251 0 010 17.522v-9c0-.348.087-.74.26-1.174.175-.435.392-.783.653-1L6.348.913c.26-.26.609-.478 1-.652C7.783.087 8.174 0 8.522 0h5.565c.348 0 .652.13.913.391.26.261.391.566.391.913v4.392c.609-.348 1.174-.522 1.696-.522h5.609V5.13zm-7.261 2.827l-4 4h4v-4zM6.87 2.827l-4 4h4v-4zm2.608 8.651l4.218-4.217V1.696h-5.13V7.26c0 .348-.131.652-.392.913-.261.26-.565.391-.913.391H1.739v8.522h6.826v-3.435c0-.348.087-.739.261-1.174.217-.434.435-.739.652-1zm12.783 10.74V6.825h-5.13v5.565c0 .348-.131.652-.392.913-.26.261-.565.392-.913.392h-5.522v8.521h11.957z',
+                  }}
+                  accessibilityLabel="Copy Code"
+                  iconColor="white"
+                  onClick={() => {
+                    copyCode({ code });
                   }}
                 />
               </Tooltip>
