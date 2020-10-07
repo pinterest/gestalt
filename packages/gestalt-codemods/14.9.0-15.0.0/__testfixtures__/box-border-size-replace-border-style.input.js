a08abc80cc9480e7b9db6726a7ce57929591323c
Ayesha Mazumdar <amazumdar@pinterest.com>
2020-10-06 18:09:41 -0700

    Create codemod to switch Box borderSize to borderStyle

---
+++ packages/gestalt-codemods/14.9.0-15.0.0/__testfixtures__/box-border-size-replace-border-style.input.js
@@ -0,0 +1,11 @@
+// @flow strict
+import React from 'react';
+import { Box } from 'gestalt';
+
+export default function TestBox() {
+  return (
+    <Box borderSize="sm">
+      <p>Hello</p>
+    </Box>
+  );
+}
