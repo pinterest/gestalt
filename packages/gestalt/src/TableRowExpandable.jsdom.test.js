2ac3022749ab2ca89d5a015447d5c8108e194a78
Faisal Gedi <fgedi@pinterest.com>
2020-10-08 09:08:24 -0700

    Table.ExpandableRow: fixing jsdom tests

--- packages/gestalt/src/TableRowExpandable.jsdom.test.js
+++ packages/gestalt/src/TableRowExpandable.jsdom.test.js
@@ -2,12 +2,16 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import Box from './Box.js';
+import TableCell from './TableCell.js';
 import TableRowExpandable from './TableRowExpandable.js';
+import Text from './Text.js';
 
 test('TableRowExpandable handles onMouseEnter callback', () => {
   const { getByText } = render(
     <TableRowExpandable expandedContents={<Box>Hello</Box>}>
-      Row Info
+      <TableCell>
+        <Text>Row Info</Text>
+      </TableCell>
     </TableRowExpandable>
   );
   fireEvent.mouseEnter(getByText('Row Info'));
@@ -19,7 +23,9 @@ test('TableRowExpandable handles onMouseEnter callback', () => {
 test('TableRowExpandable handles onMouseLeave callback', () => {
   const { getByText } = render(
     <TableRowExpandable expandedContents={<Box>Hello</Box>}>
-      Row Info
+      <TableCell>
+        <Text>Row Info</Text>
+      </TableCell>
     </TableRowExpandable>
   );
   fireEvent.mouseLeave(getByText('Row Info'));
