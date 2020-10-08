f4dd95a6712ffdcd935106dd382edccd5c06a6b5
Faisal Gedi <fgedi@pinterest.com>
2020-10-08 10:30:40 -0700

    Table.ExpandableRow: fixing jsdom tests

--- packages/gestalt/src/TableRowExpandable.jsdom.test.js
+++ packages/gestalt/src/TableRowExpandable.jsdom.test.js
@@ -2,17 +2,23 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import Box from './Box.js';
+import Table from './Table.js';
+import TableBody from './TableBody.js';
 import TableCell from './TableCell.js';
 import TableRowExpandable from './TableRowExpandable.js';
 import Text from './Text.js';
 
 test('TableRowExpandable handles onMouseEnter callback', () => {
   const { getByText } = render(
-    <TableRowExpandable expandedContents={<Box>Hello</Box>}>
-      <TableCell>
-        <Text>Row Info</Text>
-      </TableCell>
-    </TableRowExpandable>
+    <Table>
+      <TableBody>
+        <TableRowExpandable expandedContents={<Box>Hello</Box>}>
+          <TableCell>
+            <Text>Row Info</Text>
+          </TableCell>
+        </TableRowExpandable>
+      </TableBody>
+    </Table>
   );
   fireEvent.mouseEnter(getByText('Row Info'));
   expect.objectContaining({
@@ -22,11 +28,15 @@ test('TableRowExpandable handles onMouseEnter callback', () => {
 
 test('TableRowExpandable handles onMouseLeave callback', () => {
   const { getByText } = render(
-    <TableRowExpandable expandedContents={<Box>Hello</Box>}>
-      <TableCell>
-        <Text>Row Info</Text>
-      </TableCell>
-    </TableRowExpandable>
+    <Table>
+      <TableBody>
+        <TableRowExpandable expandedContents={<Box>Hello</Box>}>
+          <TableCell>
+            <Text>Row Info</Text>
+          </TableCell>
+        </TableRowExpandable>
+      </TableBody>
+    </Table>
   );
   fireEvent.mouseLeave(getByText('Row Info'));
   expect.objectContaining({
