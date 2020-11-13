3f272229ec4ff82a30d5fd81a391d824ab8f495b
Ryan James <rjames@pinterest.com>
2020-11-13 15:01:20 -0800

    lets use margins, just margins

--- scripts/flex.js
+++ scripts/flex.js
@@ -54,10 +54,10 @@ const rules = (i) => [
     'margin-bottom': `${bt(-i)}`,
     'margin-top': `${bt(-i)}`,
   }),
-  // Gap padding on children
+  // Gap margins on children
   ruleset(`.columnGap${i} > .FlexItem`, {
-    'padding-bottom': bt(i),
-    'padding-top': bt(i),
+    'margin-bottom': bt(i),
+    'margin-top': bt(i),
   }),
 
   // Negative margins on parents
@@ -65,10 +65,10 @@ const rules = (i) => [
     'margin-left': `${bt(-i)}`,
     'margin-right': `${bt(-i)}`,
   }),
-  // Gap padding on children
+  // Gap margins on children
   ruleset(`.rowGap${i} > .FlexItem`, {
-    'padding-left': bt(i),
-    'padding-right': bt(i),
+    'margin-left': bt(i),
+    'margin-right': bt(i),
   }),
 ];
 
