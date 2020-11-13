fa1f8e27767c2941c8c2d1376c24cdf5f32e17a8
Ryan James <rjames@pinterest.com>
2020-11-13 11:46:30 -0800

    filter out nullish children

--- packages/gestalt/src/Flex.js
+++ packages/gestalt/src/Flex.js
@@ -83,7 +83,7 @@ export default function Flex({
             key: index,
           },
         });
-      })
+      }).filter(Boolean)
     : childrenProp;
 
   const { passthroughProps, propsStyles } = buildStyles<Props>({
