be15cc2de721d91eeb4c8d95fd51dd5c1d66134a
Ryan James <rjames@pinterest.com>
2020-11-13 16:52:53 -0800

    add flowtest

---
+++ packages/gestalt/src/Flex.flowtest.js
@@ -0,0 +1,29 @@
+// @flow strict
+import React from 'react';
+import Flex from './Flex.js';
+
+const Valid = (
+  <Flex>
+    <div />
+  </Flex>
+);
+
+const ValidWithProps = (
+  <Flex gap={2}>
+    <div />
+  </Flex>
+);
+
+const NonExistingProp = (
+  // $FlowExpectedError[prop-missing]
+  <Flex nonexisting={33}>
+    <div />
+  </Flex>
+);
+
+const BadPropValue = (
+  // $FlowExpectedError[incompatible-type]
+  <Flex gap={false}>
+    <div />
+  </Flex>
+);
