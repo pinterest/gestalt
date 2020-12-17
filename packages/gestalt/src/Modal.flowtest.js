64c30ccb0b6cbc5c8b160b08dd2eb957379bc029
Erica Norris <enorris@pinterest.com>
2020-12-17 15:28:02 -0800

    add flow test

--- packages/gestalt/src/Modal.flowtest.js
+++ packages/gestalt/src/Modal.flowtest.js
@@ -1,6 +1,7 @@
 // @flow strict
 import React from 'react';
 import Modal from './Modal.js';
+import Test from './Test.js';
 
 const Valid = <Modal accessibilityModalLabel="Modal" onDismiss={() => {}} />;
 
@@ -9,3 +10,13 @@ const MissingProp = <Modal />;
 
 // $FlowExpectedError[prop-missing]
 const NonExistingProp = <Modal nonexisting={33} />;
+
+const InvalidProp = (
+  // $FlowExpectedError[incompatible-type]
+  <Modal
+    accessibilityModalLabel="Modal"
+    onDismiss={() => {}}
+    heading={<Text>Test</Text>}
+    subHeading="clever subheading"
+  />
+);
