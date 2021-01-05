446a042b6f7c1a51ac01eb6f89732aae77b64c75
Jack Hsu <jackhsu@pinterest.com>
2021-01-05 13:18:35 -0800

    Layer: fix a bug where Layer remounts children on rerender

--- docs/src/Layer.doc.js
+++ docs/src/Layer.doc.js
@@ -45,62 +45,6 @@ card(
 card(
   <Example
     description="
-    Since Layer is rendered within a portal element being manually added to the DOM, it is supposed to be rendered and dismissed only once. 
-    Any re-renders triggered by the parent will effectively remove the portal element from the DOM, hence it won't be recreated and internal elements like Sheet may not reappear.
-    The example below demonstrates a technique to avoid the re-rendering of Layer by making sure parent re-renders don't force children re-renders. This is accomplished by memoizing locally-defined functions with React.useCallback() if they're passed down as props, and memoizing the entire component with React.memo().
-  "
-    name="Avoid Re-rendering"
-    defaultCode={`
-function Example() {
-  // We're using React.memo() here to prevent re-renders. This is similar to
-  // shouldComponentUpdate(), but for functional components.
-  const ResilientLayer = React.memo((props) => {
-    const { onDismiss, shouldShow } = props;
-
-    if (!shouldShow) {
-      return null;
-    }
-    return (
-      <Layer>
-        <Sheet 
-          accessibilityDismissButtonLabel="Close"
-          accessibilitySheetLabel="Resilient layer"
-          onDismiss={onDismiss}
-        >
-          <Text>Resilient Layer Content</Text>
-        </Sheet>
-      </Layer>
-    );
-  });
-
-  const [showLayer, setShowLayer] = React.useState(false);
-
-  // We're using React.useCallback() here to memoize this function since it 
-  // gets passed down as a prop to <ResilientLayer>.
-  // It's very important to pass down any function prop using useCallback,
-  // otherwise, each parent re-render would recreate these functions, 
-  // which would force a children re-render as it receives different props.
-  // In our case, our children Layer would re-render and destroy its contents.
-  const onDismiss = React.useCallback(() => setShowLayer(false), []);
-
-  return (
-    <>
-      <Button
-        inline
-        text="Show Resilient Layer"
-        onClick={() => setShowLayer(true)}
-      />
-      <ResilientLayer onDismiss={onDismiss} shouldShow={showLayer} />
-    </>
-  );
-}
-`}
-  />
-);
-
-card(
-  <Example
-    description="
     Child content will be rendered outside the DOM hierarchy for easy overlaying. Click to see an example.
   "
     name="Overlaying Content"
--- packages/gestalt/src/Layer.js
+++ packages/gestalt/src/Layer.js
@@ -6,21 +6,20 @@ import styles from './Layer.css';
 
 export default function Layer({
   children,
-  zIndex,
+  zIndex: zIndexIndexable,
 }: {|
   children: Node,
   zIndex?: Indexable,
 |}): Portal | null {
   const [mounted, setMounted] = useState(false);
   const element = useRef<?HTMLDivElement>(null);
+  const zIndex: number | null = zIndexIndexable?.index() ?? null;
 
   useEffect(() => {
     setMounted(true);
 
     if (typeof document !== 'undefined' && document.createElement) {
       element.current = document.createElement('div');
-      element.current.style.zIndex = zIndex ? `${zIndex.index()}` : '';
-      element.current.className = zIndex ? styles.layer : '';
     }
 
     if (typeof document !== 'undefined' && document.body && element.current) {
@@ -32,6 +31,13 @@ export default function Layer({
         document.body.removeChild(element.current);
       }
     };
+  }, []);
+
+  useEffect(() => {
+    if (element.current) {
+      element.current.style.zIndex = zIndex === null ? '' : `${zIndex}`;
+      element.current.className = zIndex === null ? '' : styles.layer;
+    }
   }, [zIndex]);
 
   if (!mounted || !element.current) {
