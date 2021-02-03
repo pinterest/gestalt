b01c0ec95e420060ec3b34b723eb5cd04dbb3838
Alberto Carreras Carrasco <acarreras@pinterest.com>
2021-02-03 15:18:07 -0500

    Implemented OnNavigation context

---
+++ packages/gestalt/src/contexts/OnNavigation.js
@@ -0,0 +1,39 @@
+// @flow strict
+import React, { useContext, createContext, type Context, type Element, type Node } from 'react';
+import PropTypes from 'prop-types';
+
+type EventHandlerType = ({|
+  +event: SyntheticEvent<>,
+|}) => void;
+export type onNavigationOptionsType = {|
+  [string]: Node | EventHandlerType,
+|};
+export type onNavigationType = ({|
+  href: string,
+  onNavigationOptions?: onNavigationOptionsType,
+|}) => ?EventHandlerType;
+
+type OnNavigationType = {| onNavigation: onNavigationType |};
+
+type Props = {|
+  children: Node,
+  onNavigation?: onNavigationType,
+|};
+
+const OnNavigation: Context<OnNavigationType | void> = createContext<OnNavigationType | void>();
+const { Provider } = OnNavigation;
+
+function OnNavigationProvider({ onNavigation, children }: Props): Element<typeof Provider> {
+  return <Provider value={onNavigation ? { onNavigation } : undefined}>{children}</Provider>;
+}
+
+function useOnNavigation(): OnNavigationType | void {
+  return useContext(OnNavigation);
+}
+
+export { OnNavigationProvider, useOnNavigation };
+
+OnNavigationProvider.propTypes = {
+  children: PropTypes.node,
+  onNavigation: PropTypes.func,
+};
