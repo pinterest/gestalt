e1eaeaeefedc5b5a61ce2cf44a9a2bd61fe6c98a
Ryan James <dangerismycat@users.noreply.github.com>
2021-02-04 14:09:30 -0800

    Internal: update ScrollableContainer a11y test (#1365)
    This integration test wasn't configured, and needs to be as it's failing on all other PRs.

--- cypress/integration/accessibility_ScrollableContainer_spec.js
+++ cypress/integration/accessibility_ScrollableContainer_spec.js
@@ -5,6 +5,14 @@ describe('ScrollableContainer Accessibility check', () => {
   });
 
   it('Tests accessibility on the ScrollableContainer page', () => {
+    cy.configureAxe({
+      rules: [
+        {
+          id: 'color-contrast',
+          enabled: false,
+        },
+      ],
+    });
     cy.checkA11y();
   });
 });
