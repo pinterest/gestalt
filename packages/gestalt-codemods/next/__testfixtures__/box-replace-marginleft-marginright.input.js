e24dce7e3c5ac420886cae81cfc98e27eaa95f3b
Ryan James <rjames@pinterest.com>
2021-02-04 12:32:38 -0800

    add test cases for responsive props

--- packages/gestalt-codemods/next/__testfixtures__/box-replace-marginleft-marginright.input.js
+++ packages/gestalt-codemods/next/__testfixtures__/box-replace-marginleft-marginright.input.js
@@ -3,5 +3,12 @@ import React from 'react';
 import { Box } from 'gestalt';
 
 export default function TestBox() {
-  return <Box marginLeft={4} marginRight={4} />;
+  return (
+    <Box>
+      <Box marginLeft={4} marginRight={4} />
+      <Box smMarginLeft={4} smMarginRight={4} />
+      <Box mdMarginLeft={4} mdMarginRight={4} />
+      <Box lgMarginLeft={4} lgMarginRight={4} />
+    </Box>
+  );
 }
--- packages/gestalt-codemods/next/__testfixtures__/box-replace-marginleft-marginright.output.js
+++ packages/gestalt-codemods/next/__testfixtures__/box-replace-marginleft-marginright.output.js
@@ -3,5 +3,12 @@ import React from 'react';
 import { Box } from 'gestalt';
 
 export default function TestBox() {
-  return <Box marginStart={4} marginEnd={4} />;
+  return (
+    <Box>
+      <Box marginStart={4} marginEnd={4} />
+      <Box smMarginStart={4} smMarginEnd={4} />
+      <Box mdMarginStart={4} mdMarginEnd={4} />
+      <Box lgMarginStart={4} lgMarginEnd={4} />
+    </Box>
+  );
 }
