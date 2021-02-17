0b0f717b33c30630dced2111caff1b9441ed8146
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2021-02-17 17:35:01 -0500

    Tooltip: update Tooltip documentation, add best practices (#1370)
    * implemented new Tooltip Docs
    
    * Fix: address comments
    
    * fix: address last comments

--- cypress/integration/accessibility_Tooltip_spec.js
+++ cypress/integration/accessibility_Tooltip_spec.js
@@ -5,6 +5,18 @@ describe('Tooltip Accessibility check', () => {
   });
 
   it('Tests accessibility on the Tooltip page', () => {
+    cy.configureAxe({
+      rules: [
+        {
+          id: 'color-contrast',
+          enabled: false,
+        },
+        {
+          id: 'button-name',
+          enabled: false,
+        },
+      ],
+    });
     cy.checkA11y();
   });
 });
