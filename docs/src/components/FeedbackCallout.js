99f81007b65466cabf8f0e60be0ab3ff47918565
Ayesha Mazumdar <amazumdar@pinterest.com>
2021-03-01 20:49:58 -0800

    updates

--- docs/src/components/FeedbackCallout.js
+++ docs/src/components/FeedbackCallout.js
@@ -2,7 +2,7 @@
 import { Box, Callout } from 'gestalt';
 import React, { type Node } from 'react';
 
-const FeedbackCallout = (): Node => {
+export default function FeedbackCallout(): Node {
   const [showCallout, setShowCallout] = React.useState(true);
 
   return (
@@ -12,7 +12,7 @@ const FeedbackCallout = (): Node => {
           type="info"
           iconAccessibilityLabel="Info icon"
           title="We'd love your feedback!"
-          message="The Gestalt team has been working hard to extend our documentation to include design best practices, as well as create a standardized documentation format. Let us know what you think so far by answering 2 super quick questions."
+          message="The Gestalt team has been working hard to extend our documentation to include design best practices, as well as create a standardized documentation format. Let us know what you think so far by answering 2 quick questions."
           primaryAction={{
             label: 'Give feedback',
             href: 'https://forms.gle/mi1A1hxYGG6V39AH9',
@@ -28,6 +28,4 @@ const FeedbackCallout = (): Node => {
       )}
     </Box>
   );
-};
-
-export default FeedbackCallout;
+}
