1cd6e0c68613f7ac295c7608f7aca1908a70bd86
PJ Onori <ponori@pinterest.com>
2021-03-22 10:19:14 -0700

    Updated flowtest

--- packages/gestalt/src/Status.flowtest.js
+++ packages/gestalt/src/Status.flowtest.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Status from './Status.js';
 
-const Valid = <Status type="unstarted" />;
+const Valid = <Status type="unstarted" title="Unstarted" />;
 
 // $FlowExpectedError[prop-missing]
 const InvalidProps = <Status nonexisting={33} />;
