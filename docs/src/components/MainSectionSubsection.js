6a0e00ebd0eb3cde9a0c1c4c7bfd37daec86c4fb
Christian Vuerings <cvuerings@pinterest.com>
2021-04-08 09:07:25 -0700

    Docs: convert ESLint docs to new format

--- docs/src/components/MainSectionSubsection.js
+++ docs/src/components/MainSectionSubsection.js
@@ -5,7 +5,6 @@ import slugify from 'slugify';
 import CopyLinkButton from './buttons/CopyLinkButton.js';
 import Markdown from './Markdown.js';
 import { copyToClipboard } from './Card.js';
-import { capitalizeFirstLetter } from './utils.js';
 
 type Props = {|
   children?: Node,
@@ -32,7 +31,7 @@ const MainSectionSubsection = ({ children, columns = 1, description, title }: Pr
             data-anchor
           >
             <Flex alignItems="center" gap={2}>
-              <Heading size="sm">{capitalizeFirstLetter(title)}</Heading>
+              <Heading size="sm">{title}</Heading>
               <CopyLinkButton
                 name={title}
                 onClick={() => {
