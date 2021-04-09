8eaae0933a21f760bda0223c0e89d3a02d155243
Ayesha Mazumdar <amazumdar@pinterest.com>
2021-04-09 16:20:17 -0700

    Docs: Update Meetings on How to Work with Us (#1467)

--- docs/src/ScrollBoundaryContainer.doc.js
+++ docs/src/ScrollBoundaryContainer.doc.js
@@ -10,7 +10,7 @@ const card = (c) => cards.push(c);
 card(
   <PageHeader
     name="ScrollBoundaryContainer"
-    description="ScrollBoundaryContainer is used with anchored components such Popover, Tooltip, Dropdown or Typeahead. A ScrollBoundaryContainer is needed for proper positioning when the Tooltip is anchored to an element that is located within a scrolling container. The use of ScrollBoundaryContainer ensures the Tooltip remains attached to its anchor when scrolling."
+    description="ScrollBoundaryContainer is used with anchored components such as Popover, Tooltip, Dropdown or Typeahead. A ScrollBoundaryContainer is needed for proper positioning when the Tooltip is anchored to an element that is located within a scrolling container. The use of ScrollBoundaryContainer ensures the Tooltip remains attached to its anchor when scrolling."
   />,
 );
 
