e183dfe4fd526eef4d4362934a654cb13def8fd4
PJ Onori <ponori@pinterest.com>
2021-04-20 22:53:11 -0700

    Fixed 'DataPoint' references to be 'Datapoint'

--- packages/gestalt/src/DataPoint.test.js
+++ packages/gestalt/src/DataPoint.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { create } from 'react-test-renderer';
 import Datapoint from './Datapoint.js';
 
-describe('DataPoint', () => {
+describe('Datapoint', () => {
   it('renders', () => {
     const tree = create(
       <Datapoint
