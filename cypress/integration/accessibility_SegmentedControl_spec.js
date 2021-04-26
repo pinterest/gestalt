ec8e6da20cd33ddc28e17fa70bc208f420fdfed9
Ayesha Mazumdar <amazumdar@pinterest.com>
2021-04-26 11:00:45 -0700

    tests

--- cypress/integration/accessibility_SegmentedControl_spec.js
+++ cypress/integration/accessibility_SegmentedControl_spec.js
@@ -5,15 +5,6 @@ describe('SegmentedControl Accessibility check', () => {
   });
 
   it('Tests accessibility on the SegmentedControl page', () => {
-    // Contrast on unselected tabs does not pass guidelines
-    cy.configureAxe({
-      rules: [
-        {
-          id: 'color-contrast',
-          enabled: false,
-        },
-      ],
-    });
     cy.checkA11y();
   });
 });
