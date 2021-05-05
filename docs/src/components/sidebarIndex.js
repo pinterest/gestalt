290aea56dd50a5f5c7a71795c091ec6253d2bcc7
Ayesha Mazumdar <amazumdar@pinterest.com>
2021-05-05 12:02:54 -0700

    reorder

--- docs/src/components/sidebarIndex.js
+++ docs/src/components/sidebarIndex.js
@@ -27,7 +27,7 @@ const sidebarIndex: Array<sidebarIndexType> = [
   },
   {
     sectionName: 'Guidelines',
-    pages: ['Screen Sizes', 'Layouts'],
+    pages: ['Layouts', 'Screen Sizes'],
   },
   {
     sectionName: 'Foundation',
