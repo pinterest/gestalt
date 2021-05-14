0a342610778b0adafb5fbecafcb219ff43b04eaf
Christian Vuerings <cvuerings@pinterest.com>
2021-05-14 06:42:51 -0700

    ESLint: Export no-box-disallowed-props (#1509)

--- packages/eslint-plugin-gestalt/src/index.js
+++ packages/eslint-plugin-gestalt/src/index.js
@@ -1,5 +1,6 @@
 // @flow strict
 import buttonIconRestrictions from './button-icon-restrictions.js';
+import noBoxDisallowedProps from './no-box-disallowed-props.js';
 import noBoxMarginleftMarginright from './no-box-marginleft-marginright.js';
 import noDangerousStyleDuplicates from './no-dangerous-style-duplicates.js';
 import noMediumFormfields from './no-medium-formfields.js';
@@ -9,6 +10,7 @@ import preferBox from './prefer-box.js';
 module.exports = {
   rules: {
     'button-icon-restrictions': buttonIconRestrictions,
+    'no-box-disallowed-props': noBoxDisallowedProps,
     'no-box-marginleft-marginright': noBoxMarginleftMarginright,
     'no-dangerous-style-duplicates': noDangerousStyleDuplicates,
     'no-medium-formfields': noMediumFormfields,
