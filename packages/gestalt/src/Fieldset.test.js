826d0b96ac27a8c6d60fb7f84a39a50759fa2bb7
Kessy Similien <ksimilien@pinterest.com>
2021-08-03 14:20:38 -1000

    Update test with id

--- packages/gestalt/src/Fieldset.test.js
+++ packages/gestalt/src/Fieldset.test.js
@@ -29,6 +29,7 @@ describe('Fieldset', () => {
       <Fieldset
         legend="What is your favorite dog?"
         legendDisplay="hidden"
+        id="fieldset-with-error"
         errorMessage="Atleast 1 item must be selected"
       >
         <Checkbox id="Schnauzer" label="Schnauzer" onChange={() => {}} />
