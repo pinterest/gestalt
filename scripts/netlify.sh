8313ebe49a7ecc8fa697efaf32bc1b55975dbf88
Ayesha Mazumdar <amazumdar@pinterest.com>
2021-08-13 16:41:40 -0700

    add tokens to netlify build

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -4,4 +4,5 @@ set -e
 
 (cd packages/gestalt && yarn build)
 (cd packages/gestalt-datepicker && yarn build)
+(cd packages/gestalt-design-tokens && yarn build)
 (cd docs && PUBLIC_URL=$DEPLOY_PRIME_URL yarn build)
