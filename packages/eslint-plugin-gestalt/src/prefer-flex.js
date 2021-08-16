41225080a2de527371aec46d23a6ae410768a7c7
Ryan James <rjames@pinterest.com>
2021-08-16 16:50:17 -0700

    holy crap autofix actually works

--- packages/eslint-plugin-gestalt/src/prefer-flex.js
+++ packages/eslint-plugin-gestalt/src/prefer-flex.js
@@ -7,6 +7,9 @@
  */
 
 // @flow strict
+import { buildProps } from './eslintASTHelpers.js';
+import { renameTagWithPropsFixer, updateGestaltImportFixer } from './eslintASTFixers.js';
+
 const sharedProps = [
   'alignContent',
   'alignItems',
@@ -34,6 +37,7 @@ const rule = {
       description: 'Encourage usage of Flex instead of Box',
       recommended: false,
     },
+    fixable: 'code',
     schema: [
       {
         type: 'object',
@@ -45,71 +49,120 @@ const rule = {
   // $FlowFixMe[unclear-type]
   create(context: Object): Object {
     let hasImportedBox = false;
-    let importStatement;
+    let gestaltImportStatement;
+    let programNode;
+    let isImportFixerExecuted = false;
+    let hasAlignItems = false;
+    let hasJustifyContent = false;
+    let boxImportName;
+
+    const importDeclaration = (decl) => {
+      // Not a Gestalt import
+      if (decl.source.value !== 'gestalt') {
+        return;
+      }
+
+      hasImportedBox = decl.specifiers.some((node) => node.imported.name === 'Box');
+      if (hasImportedBox) {
+        const boxImportDeclaration = decl.specifiers.find((node) => node.imported.name === 'Box');
+        // Hang on to the local name, we'll need this later
+        boxImportName = boxImportDeclaration.local.name;
+        gestaltImportStatement = decl;
+      }
+    };
+
+    const jsxOpeningElement = (node) => {
+      // We have no Boxes, or this is not a Box
+      if (!hasImportedBox || node.name.name !== boxImportName) {
+        return;
+      }
+
+      const props = node.attributes.map(({ name, value }) => ({
+        key: name?.name,
+        value: value?.value,
+      }));
+
+      const displayProp = props.find(({ key }) => key === 'display');
+      // No `display` prop or not `display="flex"`
+      if (!displayProp || displayProp.value !== 'flex') {
+        return;
+      }
+
+      const notFlexProps = props
+        .filter(({ key }) => key !== 'display')
+        .filter(({ key }) => !sharedProps.includes(key));
+
+      // Props are set that Flex doesn't support, needs to remain a Box
+      if (notFlexProps.length > 0) {
+        return;
+      }
+
+      if (props.map(({ key }) => key).includes('alignItems')) {
+        hasAlignItems = true;
+      }
+      if (props.map(({ key }) => key).includes('justifyContent')) {
+        hasJustifyContent = true;
+      }
+
+      context.report({
+        node,
+        message: errorMessage,
+        fix: (fixer) => {
+          // Are there other Box nodes aside from the one we're currently on?
+          const hasOtherBoxes =
+            programNode.tokens
+              .filter(({ type }) => type === 'JSXIdentifier')
+              .filter(({ value }) => value === boxImportName).length > 1;
+          const importsToRemove = hasOtherBoxes ? [] : [boxImportName];
+
+          // Update the import statement: add Flex if needed, remove Box if no longer needed
+          const importFixers = updateGestaltImportFixer({
+            fixer,
+            gestaltImportNode: gestaltImportStatement,
+            importsToRemove,
+            newComponentName: 'Flex',
+            programNode,
+          });
+
+          const additionalProps = [];
+          // Make Box's defaults explicit since they differ from Flex's defaults
+          if (!hasAlignItems) {
+            additionalProps.push('alignItems="stretch"');
+          }
+          if (!hasJustifyContent) {
+            additionalProps.push('justifyContent="start"');
+          }
+
+          // Replace the Box node with Flex, adding additional props if necessary
+          const tagFixers = renameTagWithPropsFixer({
+            additionalPropsString: buildProps({
+              context,
+              elementNode: node,
+              newPropsString: additionalProps.join(' '),
+              propsToRemove: ['display'],
+            }),
+            context,
+            fixer,
+            elementNode: node,
+            newComponentName: 'Flex',
+            tagName: 'Box',
+          });
+
+          const fixers = !isImportFixerExecuted ? [...tagFixers, importFixers] : tagFixers;
+          isImportFixerExecuted = true;
+          return fixers;
+        },
+      });
+    };
 
     return {
-      ImportDeclaration(decl) {
-        if (decl.source.value !== 'gestalt') {
-          return;
-        }
-        hasImportedBox = decl.specifiers.some((node) => {
-          return node.imported.name === 'Box';
-        });
-        if (hasImportedBox) {
-          importStatement = decl;
-        }
+      Program: (node) => {
+        programNode = node;
       },
-      JSXOpeningElement(node) {
-        if (!hasImportedBox) {
-          return;
-        }
-
-        const props = node.attributes.map(({ name, value }) => ({
-          key: name?.name,
-          value: value?.value,
-        }));
-
-        const displayProp = props.find(({ key }) => key === 'display');
-        // No `display` prop or not `display="flex"`
-        if (!displayProp || displayProp.value !== 'flex') {
-          return;
-        }
-
-        const notFlexProps = props
-          .filter(({ key }) => key !== 'display')
-          .filter(({ key }) => !sharedProps.includes(key));
-
-        // Props are set that Flex doesn't support, needs to remain a Box
-        if (notFlexProps.length > 0) {
-          return;
-        }
-
-        context.report({
-          node,
-          message: errorMessage,
-          fix: (fixer) => {
-            // TODO: add Flex import
-
-            // node.imported.name = 'Flex';
-
-            console.log(importStatement.specifiers);
-          },
-        });
-      },
+      ImportDeclaration: importDeclaration,
+      JSXOpeningElement: jsxOpeningElement,
     };
   },
 };
 
 export default rule;
-
-// context keys
-// 'hasBOM',
-// 'text',
-// 'ast',
-// 'parserServices',
-// 'scopeManager',
-// 'visitorKeys',
-// 'tokensAndComments',
-// 'lines',
-// 'lineStartIndices',
-// '_commentCache'
