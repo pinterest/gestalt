af5f6fefd6d31020a2480cb2f8cf00225dd61dee
Alberto Carreras Carrasco <acarreras@pinterest.com>
2021-08-16 16:38:52 -0400

    Revert Color: Add design tokens package and color page (#1616)

--- packages/gestalt-design-tokens/build.js
+++
@@ -1,5 +0,0 @@
-// @flow strict
-// $FlowExpectedError[untyped-import]
-const StyleDictionary = require('style-dictionary').extend('config.json');
-
-StyleDictionary.buildAllPlatforms();
--- scripts/releaseSteps.js
+++ scripts/releaseSteps.js
@@ -19,12 +19,7 @@ function srcDirectory(item) {
   return path.join(__dirname, '..', 'packages', item, 'src');
 }
 
-const packages = [
-  'gestalt',
-  'gestalt-datepicker',
-  'gestalt-design-tokens',
-  'eslint-plugin-gestalt',
-];
+const packages = ['gestalt', 'gestalt-datepicker', 'eslint-plugin-gestalt'];
 const packageJSON = packageJSONPath('gestalt');
 const packageJSONParsed = require(packageJSON);
 
