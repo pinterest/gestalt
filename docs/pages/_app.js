1dd89b843ef121cad1869e26fe9dc541d9a29dd8
Christian Vuerings <cvuerings@pinterest.com>
2021-08-26 16:17:27 -0700

    Docs: persist scrollbar position across pages (#1661)

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -3,11 +3,16 @@ import { type Node } from 'react';
 import '../docs.css';
 import 'gestalt/dist/gestalt.css';
 import 'gestalt-datepicker/dist/gestalt-datepicker.css';
+import App from '../components/App.js';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function GestaltApp(
   // $FlowFixMe[signature-verification-failure]
   { Component, pageProps }, // eslint-disable-line react/prop-types
 ): Node {
-  return <Component {...pageProps} />;
+  return (
+    <App>
+      <Component {...pageProps} />
+    </App>
+  );
 }
