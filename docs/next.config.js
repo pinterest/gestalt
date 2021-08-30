4dc927bc9cc9bc8381b4b99e04c4874070199a6a
Christian Vuerings <cvuerings@pinterest.com>
2021-08-30 15:55:10 -0700

    Internal: fix Element type is invalid (#1666)

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,4 +1,19 @@
 // @flow strict
+
+/*::
+type WebpackConfig = {| watchOptions: {| poll: number | false |} |};
+*/
+
 module.exports = {
   reactStrictMode: true,
+  webpack: (
+    config /*: WebpackConfig */,
+    { dev } /*: {| dev: boolean |} */,
+  ) /*: WebpackConfig */ => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: dev ? 500 : false,
+    },
+  }),
 };
