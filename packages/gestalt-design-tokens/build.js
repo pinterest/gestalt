d70c18de17ec82a8c6398cdafa5cfc0273913764
Ayesha Mazumdar <amazumdar@pinterest.com>
2021-09-07 17:53:24 -0700

    Design Tokens: Update format for Android

--- packages/gestalt-design-tokens/build.js
+++ packages/gestalt-design-tokens/build.js
@@ -2,4 +2,20 @@
 // $FlowExpectedError[untyped-import]
 const StyleDictionary = require('style-dictionary').extend('config.json');
 
+StyleDictionary.registerTransform({
+  name: 'size/pxToDp',
+  type: 'value',
+  matcher(prop) {
+    return prop.value.match(/^-?[\d.]+px$/);
+  },
+  transformer(prop) {
+    return prop.value.replace(/px$/, 'dp');
+  },
+});
+
+StyleDictionary.registerTransformGroup({
+  name: 'android-custom',
+  transforms: ['attribute/cti', 'name/cti/snake', 'color/hex8android', 'size/pxToDp'],
+});
+
 StyleDictionary.buildAllPlatforms();
