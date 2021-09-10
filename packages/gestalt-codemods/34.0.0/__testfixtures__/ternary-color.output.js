816d8eab969114fbe5e4b1578bd81e88c6e49263
Ryan James <dangerismycat@users.noreply.github.com>
2021-09-10 13:59:56 -0700

    Codemods: Suppress constant condition warnings on Toast color/variant codemod (#1686)

--- packages/gestalt-codemods/34.0.0/__testfixtures__/ternary-color.output.js
+++ packages/gestalt-codemods/34.0.0/__testfixtures__/ternary-color.output.js
@@ -3,6 +3,7 @@ import { Toast } from 'gestalt';
 
 export default function TestComponent() {
   return (
+    // eslint-disable-next-line no-constant-condition
     <Toast color={true ? 'red' : 'white'} text="Toasty McToasterFace" />
   );
 }
