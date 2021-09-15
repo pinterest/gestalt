4ec66511005fecb1b7ff6eaf424c0af7fe361f87
Ayesha Mazumdar <amazumdar@pinterest.com>
2021-09-15 16:13:44 -0700

    Color: Add Brand color palette

--- docs/components/ColorPalette.js
+++ docs/components/ColorPalette.js
@@ -3,6 +3,7 @@ import type { Node } from 'react';
 import { Box, Text } from 'gestalt';
 // $FlowExpectedError[untyped-import]
 import tokens from 'gestalt-design-tokens/dist/json/variables.json';
+import ColorTile from './ColorTile.js';
 
 type Props = {|
   name: string,
@@ -13,31 +14,14 @@ const ColorPalette = ({ name, tokenId }: Props): Node => {
   const tokenNumbers = [0, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
   const colorId = `${tokenId}-${name.toLowerCase()}`;
   return (
-    <Box>
+    <Box marginTop={8} marginBottom={8}>
       <Text weight="bold">{name}</Text>
       <Box marginTop={2}>
         {tokenNumbers.map((number) => {
           const textColor = number > 400 ? 'white' : 'darkGray';
           const colorVariableName = `color-${colorId}-${number}`;
           return tokens[colorVariableName] ? (
-            <Box
-              key={`${number}-${tokenId}`}
-              dangerouslySetInlineStyle={{
-                __style: { backgroundColor: `var(--${colorVariableName})` },
-              }}
-              height={50}
-              width={300}
-              display="flex"
-              alignItems="center"
-              justifyContent="between"
-              paddingX={2}
-              borderStyle={tokenId.toLowerCase().includes('white') ? 'sm' : 'none'}
-            >
-              <Text weight="bold" color={textColor}>
-                {number}
-              </Text>
-              <Text color={textColor}>{tokens[colorVariableName]?.toUpperCase()}</Text>
-            </Box>
+            <ColorTile name={name} number={number} textColor={textColor} tokenId={tokenId} />
           ) : null;
         })}
       </Box>
---
+++ docs/components/ColorTile.js
@@ -0,0 +1,39 @@
+// @flow strict
+import type { Node } from 'react';
+import { Box, Text } from 'gestalt';
+// $FlowExpectedError[untyped-import]
+import tokens from 'gestalt-design-tokens/dist/json/variables.json';
+
+type Props = {|
+  name: string,
+  tokenId: string,
+  number: number,
+  textColor: 'darkGray' | 'white',
+  showName?: boolean,
+|};
+
+const ColorTile = ({ name, number, textColor, tokenId, showName = false }: Props): Node => {
+  const colorId = `${tokenId}-${name.toLowerCase()}`;
+  const colorVariableName = `color-${colorId}-${number}`;
+
+  return (
+    <Box
+      dangerouslySetInlineStyle={{
+        __style: { backgroundColor: `var(--${colorVariableName})` },
+      }}
+      height={50}
+      width={300}
+      display="flex"
+      alignItems="center"
+      justifyContent="between"
+      paddingX={2}
+      borderStyle={tokenId.toLowerCase().includes('white') ? 'sm' : 'none'}
+    >
+      <Text weight="bold" color={textColor}>
+        {showName && name} {number}
+      </Text>
+      <Text color={textColor}>{tokens[colorVariableName]?.toUpperCase()}</Text>
+    </Box>
+  );
+};
+export default ColorTile;
