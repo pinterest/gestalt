e920557df3088bb641b00d6931da191a09e16eaf
Ryan James <rjames@pinterest.com>
2021-10-01 14:31:41 -0700

    update tests/snaps

--- packages/gestalt/src/Status.test.js
+++ packages/gestalt/src/Status.test.js
@@ -3,14 +3,21 @@ import { create } from 'react-test-renderer';
 import Status from './Status.js';
 
 describe('Status', () => {
-  it('renders', () => {
+  it('renders with title', () => {
     const tree = create(<Status title="Unstarted" type="unstarted" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('renders with a subtext', () => {
+  it('renders with title and subtext', () => {
     const tree = create(
       <Status subtext="some subtext" title="Unstarted" type="unstarted" />,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders with accessibilityLabel', () => {
+    const tree = create(
+      <Status accessibilityLabel="some accessibilityLabel" type="unstarted" />,
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
