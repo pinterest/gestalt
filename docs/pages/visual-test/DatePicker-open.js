86a2c6d436446f0e019c101a6a85ea244dcc86ec
Alberto Carreras Carrasco <acarreras@pinterest.com>
2021-10-01 11:36:13 -0400

    fix

--- docs/pages/visual-test/DatePicker-open.js
+++ docs/pages/visual-test/DatePicker-open.js
@@ -1,12 +1,14 @@
 // @flow strict
 import type { Node } from 'react';
-import { Box } from 'gestalt';
+import { Box, ColorSchemeProvider } from 'gestalt';
 import GestaltDatePicker from 'gestalt-datepicker';
 
 export default function DatePickerSpec(): Node {
   return (
-    <Box color="white" padding={1} width={400} height={400}>
-      <GestaltDatePicker id="example-visual-testing" label="Select a date" onChange={() => {}} />
-    </Box>
+    <ColorSchemeProvider colorScheme="light">
+      <Box color="white" padding={1} width={400} height={400}>
+        <GestaltDatePicker id="example-visual-testing" label="Select a date" onChange={() => {}} />
+      </Box>
+    </ColorSchemeProvider>
   );
 }
