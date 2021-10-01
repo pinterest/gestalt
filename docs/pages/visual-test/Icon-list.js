25191f01ba7eb15f06590aa07f40cef21186c958
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2021-10-01 13:56:42 -0400

    Icon: visual testing implementation (#1720)

---
+++ docs/pages/visual-test/Icon-list.js
@@ -0,0 +1,21 @@
+// @flow strict
+import type { Node } from 'react';
+import { Box, Flex, Icon, ColorSchemeProvider } from 'gestalt';
+
+export default function IconSpec(): Node {
+  const { icons } = Icon;
+
+  return (
+    <ColorSchemeProvider colorScheme="light">
+      <Box height={375} width={575} color="white">
+        <Flex gap={1} wrap>
+          {icons.map((name, index) => (
+            <Box key={index} padding={2}>
+              <Icon color="darkGray" accessibilityLabel="" icon={name} />
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+    </ColorSchemeProvider>
+  );
+}
