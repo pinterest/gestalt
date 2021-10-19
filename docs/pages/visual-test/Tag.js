657de67c41f3654c2fc0ce1f8591f28481722a86
Ryan James <rjames@pinterest.com>
2021-10-18 17:02:24 -0700

    Tag: add visual diff testing

---
+++ docs/pages/visual-test/Tag.js
@@ -0,0 +1,23 @@
+// @flow strict
+import { type Node } from 'react';
+import { Flex, Tag } from 'gestalt';
+
+export default function TagSpec(): Node {
+  return (
+    <Flex direction="column" gap={2}>
+      <Tag onRemove={() => {}} removeIconAccessibilityLabel="Remove" text="New" />
+      <Tag disabled text="Disabled" />
+      <Tag
+        onRemove={() => {}}
+        errorMessage="NOPE"
+        removeIconAccessibilityLabel="Remove"
+        text="Error"
+      />
+      <Tag
+        onRemove={() => {}}
+        removeIconAccessibilityLabel="Remove"
+        text="Some really long text that just keeps going on and on and on and on and on and on"
+      />
+    </Flex>
+  );
+}
