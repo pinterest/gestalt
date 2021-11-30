5628945696da0eb8847d3a3370ac4498f34e8220
Alberto Carreras Carrasco <acarreras@pinterest.com>
2021-11-30 12:52:09 -0500

    fix

--- docs/pages/divider.js
+++ docs/pages/divider.js
@@ -1,34 +1,27 @@
 // @flow strict
 import type { Node } from 'react';
-import PropTable from '../components/PropTable.js';
 import PageHeader from '../components/PageHeader.js';
-import CardPage from '../components/CardPage.js';
 import MainSection from '../components/MainSection.js';
+import Page from '../components/Page.js';
 
-const cards: Array<Node> = [];
-const card = (c) => cards.push(c);
-
-card(
-  <PageHeader
-    name="Divider"
-    description="If you have two things that need to be separated, put a `Divider` between them."
-  />,
-);
-
-card(<PropTable props={[]} />);
-
-card(
-  <MainSection name="Variants">
-    <MainSection.Subsection
-      title="Orientation"
-      columns={2}
-      description="You can use this component for a visual divider between two elements. Placing it within a [Flex](/flex) layout with a direction of `row` will cause the Divider to become vertical.
+export default function DividerPage(): Node {
+  return (
+    <Page title="Divider">
+      <PageHeader
+        name="Divider"
+        description="If you have two things that need to be separated, put a `Divider` between them."
+      />
+      <MainSection name="Variants">
+        <MainSection.Subsection
+          title="Orientation"
+          columns={2}
+          description="You can use this component for a visual divider between two elements. Placing it within a [Flex](/flex) layout with a direction of `row` will cause the Divider to become vertical.
       "
-    >
-      <MainSection.Card
-        cardSize="md"
-        title="Horizontal"
-        defaultCode={`
+        >
+          <MainSection.Card
+            cardSize="md"
+            title="Horizontal"
+            defaultCode={`
 <Box color="white" width="100%">
   <Box padding={2}>
     <Text>Some content</Text>
@@ -39,11 +32,11 @@ card(
   </Box>
 </Box>
 `}
-      />
-      <MainSection.Card
-        cardSize="md"
-        title="Vertical"
-        defaultCode={`
+          />
+          <MainSection.Card
+            cardSize="md"
+            title="Vertical"
+            defaultCode={`
 <Flex>
   <Box padding={2}>
     <Text>Some content</Text>
@@ -54,11 +47,9 @@ card(
   </Box>
 </Flex>
 `}
-      />
-    </MainSection.Subsection>
-  </MainSection>,
-);
-
-export default function DividerPage(): Node {
-  return <CardPage cards={cards} page="Divider" />;
+          />
+        </MainSection.Subsection>
+      </MainSection>
+    </Page>
+  );
 }
