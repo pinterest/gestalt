235c370fc4d36c1f678416ea835c7b6a8c1c99a8
Ryan James <rjames@pinterest.com>
2021-11-30 17:45:25 -0800

    NumberField: add Usage guidelines, Best practices, A11y+L10n

--- docs/pages/numberfield.js
+++ docs/pages/numberfield.js
@@ -1,8 +1,6 @@
 // @flow strict
 import { type Node } from 'react';
-import Example from '../components/Example.js';
 import PageHeader from '../components/PageHeader.js';
-import Card from '../components/Card.js';
 import MainSection from '../components/MainSection.js';
 import Page from '../components/Page.js';
 import GeneratedPropTable from '../components/GeneratedPropTable.js';
@@ -11,7 +9,27 @@ import docgen, { type DocGen } from '../components/docgen.js';
 export default function NumberFieldPage({ generatedDocGen }: {| generatedDocGen: DocGen |}): Node {
   return (
     <Page title="NumberField">
-      <PageHeader name="NumberField" description={generatedDocGen?.description} />
+      <PageHeader
+        name="NumberField"
+        description={generatedDocGen?.description}
+        defaultCode={`
+function Example(props) {
+  const [value, setValue] = React.useState();
+
+  return (
+    <Box width={500}>
+      <NumberField
+        id="header-example"
+        label="ZIP Code"
+        onChange={({ value }) => setValue(value)}
+        placeholder="Please enter your ZIP Code"
+        value={value}
+      />
+    </Box>
+  );
+}
+`}
+      />
 
       <GeneratedPropTable generatedDocGen={generatedDocGen} />
 
@@ -30,47 +48,360 @@ export default function NumberFieldPage({ generatedDocGen }: {| generatedDocGen:
             type="don't"
             title="When Not to Use"
             description={`
+          - When accepting telephone numbers. Use [TextField](/textfield) with \`type="tel"\` instead.
           - Situations where text needs to be entered. Use [TextField](/textfield) or [TextArea](/textarea) instead.`}
           />
         </MainSection.Subsection>
       </MainSection>
 
-      <Example
-        id="basicExample"
-        name="Example"
-        description={`
-    NumberField will expand to fill the width of the parent container.
-  `}
-        defaultCode={`
+      <MainSection name="Best practices">
+        <MainSection.Subsection columns={2}>
+          <MainSection.Card
+            cardSize="sm"
+            type="do"
+            description="Use helper text for important information. Helper text helps users understand how to complete the number field or to indicate any needed input."
+            defaultCode={`
 function Example(props) {
-  const [value, setValue] = React.useState('')
+  const [value, setValue] = React.useState();
 
   return (
-    <NumberField
-      id="basicExampleNumberField"
-      label="Enter your favorite number"
-      onChange={({ value }) => {
-        setValue(value);
-      }}
-      placeholder="We recommend 42"
-      value={value}
-    />
+    <Box padding={2} color="white">
+      <NumberField
+        helperText="Code was texted to you"
+        id="best-practices-do-helper-text"
+        label="Confirmation code"
+        onChange={({ value }) => setValue(value)}
+        value={value}
+      />
+    </Box>
   );
 }
-`}
+            `}
+          />
+          <MainSection.Card
+            cardSize="sm"
+            type="don't"
+            description="Put essential information in the placeholder text, since it disappears when the user types. The placeholder text is not a replacement for the label."
+            defaultCode={`
+function Example(props) {
+  const [value, setValue] = React.useState();
+
+  return (
+    <Box padding={2} color="white">
+      <NumberField
+        id="best-practices-dont-placeholder"
+        label=""
+        onChange={({ value }) => setValue(value)}
+        placeholder="Code was texted to you"
+        value={value}
+      />
+    </Box>
+  );
+}
+            `}
+          />
+        </MainSection.Subsection>
+
+        <MainSection.Subsection columns={2}>
+          <MainSection.Card
+            cardSize="sm"
+            type="do"
+            description="Always ensure the number field has a visible label. The label provides context and supports users when filling in information."
+            defaultCode={`
+function Example(props) {
+  const [value, setValue] = React.useState();
+
+  return (
+    <Box padding={2} color="white">
+      <NumberField
+        id="best-practices-do-label"
+        label="Your age"
+        onChange={({ value }) => setValue(value)}
+        value={value}
+      />
+    </Box>
+  );
+}
+            `}
+          />
+          <MainSection.Card
+            cardSize="sm"
+            type="don't"
+            description="Remove the label, as this creates accessibility and usability issues."
+            defaultCode={`
+function Example(props) {
+  const [value, setValue] = React.useState();
+
+  return (
+    <Box padding={2} color="white">
+      <NumberField
+        id="best-practices-dont-label"
+        label=""
+        onChange={({ value }) => setValue(value)}
+        value={value}
+      />
+    </Box>
+  );
+}
+            `}
+          />
+        </MainSection.Subsection>
+
+        <MainSection.Subsection columns={2}>
+          <MainSection.Card
+            cardSize="sm"
+            type="do"
+            description="Only place related fields on the same line."
+            defaultCode={`
+function Example(props) {
+  const [firstValue, setFirstValue] = React.useState();
+  const [secondValue, setSecondValue] = React.useState();
+
+  return (
+    <Flex gap={4}>
+      <NumberField
+        id="best-practices-do-related-first"
+        label="First value"
+        onChange={({ value }) => setFirstValue(value)}
+        value={firstValue}
+      />
+      <NumberField
+        id="best-practices-do-related-second"
+        label="Second value"
+        onChange={({ value }) => setSecondValue(value)}
+        value={secondValue}
+      />
+    </Flex>
+  );
+}
+            `}
+          />
+          <MainSection.Card
+            cardSize="sm"
+            type="don't"
+            description="Place unrelated number fields on the same line, as this can create comprehension issues."
+            defaultCode={`
+function Example(props) {
+  const [ageValue, setAgeValue] = React.useState();
+  const [zipCodeValue, setZipCodeValue] = React.useState();
+
+  return (
+    <Flex gap={4}>
+      <NumberField
+        id="best-practices-dont-related-age"
+        label="Age"
+        onChange={({ value }) => setAgeValue(value)}
+        value={ageValue}
+      />
+      <NumberField
+        id="best-practices-dont-related-zip-code"
+        label="ZIP Code"
+        onChange={({ value }) => setZipCodeValue(value)}
+        value={zipCodeValue}
+      />
+    </Flex>
+  );
+}
+            `}
+          />
+        </MainSection.Subsection>
+
+        <MainSection.Subsection columns={2}>
+          <MainSection.Card
+            cardSize="sm"
+            type="do"
+            description="Provide clear and useful error messages that help the user fix the issue. Error messages should be displayed in a timely manner — typically once the field loses focus or when the form is submitted."
+            defaultCode={`
+function Example(props) {
+  const [value, setValue] = React.useState();
+
+  return (
+    <Box padding={2} color="white">
+      <NumberField
+        errorMessage="Please enter a value of at least $5"
+        helperText="Minimum is $5"
+        id="best-practices-do-error-message"
+        label="Monthly ad spend"
+        onChange={({ value }) => setValue(value)}
+        value={value}
+      />
+    </Box>
+  );
+}
+            `}
+          />
+          <MainSection.Card
+            cardSize="sm"
+            type="don't"
+            description={`Display generic error messages, such as "There is an error".`}
+            defaultCode={`
+function Example(props) {
+  const [value, setValue] = React.useState();
+
+  return (
+    <Box padding={2} color="white">
+      <NumberField
+        errorMessage="There is an error"
+        helperText="Minimum is $5"
+        id="best-practices-dont-error-message"
+        label="Monthy ad spend"
+        onChange={({ value }) => setValue(value)}
+        value={value}
+      />
+    </Box>
+  );
+}
+            `}
+          />
+        </MainSection.Subsection>
+
+        <MainSection.Subsection columns={2}>
+          <MainSection.Card
+            cardSize="md"
+            type="do"
+            description="Consider all text fields as required, unless explicitly noted as optional."
+            defaultCode={`
+function Example(props) {
+  const [name, setName] = React.useState({
+    first: undefined,
+    second: undefined,
+    third: undefined,
+  });
+
+  return (
+    <Flex direction="column" gap={3}>
+      <NumberField
+        id="best-practices-do-required-first"
+        label="First number"
+        onChange={({ value }) => setName((name) => ({ ...name, first: value }))}
+        value={name.first}
+      />
+      <NumberField
+        id="best-practices-do-required-second"
+        label="Second number"
+        onChange={({ value }) => setName((name) => ({ ...name, second: value }))}
+        value={name.second}
+      />
+      <NumberField
+        id="best-practices-do-required-third"
+        label="Third number (optional)"
+        onChange={({ value }) => setName((name) => ({ ...name, third: value }))}
+        value={name.third}
+      />
+    </Flex>
+  );
+}
+            `}
+          />
+          <MainSection.Card
+            cardSize="md"
+            type="don't"
+            description="Mark fields as required."
+            defaultCode={`
+function Example(props) {
+  const [name, setName] = React.useState({
+    first: undefined,
+    second: undefined,
+    third: undefined,
+  });
+
+  return (
+    <Flex direction="column" gap={3}>
+      <NumberField
+        helperText="* This field is required."
+        id="best-practices-dont-required-first"
+        label="First number"
+        onChange={({ value }) => setName((name) => ({ ...name, first: value }))}
+        value={name.first}
+      />
+      <NumberField
+        helperText="* This field is required."
+        id="best-practices-dont-required-second"
+        label="Second number"
+        onChange={({ value }) => setName((name) => ({ ...name, second: value }))}
+        value={name.second}
+      />
+      <NumberField
+        id="best-practices-dont-required-third"
+        label="Third number"
+        onChange={({ value }) => setName((name) => ({ ...name, third: value }))}
+        value={name.third}
+      />
+    </Flex>
+  );
+}
+            `}
+          />
+        </MainSection.Subsection>
+      </MainSection>
+
+      <MainSection name="Accessibility">
+        <MainSection.Subsection
+          title="Comprehension"
+          description="Be sure to [provide instructions](https://www.w3.org/WAI/tutorials/forms/instructions/) to help users understand how to complete the form and use individual form controls."
+        />
+        <MainSection.Subsection
+          title="Labels"
+          description="Ensure the labels are precise and concise. Labels should only describe the number field they are associated with, and they must be visible."
+        />
+        <MainSection.Subsection
+          title="Validation"
+          description={`
+    When providing a validation message, make sure the instructions are clear and help users complete the field. For example, "Value must be greater than 20". In addition, use the helper text to provide instructions to help users understand how to complete the number field or to indicate any needed input, allowed formats, timing limitations, or other pertinent information.
+    These practices give screen readers and users of assistive technologies more information about the form, helping them to fill it out.
+    `}
+        />
+        <MainSection.Subsection
+          title="Keyboard navigation"
+          description={`
+    NumberField has conventional keyboard support.
+    - Users relying on the keyboard expect to move focus to each NumberField by using the tab key or shift+tab when moving backwards
+    - Users can press the up and down arrow keys to adjust the field value
+    - Setting \`disabled\` will prevent NumberField from receiving keyboard focus or input
+    `}
+        />
+        <MainSection.Subsection
+          title="Autofocus"
+          description={`
+    NumberField intentionally lacks support for autofocus. Generally speaking,
+    autofocus interrupts normal page flow for screen readers making it an
+    anti-pattern for accessibility.
+  `}
+        />
+        <MainSection.Subsection
+          title="onSubmit"
+          description={`
+    NumberField is commonly used as an input in forms alongside submit buttons.
+    In these cases, users expect that pressing Enter or Return with the input
+    focused will submit the form.
+
+    Out of the box, NumberField doesn't expose an \`onSubmit\` handler or
+    individual key event handlers due to the complexities of handling these
+    properly. Instead, developers are encouraged to wrap NumberField
+    in a \`<form>\` and attach an \`onSubmit\` handler to that \`<form>\`.
+  `}
+        />
+      </MainSection>
+
+      <MainSection
+        name="Localization"
+        description={`Be sure to localize \`errorMessage\`, \`helperText\`, \`label\`, and \`placeholder\`.`}
       />
 
-      <Example
-        id="disabledExample"
-        name="Example: Disabled"
-        defaultCode={`
+      <MainSection name="Variants">
+        <MainSection.Subsection
+          title="Disabled"
+          description="Disabled NumberFields cannot be interacted with using the mouse or keyboard."
+        >
+          <MainSection.Card
+            defaultCode={`
 function Example(props) {
-  const [value, setValue] = React.useState('')
+  const [value, setValue] = React.useState();
 
   return (
     <NumberField
       disabled
-      id="disabledNumberField"
+      id="variant-disabled"
       label="Disabled"
       onChange={({ value }) => setValue(value)}
       placeholder="This input is disabled"
@@ -79,22 +410,24 @@ function Example(props) {
   );
 }
 `}
-      />
+          />
+        </MainSection.Subsection>
 
-      <Example
-        id="helperText"
-        name="Example: Helper Text"
-        description={`Whenever you want to provide more information about a form field, you should use \`helperText\`.`}
-        defaultCode={`
+        <MainSection.Subsection
+          title="Helper text"
+          description={`Whenever you want to provide more information about a form field, you should use \`helperText\`.`}
+        >
+          <MainSection.Card
+            defaultCode={`
 function Example(props) {
-  const [value, setValue] = React.useState('')
+  const [value, setValue] = React.useState();
 
   return (
     <Box padding={2} color="white">
       <NumberField
-        id="helperTextNumberField"
-        helperText="Digits only please, e.g. 8675309"
-        label="Phone number"
+        id="variant-helperText"
+        helperText="Round up to the nearest whole number"
+        label="Average value"
         onChange={({ value }) => setValue(value)}
         value={value}
       />
@@ -102,22 +435,24 @@ function Example(props) {
   );
 }
 `}
-      />
+          />
+        </MainSection.Subsection>
 
-      <Example
-        id="errorMessageExample"
-        name="Example: Error message"
-        description={`
+        <MainSection.Subsection
+          title="Error message"
+          description={`
     NumberField can display an error message.
     Simply pass in an \`errorMessage\` when there is an error present and we will handle the rest.`}
-        defaultCode={`
+        >
+          <MainSection.Card
+            defaultCode={`
 function Example(props) {
-  const [value, setValue] = React.useState('')
+  const [value, setValue] = React.useState();
 
   return (
     <NumberField
-      id="errorMessageNumberField"
-      errorMessage={!value ? "This field can't be blank!" : null}
+      id="variant-errorMessage"
+      errorMessage={value === null || value === undefined ? "You must enter a number" : null}
       onChange={({ value }) => setValue(value)}
       label="With an error message"
       value={value}
@@ -125,22 +460,24 @@ function Example(props) {
   );
 }
 `}
-      />
+          />
+        </MainSection.Subsection>
 
-      <Example
-        id="min-max-step example"
-        name="Example: min/max/step"
-        description={`
+        <MainSection.Subsection
+          title="Min/max/step"
+          description={`
     NumberField provides additional props specific to numerical input.
 
     \`min\` and \`max\` can be used to define the acceptable bounds of the input (see the [ref example](#ref%20example) for more about using these for validation).
 
     \`step\` determines the amount incremented or decremented when using the input's arrow buttons. Set this as a float to allow decimal input.
   `}
-        defaultCode={`
+        >
+          <MainSection.Card
+            defaultCode={`
 function Example(props) {
-  const [value1, setValue1] = React.useState('');
-  const [value2, setValue2] = React.useState('');
+  const [value1, setValue1] = React.useState();
+  const [value2, setValue2] = React.useState();
 
   return (
     <Flex direction="column" gap={2}>
@@ -172,17 +509,19 @@ function Example(props) {
   );
 }
 `}
-      />
+          />
+        </MainSection.Subsection>
 
-      <Example
-        id="ref example"
-        name="Example: ref"
-        description={`
+        <MainSection.Subsection
+          title="Refs"
+          description={`
     Set a ref on NumberField to use the [Constraint Validation API](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Constraint_validation) or to anchor a Popover-based element.
 
     Note that while the arrow buttons will not exceed the min/max (if set), the user is free to enter any number using the keyboard. Validation should be performed explicitly using the Constraint Validation API to ensure the value is within the specified range.
   `}
-        defaultCode={`
+        >
+          <MainSection.Card
+            defaultCode={`
 function Example(props) {
   const [value, setValue] = React.useState('');
   const [errorMessage, setErrorMessage] = React.useState(undefined);
@@ -214,30 +553,18 @@ function Example(props) {
   );
 }
 `}
-      />
+          />
+        </MainSection.Subsection>
+      </MainSection>
 
-      <Card
-        description={`
-    NumberField intentionally lacks support for autofocus. Generally speaking,
-    autofocus interrupts normal page flow for screen readers making it an
-    anti-pattern for accessibility.
-  `}
-        name="Autofocus"
-      />
-
-      <Card
-        description={`
-    NumberField is commonly used as an input in forms alongside submit buttons.
-    In these cases, users expect that pressing Enter or Return with the input
-    focused will submit the form.
-
-    Out of the box, NumberField doesn't expose an \`onSubmit\` handler or
-    individual key event handlers due to the complexities of handling these
-    properly. Instead, developers are encouraged to wrap NumberField
-    in a \`<form>\` and attach an \`onSubmit\` handler to that \`<form>\`.
-  `}
-        name="onSubmit"
-      />
+      <MainSection name="Related">
+        <MainSection.Subsection
+          description={`
+      **[TextField](/textfield)**
+      For text input, use TextField. (For telephone numbers, use \`<TextField type="tel" />\`.)
+    `}
+        />
+      </MainSection>
     </Page>
   );
 }
