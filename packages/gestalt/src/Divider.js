b00b1efa0c5de498cddfc9a13fe6c0b85c1b19c7
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2021-12-02 15:15:32 -0500

    Divider: updated design guidelines (#1791)
    * fix
    
    * design guidelines
    
    * comments
    
    * fixes
    
    * changes
    
    * docgen

--- packages/gestalt/src/Divider.js
+++ packages/gestalt/src/Divider.js
@@ -3,7 +3,7 @@ import type { Node } from 'react';
 import styles from './Divider.css';
 
 /**
- * https://gestalt.pinterest.systems/divider
+ * [Divider](https://gestalt.pinterest.systems/divider) is a light gray 1px horizontal or vertical line which groups and divides content in lists and layouts.
  */
 export default function Divider(): Node {
   return <hr className={styles.divider} />;
