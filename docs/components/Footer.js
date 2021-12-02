d5c0d198ef949b4a92d5aa8466ddf8cec41acabe
PJ Onori <ponori@pinterest.com>
2021-12-02 14:32:48 -0800

    Update Footer.js
    Footer tweaks

--- docs/components/Footer.js
+++ docs/components/Footer.js
@@ -1,12 +1,18 @@
 // @flow strict
 import { type Node } from 'react';
-import { Box, Flex, Link, Text } from 'gestalt';
+import { Box, Flex, Icon, Link, Text } from 'gestalt';
 
 export default function Footer(): Node {
   return (
     <Box color="white" padding={4} mdPadding={6} lgPadding={8} role="contentinfo">
       <Box paddingY={6} marginTop={10} justifyContent="center" display="flex" alignItems="center">
         <Flex alignItems="center" maxWidth={1544} width="100%">
+        <Icon
+            icon="pinterest"
+            color="darkGray"
+            size={24}
+            accessibilityLabel="Pinterest Logo"
+          />
           <Text>
             <Link href="https://www.pinterestcareers.com/">
               Interested in working with Gestalt? Check out our Careers page!
