5e1c7dcdaf782954f6b9a60feb8bd53a29c388ca
Alberto Carreras Carrasco <acarreras@pinterest.com>
2022-01-27 21:14:36 +0100

    docgen combobox

--- packages/gestalt/src/ComboBox.js
+++ packages/gestalt/src/ComboBox.js
@@ -28,42 +28,100 @@ import handleContainerScrolling, {
 type Size = 'md' | 'lg';
 
 type Props = {|
-  // REQUIRED
+  /**
+   * Label to describe the clear button's purpose.
+   */
   accessibilityClearButtonLabel: string,
+  /**
+   * Unique id to identify each ComboBox. Used for [accessibility](https://gestalt.pinterest.systems/combobox#Accessibility) purposes.
+   */
   id: string,
+  /**
+   * Provide a label to identify the ComboBox field.
+   */
   label: string,
+  /**
+   * The data for each selection option. See [subtext](https://gestalt.pinterest.systems/combobox#With-subtext) variant to learn more.
+   */
   options: $ReadOnlyArray<ComboBoxItemType>,
+  /**
+   * The text shown when the input value returns no matches.
+   */
   noResultText: string,
-  // OPTIONAL
+  /**
+   * When disabled, ComboBox looks inactive and cannot be interacted with. If tags are passed, they will appear disabled as well and cannot be removed. See [tags](https://gestalt.pinterest.systems/combobox#Tags) variant to learn more.
+   */
   disabled?: boolean,
+  /**
+   * Provide feedback when an error on selection occurs. See [error message](https://gestalt.pinterest.systems/combobox#Error-message) variant.
+   */
   errorMessage?: Node,
+  /**
+   * Provides additional information about how to select a ComboBox option.
+   */
   helperText?: string,
+  /**
+   * The user input in ComboBox for controlled components. See [controlled ComboBox](https://gestalt.pinterest.systems/combobox#Controlled-vs-Uncontrolled) variant to learn more.
+   */
   inputValue?: string | null,
+  /**
+   * Whether the label should be visible or not. If `hidden`, the label is still available for screen reader users, but does not appear visually. See the [label visibility variant](https://gestalt.pinterest.systems/combobox#Label-visibility) for more info.
+   */
   labelDisplay?: 'visible' | 'hidden',
+  /**
+   * Callback when you focus outside the component.
+   */
   onBlur?: ({|
     event: SyntheticFocusEvent<HTMLInputElement> | SyntheticEvent<HTMLInputElement>,
     value: string,
   |}) => void,
+  /**
+   * Callback when user types into the control input field.
+   */
   onChange?: ({|
     value: string,
     event: SyntheticInputEvent<HTMLInputElement>,
   |}) => void,
+  /**
+   * Callback when user clicks on clear button.
+   */
   onClear?: () => void,
+  /**
+   * Callback when you focus on the component.
+   */
   onFocus?: ({|
     event: SyntheticFocusEvent<HTMLInputElement>,
     value: string,
   |}) => void,
+  /**
+   * Callback for key stroke events. See [tags](#Tags) variant to learn more.
+   */
   onKeyDown?: ({|
     event: SyntheticKeyboardEvent<HTMLInputElement>,
     value: string,
   |}) => void,
+  /**
+   * Callback when an item is selected.
+   */
   onSelect?: ({|
     event: SyntheticInputEvent<HTMLElement> | SyntheticKeyboardEvent<HTMLElement>,
     item: ComboBoxItemType,
   |}) => void,
+  /**
+   * Specify a short description that suggests the expected input for the field.
+   */
   placeholder?: string,
+  /**
+   * The selected option in ComboBox for controlled components. See [controlled ComboBox](https://gestalt.pinterest.systems/combobox#Controlled-vs-Uncontrolled) variant to learn more.
+   */
   selectedOption?: ComboBoxItemType,
+  /**
+   * Defines the height of ComboBox: md: 40px, lg: 48px. Width is defined by parent component.
+   */
   size?: Size,
+  /**
+   * List of tags to display in the component. See [tags](https://gestalt.pinterest.systems/combobox#Tags) variant to learn more.
+   */
   tags?: $ReadOnlyArray<Element<typeof Tag>>,
 |};
 
