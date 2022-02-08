fd0124f7a7e930dd627633d3f00f523ee0fba976
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2022-02-07 22:00:41 -0500

    Revert "IconButton: refactor to fix Dropdown positioning shift on rerender #1914" (#1924)
    This reverts commit bd4ba2707a52a0e3231a7d732110d6dfd77dad45.

--- packages/gestalt/src/IconButton.js
+++ packages/gestalt/src/IconButton.js
@@ -81,6 +81,7 @@ type unionRefs = HTMLButtonElement | HTMLAnchorElement;
 /**
  * [IconButton](https://gestalt.pinterest.systems/iconbutton) allows users to take actions and make choices with a single click or tap. IconButtons use icons instead of text to convey available actions on a screen. IconButton is typically found in forms, dialogs and toolbars.
  Some buttons are specialized for particular tasks, such as navigation or presenting menus.
+
  */
 const IconButtonWithForwardRef: React$AbstractComponent<unionProps, unionRefs> = forwardRef<
   unionProps,
@@ -125,6 +126,12 @@ const IconButtonWithForwardRef: React$AbstractComponent<unionProps, unionRefs> =
   const [isHovered, setHovered] = useState(false);
 
   const { isFocusVisible } = useFocusVisible();
+
+  const buttonRoleClasses = classnames(styles.button, touchableStyles.tapTransition, {
+    [styles.disabled]: disabled,
+    [styles.enabled]: !disabled,
+    [touchableStyles.tapCompress]: props.role !== 'link' && !disabled && isTapping,
+  });
 
   const renderPogComponent = (selected?: boolean): Node => (
     <Pog
@@ -210,7 +217,7 @@ const IconButtonWithForwardRef: React$AbstractComponent<unionProps, unionRefs> =
       aria-expanded={accessibilityExpanded}
       aria-haspopup={accessibilityHaspopup}
       aria-label={accessibilityLabel}
-      className={classnames(styles.parentButton)}
+      className={buttonRoleClasses}
       disabled={disabled}
       onBlur={() => {
         handleBlur();
@@ -233,19 +240,11 @@ const IconButtonWithForwardRef: React$AbstractComponent<unionProps, unionRefs> =
       onTouchMove={handleTouchMove}
       onTouchStart={handleTouchStart}
       ref={innerRef}
+      style={compressStyle || undefined}
       tabIndex={disabled ? null : tabIndex}
       type="button"
     >
-      <div
-        className={classnames(styles.button, touchableStyles.tapTransition, {
-          [styles.disabled]: disabled,
-          [styles.enabled]: !disabled,
-          [touchableStyles.tapCompress]: props.role !== 'link' && !disabled && isTapping,
-        })}
-        style={compressStyle || undefined}
-      >
-        {renderPogComponent(selected)}
-      </div>
+      {renderPogComponent(selected)}
     </button>
   );
 
