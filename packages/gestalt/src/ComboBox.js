34cb659509b6deb853e5522662c17d4677511170
Ryan James <rjames@pinterest.com>
2022-02-08 11:28:12 -0800

    ComboBox: add reference to Typeahead

--- packages/gestalt/src/ComboBox.js
+++ packages/gestalt/src/ComboBox.js
@@ -146,6 +146,7 @@ type Props = {|
 
 /**
  * [ComboBox](https://gestalt.pinterest.systems/combobox) is the combination of a [Textfield](https://gestalt.pinterest.systems/textfield) and an associated [Dropdown](https://gestalt.pinterest.systems/dropdown) that allows the user to filter a list when selecting an option. ComboBox allows users to type the full option, type part of the option and narrow the results, or select an option from the list.
+ * Note: this is a new version of the deprecated component "Typeahead".
  */
 const ComboBoxWithForwardRef: React$AbstractComponent<Props, HTMLInputElement> = forwardRef<
   Props,
