aca1a3f03bdc5fdd6147270694a90eb2af872988
Ayesha <amazumdar@pinterest.com>
2022-03-01 11:52:01 -0800

    remove comments

--- docs/components/buttons/SidebarCategorizationButton.js
+++ docs/components/buttons/SidebarCategorizationButton.js
@@ -1,6 +1,6 @@
 // @flow strict
 import React, { type Node } from 'react';
-import { Button, Dropdown, Flex, Fieldset, RadioButton } from 'gestalt';
+import { Flex, Fieldset, RadioButton } from 'gestalt';
 import { type SidebarOrganisedBy } from '../navigationContext.js';
 import trackButtonClick from './trackButtonClick.js';
 
@@ -10,13 +10,7 @@ type Props = {|
 |};
 
 export default function SidebarCategorizationButton({ onClick, sidebarOrganisedBy }: Props): Node {
-  // const sidebarOrganisedByObject =
-  //   sidebarOrganisedBy === 'categorized'
-  //     ? { value: 'categorized', label: 'By category' }
-  //     : { value: 'alphabetical', label: 'Alphabetical' };
-  // const [open, setOpen] = React.useState(false);
   const [selected, setSelected] = React.useState(sidebarOrganisedBy);
-  // const anchorRef = React.useRef(null);
   const onSelect = ({ event }) => {
     setSelected(event.target.value);
     trackButtonClick('Sidebar Categorization', event.target.value);
@@ -24,45 +18,11 @@ export default function SidebarCategorizationButton({ onClick, sidebarOrganisedB
   };
 
   return (
-    // <Flex justifyContent="center" alignItems="center" direction="column" flex="grow">
-    //   <Button
-    //     accessibilityControls="action-variant-dropdown-example"
-    //     accessibilityExpanded={open}
-    //     accessibilityHaspopup
-    //     iconEnd="arrow-down"
-    //     fullWidth
-    //     onClick={() => setOpen((prevVal) => !prevVal)}
-    //     ref={anchorRef}
-    //     selected={open}
-    //     size="lg"
-    //     text={sidebarOrganisedByObject.label}
-    //     color="transparent"
-    //   />
-    //   {open && (
-    //     <Dropdown
-    //       anchor={anchorRef.current}
-    //       id="action-variant-dropdown-example"
-    //       onDismiss={() => setOpen(false)}
-    //     >
-    //       <Dropdown.Item
-    //         onSelect={onSelect}
-    //         option={{ value: 'categorized', label: 'By category' }}
-    //         selected={selected}
-    //       />
-    //       <Dropdown.Item
-    //         onSelect={onSelect}
-    //         option={{ value: 'alphabetical', label: 'Alphabetical' }}
-    //         selected={selected}
-    //       />
-    //     </Dropdown>
-    //   )}
-    // </Flex>
-
     <Fieldset legend="Sort by">
       <Flex gap={2}>
         <RadioButton
           checked={selected === 'categorized'}
-          id="favoritePizza"
+          id="sortCategory"
           label="Category"
           name="sidebarSort"
           onChange={onSelect}
@@ -71,7 +31,7 @@ export default function SidebarCategorizationButton({ onClick, sidebarOrganisedB
         />
         <RadioButton
           checked={selected === 'alphabetical'}
-          id="favoriteCurry"
+          id="sortAlphabetical"
           label="Alphabetical"
           name="sidebarSort"
           onChange={onSelect}
