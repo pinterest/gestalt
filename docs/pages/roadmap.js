cd2c093bde0b19fbcd359064667f1a81d1c72c27
PJ Onori <ponori@pinterest.com>
2022-03-01 12:05:41 -0800

    Added link field to link out to completed work

--- docs/pages/roadmap.js
+++ docs/pages/roadmap.js
@@ -22,180 +22,210 @@ const theme1 = [
     'deadline': 'Quarter 1',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Form component',
     'deadline': 'Quarter 1',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Badge component improvements',
     'deadline': 'Quarter 1',
     'status': 'inProgress',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'PageHeader component',
     'deadline': 'Quarter 2',
     'status': 'inProgress',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Side nav menu component',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Link component improvements',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'InfoButton integration in input components',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Gestalt Illustration Library',
     'deadline': 'Quarter 2',
     'status': 'inProgress',
     'platform': 'All',
+    'link': '',
   },
   {
     'task': 'Gestalt animation support',
     'deadline': 'Quarter 3',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Messaging components',
     'deadline': 'Quarter 3',
     'status': 'inProgress',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Table component',
     'deadline': 'Quarter 4',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Multi-select component',
     'deadline': 'Quarter 4',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Pagination component',
     'deadline': 'Quarter 4',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Modal/Sheet component',
     'deadline': 'Quarter 4',
     'status': 'unstarted',
     'platform': 'Mobile web',
+    'link': '',
   },
   {
     'task': 'Popover component',
     'deadline': 'Quarter 4',
     'status': 'unstarted',
     'platform': 'Mobile web',
+    'link': '',
   },
   {
     'task': 'Dropdown component',
     'deadline': 'Quarter 4',
     'status': 'unstarted',
     'platform': 'Mobile web',
+    'link': '',
   },
   {
     'task': 'Button component enhancements',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Button component',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'Android',
+    'link': '',
   },
   {
     'task': 'Button component',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'iOS',
+    'link': '',
   },
   {
     'task': 'IconButton component',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'Android',
+    'link': '',
   },
   {
     'task': 'IconButton component',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'iOS',
+    'link': '',
   },
   {
     'task': 'Icon component',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'Android',
+    'link': '',
   },
   {
     'task': 'Avatar component',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'Android',
+    'link': '',
   },
   {
     'task': 'ButtonGroup component',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'iOS',
+    'link': '',
   },
   {
     'task': 'Filters component',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'RadioGroup component',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Dataviz component',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Masonry component',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'App-level hooks w/in Gestalt',
     'deadline': '',
     'status': 'inProgress',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Design tokenization',
     'deadline': '',
     'status': 'inProgress',
     'platform': '',
+    'link': '',
   },
 ];
 
@@ -205,204 +235,238 @@ const theme2 = [
     'deadline': 'Quarter 1',
     'status': 'inProgress',
     'platform': '',
+    'link': '',
   },
   {
     'task': 'Visual search',
     'deadline': 'Quarter 1',
     'status': 'unstarted',
     'platform': '',
+    'link': '',
   },
   {
     'task': 'Public roadmap in Gestalt site',
     'deadline': 'Quarter 1',
     'status': 'ok',
     'platform': '',
+    'link': '/roadmap',
   },
   {
     'task': 'Elevation visual guidelines',
     'deadline': 'Quarter 1',
     'status': 'unstarted',
     'platform': '',
+    'link': '',
   },
   {
     'task': 'Status component design guidelines',
     'deadline': 'Quarter 1',
     'status': 'ok',
     'platform': 'Web',
+    'link': '/status',
   },
   {
     'task': 'Datapoint component design guidelines',
     'deadline': 'Quarter 1',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Tag component design guidelines',
     'deadline': 'Quarter 1',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Badge component design guidelines',
     'deadline': 'Quarter 1',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Button component design guidelines',
     'deadline': 'Quarter 1',
     'status': 'inProgress',
     'platform': 'Android/iOS',
+    'link': '',
   },
   {
     'task': 'IconButton component design guidelines',
     'deadline': 'Quarter 1',
     'status': 'inProgress',
     'platform': 'Android/iOS',
+    'link': '',
   },
   {
     'task': 'Modal/Sheet component design guidelines',
     'deadline': 'Quarter 1',
     'status': 'unstarted',
     'platform': 'Android/iOS',
+    'link': '',
   },
   {
     'task': 'Masonry component design guidelines',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Link component design guidelines',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Heading component design guidelines',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Text component design guidelines',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Tabs component design guidelines',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': 'Android/iOS',
+    'link': '',
   },
   {
     'task': 'Popover component design guidelines',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': 'Android/iOS',
+    'link': '',
   },
   {
     'task': 'Avatar component design guidelines',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': 'Android/iOS',
+    'link': '',
   },
   {
     'task': 'Product color visual guidelines',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': '',
+    'link': '',
   },
   {
     'task': 'Typographic visual guidelines',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': '',
+    'link': '',
   },
   {
     'task': 'Component scorecard',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': '',
+    'link': '',
   },
   {
     'task': 'Messaging pattern guidelines',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': '',
+    'link': '',
   },
   {
     'task': 'Dark mode visual guidelines',
     'deadline': 'Quarter 2',
     'status': 'unstarted',
     'platform': '',
+    'link': '',
   },
   {
     'task': 'Toast component design guidelines',
     'deadline': 'Quarter 3',
     'status': 'unstarted',
     'platform': 'Android/iOS',
+    'link': '',
   },
   {
     'task': 'Icon component design guidelines',
     'deadline': 'Quarter 3',
     'status': 'unstarted',
     'platform': 'Android/iOS',
+    'link': '',
   },
   {
     'task': 'Checkbox component design guidelines',
     'deadline': 'Quarter 3',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Toast component design guidelines',
     'deadline': 'Quarter 3',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
-    'task': 'RadioGroup  component design guidelines',
+    'task': 'RadioGroup component design guidelines',
     'deadline': 'Quarter 3',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
-    'task': 'Switch  component design guidelines',
+    'task': 'Switch component design guidelines',
     'deadline': 'Quarter 3',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
-    'task': 'TextArea  component design guidelines',
+    'task': 'TextArea component design guidelines',
     'deadline': 'Quarter 4',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
-    'task': 'ButtonGroup  component design guidelines',
+    'task': 'ButtonGroup component design guidelines',
     'deadline': 'Quarter 4',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Help/guidance pattern guidelines',
     'deadline': 'Quarter 4',
     'status': 'unstarted',
     'platform': '',
+    'link': '',
   },
   {
     'task': 'Iconography pattern guidelines',
     'deadline': 'Quarter 4',
     'status': 'unstarted',
     'platform': '',
+    'link': '',
   },
   {
     'task': 'Extended color visual guidelines',
     'deadline': 'Quarter 4',
     'status': 'unstarted',
     'platform': '',
+    'link': '',
   },
 ];
 
@@ -412,30 +476,35 @@ const theme3 = [
     'deadline': 'Quarter 1',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Box duplicate props autofix',
     'deadline': 'Quarter 1',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Generalized prop naming autofix',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Generalized prop value change autofix',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
   {
     'task': 'Build testing helpers library',
     'deadline': '',
     'status': 'unstarted',
     'platform': 'Web',
+    'link': '',
   },
 ];
 
@@ -480,16 +549,24 @@ function TableRow({
   platform,
   quarter,
   status,
+  link,
 }: {|
   text: string,
   platform: string,
   quarter: string,
   status: StatusType,
+  link: string,
 |}) {
   return (
     <Table.Row>
       <Table.Cell>
-        <Text>{text}</Text>
+        {link ? (
+          <Text underline>
+            <Link href={link}>{text}</Link>
+          </Text>
+        ) : (
+          <Text>{text}</Text>
+        )}
       </Table.Cell>
       <Table.Cell>
         {platform ? (
@@ -524,6 +601,7 @@ export default function RoadmapPage(): Node {
       platform={obj.platform}
       quarter={obj.deadline}
       status={obj.status}
+      link={obj.link}
     />
   ));
 
@@ -534,6 +612,7 @@ export default function RoadmapPage(): Node {
       platform={obj.platform}
       quarter={obj.deadline}
       status={obj.status}
+      link={obj.link}
     />
   ));
 
@@ -544,6 +623,7 @@ export default function RoadmapPage(): Node {
       platform={obj.platform}
       quarter={obj.deadline}
       status={obj.status}
+      link={obj.link}
     />
   ));
 
