73aa2defe1774ef2c1b00ccff4d7725cb166334a
Alberto Carreras Carrasco <acarreras@pinterest.com>
2022-03-08 02:26:48 +0100

    TextField

---
+++ docs/pages/visual-test/TextField-dark.js
@@ -0,0 +1,19 @@
+// @flow strict
+import { type Node } from 'react';
+import { Flex, TextField, Box, ColorSchemeProvider } from 'gestalt';
+
+export default function Screenshot(): Node {
+  return (
+    <ColorSchemeProvider colorScheme="dark">
+      <Box color="white" display="inlineBlock" padding={1} width={300}>
+        <TextField
+          helperText="Password should be at least 20 characters long"
+          id="variants-helper-text"
+          label="Password"
+          onChange={() => {}}
+          type="password"
+        />
+      </Box>
+    </ColorSchemeProvider>
+  );
+}
