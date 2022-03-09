f01eca36c11cc7e220b00f4f52e5b2d9f2501f7f
Ayesha <amazumdar@pinterest.com>
2022-03-09 15:08:31 -0800

    Docs: Add elevation guidelines page

--- docs/pages/elevation.js
+++ docs/pages/elevation.js
@@ -1,5 +1,6 @@
 // @flow strict
 import { type Node } from 'react';
+import { Box, Flex, Text } from 'gestalt';
 import MainSection from '../components/MainSection.js';
 import PageHeader from '../components/PageHeader.js';
 import Page from '../components/Page.js';
@@ -21,6 +22,18 @@ export default function ColorUsagePage(): Node {
           description="The default elevation level.
         Elevates messages temporarily appearing in front of other surfaces, such as modals and banners. "
         >
+          <Flex gap={4} direction="column">
+            <Box
+              width={300}
+              height={300}
+              rounding={3}
+              dangerouslySetInlineStyle={{
+                __style: { boxShadow: 'var(--elevation-floating' },
+              }}
+            />
+            <Text>$elevation-floating</Text>
+            <Text>10% (#000000) opacity / Blur: 8</Text>
+          </Flex>
           <MainSection.Subsection columns={2}>
             <MainSection.Card
               cardSize="md"
@@ -44,16 +57,127 @@ export default function ColorUsagePage(): Node {
         <MainSection.Subsection
           title="Raised"
           description="Presents a drop shadow on the edge of a top or bottom component, allowing surfaces to move behind when scrolled. "
-        />
+        >
+          <MainSection.Subsection columns={2}>
+            <Flex gap={4} direction="column">
+              <Text weight="bold">Raised-top</Text>
+              <Box
+                width={300}
+                height={300}
+                rounding={3}
+                dangerouslySetInlineStyle={{
+                  __style: { boxShadow: 'var(--elevation-raised-top' },
+                }}
+              />
+              <Text>$elevation-raised-top</Text>
+              <Text>12% (#000000) opacity / Y: -2 / Blur: 8</Text>
+            </Flex>
+            <Flex gap={4} direction="column">
+              <Text weight="bold">Raised-bottom</Text>
+              <Box
+                width={300}
+                height={300}
+                rounding={3}
+                dangerouslySetInlineStyle={{
+                  __style: { boxShadow: 'var(--elevation-raised-bottom' },
+                }}
+              />
+              <Text>$elevation-raised-bottom</Text>
+              <Text> 12% (#000000) opacity / Y: 2 / Blur: 8</Text>
+            </Flex>
+          </MainSection.Subsection>
+          <MainSection.Subsection columns={2}>
+            <MainSection.Card
+              cardSize="md"
+              type="do"
+              title="When to use"
+              description={`
+              - Indicating a sticky UI element where content can scroll underneath (e.g., header, footer, navigation bar)
+              `}
+            />
+            <MainSection.Card
+              cardSize="md"
+              type="don't"
+              title="When not to use"
+              description={`
+              - Elevating temporary messages (e.g. modals, banners)
+              `}
+            />
+          </MainSection.Subsection>
+        </MainSection.Subsection>
       </MainSection>
       <MainSection
         name="Elevation using color"
         description="Use as needed to accent containers when shadows are not an option."
-      />
+      >
+        <Flex gap={4} direction="column">
+          <Box
+            width={300}
+            height={300}
+            rounding={3}
+            dangerouslySetInlineStyle={{
+              __style: { backgroundColor: 'var(--color-background-elevation-accent' },
+            }}
+          />
+          <Text>$color-background-elevation-accent</Text>
+          <Text>Roboflow 100 / Shadows: none</Text>
+        </Flex>
+        <MainSection.Subsection columns={2}>
+          <MainSection.Card
+            cardSize="md"
+            type="do"
+            title="When to use"
+            description={`
+              - As an accent color to highlight containers when shadows don't work (e.g. cards)
+            `}
+          />
+          <MainSection.Card
+            cardSize="md"
+            type="don't"
+            title="When not to use"
+            description={`
+              - Elevating temporary messages (e.g. modals, banners)
+              - Raising elements fixed on the top or bottom of the screen. Use "Raised" elevation instead.
+            `}
+          />
+        </MainSection.Subsection>
+      </MainSection>
       <MainSection
         name="Elevation using borders"
         description="Technically, borders are not considered elevation; however, they can be used as an alternative to display a single contained group when shadows or color don't work. The border treatment helps to determine visible boundaries."
-      />
+      >
+        <Flex gap={4} direction="column">
+          <Box
+            width={300}
+            height={300}
+            rounding={3}
+            dangerouslySetInlineStyle={{
+              __style: { border: '1px solid var(--color-border-container' },
+            }}
+          />
+          <Text>$color-border-container</Text>
+          <Text>Border-color: Roboflow 300 / 1px (inside) / Shadows: none</Text>
+        </Flex>
+        <MainSection.Subsection columns={2}>
+          <MainSection.Card
+            cardSize="md"
+            type="do"
+            title="When to use"
+            description={`
+              - Displaying a single contained group when shadows or color don't work, setting visible boundaries.
+            `}
+          />
+          <MainSection.Card
+            cardSize="md"
+            type="don't"
+            title="When not to use"
+            description={`
+              - Elevating temporary messages (e.g. modals, banners)
+              - Raising elements fixed on the top or bottom of the screen. Use "Raised" elevation instead.
+            `}
+          />
+        </MainSection.Subsection>
+      </MainSection>
     </Page>
   );
 }
