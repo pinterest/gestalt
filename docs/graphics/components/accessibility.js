204f07c8116f5fe9738f0d27fb4acf90d152089c
Alberto Carreras Carrasco <acarreras@pinterest.com>
2022-03-16 16:45:17 +0100

    explore

---
+++ docs/graphics/components/accessibility.js
@@ -0,0 +1,17 @@
+const a = {
+  svg: (
+    <svg
+      width="325"
+      height="180"
+      viewBox="0 0 325 180"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="325" height="180" fill="#005C62" />
+    </svg>
+  ),
+  name: 'accessibility',
+  description: 'accessibility is fun',
+};
+
+export default a;
--- docs/pages/component_overview.js
+++ docs/pages/component_overview.js
@@ -8,12 +8,11 @@ import HomePageCard from '../components/HomePageCard.js';
 import A11Y from '../graphics/accessibility.svg';
 import Color from '../graphics/color.svg';
 import DesignTokens from '../graphics/design-tokens.svg';
-
 import HomePageSection from '../components/HomePageSection.js';
+import pathHelper from 'path';
 
-export default function DocsPage(): Node {
+export default function DocsPage({ svgFiles }): Node {
   const [order, setOrder] = useState(undefined);
-
   return (
     <Page title="Component overview" hideSideNav hideEditLink>
       <PageHeader
@@ -23,34 +22,46 @@ export default function DocsPage(): Node {
       />
       <Box width="100%">
         <Flex direction="column">
-          <Flex gap={2}>
-            <Box aria-hidden={true}>
-              <Text>Sort by:</Text>
-            </Box>
-            <Fieldset
-              legend="Sort Gestalt components alphabetically or categorically"
-              legendDisplay="hidden"
-            >
-              <Flex gap={2}>
-                <RadioButton
-                  checked={order === 'category'}
-                  id="category"
-                  label="Category"
-                  name="category"
-                  onChange={() => setOrder('category')}
-                  value="category"
-                />
-                <RadioButton
-                  checked={order === 'alphabetical'}
-                  id="alphabetical"
-                  label="Alphabetical"
-                  name="alphabetical"
-                  onChange={() => setOrder('alphabetical')}
-                  value="alphabetical"
-                />
-              </Flex>
-            </Fieldset>
-          </Flex>
+          <Box
+            marginStart={-8}
+            marginEnd={-8}
+            smMarginEnd={0}
+            smMarginStart={0}
+            paddingX={12}
+            smPaddingX={2}
+            mdPaddingX={8}
+            marginBottom={10}
+            display="flex"
+          >
+            <Flex gap={2}>
+              <Box aria-hidden={true}>
+                <Text>Sort by</Text>
+              </Box>
+              <Fieldset
+                legend="Sort Gestalt components alphabetically or categorically"
+                legendDisplay="hidden"
+              >
+                <Flex gap={2}>
+                  <RadioButton
+                    checked={order === 'category'}
+                    id="category"
+                    label="Category"
+                    name="category"
+                    onChange={() => setOrder('category')}
+                    value="category"
+                  />
+                  <RadioButton
+                    checked={order === 'alphabetical'}
+                    id="alphabetical"
+                    label="Alphabetical"
+                    name="alphabetical"
+                    onChange={() => setOrder('alphabetical')}
+                    value="alphabetical"
+                  />
+                </Flex>
+              </Fieldset>
+            </Flex>
+          </Box>
           <HomePageSection title="Getting started">
             <HomePageCard
               href="/accessibility"
@@ -104,4 +115,36 @@ export default function DocsPage(): Node {
       </Box>
     </Page>
   );
+}
+
+export async function getServerSideProps(): Promise<{| props: {| svgFiles: string |} |}> {
+  const globby = require('globby');
+  const fs = require('fs');
+  const { parse } = require('@babel/parser');
+  const { traverse } = require('@babel/traverse');
+
+  const svgFilesData = await Promise.all(
+    (await globby(['graphics/components/*.js'])).map(async (filePath) => {
+      const a = import(`../${filePath}`);
+      console.log(a.default);
+      // const svg = await fs.promises.readFile(filePath, 'utf8');
+
+      // const ast = parse(svg, {
+      //   sourceType: 'module',
+      //   plugins: ['jsx', 'flow'],
+      // });
+
+      // traverse(ast, {
+      //   enter(path) {
+      //     console.log(ast, path);
+      //   },
+      // });
+
+      return { name: 'accessibility', category: 'foundations', svg: 'hi' };
+    }),
+  );
+
+  return {
+    props: { svgFiles: await svgFilesData },
+  };
 }
