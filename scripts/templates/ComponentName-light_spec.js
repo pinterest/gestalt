ee5546fc697652d06975277ed849ad5343d84dbf
Alberto Carreras Carrasco <acarreras@pinterest.com>
2022-03-16 18:21:04 +0100

    test

--- scripts/templates/ComponentName-light_spec.js
+++ scripts/templates/ComponentName-light_spec.js
@@ -6,7 +6,7 @@ describe('ComponentName light mode visual regression check', () => {
   // eslint-disable-next-line jest/expect-expect
   it('Compares snapshots', () => {
     cy.get('[data-test-id="visual-test"]').toMatchImageSnapshot({
-      name: 'ComponentName-light',
+      name: 'ComponentNamggge-light',
     });
   });
 });
