0c3dedc3d32ed7cfbd6b8d391c35be136abbea44
Ryan James <rjames@pinterest.com>
2022-03-18 12:12:03 -0700

    more specific types, throw errors

--- packages/gestalt/src/contexts/I18nProvider.js
+++ packages/gestalt/src/contexts/I18nProvider.js
@@ -1,15 +1,24 @@
 // @flow strict
 import { type Context, createContext, useContext } from 'react';
 
-type PropTranslations = {|
-  [propName: string]: string,
+/**
+ * To add new translations:
+ * - Create a type for the component's translations
+ * - Add component translation type to I18nContextType keyed by component name
+ * - Add component translation type to useI18nContext return type
+ * - When consuming useI18nContext in component, throw an error if any translation isn't available
+ */
+
+type TextTranslations = {|
+  accessibilityHidePasswordLabel: string,
+  accessibilityShowPasswordLabel: string,
 |};
 
-type I18nContextType = {|
-  [componentName: string]: PropTranslations,
+type I18nContextType = ?{|
+  Text: TextTranslations,
 |};
 
-const I18nContext: Context<I18nContextType> = createContext<I18nContextType>({});
+const I18nContext: Context<I18nContextType> = createContext<I18nContextType>(null);
 
 /**
  * *ALPHA - DO NOT USE YET - MAY HAVE BREAKING CHANGES IN THE NEAR FUTURE*
@@ -17,7 +26,15 @@ const I18nContext: Context<I18nContextType> = createContext<I18nContextType>({})
 const I18nProvider = I18nContext.Provider;
 export default I18nProvider;
 
-export function useI18nContext(componentName: string): PropTranslations {
+export function useI18nContext(componentName: string): Text {
   const propsTranslations = useContext(I18nContext) ?? {};
-  return propsTranslations[componentName];
+  const componentTranslations = propsTranslations[componentName];
+
+  if (!componentTranslations) {
+    throw new Error(
+      `${componentName} translations not available — please add translations to I18nProvider`,
+    );
+  }
+
+  return componentTranslations;
 }
