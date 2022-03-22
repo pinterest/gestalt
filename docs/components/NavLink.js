9ccb527c9a6436160a76e8b9ff3db273fe55296f
Alberto Carreras Carrasco <acarreras@pinterest.com>
2022-03-22 15:57:23 -0400

    refactors

--- docs/components/NavLink.js
+++ docs/components/NavLink.js
@@ -9,27 +9,14 @@ type Props = {|
   children: Node,
 |};
 
-// $FlowIssue[prop-missing]
-const isLeftClickEvent = (event) => event.button === 0;
-const isModifiedEvent = (event) =>
-  !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey);
-
 export default function NavLink({ children, href }: Props): Node {
   const { setIsSidebarOpen } = useNavigationContext();
   const router = useRouter();
 
-  const handleClick = ({ event }) => {
-    setIsSidebarOpen(false);
-    if (event.defaultPrevented) return;
-    if (isModifiedEvent(event) || !isLeftClickEvent(event)) return;
-    event.preventDefault();
-    router.push(href);
-  };
-
   return (
     <Box color={router.asPath === href ? 'lightGray' : 'transparent'} rounding={2}>
       <Text weight="bold">
-        <Link href={href} onClick={handleClick} rounding={2}>
+        <Link href={href} onClick={() => setIsSidebarOpen(false)} rounding={2}>
           {children}
         </Link>
       </Text>
