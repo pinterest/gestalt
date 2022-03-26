be4ff38754451950392dc6f331b8da02be058c6d
Ryan James <rjames@pinterest.com>
2022-03-25 17:18:55 -0700

    Docs: add legal disclaimer re: brand icons

--- docs/pages/iconography_and_svgs.js
+++ docs/pages/iconography_and_svgs.js
@@ -33,16 +33,12 @@ export default function IconPage(): Node {
 
   const handleOnChange = ({ value }) => {
     setSelected();
-    if (value) {
-      setInputValue(value);
-      const filteredOptions = iconOptions.filter((item) =>
-        item.label.toLowerCase().includes(value.toLowerCase()),
-      );
-      setSuggestedOptions(filteredOptions);
-    } else {
-      setInputValue(value);
-      setSuggestedOptions(iconOptions);
-    }
+    setInputValue(value);
+    setSuggestedOptions(
+      value
+        ? iconOptions.filter(({ label }) => label.toLowerCase().includes(value.toLowerCase()))
+        : iconOptions,
+    );
   };
 
   const handleSelect = ({ item }) => {
@@ -57,23 +53,23 @@ export default function IconPage(): Node {
       setShowToastText(`Icon name ("${iconName}") successfully copied!`);
       setTimeout(() => setShowToastText(), 3000);
     } catch (err) {
-      return undefined;
+      // Not handling error
     }
-    return undefined;
   };
 
   return (
     <Page title="Iconography and SVGs">
       <PageHeader name="Iconography and SVGs" folderName="icons" />
+
       <MainSection
-        name="Search icon library"
-        description="The combobox on the left allows to search icons by name. On the right, the icon list renders the filtered results. Furthermore, the icon list can be used to visually search for icons. On hover, each tooltip displays their name. On click, the icon name will be copied."
+        name="Icon library"
+        description="Use the combobox on the left to search icons by name. The icon list on the right renders the filtered results. You can also use the icon list to visually search for icons. On hover, a tooltip displays the icon name. On click, the icon name will be copied."
       >
-        <Box width="100%" display="flex">
+        <Flex width="100%">
           <Box width="50%">
             <ComboBox
               accessibilityClearButtonLabel="Clear the current value"
-              label="Search icon by name"
+              label="Search icons by name"
               id="controlled"
               inputValue={inputValue}
               noResultText="No results for your selection"
@@ -88,7 +84,7 @@ export default function IconPage(): Node {
                 setSuggestedOptions(iconOptions);
               }}
               selectedOption={selected}
-              placeholder="Search icon by name"
+              placeholder="Search icons by name"
               onChange={handleOnChange}
               onSelect={handleSelect}
             />
@@ -96,10 +92,10 @@ export default function IconPage(): Node {
           <Box
             borderStyle="shadow"
             height={400}
-            width="50%"
+            marginStart={4}
             overflow="auto"
             padding={4}
-            marginStart={4}
+            width="50%"
           >
             <Flex gap={1} wrap>
               {selected?.label ? (
@@ -118,7 +114,8 @@ export default function IconPage(): Node {
               )}
             </Flex>
           </Box>
-        </Box>
+        </Flex>
+
         {showToastText && (
           <Layer>
             <Box
@@ -138,6 +135,7 @@ export default function IconPage(): Node {
           </Layer>
         )}
       </MainSection>
+
       <MainSection name="Custom SVG icons">
         <MainSection.Subsection
           description={`
@@ -145,9 +143,9 @@ If you need a new icon for an experiment that is not listed on our [Icon](/icon)
 
 However, \`dangerouslySetSvgPath\` only works with one SVG path. For icons with multiple paths and groups, use [Box](/box) and \`dangerouslySetInlineStyle\` to pass the custom icon as \`backgroundImage\`.
 
-Once your experiment ships to 100%, ask your designer to follow the directions in the [Icon kit](https://www.figma.com/file/N60WnDx9j6Moz3Dt1rNsq9/Icon-Kit). Once the asset is ready, we can add the Icon to Gestalt.
+Once your experiment ships to 100%, ask your designer to follow the directions in the [Icon kit](https://www.figma.com/file/N60WnDx9j6Moz3Dt1rNsq9/Icon-Kit). Once the asset is ready, we can add the icon to Gestalt.
 
-Gestalt Icon svg files follow a particular format and use automatic file validation testing.
+Gestalt icon svg files follow a particular format and use automatic file validation testing.
 
 \`<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
 <path d="_______________"/>
@@ -155,7 +153,7 @@ Gestalt Icon svg files follow a particular format and use automatic file validat
 
 We override the color in the Gestalt Icon component and Gestalt only uses the \`d\` attribute in the \`path\` tag and the basic attributes for visualizing the raw file in the \`svg\` tag . For consistency, we don't include unnecessary attributes in the \`svg\` and \`path\` tags.
 
-We recommend streamlining (removing strokes, transforms, ...) and optimizing the SVGs to improve the performance and the pinner experience using the tools [svgo](https://github.com/svg/svgo) or [ImageOptim](https://imageoptim.com/mac)
+We recommend streamlining (removing strokes, transforms, etc.) and optimizing the SVGs to improve performance and the pinner experience using the tools [svgo](https://github.com/svg/svgo) or [ImageOptim](https://imageoptim.com/mac)
 
 To use svgo, install
 
@@ -163,17 +161,28 @@ To use svgo, install
 
 and run
 
-\`svgo -f packages/gestalt/src/icons --config=packages/gestalt/src/icons/svgo.config.js\`.`}
+\`svgo -f packages/gestalt/src/icons --config=packages/gestalt/src/icons/svgo.config.js\``}
         />
       </MainSection>
-      <MainSection
-        name="Accessibility"
-        description="- Icons must meet the [Non-Text Contrast](https://www.w3.org/WAI/WCAG21/Understanding/non-text-contrast.html) requirement.
+
+      <MainSection name="Accessibility">
+        <MainSection.Subsection
+          description="
+- Icons must meet the [Non-Text Contrast](https://www.w3.org/WAI/WCAG21/Understanding/non-text-contrast.html) requirement.
 - Avoid using unfamiliar icons. Always refer to Gestalt available icons. A new icon needs to be user tested to evaluate comprehension.
 - Icons should be universal across cultures, regions, ages, and backgrounds without need for translation. Be mindful of your audience and use symbols and labels that resonate with them.
-- Some icons don’t translate well in all cultures, so it's preferred to user-test each Icon before it gets added to Gestalt.
+- Some icons don't translate well in all cultures, so it's preferred to user-test each icon before it is added to Gestalt.
 "
-      />
+        />
+      </MainSection>
+
+      <MainSection name="Brand icons">
+        <MainSection.Subsection
+          description={`
+        All brand icons are trademarks of their respective owners. The inclusion of these trademarks does not indicate endorsement of the trademark holder by Pinterest, nor vice-versa. Please do not use brand logos for any purpose except to represent the company, product, or service to which they refer.
+        `}
+        />
+      </MainSection>
     </Page>
   );
 }
