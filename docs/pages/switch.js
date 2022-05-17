de193f5de65ada71e01a4506a5b4184d3e239ef2
PJ Onori <ponori@pinterest.com>
2022-05-17 14:49:41 -0700

    Initial iteration on Switch docs

--- docs/pages/switch.js
+++ docs/pages/switch.js
@@ -57,6 +57,211 @@ export default function DocsPage({ generatedDocGen }: {| generatedDocGen: DocGen
           />
         </MainSection.Subsection>
       </MainSection>
+      <MainSection name="Best practices">
+        <MainSection.Subsection columns={2}>
+          <MainSection.Card
+            cardSize="md"
+            type="do"
+            description="Use a label to give the Switch context when possible."
+            defaultCode={`
+<Flex gap={2} alignItems="center" width="100%" maxWidth={300}>
+  <Flex.Item flex="grow">
+    <Label htmlFor="showsecretboards">
+      <Text weight="bold">Make this board secret</Text>
+    </Label>
+  </Flex.Item>
+  <Switch
+    onChange={() => {}}
+    id="showsecretboards"
+    switched={true}
+  />
+</Flex>
+`}
+          />
+          <MainSection.Card
+            cardSize="md"
+            type="don't"
+            description="Truncate label text, instead allow it to wrap to form another line. "
+            defaultCode={`
+<Flex gap={2} alignItems="center">
+  <Switch
+    onChange={() => {}}
+    id="setboard"
+    switched={false}
+  />
+  <Label htmlFor="setboard">
+    <Text>Set board to...</Text>
+  </Label>
+</Flex>
+`}
+          />
+          <MainSection.Card
+            cardSize="md"
+            type="do"
+            description="Communicate why a switch is disabled and how to enable it if possible."
+            defaultCode={`
+<Flex gap={2} alignItems="center">
+  <Switch
+    onChange={() => {}}
+    id="showsecretboards"
+    switched={false}
+    disabled={true}
+  />
+  <Label htmlFor="showsecretboards">
+    <Flex direction="column" gap={1}>
+      <Text color="subtle">Show secret boards</Text>
+      <Text size="100" color="subtle">You don't have any secret boards</Text>
+    </Flex>
+  </Label>
+</Flex>
+`}
+          />
+          <MainSection.Card
+            cardSize="md"
+            type="don't"
+            description="Use alternative styling to represent the functionality of a switch. Use Switch instead."
+            defaultCode={`
+<Flex direction="column" gap={2} width="100%" maxWidth={300}>
+  <Heading size="300">On Pinterest notifications</Heading>
+  <Flex direction="column" gap={4}>
+    <Flex>
+      <Flex.Item flex="grow">
+        <Text>Comments</Text>
+      </Flex.Item>
+      <Icon icon="check-circle" color="default" />
+    </Flex>
+    <Flex>
+      <Flex.Item flex="grow">
+        <Text>Mentions</Text>
+      </Flex.Item>
+      <Icon icon="check-circle" color="default" />
+    </Flex>
+    <Flex>
+      <Flex.Item flex="grow">
+        <Text>Views</Text>
+      </Flex.Item>
+      <Icon icon="circle-outline" />
+    </Flex>
+    <Flex>
+      <Flex.Item flex="grow">
+        <Text>Saves</Text>
+      </Flex.Item>
+      <Icon icon="check-circle" color="default" />
+    </Flex>
+  </Flex>
+</Flex>
+`}
+          />
+          <MainSection.Card
+            cardSize="md"
+            type="do"
+            description="Use a standalone switch if the surrounding context is clear without an associated label. However, be sure to include a visually hidden label for screen readers."
+            defaultCode={`
+<Flex direction="column" gap={2} width="100%" maxWidth={300}>
+  <Heading size="300">Tune your home feed</Heading>
+  <Text>Turn off any boards that you don't want us to use for your home feed recommendations</Text>
+  <Flex direction="column" gap={4}>
+    <Flex>
+      <Flex.Item flex="grow">
+        <Label htmlFor="boardfood">
+          <Flex direction="column" gap={1}>
+            <Text>Food</Text>
+            <Text size="100">55 pins · 1 section</Text>
+          </Flex>
+        </Label>
+      </Flex.Item>
+      <Switch
+        onChange={() => {}}
+        id="boardfood"
+        switched={false}
+      />
+    </Flex>
+    <Flex>
+      <Flex.Item flex="grow">
+        <Label htmlFor="boardoutfits">
+          <Flex direction="column" gap={1}>
+            <Text>Outfits</Text>
+            <Text size="100">138 pins · 5 sections</Text>
+          </Flex>
+        </Label>
+      </Flex.Item>
+      <Switch
+        onChange={() => {}}
+        id="boardoutfits"
+        switched={true}
+      />
+    </Flex>
+    <Flex>
+      <Flex.Item flex="grow">
+        <Label htmlFor="boardhomedecor">
+          <Flex direction="column" gap={1}>
+            <Text>Home Decor</Text>
+            <Text size="100">33 pins · 2 sections</Text>
+          </Flex>
+        </Label>
+      </Flex.Item>
+      <Switch
+        onChange={() => {}}
+        id="boardhomedecor"
+        switched={true}
+      />
+    </Flex>
+  </Flex>
+</Flex>
+`}
+          />
+        </MainSection.Subsection>
+      </MainSection>
+      <MainSection
+        name="Accessibility"
+        description={`
+    Switches should have labels that can be read by screen readers, and that can be clicked or tapped to make it easier for users to select and deselect. Therefore, make sure to supply the \`label\` prop. If that is not possible, make sure your stand-alone Label has an \`htmlFor\` prop that matches the \`id\` on the Switch. Test that the Switch and label are properly connected by clicking or tapping on the label and confirming that it activates the Switch next to it.
+
+      If Switch is labeled by content elsewhere on the page, or a more complex label is needed, the \`labelDisplay\` prop can be used to visually hide the label. In this case, it is still available to screen reader users, but will not appear visually on the screen. See the [Label visibility example](https://gestalt.netlify.app/checkbox#Label-visibility) for more detail.`}
+      >
+        <MainSection.Subsection
+          title="Keyboard navigation"
+          columns={2}
+          description={`
+    Switch has conventional keyboard support.
+    - Users relying on the keyboard expect to move focus to the Switch by using the tab key or shift+tab when moving backwards.
+    - Setting \`disabled\` will prevent Switch from receiving keyboard focus or input
+`}
+        />
+      </MainSection>
+      <MainSection
+        name="Localization"
+        description={`Be sure to localize \`label\` and any \`subtext\`. Be mindful of label length so that it doesn’t truncate in languages with lengthier character counts.`}
+      >
+        <MainSection.Subsection
+          title="With a label"
+          description={`
+        Whenever using Switch, always use a [Label](/label) with it to make your component accessible.`}
+        >
+          <MainSection.Card
+            defaultCode={`
+        function SwitchExample() {
+          const [switched, setSwitched] = React.useState(false);
+
+          return (
+            <Box display="flex" alignItems="center">
+              <Box paddingX={2}>
+                <Label htmlFor="emailNotifications">
+                  <Text>Airplane mode</Text>
+                </Label>
+              </Box>
+              <Switch
+                onChange={() => setSwitched(!switched)}
+                id="emailNotifications"
+                switched={switched}
+              />
+            </Box>
+          );
+        }
+        `}
+          />
+        </MainSection.Subsection>
+      </MainSection>
       <MainSection name="Variants">
         <MainSection.Subsection
           title="With a label"
