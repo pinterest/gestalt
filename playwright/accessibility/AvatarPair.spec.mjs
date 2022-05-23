0118aba742a58dfb3f944bcc5039e41e261fd144
Eduardo Costa <ecosta@pinterest.com>
2022-05-23 18:50:11 +0000

    Remove AvatarPair reference on Playwright folder

--- playwright/accessibility/AvatarPair.spec.mjs
+++
@@ -1,8 +0,0 @@
-// @flow strict
-import { test } from '@playwright/test';
-import expectAccessiblePage from './expectAccessiblePage.mjs';
-
-test('AvatarPair Accessibility check', async ({ page }) => {
-  await page.goto('/avatarpair');
-  await expectAccessiblePage({ page });
-});
