e047c02917708411ec929ca0c17bbac59d6074af
Ayesha Mazumdar <ayeshakmaz@gmail.com>
2022-05-24 18:53:16 -0700

    Feedback

--- docs/pages/data_visualization_colors.js
+++ docs/pages/data_visualization_colors.js
@@ -48,18 +48,18 @@ export default function ColorPage(): Node {
     <Page title="Color palette">
       <PageHeader
         name="Color palette"
-        description={`The data visualization color palette is used to represent discrete categories of data. This palette utilizes Gestalt color palette and is optimized for improving accessibility in data visualizations. The palette is comprised of a 12-color categorical palette and semantic colors.  `}
+        description={`The data visualization color palette is used to represent discrete categories of data. This palette utilizes the Gestalt color palette and is optimized for accessibility in data visualizations. The palette is comprised of a 12-color categorical palette and semantic colors.  `}
         showSourceLink={false}
       />
       <MainSection
         name="12-Color categorical palette"
         description={`
-        This palette helps create a consistent brand image among visualizations across Pinterest products, while ensuring enough color contrast against background and between individual colors. There are 12 colors in the palette, and is divided into a main palette and an extended palette.
+        This palette helps create a consistent brand image among visualizations across Pinterest products, while ensuring enough color contrast against background and between individual colors. There are 12 colors in the palette, divided into a main palette and an extended palette.
 
         - The colors in the main palette are used in most cases.
         - Use the extended palette only when more than 6 colors are needed.
-        - When there is only one color needed in a visualization, always use Data Visualization #05, which is reserved for showing total value.
-        - When more than one color is needed, colors should be applied in the exact order in the palette to maximize contrast between adjacent colors, in order to help with visual differentiation.
+        - When there is only 1 color needed in a visualization, always use Data Visualization #05, which is reserved for showing total value.
+        - When more than 1 color is needed, colors should be applied in the exact order in the palette to maximize contrast between adjacent colors, in order to help with visual differentiation.
         `}
       >
         <MainSection.Subsection title="Light mode">
