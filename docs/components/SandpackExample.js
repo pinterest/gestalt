fb4123cd3e9f6c38128b55fa54e803f2a3bb21cc
Christian Vuerings <cvuerings@pinterest.com>
2022-07-30 12:14:35 +0200

    Internal: add ability to use local Gestalt CSS & JS files

--- docs/components/SandpackExample.js
+++ docs/components/SandpackExample.js
@@ -6,7 +6,7 @@ import {
   SandpackCodeEditor,
   useSandpack,
 } from '@codesandbox/sandpack-react';
-import React, { type Node } from 'react';
+import React, { useEffect, type Node } from 'react';
 import { Box, Flex } from 'gestalt';
 import CopyCodeButton from './buttons/CopyCodeButton.js';
 import clipboardCopy from './clipboardCopy.js';
@@ -96,6 +96,31 @@ export default function SandpackExample({
   previewHeight?: number,
   showEditor?: boolean,
 |}): Node {
+  const [localFiles, setLocalFiles] = React.useState(false);
+  const [localCSS, setLocalCSS] = React.useState(null);
+  const [localJS, setLocalJS] = React.useState(null);
+
+  useEffect(() => {
+    if (typeof document === 'undefined' || !document.location) {
+      return;
+    }
+    // Use local gestalt JS and CSS when `?localFiles=true` is in the URL
+    const params = new URL(document.location.toString()).searchParams;
+    setLocalFiles(params.get('localFiles') === 'true');
+  }, []);
+
+  useEffect(() => {
+    if (localFiles === false) {
+      return;
+    }
+    fetch('/api/localFiles')
+      .then((res) => res.json())
+      .then((data) => {
+        setLocalCSS(data.css);
+        setLocalJS(data.js);
+      });
+  }, [localFiles]);
+
   // Based on
   // https://github.com/codesandbox/sandpack/blob/53811bb4fdfb66ea95b9881ff18c93307f12ce0d/sandpack-react/src/presets/Sandpack.tsx#L67
   return (
@@ -108,6 +133,25 @@ export default function SandpackExample({
           body, html, #root { height: 100%; }`,
           hidden: true,
         },
+        ...(localFiles
+          ? {
+              // More info at https://twitter.com/CompuIves/status/1466464916441903116
+              // Example: https://codesandbox.io/s/custom-library-in-sandpack-gq12p?file=/src/App.js:407-672
+              '/node_modules/gestalt/package.json': {
+                code: JSON.stringify({
+                  name: 'gestalt',
+                  main: './dist/gestalt.js',
+                  style: 'dist/gestalt.css',
+                }),
+              },
+              '/node_modules/gestalt/dist/gestalt.js': {
+                code: localJS,
+              },
+              '/node_modules/gestalt/dist/gestalt.css': {
+                code: localCSS,
+              },
+            }
+          : {}),
         '/App.js': {
           code,
         },
@@ -115,7 +159,7 @@ export default function SandpackExample({
       theme="dark"
       customSetup={{
         dependencies: {
-          gestalt: 'latest',
+          ...(localFiles ? { classnames: 'latest' } : { gestalt: 'latest' }),
           react: '18.2.0',
           'react-dom': '18.2.0',
         },
