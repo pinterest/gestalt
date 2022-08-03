a57211929a97a7e416abd248e0a65c4cef09348b
Ayesha <amazumdar@pinterest.com>
2022-08-03 11:32:06 -0700

    fix

--- playwright/accessibility/Divider.spec.mjs
+++ playwright/accessibility/Divider.spec.mjs
@@ -4,5 +4,11 @@ import expectAccessiblePage from './expectAccessiblePage.mjs';
 
 test('Divider Accessibility check', async ({ page }) => {
   await page.goto('/web/divider');
-  await expectAccessiblePage({ page });
+  await expectAccessiblePage({
+    page,
+    rules: {
+      // aria-label attribute cannot be used on a div with no valid role attribute.
+      'aria-allowed-attr': { enabled: false },
+    },
+  });
 });
