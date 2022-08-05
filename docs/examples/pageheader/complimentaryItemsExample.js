5d0af9cc43fa996dbb7562c2ffb71cc36654a24e
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2022-08-05 16:20:11 -0400

    PageHeader: improved Docs with examples and descriptions (#2262)

--- docs/examples/pageheader/complimentaryItemsExample.js
+++ docs/examples/pageheader/complimentaryItemsExample.js
@@ -1,54 +1,71 @@
 // @flow strict
 import React, { type Node } from 'react';
-import { Button, Datapoint, Dropdown, PageHeader } from 'gestalt';
+import { Button, Datapoint, Dropdown, PageHeader, Sheet, Text } from 'gestalt';
 
 export default function IncludeImageExample(): Node {
+  const [open, setOpen] = React.useState(false);
+
   return (
-    <PageHeader
-      title="Ads overview"
-      helperIconButton={{
-        accessibilityControls: '',
-        accessibilityExpanded: false,
-        accessibilityLabel: 'Read more information about Ads overview',
-        onClick: () => {},
-      }}
-      items={[
-        <Datapoint
-          key="impressions"
-          size="md"
-          title="Impressions"
-          value="$1.25M"
-          trend={{ value: 30, accessibilityLabel: 'Trending up' }}
-        />,
-        <Datapoint
-          key="engagement"
-          size="md"
-          title="Engagement"
-          value="10%"
-          trend={{ value: 5, accessibilityLabel: 'Trending up' }}
-        />,
-      ]}
-      primaryAction={{
-        component: <Button color="red" size="lg" text="Promote" />,
-        dropdownItems: [
-          <Dropdown.Item
-            option={{ value: 'Promote', label: 'Promote' }}
-            onSelect={() => {}}
-            key="promote"
+    <React.Fragment>
+      <PageHeader
+        title="Ads overview"
+        helperIconButton={{
+          accessibilityControls: '',
+          accessibilityExpanded: false,
+          accessibilityLabel: 'Read more information about Ads overview',
+          onClick: () => setOpen(true),
+        }}
+        items={[
+          <Datapoint
+            key="impressions"
+            size="md"
+            title="Impressions"
+            value="$1.25M"
+            trend={{ value: 30, accessibilityLabel: 'Trending up' }}
           />,
-        ],
-      }}
-      secondaryAction={{
-        component: <Button size="lg" text="View analytics" />,
-        dropdownItems: [
-          <Dropdown.Link
-            key="view-analytics"
-            option={{ value: 'View analytics', label: 'View analytics' }}
-            href="https://pinterest.com"
+          <Datapoint
+            key="engagement"
+            size="md"
+            title="Engagement"
+            value="10%"
+            trend={{ value: 5, accessibilityLabel: 'Trending up' }}
           />,
-        ],
-      }}
-      dropdownAccessibilityLabel="More options"
-    />
+        ]}
+        primaryAction={{
+          component: <Button color="red" size="lg" text="Promote" />,
+          dropdownItems: [
+            <Dropdown.Item
+              option={{ value: 'Promote', label: 'Promote' }}
+              onSelect={() => {}}
+              key="promote"
+            />,
+          ],
+        }}
+        secondaryAction={{
+          component: <Button size="lg" text="View analytics" />,
+          dropdownItems: [
+            <Dropdown.Link
+              key="view-analytics"
+              option={{ value: 'View analytics', label: 'View analytics' }}
+              href="https://pinterest.com"
+            />,
+          ],
+        }}
+        dropdownAccessibilityLabel="More options"
+      />
+      {open ? (
+        <Sheet
+          accessibilityDismissButtonLabel="Close"
+          accessibilitySheetLabel="Example sheet for demonstration"
+          heading="Guidance"
+          onDismiss={() => setOpen(false)}
+          size="md"
+        >
+          <Text>1</Text>
+          <Text>2</Text>
+          <Text>3</Text>
+        </Sheet>
+      ) : null}
+    </React.Fragment>
   );
 }
