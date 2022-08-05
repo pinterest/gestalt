49d8083e04a7828b963a4485babc92c2f0b9a153
Ryan James <rjames@pinterest.com>
2022-08-05 15:23:49 -0700

    get label too

--- packages/gestalt/src/Label.js
+++ packages/gestalt/src/Label.js
@@ -14,7 +14,7 @@ type Props = {|
 |};
 
 /**
- * Use the [Label](https://gestalt.pinterest.systems/labels) component to connect a label with a form component in an accessible way.
+ * Use the [Label](https://gestalt.pinterest.systems/web/label) component to connect a label with a form component in an accessible way.
  *
  * ![Label light mode](https://raw.githubusercontent.com/pinterest/gestalt/master/playwright/visual-test/Label.spec.mjs-snapshots/Label-chromium-darwin.png)
  * ![Label dark mode](https://raw.githubusercontent.com/pinterest/gestalt/master/playwright/visual-test/Label-dark.spec.mjs-snapshots/Label-dark-chromium-darwin.png)
