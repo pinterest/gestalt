9dd004830924fdc69963a8b840ed82c5d327db8b
Christian Vuerings <cvuerings@pinterest.com>
2022-08-17 13:50:08 +0200

    Internal: re-enable playwright visual tests

--- playwright/visual-test/Collage.spec.mjs
+++ playwright/visual-test/Collage.spec.mjs
@@ -2,7 +2,10 @@
 import { test, expect } from '@playwright/test';
 
 test('Collage visual regression check', async ({ page }) => {
-  await page.goto('/visual-test/Collage');
+  await page.goto('/visual-test/Collage', {
+    // Wait until all network requests have finished
+    waitUntil: 'networkidle',
+  });
   const locator = page.locator('[data-test-id="visual-test"]');
   await expect(locator).toHaveScreenshot('Collage.png');
 });
