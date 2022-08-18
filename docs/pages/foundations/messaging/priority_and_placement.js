2969f439bec5aedd0765a48f6b559b02fe2f2abb
ponori <50343812+ponori@users.noreply.github.com>
2022-08-18 12:28:17 -0700

    Added dos/donts to Messaging Priority page

--- docs/pages/foundations/messaging/priority_and_placement.js
+++ docs/pages/foundations/messaging/priority_and_placement.js
@@ -1,5 +1,5 @@
 // @flow strict
-import { Text, Table, Icon, Flex, Heading, Box, Link } from 'gestalt';
+import { Text, Table, Icon, Flex, Heading, Box, Link, Divider, Status } from 'gestalt';
 import { type Node } from 'react';
 import MainSection from '../../../docs-components/MainSection.js';
 import Page from '../../../docs-components/Page.js';
@@ -8,6 +8,13 @@ import Blocking from '../../../graphics/messaging/blocking.svg';
 import Emphemeral from '../../../graphics/messaging/ephemeral.svg';
 import Section from '../../../graphics/messaging/section.svg';
 import TopPage from '../../../graphics/messaging/topPage.svg';
+import DeleteBoard from '../../../graphics/messaging/modals/deleteBoard.svg';
+import WebsiteBlocked from '../../../graphics/messaging/modals/websiteBlocked.svg';
+import SiteSpam from '../../../graphics/messaging/modals/siteSpam.svg';
+import ExperienceTrends from '../../../graphics/messaging/modals/experienceTrends.svg';
+import PinSaved from '../../../graphics/messaging/modals/pinSaved.svg';
+import NewRecommendations from '../../../graphics/messaging/modals/newRecommendations.svg';
+import CampaignIssue from '../../../graphics/messaging/modals/campaignIssue.svg';
 
 export default function MessagingPriorityAndPlacementPage(): Node {
   return (
@@ -16,36 +23,38 @@ export default function MessagingPriorityAndPlacementPage(): Node {
       <MainSection name="Priority">
         <Text>In order to avoid too many messages stacked on the screen, we prioritize which messages to send first based on the severity of the issue. </Text>
         <MainSection.Subsection title="What goes first">
-          <Table>
-            <Table.Header>
-              <Table.Row>
-                <Table.Cell><Text weight='bold'>Priority</Text></Table.Cell>
-                <Table.Cell><Text weight='bold'>Status type</Text></Table.Cell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell><Text>Highest</Text></Table.Cell>
-                <Table.Cell><Flex gap={2}><Icon icon="workflow-status-problem" color='error' /><Text>Error</Text></Flex></Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell><Text>High</Text></Table.Cell>
-                <Table.Cell><Flex gap={2}><Icon icon="workflow-status-warning" color='warning' /><Text>Warning</Text></Flex></Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell><Text>Medium</Text></Table.Cell>
-                <Table.Cell><Flex gap={2}><Icon icon="workflow-status-ok" color='success' /><Text>Success</Text></Flex></Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell><Text>Low</Text></Table.Cell>
-                <Table.Cell><Flex gap={2}><Icon icon="sparkle" color='recommendation' /><Text>Recommendations</Text></Flex></Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell><Text>Lowest</Text></Table.Cell>
-                <Table.Cell><Flex gap={2}><Icon icon="info-circle" color='info' /><Text>Info</Text></Flex></Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          </Table>
+          <Box maxWidth={400}>
+            <Table>
+              <Table.Header>
+                <Table.Row>
+                  <Table.Cell><Text weight='bold'>Priority</Text></Table.Cell>
+                  <Table.Cell><Text weight='bold'>Status type</Text></Table.Cell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell><Text>Highest</Text></Table.Cell>
+                  <Table.Cell><Flex gap={2}><Icon icon="workflow-status-problem" color='error' /><Text>Error</Text></Flex></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell><Text>High</Text></Table.Cell>
+                  <Table.Cell><Flex gap={2}><Icon icon="workflow-status-warning" color='warning' /><Text>Warning</Text></Flex></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell><Text>Medium</Text></Table.Cell>
+                  <Table.Cell><Flex gap={2}><Icon icon="workflow-status-ok" color='success' /><Text>Success</Text></Flex></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell><Text>Low</Text></Table.Cell>
+                  <Table.Cell><Flex gap={2}><Icon icon="sparkle" color='recommendation' /><Text>Recommendations</Text></Flex></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell><Text>Lowest</Text></Table.Cell>
+                  <Table.Cell><Flex gap={2}><Icon icon="info-circle" color='info' /><Text>Info</Text></Flex></Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
+          </Box>
         </MainSection.Subsection>
 
       </MainSection>
@@ -57,15 +66,35 @@ export default function MessagingPriorityAndPlacementPage(): Node {
 
           <Heading size={300} accessibilityLevel={4}>Current components: Modal</Heading>
           <Text>Blocking content slows a person down from their usual flow, so here are some guidelines on when it’s okay to block content.</Text>
-
-          <Box color='successWeak'>
-            <Flex direction='column'>
-              <Flex><Box><Text>To confirm that someone wants to peform a destructive//irreversible action</Text></Box><Box>foo</Box></Flex>
-              <Flex><Box><Text>When an issue is so severe that it’s hard to continue with a good experience</Text></Box><Box>foo</Box></Flex>
-            </Flex>
+          <Box>
+          <Flex alignItems='center'><Box padding={4} width="50%"><Flex gap={1} alignItems='center'><Status type='ok' /> <Text weight='bold'>When to use</Text></Flex></Box><Box padding={4} width="50%"><Text weight='bold'>Example wireframe</Text></Box></Flex>
+            <Box color='successWeak'>
+              <Flex direction='column'>
+                <Box color='successBase' height={4} width="100%" />
+                <Flex alignItems='center'><Box padding={4} width="50%"><Text>To confirm that someone wants to peform a destructive/irreversible action</Text></Box><Box padding={4} width="50%"><DeleteBoard /></Box></Flex>
+                <Divider />
+                <Flex alignItems='center'><Box padding={4} width="50%"><Text>When an issue is so severe that it’s hard to continue with a good experience</Text></Box><Box padding={4} width="50%"><WebsiteBlocked /></Box></Flex>
+                <Divider />
+                <Flex alignItems='center'><Box padding={4} width="50%"><Text>To warn a person of something that can have serious consequences before they continue</Text></Box><Box padding={4} width="50%"><SiteSpam /></Box></Flex>
+                <Divider />
+                <Flex alignItems='center'><Box padding={4} width="50%"><Text>As an interstitial that leads to a much better experience we think a person shouldn’t miss</Text></Box><Box padding={4} width="50%"><ExperienceTrends /></Box></Flex>
+              </Flex>
+            </Box>
           </Box>
 
-          <Heading size={300} accessibilityLevel={4}>Foo</Heading>
+          <Box>
+            <Flex alignItems='center'><Box padding={4} width="50%"><Flex gap={1} alignItems='center'><Status type='problem' /> <Text weight='bold'>When not to use</Text></Flex></Box><Box padding={4} width="50%"><Text weight='bold'>Example wireframe</Text></Box></Flex>
+            <Box color='errorWeak'>
+              <Flex direction='column'>
+                <Box color='errorBase' height={4} width="100%" />
+                <Flex alignItems='center'><Box padding={4} width="50%"><Text>To acknowledge completion of a task that doesn’t require follow up, like pinning to a board. Use <Link href='/web/toast' underlined inline>Toast</Link> instead.</Text></Box><Box padding={4} width="50%"><PinSaved /></Box></Flex>
+                <Divider />
+                <Flex alignItems='center'><Box padding={4} width="50%"><Text>To provide general information and recommendations. Use <Link href='/web/callout' underlined inline>Callout</Link> instead.</Text></Box><Box padding={4} width="50%"><NewRecommendations /></Box></Flex>
+                <Divider />
+                <Flex alignItems='center'><Box padding={4} width="50%"><Text>To show an error or warning that applies to a section of a page, or that doesn’t severely limit the user experience.</Text></Box><Box padding={4} width="50%"><CampaignIssue /></Box></Flex>
+              </Flex>
+            </Box>
+          </Box>
         </MainSection.Subsection>
         <MainSection.Subsection title="Not-blocking">
           <Heading size={300} accessibilityLevel={4}>Ephemeral—less prominent</Heading>
