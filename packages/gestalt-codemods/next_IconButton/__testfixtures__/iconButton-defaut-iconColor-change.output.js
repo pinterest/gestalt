f39945252ebea9bae4d45cbd555b28af41a818ce
Alberto Carreras Carrasco <acarreras@pinterest.com>
2022-08-18 17:03:47 +0200

    codemod

---
+++ packages/gestalt-codemods/next_IconButton/__testfixtures__/iconButton-defaut-iconColor-change.output.js
@@ -0,0 +1,29 @@
+// @flow strict
+import { Box, IconButton } from 'gestalt';
+
+export default function TestComponent() {
+  return (
+    <Box>
+      <IconButton
+        bgColor="darkGray"
+        accessibilityLabel="test 1"
+        icon="directional-arrow-right"
+      />
+      <IconButton
+        bgColor="transparent"
+        iconColor="red"
+        accessibilityLabel="test 1"
+        icon="directional-arrow-right"
+      />
+      <IconButton
+        bgColor="transparent"
+        accessibilityLabel="test 1"
+        icon="directional-arrow-right"
+        iconColor="gray" />
+      <IconButton
+        accessibilityLabel="test 1"
+        icon="directional-arrow-right"
+        iconColor="gray" />
+    </Box>
+  );
+}
