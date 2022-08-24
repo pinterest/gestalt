3a68994011a2260096b3acedfe35286656cbd1ef
Ayesha <amazumdar@pinterest.com>
2022-08-24 23:24:23 +0100

    Popover: Improve a11y

--- playwright/accessibility/Popover.spec.mjs
+++ playwright/accessibility/Popover.spec.mjs
@@ -6,8 +6,5 @@ test('Popover Accessibility check', async ({ page }) => {
   await page.goto('/web/popover');
   await expectAccessiblePage({
     page,
-    rules: {
-      region: { enabled: false },
-    },
   });
 });
