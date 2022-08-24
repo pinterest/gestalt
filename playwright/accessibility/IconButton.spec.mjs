811c5d878ec0acebdb171b4c72c9d633194c4cf3
ponori <50343812+ponori@users.noreply.github.com>
2022-08-24 11:25:07 -0700

    Added a11y tests for IconButton

--- playwright/accessibility/IconButton.spec.mjs
+++ playwright/accessibility/IconButton.spec.mjs
@@ -6,3 +6,13 @@ test('IconButton Accessibility check', async ({ page }) => {
   await page.goto('/web/iconbutton');
   await expectAccessiblePage({ page });
 });
+
+test('Android IconButton Accessibility check', async ({ page }) => {
+  await page.goto('/android/iconbutton');
+  await expectAccessiblePage({ page });
+});
+
+test('iOS IconButton Accessibility check', async ({ page }) => {
+  await page.goto('/ios/iconbutton');
+  await expectAccessiblePage({ page });
+});
