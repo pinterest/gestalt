68b6e9d2a2e35b58019dfbdc09094e45f0545cba
Ryan James <dangerismycat@users.noreply.github.com>
2022-08-24 12:04:24 -0700

    Masonry: rename `comp` to `Item` (#2325)

--- packages/gestalt/src/Masonry.flowtest.js
+++ packages/gestalt/src/Masonry.flowtest.js
@@ -4,7 +4,7 @@ import Masonry from './Masonry.js';
 function Item() {
   return <div />;
 }
-const Valid = <Masonry items={[]} comp={Item} />;
+const Valid = <Masonry items={[]} Item={Item} />;
 
 // $FlowExpectedError[prop-missing]
 const MissingProp = <Masonry />;
