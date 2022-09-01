d9d27502c4fde620af0d3d5f2de3e9bd39f4a46c
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2022-09-01 13:53:13 -0400

    Avatar: implement Sandpack code editors (#2353)

---
+++ docs/examples/avatar/ideasExample.js
@@ -0,0 +1,14 @@
+// @flow strict
+import { type Node } from 'react';
+import { Flex, Avatar, Text } from 'gestalt';
+
+export default function Example(): Node {
+  return (
+    <Flex height="100%" width="100%" alignItems="center" justifyContent="center">
+      <Flex direction="column" gap={{ column: 2, row: 0 }} alignItems="center">
+        <Avatar size="xl" src="https://i.ibb.co/jVR29XV/stock5.jpg" name="Artwork" />
+        <Text weight="bold">Explore Typographic Art</Text>
+      </Flex>
+    </Flex>
+  );
+}
---
+++ docs/examples/avatar/noImageSourceExample.js
@@ -0,0 +1,11 @@
+// @flow strict
+import { type Node } from 'react';
+import { Avatar, Flex } from 'gestalt';
+
+export default function Example(): Node {
+  return (
+    <Flex height="100%" width="100%" alignItems="center" justifyContent="center">
+      <Avatar size="lg" name="Keerthi" />
+    </Flex>
+  );
+}
