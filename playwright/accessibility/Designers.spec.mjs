d6dbc47e23af3bbeaf509ae20653b6c36d2e9d2f
Hector Jaime <hjaime@pinterest.com>
2022-08-31 17:12:10 -0700

    updated await
    Updated URL in playwright/accessibility file to "designers".

--- playwright/accessibility/Designers.spec.mjs
+++ playwright/accessibility/Designers.spec.mjs
@@ -3,6 +3,6 @@ import { test } from '@playwright/test';
 import expectAccessiblePage from './expectAccessiblePage.mjs';
 
 test('Design Accessibility check', async ({ page }) => {
-  await page.goto('/get_started/design');
+  await page.goto('/get_started/designers');
   await expectAccessiblePage({ page });
 });
