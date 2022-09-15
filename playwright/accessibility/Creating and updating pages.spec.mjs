6228202d7a6a2e66d9abe745366a5b3d3058e335
Ayesha <amazumdar@pinterest.com>
2022-09-14 18:38:06 -0700

    add test

--- playwright/accessibility/Creating and updating pages.spec.mjs
+++ playwright/accessibility/Creating and updating pages.spec.mjs
@@ -3,6 +3,8 @@ import { test } from '@playwright/test';
 import expectAccessiblePage from './expectAccessiblePage.mjs';
 
 test('Development Accessibility check', async ({ page }) => {
-  await page.goto('/get_started/developers/contributing/development_process');
+  await page.goto(
+    '/get_started/developers/contributing/creating_and_updating_pages'
+  );
   await expectAccessiblePage({ page });
 });
