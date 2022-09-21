5608f8285c3a6c13b7090035d9b33d5d75f3d0ad
Ryan James <rjames@pinterest.com>
2022-09-21 15:47:52 -0700

    add link to TS package

--- docs/pages/get_started/faq.js
+++ docs/pages/get_started/faq.js
@@ -104,6 +104,17 @@ $ElementType<React$ElementConfig<typeof ComponentName>, 'propName'>
 ~~~"
           />
 
+          <Heading size="400">Does Gestalt have TypeScript support?</Heading>
+          <Text>
+            Not officially. However, a group of dedicated engineers who work on internal tools at
+            Pinterest created
+            <InlineLink href="https://www.npmjs.com/package/@types/gestalt">
+              this package
+            </InlineLink>
+            with TypeScript definitions. We hope to offer better official TypeScript support in the
+            future, but currently lack the resources for proper support.
+          </Text>
+
           <Heading size="400">What&apos;s required to support IE11?</Heading>
           <Text>
             Gestalt supports IE11 currently, but you will need to use a polyfill because the css
