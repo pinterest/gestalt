6ef8516882df22b15a3a440c95f9daefb1d5f626
ponori <50343812+ponori@users.noreply.github.com>
2022-10-04 14:05:02 -0700

    Docs: Updated main Link example (#2422)

--- docs/pages/web/link.js
+++ docs/pages/web/link.js
@@ -17,8 +17,8 @@ export default function DocsPage({ generatedDocGen }: {| generatedDocGen: DocGen
         description={generatedDocGen?.description}
         defaultCode={`
 <Text inline>
-  Find tips and best practices on the
-  <Link href="https://business.pinterest.com/" inline> Pinterest Business Site </Link>
+To see how you can grow your business, visit
+  <Link href="https://business.pinterest.com/advertise" inline externalLinkIcon="default" target="blank">Pinterest Ads</Link>
 </Text>
         `}
       />
