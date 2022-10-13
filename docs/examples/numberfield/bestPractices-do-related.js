28ae8ad1736bc858b0e7d7ba4a92311fee60d1fb
Ryan James <rjames@pinterest.com>
2022-10-12 17:18:22 -0700

    padding for related examples

--- docs/examples/numberfield/bestPractices-do-related.js
+++ docs/examples/numberfield/bestPractices-do-related.js
@@ -1,29 +1,31 @@
 // @flow strict
 import { useState } from 'react';
-import { Flex, NumberField } from 'gestalt';
+import { Box, Flex, NumberField } from 'gestalt';
 
 export default function Example(): React$Node {
   const [firstValue, setFirstValue] = useState();
   const [secondValue, setSecondValue] = useState();
 
   return (
-    <Flex alignItems="center" gap={4} justifyContent="center" height="100%" width="100%">
-      <NumberField
-        id="best-practices-do-related-first"
-        label="First value"
-        onChange={({ value }) => {
-          setFirstValue(value);
-        }}
-        value={firstValue}
-      />
-      <NumberField
-        id="best-practices-do-related-second"
-        label="Second value"
-        onChange={({ value }) => {
-          setSecondValue(value);
-        }}
-        value={secondValue}
-      />
-    </Flex>
+    <Box height="100%" padding={3}>
+      <Flex alignItems="center" gap={4} justifyContent="center" height="100%" width="100%">
+        <NumberField
+          id="best-practices-do-related-first"
+          label="First value"
+          onChange={({ value }) => {
+            setFirstValue(value);
+          }}
+          value={firstValue}
+        />
+        <NumberField
+          id="best-practices-do-related-second"
+          label="Second value"
+          onChange={({ value }) => {
+            setSecondValue(value);
+          }}
+          value={secondValue}
+        />
+      </Flex>
+    </Box>
   );
 }
