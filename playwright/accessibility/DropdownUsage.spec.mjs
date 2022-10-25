10fc144364be85e9a0210556404f001542c1250e
Eduardo Costa <ecosta@pinterest.com>
2022-10-25 21:42:34 +0000

    Add playwrite tests

--- playwright/accessibility/DropdownUsage.spec.mjs
+++ playwright/accessibility/DropdownUsage.spec.mjs
@@ -3,6 +3,6 @@ import { test } from '@playwright/test';
 import expectAccessiblePage from './expectAccessiblePage.mjs';
 
 test('Dropdown Accessibility check', async ({ page }) => {
-  await page.goto('/web/dropdown');
+  await page.goto('/web/dropdown/usage');
   await expectAccessiblePage({ page });
 });
