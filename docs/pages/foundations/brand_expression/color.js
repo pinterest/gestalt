dad50e9a5ed1efcd3b14b8be410282cf683eb9eb
ponori <50343812+ponori@users.noreply.github.com>
2022-11-15 11:10:03 -0800

    Content blocked stubbed out

--- docs/pages/foundations/brand_expression/color.js
+++ docs/pages/foundations/brand_expression/color.js
@@ -1,16 +1,149 @@
 // @flow strict
-import { Text, Flex, Box, Link } from 'gestalt';
+import { Text, Flex, Box, Link, Image, Heading } from 'gestalt';
 import { type Node } from 'react';
 import MainSection from '../../../docs-components/MainSection.js';
 import Page from '../../../docs-components/Page.js';
 import PageHeader from '../../../docs-components/PageHeader.js';
+import Markdown from '../../../docs-components/Markdown.js';
 
 export default function MessagingComponentsPage(): Node {
   return (
     <Page title="Color">
       <PageHeader badge="pilot" name="Color" type="guidelines" />
-      <MainSection name="We need a title here">Foo</MainSection>
-      <MainSection name="Accessibility">Foo</MainSection>
+      <Markdown
+        text={`Pinterest product UI should be comprised of our hero and neutrals colors. We should continue to use our baseline color palette to allow photography, actions, and content to stand out. However, there are situations where color fills can help to express brand moments on product surfaces.
+
+We recommend using the extended color palette to bring attention to a specific brand or marketing moment, such as celebratory news, a new feature announcement, or educational flows. However, be mindful of color choices when applying color fills to UI elements next to status messages, as they use semantic colors to convey a specific meaning. See background color usage for reference.
+
+The Core brand team created a baseline brand palette with some of our extended colors; you can enable it as a library in your Figma file.`}
+      />
+      <Image src="https://i.ibb.co/7bQQYkX/stock2.jpg" />
+      <Text>Brand color swatches available in Figma (Baseline brand colors library)</Text>
+      <Text>
+        See below an example of brand expression in the product UI using color fill combined with
+        other visual assets.{' '}
+      </Text>
+      <Flex>
+        <Flex.Item>Image 1</Flex.Item>
+        <Flex.Item>Image 2</Flex.Item>
+        <Flex.Item>Image 3</Flex.Item>
+      </Flex>
+      <Text>Idea Pin tips - Using color fills to bring brand expression </Text>
+      <Text>
+        Reach out to Core Brand for photography guidance, imagery and other types of visual assets.{' '}
+      </Text>
+      <MainSection.Subsection title="Color fills and dark-mode">
+        <Markdown
+          text={`Aim for sufficient color contrast in all appearances, no matter the color theme (light or dark mode). Our colors were defined to support you in achieving a reasonable contrast ratio between your foreground and background content in both appearances. At a minimum, ensure the contrast ratio between colors is no lower than 4.5:1, as this ratio ensures that your foreground content stands out from the background and helps your content meet recommended WCAG 2.1 AA accessibility standards.
+
+We suggest checking Brand color expression + accessibility for references on color combination considering both themes. You can also use color pairing Figma plugin to test your color contrast level.
+When using images or icons over color fills, ensure they look good in both themes. For icons, use the appropriate color tokens for each theme to provide readability and a great usability experience. For images, we suggest using the same asset if it looks good in both light and dark appearances. Consider softening the image treatment to avoid eye strain or to improve visibility; use your best judgment.
+
+For more information about colors, check out our color documentation.
+`}
+        />
+      </MainSection.Subsection>
+      <MainSection.Subsection title="Color fills and typography">
+        <Heading>Headings</Heading>
+        <Text>
+          Avoid using extended colors to text elements, as changing colors could lead to usability
+          and cognitive issues due to their semantic meaning and required contrast level. Refer to
+          our text color tokens for guidance.
+        </Text>
+        <Heading>Body</Heading>
+        <Text>
+          <p>
+            Body should always be $color-text-default (default color), $color-text-inverse (text
+            over a dark-colored background), or $color-text-subtle (secondary color), with the
+            exception of status text colors, as they are needed to convey a certain meaning for
+            specific cases.
+          </p>
+          <p>
+            Avoid using alternative colors on body content, which could lead to accessibility
+            issues.{' '}
+          </p>
+          <p>
+            Please note: It only applies to product UI elements. For marketing websites, materials
+            or promotional landing pages reach out to Core Brand for text color guidance.
+          </p>
+          <p>
+            See below examples of UI elements using the appropriate text colors over color fills.
+          </p>
+        </Text>
+        <Flex>
+          <Flex.Item>
+            <Image src="https://i.ibb.co/7bQQYkX/stock2.jpg" />
+            <Text>Matchacado-green-500 + $color-text-inverse</Text>
+          </Flex.Item>
+          <Flex.Item>
+            <Image src="https://i.ibb.co/7bQQYkX/stock2.jpg" />
+            <Text>Firetine-orange-100 + $color-text-default</Text>
+          </Flex.Item>
+          <Flex.Item>
+            <Image src="https://i.ibb.co/7bQQYkX/stock2.jpg" />
+            <Text>Spabattical-teal-700 + $color-text-inverse</Text>
+          </Flex.Item>
+          <Flex.Item>
+            <Image src="https://i.ibb.co/7bQQYkX/stock2.jpg" />
+            <Text>Spabattical-teal-100 + $color-text-default</Text>
+          </Flex.Item>
+        </Flex>
+      </MainSection.Subsection>
+      <MainSection.Subsection title="Color fills and iconography">
+        <Text>
+          <p>
+            When playing with color fills, we recommend using a tool like aremycolorsaccessible.com
+            to check the foreground color against the background color. In Figma, you can use the
+            Able or Pinterest brand color palettes plugins to check color contrast in your designs.
+          </p>
+          <p>
+            Section 508, which aligns with WCAG 2.0 Level AA, establishes a legal standard for the
+            contrast level essential between text and its background. The baseline AA contrast
+            standard is 4.5:1 for most text and 3:1 for large text.{' '}
+          </p>
+          <p>
+            Make sure your color fills have great contrast in our light and dark mode color themes.
+          </p>
+          <p>
+            Don’t use color exclusively to convey meaning. Color should only be used as an
+            enhancement — if color is the only cue, that cue won’t get through as intended to
+            everyone.{' '}
+          </p>
+        </Text>
+      </MainSection.Subsection>
+      <MainSection name="Accessibility">
+        <MainSection.Subsection>
+          <Text>
+            <p>
+              Gestalt icons should always use established colors, as our color props are
+              semantically aligned with our iconography principles.
+            </p>
+            <p>
+              If colors are needed to express a brand moment on the product, consider adding a
+              colored background to the icon instead of adding alternative colors to the icon
+              component. Ensure it meets the WCAG 2.1 AA accessibility standard with the proper
+              contrast between foreground and background. See the examples below.
+            </p>
+          </Text>
+        </MainSection.Subsection>
+        <MainSection.Subsection title="Extended color palette">
+          <Text>
+            <p>
+              The extended color palette displays all the available shades and tints of each color
+              in the palette. The colors are named and numbered for easy reference. The usage of
+              these colors come in handy for brand moments. In order to ensure accessible contrast
+              for color pairings, we require our darkGray Text color to be used for any colors 400
+              and below. For 500 and above, we recommend using white. Learn more by checking our
+              color palette documentation.
+            </p>
+            <p>
+              The 450 colors are primarily reserved for Brand usage as they are among the least
+              accessible colors. This set works best within larger brand moments, and is not
+              commonly used for functional color pairings.
+            </p>
+          </Text>
+        </MainSection.Subsection>
+      </MainSection>
     </Page>
   );
 }
--- docs/pages/foundations/brand_expression/guidelines.js
+++ docs/pages/foundations/brand_expression/guidelines.js
@@ -1,10 +1,10 @@
 // @flow strict
-import { Text, Flex, Box, Link } from 'gestalt';
+import { Text, Flex, Link, Heading } from 'gestalt';
 import { type Node } from 'react';
 import MainSection from '../../../docs-components/MainSection.js';
 import Page from '../../../docs-components/Page.js';
 import PageHeader from '../../../docs-components/PageHeader.js';
-import Markdown from '../../../docs-components/Markdown';
+import Markdown from '../../../docs-components/Markdown.js';
 
 export default function MessagingComponentsPage(): Node {
   return (
@@ -15,7 +15,7 @@ export default function MessagingComponentsPage(): Node {
 
 The guidelines below aim to guide you on how to incorporate brand expression into the product, adding unique value to Pinners. They aren't intended to determine what specific surfaces must look like; instead, it is meant to illustrate the principles and general usage of brand expressions respecting usability and accessibility best practices.
 
-These guidelines will broadly align with the brand standards. Still, they will follow our design principles, and there are areas where they may diverge as our focus is brand expression specific to product UI rather than marketing materials and campaigns.
+These guidelines will broadly align with the brand standards. Still, they will follow [our design principles](/get_started/about_us#Our-design-principles), and there are areas where they may diverge as our focus is brand expression specific to product UI rather than marketing materials and campaigns.
 We suggest continuously checking for comprehension and usability when bringing brand moments to the product.
 
 We suggest continuously checking for comprehension and usability when bringing brand moments to the product.  `}
@@ -27,7 +27,11 @@ We suggest continuously checking for comprehension and usability when bringing b
             type="do"
             title="When to use"
             description={`
-          - To reflect a person, company or brand within the product.
+          - Onboarding flows
+          - Celebratory moments (first launching the app, first boards, reaching a certain amount of followers, special milestones)
+          - In-product marketing moments
+          - Ads campaigns, challenges or trends
+          - Launch or login screens
         `}
           />
           <MainSection.Card
@@ -35,30 +39,169 @@ We suggest continuously checking for comprehension and usability when bringing b
             type="don't"
             title="When not to use"
             description={`
-          - To represent a group of people, companies and/or brands. Use [AvatarGroup](/web/avatargroup) instead.
+          - Performing critical tasks or blocking users to complete a task
+          - Competing with content and Pins
+          - Modifying product UI patterns or Gestalt components
+          - Causing comprehension or accessibility issues
+          - Without a purpose as a decorative element
         `}
           />
         </MainSection.Subsection>
+        <MainSection.Subsection>
+          <Text>
+            Be mindful of when and where to bring brand expressions into your design. If color
+            fills, imagery and other brand elements are needed to reinforce brand touch-points
+            within your design, then go for it; if not, aim to follow our{' '}
+            <Link inline href="/foundations/overview">
+              foundation guidelines
+            </Link>
+            . A consistent design allows Pinterest content to shine while ensuring a great usability
+            experience.
+          </Text>
+          <Text>
+            Below we list the UI elements you can use and the ones you should avoid when bringing
+            brand expression within your product UI.
+          </Text>
+        </MainSection.Subsection>
+        <MainSection.Subsection title="UI elements and Brand expression">
+          <Flex>
+            <Flex.Item>
+              <Heading color="success" size="200">
+                UI elements to bring brand expression
+              </Heading>
+              <Text>
+                <ul>
+                  <li>Product upsells</li>
+                  <li>Hero banners</li>
+                  <li>Cards</li>
+                  <li>Carousels</li>
+                  <li>Modals/Sheets</li>
+                </ul>
+              </Text>
+            </Flex.Item>
+            <Flex.Item>
+              <Heading color="subtle" size="200">
+                Consider brand expression
+              </Heading>
+              <Text>
+                <ul>
+                  <li>Dashboards(when applicable and it doesn't break a component)</li>
+                </ul>
+              </Text>
+            </Flex.Item>
+            <Flex.Item>
+              <Heading color="error" size="200">
+                Elements to avoid brand expression
+              </Heading>
+              <Text>
+                <ul>
+                  <li>Core UI patterns</li>
+                  <li>Text components</li>
+                  <li>CTAs and other action components </li>
+                  <li>Tables</li>
+                  <li>Form components </li>
+                </ul>
+              </Text>
+            </Flex.Item>
+          </Flex>
+        </MainSection.Subsection>
       </MainSection>
-      <MainSection name="Best practices">Foo</MainSection>
-      <MainSection name="Accessibility">Foo</MainSection>
-      <MainSection name="Localization">Foo</MainSection>
-      <MainSection name="Voice and tone">Foo</MainSection>
+      <MainSection name="Best practices">
+        <MainSection.Subsection columns={2}>
+          <MainSection.Card
+            type="do"
+            description="Use color fills (brand baseline colors) to emphasize brand moments if appropriate to create a delightful Pinner experience (e.g., feature improvement announcement, in-product marketing moments). Ensure to check color contrast achieving [WCAG 2.1 AA accessibility standards](https://www.w3.org/TR/WCAG21/)."
+          />
+          <MainSection.Card
+            type="don't"
+            description="Use alternative colors to modify components colors or UI patterns. Color should be applied purposefully as it can convey meaning in multiple ways."
+          />
+          <MainSection.Card
+            type="do"
+            description="Use color fills in celebratory moments, such as telling good news or highlighting Pinner's achievements."
+          />
+          <MainSection.Card
+            type="don't"
+            description="Apply text over photography as it could lead to contrast issues and make it inaccessible through assistive devices."
+          />
+          <MainSection.Card
+            type="do"
+            description="Apply text over color fills using the appropriate text [color tokens](/foundations/design_tokens#Text-color). It will ensure an accessible, readable experience."
+          />
+          <MainSection.Card
+            type="don't"
+            description="Use alternative colors on text content and icons as it could lead to major contrast accessibility and usability issues. **This rule doesn't cover marketing materials, and promotional landing pages.**"
+          />
+          <MainSection.Card
+            type="do"
+            description="Use color fills when announcing new features (e.g., new way to search for hairstyle ideas)."
+          />
+          <MainSection.Card
+            type="don't"
+            description="Apply color fills on core UI patterns and critical tasks actions. It can distract Pinners from completing a task."
+          />
+        </MainSection.Subsection>
+      </MainSection>
+      <MainSection name="Accessibility">
+        <Text>
+          <p>
+            Be mindful of color usage. Pinterest's goal as a company is to meet{' '}
+            <Link inline href="https://www.w3.org/TR/WCAG21/">
+              WCAG 2.1 AA standards
+            </Link>
+            , so we recommend using a tool like{' '}
+            <Link inline href="https://www.aremycolorsaccessible.com/">
+              aremycolorsaccessible.com
+            </Link>{' '}
+            to check the foreground color against the background color. In Figma, you can use the{' '}
+            <Link
+              inline
+              href="https://www.figma.com/community/plugin/734693888346260052/Able-%E2%80%93-Friction-free-accessibility"
+            >
+              Able
+            </Link>{' '}
+            or{' '}
+            <Link
+              inline
+              href="https://www.figma.com/community/plugin/1019681360638128106/Pinterest-Brand-color-palettes"
+            >
+              Pinterest brand color palettes
+            </Link>{' '}
+            plugins to check color contrast in your designs.
+          </p>
+          <p>
+            When adding photography or any imagery source, ensure it translates well in both themes
+            (light and dark-mode). Your image source should be equally visible in both themes,
+            especially when you have colored backgrounds contrasting with a photo or an
+            illustration.
+          </p>
+          <p>
+            Ensure to add ALT text to your image, so users who rely on assistive technologies can
+            access your image content.
+          </p>
+        </Text>
+      </MainSection>
+      <MainSection name="Localization">
+        <Text>
+          Make sure to localize in-product marketing assets considering all the languages and
+          cultures.
+        </Text>
+      </MainSection>
+      <MainSection name="Voice and tone">
+        <Text>Check out our brand voice guidelines.</Text>
+      </MainSection>
       <MainSection name="Related">
         <MainSection.Subsection
           description={`
-**[Popover](/web/popover)**
-A popover can be formatted to provide help and guidance around new features, or features a user may not be aware of.
+**[Accessibility](/web/popover)**
 
-**[Upsell](/web/upsell)**
-Upsells are on-page banners that encourage more ad spend and upgrades. They can also be used to promote and market new features.
 
-**Error message variants**
-Variants for other non-messaging components that provide a way to show an error message (simple error status indicators are not included)..
+**[Color](/web/upsell)**
 
-- **[Text field error message](/web/textfield#Error-message)**
-- **[RadioGroup error message](/web/radiogroup#With-an-error)**
-- **[Checkbox error message](/web/checkbox#Error-message)**
+
+**[Design tokens](/web/upsell)**
+
+
       `}
         />
       </MainSection>
