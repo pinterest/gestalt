2af069f5806d90d3f686285b0b3d9e70c3d5e326
ponori <50343812+ponori@users.noreply.github.com>
2022-11-15 14:44:18 -0800

    Added a11y test for color page

---
+++ playwright/accessibility/Color_Brand expression.mjs
@@ -0,0 +1,8 @@
+// @flow strict
+import { test } from '@playwright/test';
+import expectAccessiblePage from './expectAccessiblePage.mjs';
+
+test('Brand expression guidelines check', async ({ page }) => {
+  await page.goto('/foundations/brand_expression/color');
+  await expectAccessiblePage({ page });
+});
