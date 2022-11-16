71fa2e28c911a3be3b18f729585a71153709097e
ponori <50343812+ponori@users.noreply.github.com>
2022-11-15 21:09:03 -0800

    Wrestling image layouts

--- docs/pages/foundations/brand_expression/color.js
+++ docs/pages/foundations/brand_expression/color.js
@@ -1,5 +1,5 @@
 // @flow strict
-import { Text, Flex, Image, Heading } from 'gestalt';
+import { Text, Flex, Image, Heading, Box } from 'gestalt';
 import { type Node } from 'react';
 import MainSection from '../../../docs-components/MainSection.js';
 import Page from '../../../docs-components/Page.js';
@@ -57,92 +57,134 @@ For more information about colors, check out our color documentation.
         <Heading size="300" accessibilityLevel={4}>
           Headings
         </Heading>
-        <Markdown text="Avoid using extended colors to text elements, as changing colors could lead to usability and cognitive issues due to their semantic meaning and required contrast level. Refer to our text color tokens for guidance." />
+        <Markdown text="Avoid using extended colors to text elements, as changing colors could lead to usability and cognitive issues due to their semantic meaning and required contrast level. Refer to our [text color tokens](/foundations/design_tokens#Text-color) for guidance." />
         <Heading size="300" accessibilityLevel={4}>
           Body
         </Heading>
         <Markdown
-          text={`Body should always be $color-text-default (default color), $color-text-inverse (text over a dark-colored background), or $color-text-subtle (secondary color), with the exception of status text colors, as they are needed to convey a certain meaning for specific cases.
+          text={`Body should always be $color-text-default (default color), $color-text-inverse (text over a dark-colored background), or $color-text-subtle (secondary color), with the exception of [status text colors](/foundations/color/usage#Background-color), as they are needed to convey a certain meaning for specific cases. Avoid using alternative colors on body content, which could lead to [accessibility](/foundations/accessibility#Visuals) issues.
 
-Avoid using alternative colors on body content, which could lead to accessibility issues.
-
-Please note: It only applies to product UI elements. For marketing websites, materials or promotional landing pages reach out to Core Brand for text color guidance.
+**Please note:** It only applies to product UI elements. For marketing websites, materials or promotional landing pages [reach out to Core Brand](https://brand.pinterest.com/) for text color guidance.
 
 See below examples of UI elements using the appropriate text colors over color fills.`}
         />
         <Flex gap={12} direction="column">
           <Flex gap={12} wrap alignItems="end">
             <Flex.Item minWidth={320} flex="grow">
-              <Image
-                src="https://i.pinimg.com/originals/6e/f3/5c/6ef35ce8021c2aa5502c18f50f2ef966.png"
-                naturalHeight={480}
-                naturalWidth={718}
-                alt=""
-              />
-              <Text align="center" size="100">
-                Matchacado-green-500 + $color-text-inverse
-              </Text>
+              <Flex direction="column" gap={4}>
+                <Image
+                  src="https://i.pinimg.com/originals/6e/f3/5c/6ef35ce8021c2aa5502c18f50f2ef966.png"
+                  naturalHeight={480}
+                  naturalWidth={718}
+                  alt=""
+                />
+                <Text align="center" size="100">
+                  Matchacado-green-500 + $color-text-inverse
+                </Text>
+              </Flex>
             </Flex.Item>
             <Flex.Item minWidth={320} flex="grow">
-              <Image
-                src="https://i.pinimg.com/originals/e8/49/7c/e8497ce56cdab39f1af97864e31d5a99.png"
-                naturalHeight={416}
-                naturalWidth={686}
-                alt=""
-              />
-              <Text align="center" size="100">
-                Firetine-orange-100 + $color-text-default
-              </Text>
+              <Flex direction="column" gap={4}>
+                <Image
+                  src="https://i.pinimg.com/originals/e8/49/7c/e8497ce56cdab39f1af97864e31d5a99.png"
+                  naturalHeight={416}
+                  naturalWidth={686}
+                  alt=""
+                />
+                <Text align="center" size="100">
+                  Firetine-orange-100 + $color-text-default
+                </Text>
+              </Flex>
             </Flex.Item>
           </Flex>
           <Flex gap={12} wrap alignItems="end">
             <Flex.Item minWidth={320} flex="grow">
-              <Image
-                src="https://i.pinimg.com/originals/b2/f4/a4/b2f4a4bb96fa7579d13be3c6b1a60874.png"
-                naturalHeight={410}
-                naturalWidth={694}
-                alt=""
-              />
-              <Text align="center" size="100">
-                Spabattical-teal-700 + $color-text-inverse
-              </Text>
+              <Flex direction="column" gap={4}>
+                <Image
+                  src="https://i.pinimg.com/originals/b2/f4/a4/b2f4a4bb96fa7579d13be3c6b1a60874.png"
+                  naturalHeight={410}
+                  naturalWidth={694}
+                  alt=""
+                />
+                <Text align="center" size="100">
+                  Spabattical-teal-700 + $color-text-inverse
+                </Text>
+              </Flex>
             </Flex.Item>
             <Flex.Item minWidth={320} flex="grow">
-              <Image
-                src="https://i.pinimg.com/originals/c6/3a/6e/c63a6e92a920f4b25698e64c4648b004.png"
-                naturalHeight={358}
-                naturalWidth={686}
-                alt=""
-              />
-              <Text align="center" size="100">
-                Spabattical-teal-100 + $color-text-default
-              </Text>
+              <Flex direction="column" gap={4}>
+                <Image
+                  src="https://i.pinimg.com/originals/c6/3a/6e/c63a6e92a920f4b25698e64c4648b004.png"
+                  naturalHeight={358}
+                  naturalWidth={686}
+                  alt=""
+                />
+                <Text align="center" size="100">
+                  Spabattical-teal-100 + $color-text-default
+                </Text>
+              </Flex>
             </Flex.Item>
           </Flex>
         </Flex>
       </MainSection.Subsection>
       <MainSection.Subsection title="Color fills and iconography">
         <Markdown
-          text={`When playing with color fills, we recommend using a tool like aremycolorsaccessible.com to check the foreground color against the background color. In Figma, you can use the Able or Pinterest brand color palettes plugins to check color contrast in your designs.
+          text={`Gestalt icons should always use established colors, as our color props are semantically aligned with our [iconography principles](/foundations/iconography/usage#Principles).
 
-Section 508, which aligns with WCAG 2.0 Level AA, establishes a legal standard for the contrast level essential between text and its background. The baseline AA contrast standard is 4.5:1 for most text and 3:1 for large text.
-
-Make sure your color fills have great contrast in our light and dark mode color themes.
-
-Don’t use color exclusively to convey meaning. Color should only be used as an enhancement — if color is the only cue, that cue won’t get through as intended to everyone.`}
+If colors are needed to express a brand moment on the product, consider adding a colored background to the icon instead of adding alternative colors to the icon component. Ensure it meets the [WCAG 2.1 AA accessibility standard](https://www.w3.org/TR/WCAG21/) with the proper contrast between foreground and background. See the examples below.`}
         />
+        <Box gap={8} wrap alignItems="center" mdDisplay="flex" lgDisplay="flex" smDisplay="block">
+          <Box mdColumn={6} lgColumn={2} smColumn={12}>
+            <Image
+              src="https://i.pinimg.com/originals/01/1c/d3/011cd3c155d18d68e5c66ab8a5dcee81.png"
+              naturalWidth={261}
+              naturalHeight={242}
+              alt=""
+            />
+          </Box>
+          <Box mdColumn={6} lgColumn={2} smColumn={12}>
+            <Image
+              src="https://i.pinimg.com/originals/56/70/3b/56703b409d6b0b89f6d585e180771dde.png"
+              naturalWidth={262}
+              naturalHeight={242}
+              alt=""
+            />
+          </Box>
+          <Box mdColumn={6} lgColumn={2} smColumn={12}>
+            <Image
+              src="https://i.pinimg.com/originals/e2/2e/af/e22eaf06ab4771f4aa295d8a63ffbcb7.png"
+              naturalWidth={261}
+              naturalHeight={242}
+              alt=""
+            />
+          </Box>
+          <Box mdColumn={6} lgColumn={6} smColumn={12}>
+            <Image
+              src="https://i.pinimg.com/originals/15/cc/dc/15ccdc0c40829fbfc48622f886f16a93.png"
+              naturalWidth={718}
+              naturalHeight={332}
+              alt=""
+            />
+          </Box>
+        </Box>
       </MainSection.Subsection>
       <MainSection name="Accessibility">
         <MainSection.Subsection>
           <Markdown
-            text={`Gestalt icons should always use established colors, as our color props are semantically aligned with our iconography principles.
+            text={`When playing with color fills, we recommend using a tool like [aremycolorsaccessible.com](https://www.aremycolorsaccessible.com/) to check the foreground color against the background color. In Figma, you can use the [Able](https://www.figma.com/community/plugin/734693888346260052/Able-%E2%80%93-Friction-free-accessibility) or [Pinterest brand color palettes](https://www.figma.com/community/plugin/1019681360638128106/Pinterest-Brand-color-palettes) plugins to check color contrast in your designs.
 
-If colors are needed to express a brand moment on the product, consider adding a colored background to the icon instead of adding alternative colors to the icon component. Ensure it meets the WCAG 2.1 AA accessibility standard with the proper contrast between foreground and background. See the examples below.`}
+[Section 508](https://webaim.org/standards/508/checklist), which aligns with [WCAG 2.0 Level AA](https://www.w3.org/TR/WCAG21/), establishes a legal standard for the contrast level essential between text and its background. The baseline AA contrast standard is 4.5:1 for most text and 3:1 for large text.
+
+Make sure your color fills have great contrast in our light and dark mode color themes.
+
+Don’t use color exclusively to convey meaning. Color should only be used as an enhancement — if color is the only cue, that cue won’t get through as intended to everyone.
+
+            `}
           />
         </MainSection.Subsection>
         <MainSection.Subsection title="Extended color palette">
           <Markdown
-            text={`The extended color palette displays all the available shades and tints of each color in the palette. The colors are named and numbered for easy reference. The usage of these colors come in handy for brand moments. In order to ensure accessible contrast for color pairings, we require our darkGray Text color to be used for any colors 400 and below. For 500 and above, we recommend using white. Learn more by checking our color palette documentation.
+            text={`The extended color palette displays all the available shades and tints of each color in the palette. The colors are named and numbered for easy reference. The usage of these colors come in handy for brand moments. In order to ensure accessible contrast for color pairings, we require our darkGray [Text](/web/text) color to be used for any colors 400 and below. For 500 and above, we recommend using white. Learn more by checking our [color palette documentation](https://gestalt.pinterest.systems/foundations/color/palette#Extended-palette).
 
 The 450 colors are primarily reserved for Brand usage as they are among the least accessible colors. This set works best within larger brand moments, and is not commonly used for functional color pairings.`}
           />
