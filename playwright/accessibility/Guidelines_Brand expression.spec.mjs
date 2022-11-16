ee49a1ed9012a8605968f79fe89d5424101df194
ponori <50343812+ponori@users.noreply.github.com>
2022-11-15 21:38:11 -0800

    I love playwright tests

--- playwright/accessibility/Guidelines_Brand expression.spec.mjs
+++ playwright/accessibility/Guidelines_Brand expression.spec.mjs
@@ -2,7 +2,7 @@
 import { test } from '@playwright/test';
 import expectAccessiblePage from './expectAccessiblePage.mjs';
 
-test('Brand expression guidelines check', async ({ page }) => {
+test('Brand expression guidelines Accessibility check', async ({ page }) => {
   await page.goto('/foundations/brand_expression/guidelines');
   await expectAccessiblePage({ page });
 });
