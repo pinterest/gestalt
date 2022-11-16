064454fd659ef40fb72afff7552a31800d570de5
ponori <50343812+ponori@users.noreply.github.com>
2022-11-15 17:07:53 -0800

    More content additions

--- docs/pages/foundations/brand_expression/guidelines.js
+++ docs/pages/foundations/brand_expression/guidelines.js
@@ -1,5 +1,5 @@
 // @flow strict
-import { Text, Flex, Link, Heading } from 'gestalt';
+import { Text, Flex, Link, Heading, Image, Box } from 'gestalt';
 import { type Node } from 'react';
 import MainSection from '../../../docs-components/MainSection.js';
 import Page from '../../../docs-components/Page.js';
@@ -48,24 +48,15 @@ We suggest continuously checking for comprehension and usability when bringing b
           />
         </MainSection.Subsection>
         <MainSection.Subsection>
-          <Text>
-            Be mindful of when and where to bring brand expressions into your design. If color
-            fills, imagery and other brand elements are needed to reinforce brand touch-points
-            within your design, then go for it; if not, aim to follow our{' '}
-            <Link inline href="/foundations/overview">
-              foundation guidelines
-            </Link>
-            . A consistent design allows Pinterest content to shine while ensuring a great usability
-            experience.
-          </Text>
-          <Text>
-            Below we list the UI elements you can use and the ones you should avoid when bringing
-            brand expression within your product UI.
-          </Text>
+          <Markdown
+            text={`Be mindful of when and where to bring brand expressions into your design. If color fills, imagery and other brand elements are needed to reinforce brand touch-points within your design, then go for it; if not, aim to follow our [foundation guidelines](/foundations/overview). A consistent design allows Pinterest content to shine while ensuring a great usability experience.
+
+Below we list the UI elements you can use and the ones you should avoid when bringing brand expression within your product UI. `}
+          />
         </MainSection.Subsection>
         <MainSection.Subsection title="UI elements and Brand expression">
-          <Flex>
-            <Flex.Item>
+          <Flex gap={8} wrap>
+            <Flex.Item flexBasis="0" flex="grow" minWidth={300} maxWidth="33%">
               <Heading color="success" size="200">
                 UI elements to bring brand expression
               </Heading>
@@ -79,7 +70,7 @@ We suggest continuously checking for comprehension and usability when bringing b
                 </ul>
               </Text>
             </Flex.Item>
-            <Flex.Item>
+            <Flex.Item flexBasis="0" flex="grow" minWidth={300} maxWidth="33%">
               <Heading color="subtle" size="200">
                 Consider brand expression
               </Heading>
@@ -89,7 +80,7 @@ We suggest continuously checking for comprehension and usability when bringing b
                 </ul>
               </Text>
             </Flex.Item>
-            <Flex.Item>
+            <Flex.Item flexBasis="0" flex="grow" minWidth={300} maxWidth="33%">
               <Heading color="error" size="200">
                 Elements to avoid brand expression
               </Heading>
@@ -111,75 +102,117 @@ We suggest continuously checking for comprehension and usability when bringing b
           <MainSection.Card
             type="do"
             description="Use color fills (brand baseline colors) to emphasize brand moments if appropriate to create a delightful Pinner experience (e.g., feature improvement announcement, in-product marketing moments). Ensure to check color contrast achieving [WCAG 2.1 AA accessibility standards](https://www.w3.org/TR/WCAG21/)."
-          />
+          >
+            <Box width="100%" height="100%" overflow="hidden">
+              <Image
+                src="https://i.pinimg.com/originals/f6/9e/e2/f69ee288852ea88d893d33f85616e0ce.png"
+                naturalWidth={762}
+                naturalHeight={610}
+                alt=""
+              />
+            </Box>
+          </MainSection.Card>
           <MainSection.Card
             type="don't"
             description="Use alternative colors to modify components colors or UI patterns. Color should be applied purposefully as it can convey meaning in multiple ways."
-          />
+          >
+            <Box width="100%" height="100%" overflow="hidden">
+              <Image
+                src="https://i.pinimg.com/originals/47/dd/95/47dd959f8b00e15ca9d1fad1c41508ca.png"
+                naturalWidth={752}
+                naturalHeight={612}
+                alt=""
+              />
+            </Box>
+          </MainSection.Card>
           <MainSection.Card
             type="do"
             description="Use color fills in celebratory moments, such as telling good news or highlighting Pinner's achievements."
-          />
+          >
+            <Box width="100%" height="100%" overflow="hidden">
+              <Image
+                src="https://i.pinimg.com/originals/f3/28/c9/f328c971f43eabac1a9577ac229a90ef.png"
+                naturalWidth={767}
+                naturalHeight={590}
+                alt=""
+              />
+            </Box>
+          </MainSection.Card>
           <MainSection.Card
             type="don't"
             description="Apply text over photography as it could lead to contrast issues and make it inaccessible through assistive devices."
-          />
+          >
+            <Box width="100%" height="100%" overflow="hidden">
+              <Image
+                src="https://i.pinimg.com/originals/4d/d8/a5/4dd8a5d093c6bcce384a3b50dc57e9d6.png"
+                naturalWidth={834}
+                naturalHeight={614}
+                alt=""
+              />
+            </Box>
+          </MainSection.Card>
           <MainSection.Card
             type="do"
             description="Apply text over color fills using the appropriate text [color tokens](/foundations/design_tokens#Text-color). It will ensure an accessible, readable experience."
-          />
+          >
+            <Box width="100%" height="100%" overflow="hidden">
+              <Image
+                src="https://i.pinimg.com/originals/ee/92/be/ee92be2b2750c84fcf3073fb13dec636.png"
+                naturalWidth={686}
+                naturalHeight={358}
+                alt=""
+              />
+            </Box>
+          </MainSection.Card>
           <MainSection.Card
             type="don't"
             description="Use alternative colors on text content and icons as it could lead to major contrast accessibility and usability issues. **This rule doesn't cover marketing materials, and promotional landing pages.**"
-          />
+          >
+            <Box width="100%" height="100%" overflow="hidden">
+              <Image
+                src="https://i.pinimg.com/originals/31/d0/f0/31d0f02d6ed561591968a8cea0aa1abe.png"
+                naturalWidth={734}
+                naturalHeight={614}
+                alt=""
+              />
+            </Box>
+          </MainSection.Card>
           <MainSection.Card
             type="do"
             description="Use color fills when announcing new features (e.g., new way to search for hairstyle ideas)."
-          />
+          >
+            <Box width="100%" height="100%" overflow="hidden">
+              <Image
+                src="https://i.pinimg.com/originals/dd/8e/a4/dd8ea46b19b81846ee2f66ed322789f5.png"
+                naturalWidth={750}
+                naturalHeight={580}
+                alt=""
+              />
+            </Box>
+          </MainSection.Card>
           <MainSection.Card
             type="don't"
             description="Apply color fills on core UI patterns and critical tasks actions. It can distract Pinners from completing a task."
-          />
+          >
+            <Box width="100%" height="100%" overflow="hidden">
+              <Image
+                src="https://i.pinimg.com/originals/2e/d8/60/2ed8607acc2c012c841e8aba59b38cae.png"
+                naturalWidth={887}
+                naturalHeight={614}
+                alt=""
+              />
+            </Box>
+          </MainSection.Card>
         </MainSection.Subsection>
       </MainSection>
       <MainSection name="Accessibility">
-        <Text>
-          <p>
-            Be mindful of color usage. Pinterest’s goal as a company is to meet{' '}
-            <Link inline href="https://www.w3.org/TR/WCAG21/">
-              WCAG 2.1 AA standards
-            </Link>
-            , so we recommend using a tool like{' '}
-            <Link inline href="https://www.aremycolorsaccessible.com/">
-              aremycolorsaccessible.com
-            </Link>{' '}
-            to check the foreground color against the background color. In Figma, you can use the{' '}
-            <Link
-              inline
-              href="https://www.figma.com/community/plugin/734693888346260052/Able-%E2%80%93-Friction-free-accessibility"
-            >
-              Able
-            </Link>{' '}
-            or{' '}
-            <Link
-              inline
-              href="https://www.figma.com/community/plugin/1019681360638128106/Pinterest-Brand-color-palettes"
-            >
-              Pinterest brand color palettes
-            </Link>{' '}
-            plugins to check color contrast in your designs.
-          </p>
-          <p>
-            When adding photography or any imagery source, ensure it translates well in both themes
-            (light and dark-mode). Your image source should be equally visible in both themes,
-            especially when you have colored backgrounds contrasting with a photo or an
-            illustration.
-          </p>
-          <p>
-            Ensure to add ALT text to your image, so users who rely on assistive technologies can
-            access your image content.
-          </p>
-        </Text>
+        <Markdown
+          text={`Be mindful of color usage. Pinterest’s goal as a company is to meet [WCAG 2.1 AA standards](https://www.w3.org/TR/WCAG21/), so we recommend using a tool like [aremycolorsaccessible.com](https://www.aremycolorsaccessible.com/)  to check the foreground color against the background color. In Figma, you can use the [Able](https://www.figma.com/community/plugin/734693888346260052/Able-%E2%80%93-Friction-free-accessibility) or [Pinterest brand color palettes](https://www.figma.com/community/plugin/1019681360638128106/Pinterest-Brand-color-palettes) plugins to check color contrast in your designs.
+
+When adding photography or any imagery source, ensure it translates well in both themes (light and dark-mode). Your image source should be equally visible in both themes, especially when you have colored backgrounds contrasting with a photo or an illustration.
+
+Ensure to add ALT text to your image, so users who rely on assistive technologies can access your image content.`}
+        />
       </MainSection>
       <MainSection name="Localization">
         <Text>
