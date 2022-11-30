2e2843be4d31995f9cd25aa6d20ffc4e6917bca0
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2022-11-30 12:33:57 -0800

    Text: add forwardRef to component (#2526)

--- docs/pages/web/text.js
+++ docs/pages/web/text.js
@@ -405,6 +405,111 @@ export default function TextPage({ generatedDocGen }: {| generatedDocGen: DocGen
 `}
           />
         </MainSection.Subsection>
+
+        <MainSection.Subsection
+          title="Refs"
+          description={`Don't use \`ref\` to manipulate the underlaying HTML div or span elements. Use \`ref\` to only read HTML attributes. For example, a valid use case can be detecting [truncation](#Overflow-and-truncation). The example below illustrates a case where detecting truncation allows rendering links contained within Text.`}
+        >
+          <MainSection.Card
+            defaultCode={`
+function Example() {
+  const [showLongText, setShowLongText] = React.useState(false);
+  const [applyTruncationDetection, setApplyTruncationDetection] = React.useState(false);
+
+
+  const text = "Tag brand partners in your Idea Pins with the paid partnership tool."
+
+  const veryLongText = "Tag brand partners in your Idea Pins with the paid partnership tool. Just make an Idea Pin in the app, add the paid partnership label and tag your partner brand. Once they approve the request, their brand name will show up on your Idea Pin. Brands can also choose to promote your Idea Pins as ads, boosting your reach to even more people. When you use the paid partnership tool, you work directly with brands to define the payment terms and process. Pinterest will not be directly involved in payment."
+
+  const textRef = React.useRef(null);
+  const [ellipsisActive, setEllipsisActive] = React.useState(false);
+
+  const isEllipsisActive = (element) =>
+    element.offsetHeight < element.scrollHeight || element.offsetWidth < element.scrollWidth;
+
+  const checkEllipsisActive = () => {
+    if (textRef.current && !ellipsisActive && isEllipsisActive(textRef.current)) {
+      setEllipsisActive(true);
+    } else if (textRef.current && ellipsisActive && !isEllipsisActive(textRef.current)) {
+      setEllipsisActive(false);
+    }
+  };
+
+  React.useEffect(() => {
+    checkEllipsisActive();
+    window.addEventListener('resize', checkEllipsisActive);
+    return () => {
+      window.removeEventListener('resize', checkEllipsisActive);
+    };
+  }, [checkEllipsisActive]);
+
+  return (
+    <Flex gap={8} direction="column" width="100%">
+      <Flex gap={2} direction="column">
+        <Box display="flex" alignItems="center">
+          <Box paddingX={2} WIDTH>
+            <Label htmlFor="longText">
+              <Text>Show long text</Text>
+            </Label>
+          </Box>
+          <Switch
+            onChange={() => setShowLongText(!showLongText)}
+            id="longText"
+            switched={showLongText}
+          />
+        </Box>
+        <Box display="flex" alignItems="center">
+          <Box paddingX={2}>
+            <Label htmlFor="truncation">
+              <Text>Apply truncation detection</Text>
+            </Label>
+          </Box>
+          <Switch
+            onChange={() => setApplyTruncationDetection(!applyTruncationDetection)}
+            id="truncation"
+            switched={applyTruncationDetection}
+          />
+        </Box>
+      </Flex >
+      <Flex direction="column">
+        <Text
+          inline
+          align="start"
+          lineClamp={2}
+          ref={textRef}
+          title={ellipsisActive && typeof veryLongText === 'string' ? veryLongText : undefined}
+        >
+          {showLongText ? veryLongText : text }
+          {' '}
+          <Text inline>
+            <Link
+              accessibilityLabel="Visit our Help Site"
+              href="#"
+              inline
+            >
+              Visit our Help Site
+            </Link>
+          </Text>
+        </Text>
+        {ellipsisActive && applyTruncationDetection? (
+          <Text>
+            <Link
+              accessibilityLabel="Visit our Help Site"
+              href="#"
+              inline
+            >
+            Visit our Help Site
+            </Link>
+          </Text>
+        ) : null}
+      </Flex>
+    </Flex>
+  );
+}
+`}
+          />
+        </MainSection.Subsection>
+
         <MainSection name="Writing">
           <MainSection.Subsection columns={2}>
             <MainSection.Card
