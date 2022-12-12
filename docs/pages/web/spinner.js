46fa846a627b5218170622fa1960695b6a33a165
Ryan James <dangerismycat@users.noreply.github.com>
2022-12-12 12:52:51 -0800

    Spinner: add Best Practices and Localization sections (#2556)

--- docs/pages/web/spinner.js
+++ docs/pages/web/spinner.js
@@ -3,6 +3,12 @@ import { type Node } from 'react';
 import AccessibilitySection from '../../docs-components/AccessibilitySection.js';
 import delay from '../../examples/spinner/delay.js';
 import docgen, { type DocGen } from '../../docs-components/docgen.js';
+import doLocation from '../../examples/spinner/doLocation.js';
+import dontLabel from '../../examples/spinner/dontLabel.js';
+import dontMultiple from '../../examples/spinner/dontMultiple.js';
+import dontWait from '../../examples/spinner/dontWait.js';
+import doOverlay from '../../examples/spinner/doOverlay.js';
+import doWait from '../../examples/spinner/doWait.js';
 import GeneratedPropTable from '../../docs-components/GeneratedPropTable.js';
 import main from '../../examples/spinner/main.js';
 import MainSection from '../../docs-components/MainSection.js';
@@ -46,11 +52,103 @@ export default function DocsPage({ generatedDocGen }: {| generatedDocGen: DocGen
         </MainSection.Subsection>
       </MainSection>
 
+      <MainSection name="Best Practices">
+        <MainSection.Subsection columns={2}>
+          <MainSection.Card
+            cardSize="md"
+            type="do"
+            description="Only show Spinner if the expected wait time is perceptible — typically more than a second. Remember that wait times can vary based on the user's network connection."
+            sandpackExample={
+              <SandpackExample code={doWait} name="Do - Wait" hideEditor previewHeight={400} />
+            }
+          />
+          <MainSection.Card
+            cardSize="md"
+            type="don't"
+            description="Use Spinner if the wait time is likely longer than 10 seconds. Show incremental loading/completion progress instead."
+            sandpackExample={
+              <SandpackExample
+                code={dontWait}
+                name="Don't - Wait"
+                hideControls
+                hideEditor
+                previewHeight={400}
+              />
+            }
+          />
+        </MainSection.Subsection>
+
+        <MainSection.Subsection columns={2}>
+          <MainSection.Card
+            cardSize="md"
+            type="do"
+            description="Show Spinner where the content is being loaded or updated to create a clear association with where results will appear."
+            sandpackExample={
+              <SandpackExample
+                code={doLocation}
+                name="Do - Location"
+                hideEditor
+                previewHeight={400}
+              />
+            }
+          />
+          <MainSection.Card
+            cardSize="md"
+            type="don't"
+            description="Show more than one Spinner at a time to avoid an overly-busy interface. Show a single Spinner over the collection of loading content instead."
+            sandpackExample={
+              <SandpackExample
+                code={dontMultiple}
+                name="Don't - Multiple"
+                hideControls
+                hideEditor
+                previewHeight={400}
+              />
+            }
+          />
+        </MainSection.Subsection>
+
+        <MainSection.Subsection columns={2}>
+          <MainSection.Card
+            cardSize="md"
+            type="do"
+            description="Screen underlying content when overlaid by Spinner."
+            sandpackExample={
+              <SandpackExample
+                code={doOverlay}
+                name="Do - Overlay"
+                hideEditor
+                previewHeight={400}
+              />
+            }
+          />
+          <MainSection.Card
+            cardSize="md"
+            type="don't"
+            description="Display a loading label adjacent to Spinner when the label is redundant."
+            sandpackExample={
+              <SandpackExample
+                code={dontLabel}
+                name="Don't - Label"
+                hideControls
+                hideEditor
+                previewHeight={400}
+              />
+            }
+          />
+        </MainSection.Subsection>
+      </MainSection>
+
       <AccessibilitySection
         name={generatedDocGen?.displayName}
         description={`
       Be sure to include \`accessibilityLabel\`. Labels should relate to the specific part of the product where Spinner is being used (e.g. "Loading homefeed" when used on the homefeed surface). Don't forget to localize the label!
       `}
+      />
+
+      <MainSection
+        name="Localization"
+        description={`Be sure to localize \`accessibilityLabel\`. Be mindful of label length so that it isn't truncated in languages with lengthier character counts.`}
       />
 
       <MainSection name="Variants">
