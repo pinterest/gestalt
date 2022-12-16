5d5b66d7209cc5d6da5229d4191c959768052fe4
Ryan James <rjames@pinterest.com>
2022-12-16 10:10:55 -0800

    add taparea to example

--- docs/examples/tag/dontInteractive.js
+++ docs/examples/tag/dontInteractive.js
@@ -1,11 +1,13 @@
 // @flow strict
 import { type Node } from 'react';
-import { Flex, Tag } from 'gestalt';
+import { Flex, Tag, TapArea } from 'gestalt';
 
 export default function Example(): Node {
   return (
     <Flex alignItems="center" justifyContent="center" height="100%" width="100%">
-      <Tag disabled onRemove={() => {}} text="Visit Pinterest" />
+      <TapArea fullWidth={false}>
+        <Tag disabled onRemove={() => {}} text="Visit Pinterest" />
+      </TapArea>
     </Flex>
   );
 }
