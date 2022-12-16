e352c15e863067011e8787c8517aae7f86c923ba
Ayesha <amazumdar@pinterest.com>
2022-12-15 21:04:07 -0500

    fix

--- playwright/accessibility/Year in Review 2022.spec.mjs
+++ playwright/accessibility/Year in Review 2022.spec.mjs
@@ -2,9 +2,7 @@
 import { test } from '@playwright/test';
 import expectAccessiblePage from './expectAccessiblePage.mjs';
 
-// Skip since the tests times out
-// eslint-disable-next-line jest/no-disabled-tests
-test.skip('Year in review  Accessibility check', async ({ page }) => {
+test('Year in review  Accessibility check', async ({ page }) => {
   await page.goto('/year_in_review_2022');
   await expectAccessiblePage({ page });
 });
