e6d2f26ab22e183aa7e87a003b2e793426400a84
Ryan James <rjames@pinterest.com>
2023-01-05 14:52:20 -0800

    Internal: add Flow declaration for roadmap data json

--- docs/pages/roadmap.js
+++ docs/pages/roadmap.js
@@ -1,12 +1,14 @@
 // @flow strict
 import { type Node } from 'react';
 import { Box, Flex, Text, Link } from 'gestalt';
-import Page from '../docs-components/Page.js';
-import MainSection from '../docs-components/MainSection.js';
-import PageHeader from '../docs-components/PageHeader.js';
-// $FlowExpectedError[untyped-import]
-import roadmapData from './RoadmapData.json';
 import InternalOnlyIconButton from '../docs-components/InternalOnlyIconButton.js';
+import MainSection from '../docs-components/MainSection.js';
+import Page from '../docs-components/Page.js';
+import PageHeader from '../docs-components/PageHeader.js';
+import roadmapData from './RoadmapData.json';
+
+export type Platform = 'Android' | 'iOS' | 'Mobile web' | 'Web';
+export type Status = 'inProgress' | 'problem' | 'ok' | 'unstarted';
 
 const inProgress = roadmapData.tasks.filter((x) => x.status === 'inProgress');
 
@@ -20,106 +22,65 @@ function Task({
   text,
   description,
   delivery,
-  platform,
+  platforms,
 }: {|
   text: string,
   description: string,
   delivery: string,
-  platform: string,
+  platforms: $ReadOnlyArray<Platform>,
 |}) {
-  const title = platform ? `${text} (${platform})` : text;
-
-  const desc = (
-    <Box marginTop={-6}>
-      <Flex
-        direction="column"
-        gap={{
-          row: 0,
-          column: 2,
-        }}
-      >
-        <Text>{description}</Text>
-        {delivery ? (
-          <Text size="100">
-            Scheduled for {delivery}, {roadmapData.year}
-          </Text>
-        ) : null}
-      </Flex>
-    </Box>
+  return (
+    <MainSection.Subsection
+      title={platforms.length > 0 ? `${text} (${platforms.join('/')})` : text}
+    >
+      <Box marginTop={-6}>
+        <Flex direction="column" gap={2}>
+          <Text>{description}</Text>
+          {delivery ? (
+            <Text size="100">
+              Scheduled for {delivery}, {roadmapData.year}
+            </Text>
+          ) : null}
+        </Flex>
+      </Box>
+    </MainSection.Subsection>
   );
-
-  return <MainSection.Subsection title={title}>{desc}</MainSection.Subsection>;
 }
 
 export default function RoadmapPage(): Node {
-  const inProgressItems = inProgress.map((obj) => (
-    <Task
-      key={obj.task}
-      text={obj.task}
-      description={obj.description}
-      delivery={obj.deadline}
-      platform={obj.platform}
-    />
-  ));
-
-  const futureItems = future.map((obj) => (
-    <Task
-      key={obj.task}
-      text={obj.task}
-      description={obj.description}
-      delivery={obj.deadline}
-      platform={obj.platform}
-    />
-  ));
-
-  const completeItems = complete.map((obj) => (
-    <Task
-      key={obj.task}
-      text={obj.task}
-      description={obj.description}
-      delivery={obj.deadline}
-      platform={obj.platform}
-    />
-  ));
-
-  const abandonedItems = abandoned.map((obj) => (
-    <Task
-      key={obj.task}
-      text={obj.task}
-      description={obj.description}
-      delivery=""
-      platform={obj.platform}
-    />
-  ));
+  const [inProgressItems, futureItems, completeItems, abandonedItems] = [
+    inProgress,
+    future,
+    complete,
+    abandoned,
+  ].map((itemType) =>
+    itemType.map((item) => (
+      <Task
+        key={item.task}
+        text={item.task}
+        description={item.description}
+        delivery={item.deadline}
+        platforms={item.platforms}
+      />
+    )),
+  );
 
   return (
     <Page title={`Gestalt ${roadmapData.year} roadmap`}>
       <PageHeader name={`Gestalt ${roadmapData.year} roadmap`} type="guidelines" />
-      <Flex
-        direction="column"
-        gap={{
-          row: 0,
-          column: 4,
-        }}
-      >
+      <Flex direction="column" gap={4}>
         <Text>
           {`The following reflects all public-facing work the Gestalt team plans to ship in ${roadmapData.year}.`}{' '}
           For more details on our latest updates, visit the{' '}
-          <Link href="/whats_new" display="inlineBlock">
+          <Link href="/whats_new" display="inline">
             What&apos;s New page.
           </Link>
         </Text>
-        <Flex
-          gap={{
-            row: 1,
-            column: 0,
-          }}
-          alignItems="center"
-        >
+        <Flex gap={1} alignItems="center">
           <Text>
             <Link
               href="https://jira.pinadmin.com/secure/PortfolioPlanView.jspa?id=525&sid=530&vid=1684#plan/backlog"
-              display="inlineBlock"
+              underline="always"
             >
               Visit our internal roadmap <InternalOnlyIconButton />
             </Link>
@@ -127,57 +88,31 @@ export default function RoadmapPage(): Node {
         </Flex>
       </Flex>
 
-      <Flex
-        direction="column"
-        gap={{
-          row: 0,
-          column: 12,
-        }}
-      >
-        <MainSection name="In progress">
-          <Flex
-            direction="column"
-            gap={{
-              row: 0,
-              column: 8,
-            }}
-          >
-            {inProgressItems}
-          </Flex>
-        </MainSection>
-        <MainSection name="Upcoming">
-          <Flex
-            direction="column"
-            gap={{
-              row: 0,
-              column: 8,
-            }}
-          >
-            {futureItems}
-          </Flex>
-        </MainSection>
-        <MainSection name="Complete">
-          <Flex
-            direction="column"
-            gap={{
-              row: 0,
-              column: 8,
-            }}
-          >
-            {completeItems}
-          </Flex>
-        </MainSection>
-        <MainSection name="Abandoned">
-          <Flex
-            direction="column"
-            gap={{
-              row: 0,
-              column: 8,
-            }}
-          >
-            {abandonedItems}
-          </Flex>
-        </MainSection>
+      <Flex direction="column" gap={12}>
+        {[
+          {
+            name: 'In progress',
+            items: inProgressItems,
+          },
+          {
+            name: 'Upcoming',
+            items: futureItems,
+          },
+          {
+            name: 'Complete',
+            items: completeItems,
+          },
+          {
+            name: 'Abandoned',
+            items: abandonedItems,
+          },
+        ].map(({ name, items }) => (
+          <MainSection key={name} name={name}>
+            <Flex direction="column" gap={8}>
+              {items}
+            </Flex>
+          </MainSection>
+        ))}
       </Flex>
     </Page>
   );
