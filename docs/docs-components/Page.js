463577d5d19e8a9e9cc6c4321e7b5c304a3a7c1c
Ayesha Mazumdar <amazumdar@pinterest.com>
2023-01-10 22:27:42 -0500

    Docs: Update site layout (#2596)

--- docs/docs-components/Page.js
+++ docs/docs-components/Page.js
@@ -3,8 +3,9 @@ import { type Node, useEffect, Children } from 'react';
 import { Box, Flex, Link, Text } from 'gestalt';
 import SearchContent from './SearchContent.js';
 import Toc from './Toc.js';
+import { CONTENT_MAX_WIDTH_PX } from './AppLayout.js';
 
-const DETAIL_PAGE_MAX_WIDTH = 1314;
+const DETAIL_PAGE_MAX_WIDTH = 894;
 
 type Props = {|
   children: Node,
@@ -34,8 +35,8 @@ export default function Page({
   }, [page]);
 
   return (
-    <Flex>
-      <Box flex="grow" maxWidth={hideSideNav ? '100%' : DETAIL_PAGE_MAX_WIDTH}>
+    <Flex width="100%">
+      <Box flex="grow" maxWidth={hideSideNav ? CONTENT_MAX_WIDTH_PX : DETAIL_PAGE_MAX_WIDTH}>
         <SearchContent>
           <Flex
             gap={{
