10b2a8e396f6c4e4674ce10ab4b4d618776540a2
Eduardo Costa <ecosta@pinterest.com>
2023-02-06 11:00:35 -0300

    Fix reds

--- docs/examples/helpbutton/withLink.js
+++ docs/examples/helpbutton/withLink.js
@@ -10,7 +10,7 @@ export default function Example(): Node {
 
   return (
     <Flex height="100%" justifyContent="center" alignItems="center" gap={1}>
-      <Text>Gestalt is Pinterest's design system.</Text>
+      <Text>Gestalt is Pinterest`s design system.</Text>
       <HelpButton
         text="Is Pinterest's design system"
         accessibilityPopoverLabel="Gestalt meaning description"
