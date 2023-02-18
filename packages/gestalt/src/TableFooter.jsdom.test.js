75be266be26d6f64759083f5edf7897fde7e681f
Alberto Carreras Carrasco <acarreras@pinterest.com>
2023-02-18 02:30:30 -0500

    fix

---
+++ packages/gestalt/src/TableFooter.jsdom.test.js
@@ -0,0 +1,38 @@
+// @flow strict
+import { render } from '@testing-library/react';
+import Table from './Table.js';
+import Text from './Text.js';
+
+describe('Table.Footer', () => {
+  test('renders correctly', () => {
+    const { baseElement } = render(
+      <Table accessibilityLabel="Sticky footer" maxHeight={200}>
+        <Table.Footer>
+          <Table.Row>
+            <Table.Cell>
+              <Text weight="bold">Total</Text>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>,
+    );
+
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  test('sticky footer renders correctly', () => {
+    const { baseElement } = render(
+      <Table accessibilityLabel="Sticky footer" maxHeight={200}>
+        <Table.Footer sticky>
+          <Table.Row>
+            <Table.Cell>
+              <Text weight="bold">Total</Text>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>,
+    );
+
+    expect(baseElement).toMatchSnapshot();
+  });
+});
