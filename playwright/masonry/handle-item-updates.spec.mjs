856df3c5cb4c41a081ad4c3ea67761a5c255b8c7
Ryan James <rjames@pinterest.com>
2023-02-27 17:43:52 -0800

    got it working!

--- playwright/masonry/handle-item-updates.spec.mjs
+++ playwright/masonry/handle-item-updates.spec.mjs
@@ -4,23 +4,27 @@ import selectors from './utils/selectors.mjs';
 import getServerURL from './utils/getServerURL.mjs';
 import waitForRenderedItems from './utils/waitForRenderedItems.mjs';
 
+async function getGridItems(page) {
+  return page.locator(selectors.gridItem).all();
+}
+
 test.describe('Masonry: handle item updates', () => {
   test('should correctly update grid item heights', async ({ page }) => {
     await page.setViewportSize({ width: 800, height: 800 });
     await page.goto(getServerURL({ virtualize: true }));
-    await waitForRenderedItems(page, { targetItems: 18, scrollHeight: 3412 });
+    await waitForRenderedItems(page, { targetItemsGT: 10 });
 
     // get initial size of first element
-    const gridItems1 = await page.locator(selectors.gridItem);
+    const gridItems1 = await getGridItems(page);
     const itemRect1 = await gridItems1[0].boundingBox();
 
     // trigger item expand
     const pushTrigger = await page.locator(selectors.expandGridItems);
     await pushTrigger.click();
-    await waitForRenderedItems(page, { targetItems: 12, scrollHeight: 4812 });
+    await waitForRenderedItems(page, { targetItemsGT: 8 });
 
     // get new size of first element
-    const gridItems2 = await page.locator(selectors.gridItem);
+    const gridItems2 = await getGridItems(page);
     const itemRect2 = await gridItems2[0].boundingBox();
 
     // verify that the first element has grown in height
@@ -28,9 +32,9 @@ test.describe('Masonry: handle item updates', () => {
 
     // trigger item collapse
     await pushTrigger.click();
-    await waitForRenderedItems(page, { targetItems: 18, scrollHeight: 3412 });
+    await waitForRenderedItems(page, { targetItemsGT: 10 });
 
-    const gridItems3 = await page.locator(selectors.gridItem);
+    const gridItems3 = await getGridItems(page);
     const itemRect3 = await gridItems3[0].boundingBox();
 
     // item height should have reverted to original
