e1ef8bbe4c7c7562ccca9cd47bc342a14ec1db7f
Ryan James <rjames@pinterest.com>
2023-03-01 13:40:35 -0800

    fix types

--- docs/integration-test-helpers/masonry/MasonryContainer.js
+++ docs/integration-test-helpers/masonry/MasonryContainer.js
@@ -1,5 +1,6 @@
 // @flow strict
 import { Component, createRef, type Element, type Node } from 'react';
+import { Masonry } from 'gestalt';
 import ExampleGridItem from './ExampleGridItem.js';
 import getClassicGridServerStyles from './getClassicGridServerStyles.js';
 import getFlexibleGridServerStyles from './getFlexibleGridServerStyles.js';
@@ -15,10 +16,9 @@ import { getRandomNumberGenerator, generateExampleItems } from './items-utils.js
 // Masonry works, MasonryContainer also supports certain SSR functionality such
 // as generating styles that affect pre-hydration Masonry content.
 
-type Props = {
+type Props = {|
   // The actual Masonry component to be used (if using an experimental version of Masonry).
-  // $FlowFixMe[unclear-type]
-  MasonryComponent: any,
+  MasonryComponent: typeof Masonry,
   // Sets up props to display a collage layout.
   collage?: boolean,
   // Constrains the width of the grid rendering.
@@ -49,18 +49,16 @@ type Props = {
   virtualBoundsTop?: number,
   // The relative amount in pixel to extend the virtualized viewport bottom value.
   virtualBoundsBottom?: number,
-  ...
-};
+|};
 
-type State = {
+type State = {|
   expanded: boolean,
   hasScrollContainer: boolean,
   // $FlowFixMe[unclear-type]
-  items: $ReadOnlyArray<?Object>,
+  items: $ReadOnlyArray<Object>,
   mountGrid: boolean,
   mounted: boolean,
-  ...
-};
+|};
 
 export default class MasonryContainer extends Component<Props, State> {
   state: State = {
@@ -238,7 +236,10 @@ export default class MasonryContainer extends Component<Props, State> {
   };
 
   // $FlowFixMe[unclear-type]
-  renderItem: ({| data: any, itemIdx: number |}) => Node = ({ data, itemIdx }) => {
+  renderItem: ({| +data: any, +itemIdx: number, +isMeasuring: boolean |}) => Node = ({
+    data,
+    itemIdx,
+  }) => {
     const { expanded } = this.state;
     return <ExampleGridItem expanded={expanded} data={data} itemIdx={itemIdx} />;
   };
@@ -311,6 +312,7 @@ export default class MasonryContainer extends Component<Props, State> {
       <div id="gridWrapper" className="gridCentered" style={gridStyle}>
         <div id="top-sibling" />
         {mountGrid && (
+          // $FlowFixMe[incompatible-exact]
           <MasonryComponent
             ref={this.gridRef}
             renderItem={this.renderItem}
