c99adc3ae1fb8774c3661811e334c56b01faf29e
Ryan James <rjames@pinterest.com>
2023-03-06 10:07:51 -0800

    Internal: add `handle-update-during-insertions` Masonry integration test

---
+++ playwright/masonry/handle-update-during-insertions.spec.mjs
@@ -0,0 +1,22 @@
+// @flow strict
+import { expect, test } from '@playwright/test';
+import selectors from './utils/selectors.mjs';
+import getServerURL from './utils/getServerURL.mjs';
+
+test.describe('Masonry: Update during insertions', () => {
+  test('should not throw an error when Masonry is updated during insertions', async ({
+    page,
+  }) => {
+    await page.setViewportSize({ width: 800, height: 800 });
+    await page.goto(getServerURL({ virtualize: true }));
+
+    const initialErrors = await page.evaluate(() => window.ERROR_COUNT);
+    expect(initialErrors).toEqual(0);
+    // click the insert null items button
+    const insertTrigger = await page.locator(selectors.updateGridItems);
+    await insertTrigger.click();
+
+    const afterErrors = await page.evaluate(() => window.ERROR_COUNT);
+    expect(afterErrors).toEqual(0);
+  });
+});
