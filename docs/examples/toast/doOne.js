0d8499f5be8bc5cac0b6bce3f5c80cf66508ffa7
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2023-03-10 13:38:08 -0800

    Toast: new Docs for redesign Toast (#2551)

---
+++ docs/examples/toast/doOne.js
@@ -0,0 +1,18 @@
+// @flow strict
+import { type Node } from 'react';
+import { Box, Flex, Toast, Avatar } from 'gestalt';
+
+export default function Example(): Node {
+  return (
+    <Box paddingY={4} width="100%" height="100%">
+      <Flex alignItems="end" justifyContent="center" width="100%" height="100%">
+        <Toast
+          thumbnail={{
+            avatar: <Avatar src="https://i.ibb.co/ZfCZrY8/keerthi.jpg" name="Keerthi" />,
+          }}
+          text="2-factor authentication added for Sabina Romero"
+        />
+      </Flex>
+    </Box>
+  );
+}
---
+++ docs/examples/toast/image.js
@@ -0,0 +1,23 @@
+// @flow strict
+import { type Node } from 'react';
+import { Flex, Toast, Image } from 'gestalt';
+
+export default function Example(): Node {
+  return (
+    <Flex justifyContent="center" alignItems="center" width="100%" height="100%">
+      <Toast
+        text="Pin promoted"
+        thumbnail={{
+          image: (
+            <Image
+              alt="A regular sandwich with tomato, lettuce, and cheese."
+              naturalHeight={564}
+              naturalWidth={564}
+              src="https://i.pinimg.com/564x/90/e2/17/90e217ea5513fcfcada465c763250b7e.jpg"
+            />
+          ),
+        }}
+      />
+    </Flex>
+  );
+}
