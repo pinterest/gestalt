f253be3615b093cd5de0512e87b20ff49c57ac2c
Ryan James <dangerismycat@users.noreply.github.com>
2023-03-28 11:31:03 -0700

    Badge: convert examples to use Sandpack (#2771)

---
+++ docs/examples/badge/dontColor.js
@@ -0,0 +1,24 @@
+// @flow strict
+import { type Node } from 'react';
+import { Badge, Box, Flex, Image, Mask } from 'gestalt';
+
+export default function Example(): Node {
+  return (
+    <Flex alignItems="center" gap={4} height="100%" justifyContent="center" width="100%">
+      <Mask height={150} width={280} rounding={2}>
+        <Image
+          alt="Botanical art in coral and green"
+          fit="cover"
+          src="https://i.ibb.co/cbjgZft/img-door.jpg"
+          naturalWidth={1}
+          naturalHeight={1}
+        />
+        <Box position="absolute" top>
+          <Box padding={4}>
+            <Badge text="Active" type="success" />
+          </Box>
+        </Box>
+      </Mask>
+    </Flex>
+  );
+}
