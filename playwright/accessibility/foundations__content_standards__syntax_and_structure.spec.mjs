d76f1fc72ec251fb6da9a1293320b680075f83f4
Ryan James <rjames@pinterest.com>
2023-03-29 20:16:36 -0700

    fix incorrect path/name

--- playwright/accessibility/foundations__content_standards__syntax_and_structure.spec.mjs
+++ playwright/accessibility/foundations__content_standards__syntax_and_structure.spec.mjs
@@ -3,6 +3,6 @@ import { test } from '@playwright/test';
 import expectAccessiblePage from './expectAccessiblePage.mjs';
 
 test('Content standards Syntax Structure check', async ({ page }) => {
-  await page.goto('/foundations/content_standards/syntax_structure');
+  await page.goto('/foundations/content_standards/syntax_and_structure');
   await expectAccessiblePage({ page });
 });
