798bc9c7f9d4afe4664a3f958a6dfef6c8586ba6
laylaleeal <63257116+laylaleeal@users.noreply.github.com>
2023-04-12 16:28:17 -0700

    Text: Android component specs and guidelines (#2824)
    * Added Text to sideIndex
    
    * Add content to Android Text
    
    * create accessibility android text page
    
    * changed heading and titles id
    
    * changed id attribute

--- docs/docs-components/COMPONENT_DATA.js
+++ docs/docs-components/COMPONENT_DATA.js
@@ -2598,7 +2598,7 @@ const GENERAL_COMPONENT_LIST: $ReadOnlyArray<ListItemType> = [
   {
     svg: <Text />,
     name: 'Text',
-    aliases: ['Copy'],
+    aliases: ['Copy, Subtext, Caption, Footer, Helper text'],
     description: 'Text is used for all text on the page.',
     category: 'Text',
     status: {
