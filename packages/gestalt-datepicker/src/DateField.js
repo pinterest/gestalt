dbe2c1e0b101b860dcb105b9184741cde087f6ad
acarreras <acarreras@pinterest.com>
2023-04-12 20:11:28 -0400

    progress

--- packages/gestalt-datepicker/src/DateField.js
+++ packages/gestalt-datepicker/src/DateField.js
@@ -1,12 +1,16 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable react/display-name */
 // @flow strict-local
-import { forwardRef, useState } from 'react';
+import { forwardRef, useState, type Node } from 'react';
 import { StyledEngineProvider } from '@mui/material/styles';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { unstable_useDateField as useDateField } from '@mui/x-date-pickers/DateField';
 import { DatePicker as MUIDatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import classnames from 'classnames';
 import de from 'date-fns/locale/de';
-import { Box } from 'gestalt';
+import { Box, Text, Flex, Status } from 'gestalt';
+import styles from './DateField.css';
 
 /**
  * [DateField](https://gestalt.pinterest.systems/web/DateField)
@@ -14,36 +18,29 @@ import { Box } from 'gestalt';
  * ![DateField closed light mode](https://raw.githubusercontent.com/pinterest/gestalt/master/playwright/visual-test/DateField-closed.spec.mjs-snapshots/DateField-closed-chromium-darwin.png)
  * ![DateField closed dark mode](https://raw.githubusercontent.com/pinterest/gestalt/master/playwright/visual-test/DateField-closed-dark.spec.mjs-snapshots/DateField-closed-dark-chromium-darwin.png)
  */
-const CustomTextField = forwardRef((props, inputRef) => {
-  const {
-    disabled,
-    id,
-    label,
-    InputProps: { ref: containerRef } = {},
-    // extracting `error`, 'focused', and `ownerState` as `input` does not support those props
-    error,
-    focused,
-    ownerState,
-    placeholder,
-    autoComplete,
-    className,
-    sx,
-    autoFocus,
-    value,
-    inputMode,
-    readOnly,
-    onClick,
-    onFocus,
-    onBlur,
-    onPaste,
-    onChange,
-    onKeyDown,
-    onMouseUp,
-  } = props;
-
-  // console.log(props);
-
-  return (
+const CustomTextField = forwardRef(
+  (
+    {
+      disabled,
+      id,
+      InputProps: { ref: containerRef } = {},
+      focused,
+      placeholder,
+      autoComplete,
+      className,
+      value,
+      inputMode,
+      readOnly,
+      onClick,
+      onFocus,
+      onBlur,
+      onPaste,
+      onChange,
+      onKeyDown,
+      onMouseUp,
+    },
+    inputRef,
+  ): Node => (
     <Box
       padding={12}
       display="flex"
@@ -56,11 +53,9 @@ const CustomTextField = forwardRef((props, inputRef) => {
         disabled={disabled}
         ref={inputRef}
         focused={focused}
-        // ownerState={ownerState}
         placeholder={placeholder}
         autoComplete={autoComplete}
         className={className}
-        autoFocus={autoFocus}
         value={value}
         inputMode={inputMode}
         readOnly={readOnly}
@@ -68,18 +63,15 @@ const CustomTextField = forwardRef((props, inputRef) => {
         onFocus={onFocus}
         onBlur={onBlur}
         onPaste={onPaste}
-        onChange={(event) => {
-          console.log(event.target.value);
-          onChange(event);
-        }}
+        onChange={onChange}
         onKeyDown={onKeyDown}
         onMouseUp={onMouseUp}
       />
     </Box>
-  );
-});
+  ),
+);
 
-function CustomDateField(props) {
+function CustomDateField(props): Node {
   const { inputRef: externalInputRef, slots, slotProps, ...textFieldProps } = props;
 
   const response = useDateField({
@@ -90,15 +82,78 @@ function CustomDateField(props) {
   return <CustomTextField {...response} />;
 }
 
-function InternalDateField(props) {
-  return <MUIDatePicker slots={{ field: CustomDateField, ...props.slots }} {...props} />;
+type Props = {|
+  label: string,
+  labelDisplay?: 'visible' | 'hidden',
+  id: string,
+  errorMessage?: Node,
+  helperText?: string,
+|};
+
+function InternalDateField({ label, labelDisplay, id, helperText, errorMessage }: Props): Node {
+  const [value, setValue] = useState(null);
+
+  return (
+    <Box>
+      <label
+        className={classnames(styles.label, {
+          [styles.visuallyHidden]: labelDisplay === 'hidden',
+        })}
+        htmlFor={id}
+      >
+        <div className={styles.formLabel}>
+          <Text size="100">{label}</Text>
+        </div>
+      </label>
+      <MUIDatePicker
+        onChange={setValue}
+        onError={console.log}
+        disableFuture
+        slots={{ field: CustomDateField }}
+        value={value}
+      />
+      {helperText && !errorMessage ? (
+        <Box marginTop={2} id={`${id}-helperText`}>
+          <Flex gap={4}>
+            <Flex.Item flex="grow">
+              {helperText ? (
+                <Text color="subtle" size="100">
+                  {helperText}
+                </Text>
+              ) : null}
+            </Flex.Item>
+          </Flex>
+        </Box>
+      ) : null}
+      {errorMessage ? (
+        <Box marginTop={2}>
+          <Text color="error" size="100">
+            <span className={styles.formErrorMessage} id={`${id}-error`}>
+              <Box role="alert">
+                <Flex gap={2}>
+                  <Status type="problem" />
+                  {errorMessage}
+                </Flex>
+              </Box>
+            </span>
+          </Text>
+        </Box>
+      ) : null}
+    </Box>
+  );
 }
 
-function DateField() {
+function DateField({ label, labelDisplay = 'default', id, helperText, errorMessage }: Props): Node {
   return (
     <StyledEngineProvider injectFirst>
       <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={de}>
-        <InternalDateField />
+        <InternalDateField
+          label={label}
+          labelDisplay={labelDisplay}
+          id={id}
+          helperText={helperText}
+          errorMessage={errorMessage}
+        />
       </LocalizationProvider>
     </StyledEngineProvider>
   );
