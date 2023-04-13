71cc4ec601c6f44d5de23634c6478cadaefb9045
eniomoura <erochamoura@pinterest.com>
2023-04-13 03:46:47 -0300

    Link: convert examples to use Sandpack

---
+++ docs/examples/link/dontWeightTexts.js
@@ -0,0 +1,33 @@
+// @flow strict
+import { type Node } from 'react';
+import { Link, Flex, Text } from 'gestalt';
+
+export default function Example(): Node {
+  return (
+    <Flex height="100%" alignItems="center" justifyContent="center">
+      <Flex gap={{ column: 4, row: 0 }} direction="column" width="90%">
+        <Text weight="bold"> Need help? </Text>
+        <Text inline>
+          {' '}
+          Find tips and best practices on the
+          <Text weight="bold" inline>
+            <Link href="https://business.pinterest.com/" display="inline" underline="hover">
+              {' '}
+              Pinterest Business Site{' '}
+            </Link>
+          </Text>
+        </Text>
+        <Text inline>
+          {' '}
+          Troubleshoot issues with the
+          <Text weight="bold" inline>
+            <Link href="https://help.pinterest.com" display="inline" underline="hover">
+              {' '}
+              Pinterest Help Center{' '}
+            </Link>
+          </Text>
+        </Text>
+      </Flex>
+    </Flex>
+  );
+}
---
+++ docs/examples/link/variantLinkText.js
@@ -0,0 +1,25 @@
+// @flow strict
+import { type Node } from 'react';
+import { Link, Flex, Text, Box } from 'gestalt';
+
+export default function Example(): Node {
+  return (
+    <Flex height="100%" alignItems="center" justifyContent="center">
+      <Box color="infoBase" width="50%" padding={4} rounding={3}>
+        <Flex direction="column" gap={{ column: 3, row: 0 }} alignItems="center">
+          <Text color="inverse" weight="bold" size="600">
+            Tips
+          </Text>
+          <Flex gap={{ row: 1, column: 0 }} alignItems="center">
+            <Text color="inverse" size="400" align="center" weight="bold">
+              <Link href="https://pinterest.com" display="inline">
+                Add a Pinterest widget
+              </Link>{' '}
+              and get inspired right from your phone&lsquo;s home screen.
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
+    </Flex>
+  );
+}
