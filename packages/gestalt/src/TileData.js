4a29e4f6fa0c04cf423b631a11e142c872d05129
Ravi Lingineni <rlingineni@pinterest.com>
2023-04-14 17:05:21 -0700

    Adding disabled state overlay

--- packages/gestalt/src/TileData.js
+++ packages/gestalt/src/TileData.js
@@ -1,17 +1,18 @@
 // @flow strict
-import { type Node } from 'react';
+import { useEffect, type Node, useState } from 'react';
 import styles from './TileData.css';
 import Box from './Box.js';
 import Tile from './Tile/Tile.js';
 import DataPoint from './Datapoint';
 import generateColorShade from './utils/generateColorShade';
-import designTokens from 'gestalt-design-tokens/dist/json/variables.json';
+import { useColorScheme } from './contexts/ColorSchemeProvider';
 
 type DataVisualizationColors =
-  | 'data-visualization-1'
-  | 'data-visualization-2'
-  | 'data-visualization-3'
-  | 'data-visualization-4';
+  | 'data-visualization-01'
+  | 'data-visualization-02'
+  | 'data-visualization-03'
+  | 'data-visualization-04'
+  | 'data-visualization-05';
 
 type Props = {|
   /**
@@ -21,7 +22,7 @@ type Props = {|
   /**
    * A color from the data visualization palette. if it's not in the pallete
    */
-  selectedColor?: DataVisualizationColors,
+  selectedColor: DataVisualizationColors,
   /**
    * An identifier to be passed in a callback, and distinguish multiple DataPoints
    */
@@ -45,20 +46,43 @@ type Props = {|
 |};
 
 /**
- * [TileData] https://gestalt.pinterest.systems/web/tiledata component should be used for ... on the page.
- * ![TileData light mode](https://raw.githubusercontent.com/pinterest/gestalt/master/playwright/visual-test/TileData.spec.mjs-snapshots/TileData-chromium-darwin.png)
- * ![TileData dark mode](https://raw.githubusercontent.com/pinterest/gestalt/master/playwright/visual-test/TileData-dark.spec.mjs-snapshots/TileData-dark-chromium-darwin.png)
+ * Use TileData to select a multiple categories to compare with each other in a graph or chart view, while still being able to see all of the data points
  */
-export default function TileData({ accessibilityLabel }: Props): Node {
+export default function TileData({
+  accessibilityLabel,
+  disabled = false,
+  id,
+  selected,
+  selectedColor = 'data-visualization-05',
+  showCheckbox,
+}: Props): Node {
+  // bg color is a standard hex
+  const [borderColor, setBorderColor] = useState('');
+
+  const getColorHex = (color: string) =>
+    getComputedStyle(document.documentElement).getPropertyValue(`--color-${color}`);
+
   /**
-   * Generates a background shade that's 10% lighter. This is dynamic because the data-viz pallete supports higher settings
+   * Generates a background shade that's 10% lighter. This is dynamic
    */
   const getBackgroundShade = () => {
-    generateColorShade();
+    // value of the codes are injected
+    const shade = getColorHex(selectedColor);
+    // add an alpha channel to the hex, at 10% opacity
+    const bgColor = shade + '10';
+    return bgColor;
   };
 
   return (
-    <Tile tooltip="The number of times your Pins were on screen">
+    <Tile
+      tooltip="The number of times your Pins were on screen"
+      selected={selected}
+      showCheckbox={showCheckbox}
+      disabled={disabled}
+      id={id}
+      bgColor={getBackgroundShade(selectedColor)}
+      borderColor={getColorHex(selectedColor)}
+    >
       <DataPoint
         title="Werbeausgaben zurückgeben"
         value="1.5M"
