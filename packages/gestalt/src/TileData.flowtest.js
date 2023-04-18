842ecaf90239ecd990ee8d6e861e400c29202558
Ravi Lingineni <rlingineni@pinterest.com>
2023-04-17 17:22:37 -0700

    Adding Datapoint Examples

--- packages/gestalt/src/TileData.flowtest.js
+++ packages/gestalt/src/TileData.flowtest.js
@@ -5,3 +5,6 @@ const Valid = <TileData />;
 
 // $FlowExpectedError[prop-missing]
 const InvalidProps = <TileData nonexisting={33} />;
+
+// $FlowExpectedError[incompatible-type]
+const wrongColor = <TileData selectedColor="my-no-good-color" />;
