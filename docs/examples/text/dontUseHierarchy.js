d56ef209a53db449abe01b88a6a708f346e67f35
eniomoura <erochamoura@pinterest.com>
2023-04-19 00:09:08 -0300

    Text: convert examples to use Sandpack

---
+++ docs/examples/text/dontUseHierarchy.js
@@ -0,0 +1,16 @@
+// @flow strict
+import { type Node } from 'react';
+import { Flex, Text } from 'gestalt';
+
+export default function Example(): Node {
+  return (
+    <Flex alignItems="center" height="100%" justifyContent="center" width="100%">
+      <Flex direction="column" gap={2}>
+        <Text size="300" weight="bold">
+          Impressions
+        </Text>
+        <Text size="100">1,250,000</Text>
+      </Flex>
+    </Flex>
+  );
+}
---
+++ docs/examples/text/variantOverflowTruncation.js
@@ -0,0 +1,35 @@
+// @flow strict
+import { type Node } from 'react';
+import { Box, Flex, Text } from 'gestalt';
+
+export default function Example(): Node {
+  return (
+    <Flex alignItems="center" height="100%" justifyContent="center" width="100%">
+      <Flex direction="column" gap={{ column: 2, row: 0 }} width={200}>
+        <Text>breakWord (default):</Text>
+        <Box color="secondary" padding={2} rounding={2}>
+          <Text>
+            This is a long and Supercalifragilisticexpialidocious sentence.
+            次の単語グレートブリテンおよび北アイルランド連合王国で本当に大きな言葉
+          </Text>
+        </Box>
+
+        <Text>normal:</Text>
+        <Box color="secondary" padding={2} rounding={2}>
+          <Text overflow="normal">
+            This is a long and Supercalifragilisticexpialidocious sentence.
+            次の単語グレートブリテンおよび北アイルランド連合王国で本当に大きな言葉
+          </Text>
+        </Box>
+
+        <Text>lineClamp:</Text>
+        <Box color="secondary" padding={2} rounding={2}>
+          <Text lineClamp={2}>
+            This is a long and Supercalifragilisticexpialidocious sentence.
+            次の単語グレートブリテンおよび北アイルランド連合王国で本当に大きな言葉
+          </Text>
+        </Box>
+      </Flex>
+    </Flex>
+  );
+}
