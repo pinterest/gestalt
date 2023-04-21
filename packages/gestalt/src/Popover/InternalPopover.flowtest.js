31d7cb5f24f1cecd4df23ced1a0675337c2d6727
eniomoura <erochamoura@pinterest.com>
2023-04-21 13:49:38 -0300

    implementing changes requested

--- packages/gestalt/src/Popover/InternalPopover.flowtest.js
+++
@@ -1,7 +0,0 @@
-// @flow strict
-import InternalPopover from './InternalPopover.js';
-
-const Valid = <InternalPopover anchor={document.createElement('div')} onDismiss={() => {}} />;
-
-// $FlowExpectedError[prop-missing]
-const NonExistingProp = <InternalPopover nonexisting={33} />;
