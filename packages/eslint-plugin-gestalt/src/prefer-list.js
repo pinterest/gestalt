dd9de79af96f976ffefab69d701e2fd9dc6c743b
eniomoura <erochamoura@pinterest.com>
2023-05-03 17:30:17 -0300

    creating prefer-list eslint rule

---
+++ packages/eslint-plugin-gestalt/src/prefer-list.js
@@ -0,0 +1,38 @@
+/**
+ * @fileoverview Prefer List: Prevent ul or ol list tags for basic use cases.
+ */
+
+// @flow strict
+import { type ESLintRule } from './helpers/eslintFlowTypes.js';
+
+const rule: ESLintRule = {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description:
+        'Prefer List: Prevent ul or ol list tags for basic use cases. Use Gestalt List, instead',
+      category: 'Gestalt alternatives',
+      recommended: true,
+      url: '',
+    },
+    schema: [],
+    messages: {
+      messageList: `Use List from Gestalt, <List><List.Item text=""/></List>\n`,
+    },
+  },
+
+  create(context) {
+    return {
+      JSXOpeningElement(node) {
+        if (node.name.name === 'ul' || node.name.name === 'ol') {
+          context.report({
+            node,
+            messageId: 'useGestaltList',
+          });
+        }
+      },
+    };
+  },
+};
+
+export default rule;
