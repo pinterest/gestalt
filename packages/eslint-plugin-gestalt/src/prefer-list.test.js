0bbaa628500877cb94ac35c70c87088b0e23e232
eniomoura <erochamoura@pinterest.com>
2023-05-03 19:17:48 -0300

    adding tests and adding rule to index

---
+++ packages/eslint-plugin-gestalt/src/prefer-list.test.js
@@ -0,0 +1,33 @@
+// @flow strict
+import { readFileSync } from 'fs';
+import path from 'path';
+import { RuleTester } from 'eslint';
+import rule from './prefer-list.js';
+import { parserOptions } from './helpers/testHelpers.js';
+
+const ruleTester = new RuleTester({ parserOptions });
+
+const validCode = readFileSync(
+  path.resolve(__dirname, './__fixtures__/prefer-list/valid.js'),
+  'utf-8',
+);
+
+const noGestaltListOl = readFileSync(
+  path.resolve(__dirname, './__fixtures__/prefer-list/invalid/no-gestalt-list-ol.js'),
+  'utf-8',
+);
+
+const noGestaltListUl = readFileSync(
+  path.resolve(__dirname, './__fixtures__/prefer-list/invalid/no-gestalt-list-ul.js'),
+  'utf-8',
+);
+
+const messageList = `Use List from Gestalt, <List><List.Item text=""/></List>\n`;
+
+ruleTester.run('prefer-list', rule, {
+  valid: [{ code: validCode }],
+  invalid: [
+    [noGestaltListOl, messageList],
+    [noGestaltListUl, messageList],
+  ].map(([input, output, errors]) => ({ code: input, output, errors })),
+});
