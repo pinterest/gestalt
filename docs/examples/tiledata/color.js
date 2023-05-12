9b1f99c744d1d8c6e893ebb71b65629993ce20a1
Ravi Lingineni <rlingineni@pinterest.com>
2023-05-11 17:20:20 -0700

    Update examples

--- docs/examples/tiledata/color.js
+++ docs/examples/tiledata/color.js
@@ -3,52 +3,21 @@ import { type Node, useState } from 'react';
 import { TileData, Flex } from 'gestalt';
 
 export default function Example(): Node {
-  const [selectedTileId, setSelectedTileId] = useState('1');
-
-  const isSelected = (id: string) => selectedTileId === id;
+  // note 03 doesn't exist as a color
+  const allColors = ['01', '02', '04', '05', '06', '08', '09', '10', '11', '12'];
 
   return (
     <Flex gap={4} wrap justifyContent="center">
-      <TileData
-        id="1"
-        color="01"
-        selected={isSelected('1')}
-        onChange={({ id }) => setSelectedTileId(id)}
-        title="Impressions"
-        value="41.25m"
-      />
-      <TileData
-        id="2"
-        color="02"
-        selected={isSelected('2')}
-        onChange={({ id }) => setSelectedTileId(id)}
-        title="CPM"
-        value="$1.57"
-      />
-      <TileData
-        id="4"
-        color="04"
-        selected={isSelected('4')}
-        onChange={({ id }) => setSelectedTileId(id)}
-        title="CPC"
-        value="$1.57"
-      />
-      <TileData
-        id="5"
-        color="05"
-        selected={isSelected('5')}
-        onChange={({ id }) => setSelectedTileId(id)}
-        title="ROAS"
-        value="$1.57"
-      />
-      <TileData
-        id="6"
-        color="06"
-        selected={isSelected('6')}
-        onChange={({ id }) => setSelectedTileId(id)}
-        title="Spend"
-        value="$1.57"
-      />
+      {allColors.map((color) => (
+        <TileData
+          key={color}
+          color={color}
+          selected
+          onChange={() => {}}
+          title={`Color ${color}`}
+          value="41.25m"
+        />
+      ))}
     </Flex>
   );
 }
--- docs/examples/tiledata/disabled.js
+++ docs/examples/tiledata/disabled.js
@@ -3,5 +3,13 @@ import { type Node } from 'react';
 import { TileData } from 'gestalt';
 
 export default function Example(): Node {
-  return <TileData tooltip={{ text: 'Weekly Active Users' }} title="WAU" value="1.25M" disabled />;
+  return (
+    <TileData
+      tooltip={{ text: 'Weekly Active Users' }}
+      title="WAU"
+      value="1.25M"
+      disabled
+      trend={{ value: 20, accessibilityLabel: 'Trending up' }}
+    />
+  );
 }
