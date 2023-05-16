26cc7ec2e68bbd32488eb67b290ee1d96f4db076
Ravi Lingineni <rlingineni@pinterest.com>
2023-05-16 12:33:49 -0700

    adding missing color to test

--- packages/gestalt/src/TileData.test.js
+++ packages/gestalt/src/TileData.test.js
@@ -26,7 +26,7 @@ describe('TileData', () => {
   });
 
   test('TileData renders all colors as expected', () => {
-    const colors = ['01', '02', '03', '04', '06', '07', '08', '09', '10', '11', '12'];
+    const colors = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
     const component = create(
       <Flex gap={2}>
         {colors.map((color) => (
