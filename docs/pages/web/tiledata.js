800f5d467b3cfd7fc2c68fd99052e553edd60e2a
Raviteja Lingineni <rlingineni@pinterest.com>
2023-05-24 14:02:37 -0700

    fix casing in url

--- docs/pages/web/tiledata.js
+++ docs/pages/web/tiledata.js
@@ -163,7 +163,7 @@ export default function TileDataPage({ generatedDocGen }: {| generatedDocGen: Do
       <MainSection name="Related">
         <MainSection.Subsection
           description={`
-      **[Datapoint](/web/Datapoint)**
+      **[Datapoint](/web/datapoint)**
       Used to display data at-a-glance data for a user to quickly view key metrics.
       
       **[Checkbox](/web/checkbox)**
