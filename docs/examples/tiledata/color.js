e0dcec453e71b81fbe9b5bf77f73f4196d30686f
Ravi Lingineni <rlingineni@pinterest.com>
2023-05-25 17:18:19 -0700

    Fix TileData Examples

--- docs/examples/tiledata/color.js
+++ docs/examples/tiledata/color.js
@@ -1,6 +1,6 @@
 // @flow strict
 import { type Node, useState } from 'react';
-import { TileData, Flex } from 'gestalt';
+import { Box, TileData, Flex } from 'gestalt';
 
 type DataVisualizationColors =
   | '01'
@@ -34,28 +34,30 @@ export default function Example(): Node {
   const [selectedColors, setSelectedColors] = useState(allColors);
 
   return (
-    <Flex gap={4} wrap justifyContent="center">
-      {allColors.map((color) => (
-        <TileData
-          id={color}
-          key={color}
-          color={color}
-          selected={selectedColors.includes(color)}
-          onTap={({ id: selectedId, selected }) => {
-            if (!selectedId) {
-              return;
-            }
+    <Box overflow="scrollY" height="90%" padding={2}>
+      <Flex gap={4} wrap justifyContent="center">
+        {allColors.map((color) => (
+          <TileData
+            id={color}
+            key={color}
+            color={color}
+            selected={selectedColors.includes(color)}
+            onTap={({ id: selectedId, selected }) => {
+              if (!selectedId) {
+                return;
+              }
 
-            setSelectedColors((currSelectedColors) =>
-              !selected
-                ? currSelectedColors.filter((tileId) => tileId !== color)
-                : currSelectedColors.concat([color]),
-            );
-          }}
-          title={`Color ${color}`}
-          value="41.25m"
-        />
-      ))}
-    </Flex>
+              setSelectedColors((currSelectedColors) =>
+                !selected
+                  ? currSelectedColors.filter((tileId) => tileId !== color)
+                  : currSelectedColors.concat([color]),
+              );
+            }}
+            title={`Color ${color}`}
+            value="41.25m"
+          />
+        ))}
+      </Flex>
+    </Box>
   );
 }
