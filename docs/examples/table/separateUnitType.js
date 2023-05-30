f400db46fb382d551b43f0c5dfa2e6e428e1ea35
Diyorbek Sadullaev <dsadullaev@pinterest.com>
2023-05-30 20:05:27 +0200

    fix eslint error

--- docs/examples/table/separateUnitType.js
+++ docs/examples/table/separateUnitType.js
@@ -1,6 +1,6 @@
 // @flow strict
 import { type Node } from 'react';
-import { Table, Text, Status, Box } from 'gestalt';
+import { Table, Text, Status } from 'gestalt';
 
 function HeaderRow() {
   return (
