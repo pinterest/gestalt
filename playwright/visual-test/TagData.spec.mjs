bec68dd1612ad8eb3b80ee987ae8f3d00b883af1
Ravi Lingineni <rlingineni@pinterest.com>
2023-06-07 10:28:42 -0700

    Update TagData.spec.mjs

--- playwright/visual-test/TagData.spec.mjs
+++ playwright/visual-test/TagData.spec.mjs
@@ -4,5 +4,5 @@ import { test, expect } from '@playwright/test';
 test('TagData light mode visual regression check', async ({ page }) => {
   await page.goto('/visual-test/TagData');
   const locator = page.locator('[data-test-id="visual-test"]');
-  await expect(locator).toHaveScreenshot('TagData-light.png');
+  await expect(locator).toHaveScreenshot('TagData.png');
 });
