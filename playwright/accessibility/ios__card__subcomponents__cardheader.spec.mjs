5bdf8d93107f4bc6b45ca0ab741a4f21a8634e6d
Hector Jaime <hjaime@pinterest.com>
2023-06-08 10:43:50 -0700

    fixed side nav path
    removed capitalization in side nav. Subhead to subhead

--- playwright/accessibility/ios__card__subcomponents__cardheader.spec.mjs
+++ playwright/accessibility/ios__card__subcomponents__cardheader.spec.mjs
@@ -2,7 +2,7 @@
 import { test } from '@playwright/test';
 import expectAccessiblePage from './expectAccessiblePage.mjs';
 
-test('iOS Card Accessibility check', async ({ page }) => {
+test('iOS Card.Header Accessibility check', async ({ page }) => {
   await page.goto('/ios/card/subcomponents/cardheader');
   await expectAccessiblePage({ page });
 });
