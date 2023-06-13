6b7886a8e66e0137235def3ee78f99a63ee3bfb2
Ryan James <rjames@pinterest.com>
2023-06-12 17:25:52 -0700

    fix link

--- docs/pages/web/masonry.js
+++ docs/pages/web/masonry.js
@@ -35,7 +35,7 @@ export default function DocsPage({ generatedDocGen }: {| generatedDocGen: DocGen
       <MainSection name="How Masonry works">
         <MainSection.Subsection
           description={`
-          Generally, Masonry renders items in two passes: an initial render off-screen to collect measurements, then an on-screen render with the correct measurements. This is necessary because we need to know the height of each item before we can render it in the correct position. This mental model is necessary to understand [the \`serverRenderedFlexible\` layout](/web/masonry#Flexible-layouts), as well as [the common overlap / extra vertical whitespace bug]().
+          Generally, Masonry renders items in two passes: an initial render off-screen to collect measurements, then an on-screen render with the correct measurements. This is necessary because we need to know the height of each item before we can render it in the correct position. This mental model is necessary to understand [the \`serverRenderedFlexible\` layout](/web/masonry#Flexible-layouts), as well as [the common overlap / extra vertical whitespace bug](/web/masonry#Why-is-there-too-much-too-little-vertical-whitespace-between-items).
 
           Check out [this README](https://github.com/pinterest/gestalt/blob/master/packages/gestalt/src/Masonry/README.md) for more details about how Masonry works. Pinterest employees can also check out [this PDocs page](https://pdocs.pinadmin.com/docs/webapp/masonry-ssr) to learn more about our Masonry SSR optimizations in Pinboard.
           `}
