be8ca4cfef09815a481ba6d7da96246f3f4bf66d
Ravi Lingineni <rlingineni@pinterest.com>
2023-06-28 14:01:07 -0700

    Support Text Type in Callout

---
+++ docs/examples/callout/variantMessage.js
@@ -0,0 +1,43 @@
+// @flow strict
+import React, { type Node } from 'react';
+import { Box, Callout, Flex, Text } from 'gestalt';
+
+export default function ResponsiveExample(): Node {
+  return (
+    <Flex alignItems="center" height="100%" justifyContent="center" width="100%">
+      <Box paddingY={8} paddingX={8}>
+        <Callout
+          dismissButton={{
+            accessibilityLabel: 'Dismiss this banner',
+            onDismiss: () => {},
+          }}
+          iconAccessibilityLabel="Info"
+          message={
+            <Text inline>
+              It may take up to 10 minutes to automatically detect a newly installed tag. If you
+              would like to manually verify your tag, please click the{' '}
+              <Text inline weight="bold">
+                Verify Tag
+              </Text>{' '}
+              button.
+            </Text>
+          }
+          primaryAction={{
+            accessibilityLabel: 'Get started: Verified Merchant Program',
+            href: 'https://pinterest.com',
+            label: 'Get started',
+            target: 'blank',
+          }}
+          secondaryAction={{
+            accessibilityLabel: 'Learn more: Verified Merchant Program',
+            href: 'https://pinterest.com',
+            label: 'Learn more',
+            target: 'blank',
+          }}
+          title="We have not yet detected your tag"
+          type="info"
+        />
+      </Box>
+    </Flex>
+  );
+}
