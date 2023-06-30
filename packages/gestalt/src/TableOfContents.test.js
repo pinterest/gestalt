18023958a2a28342befd868136243e4519da5405
Diyorbek Sadullaev <dsadullaev@pinterest.com>
2023-06-30 19:01:11 +0200

    add tests

--- packages/gestalt/src/TableOfContents.test.js
+++ packages/gestalt/src/TableOfContents.test.js
@@ -4,12 +4,20 @@ import TableOfContents from './TableOfContents.js';
 
 describe('TableOfContents', () => {
   it('renders', () => {
-    const tree = create(<TableOfContents />).toJSON();
+    const tree = create(
+      <TableOfContents title="Title" items={[{ label: 'Label', href: '#' }]} />,
+    ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('renders an accessibility label', () => {
-    const tree = create(<TableOfContents />).toJSON();
+    const tree = create(
+      <TableOfContents
+        title="Title"
+        items={[{ label: 'Label', href: '#' }]}
+        accessibilityLabel="Page table of contents"
+      />,
+    ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
