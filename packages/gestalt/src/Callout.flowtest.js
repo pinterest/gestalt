ade73ff81de36ec833f40371c562509ddb155835
Ravi Lingineni <rlingineni@pinterest.com>
2023-07-05 11:07:54 -0700

    add flowtest

--- packages/gestalt/src/Callout.flowtest.js
+++ packages/gestalt/src/Callout.flowtest.js
@@ -1,8 +1,19 @@
 // @flow strict
+import Box from './Box.js';
 import Callout from './Callout.js';
+import Text from './Text.js';
 
 const Valid = (
   <Callout message="Callout error message" iconAccessibilityLabel="error" type="error" />
+);
+
+const validTextProp = (
+  <Callout message={<Text>Hello</Text>} iconAccessibilityLabel="error" type="error" />
+);
+
+const invalidTextProp = (
+  // $FlowExpectedError[incompatible-type]
+  <Callout message={<Box>Hello</Box>} iconAccessibilityLabel="error" type="error" />
 );
 
 // $FlowExpectedError[prop-missing]
