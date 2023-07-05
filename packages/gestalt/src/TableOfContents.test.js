23c8713ea81c0cdbc74ae4a7aab43605c0fbbe1b
Diyorbek Sadullaev <dsadullaev@pinterest.com>
2023-07-05 19:01:43 +0200

    add more snapshots

--- packages/gestalt/src/TableOfContents.test.js
+++ packages/gestalt/src/TableOfContents.test.js
@@ -5,7 +5,10 @@ import TableOfContents from './TableOfContents.js';
 describe('TableOfContents', () => {
   it('renders', () => {
     const tree = create(
-      <TableOfContents title="Title" items={[{ label: 'Label', href: '#' }]} />,
+      <TableOfContents
+        title="Title"
+        items={[{ label: 'Item', href: '#', active: true, onClick: () => {} }]}
+      />,
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
@@ -14,8 +17,38 @@ describe('TableOfContents', () => {
     const tree = create(
       <TableOfContents
         title="Title"
-        items={[{ label: 'Label', href: '#' }]}
+        items={[{ label: 'Item', href: '#' }]}
         accessibilityLabel="Page table of contents"
+      />,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders without title', () => {
+    const tree = create(
+      <TableOfContents
+        items={[
+          { label: 'Item 1', href: '#1', active: true },
+          { label: 'Item 2', href: '#2' },
+        ]}
+      />,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders nested items', () => {
+    const tree = create(
+      <TableOfContents
+        items={[
+          {
+            label: 'Item',
+            href: '#1',
+            nestedItems: [
+              { label: 'Subitem 1', href: '#11', active: true },
+              { label: 'Subitem 2', href: '#12' },
+            ],
+          },
+        ]}
       />,
     ).toJSON();
     expect(tree).toMatchSnapshot();
