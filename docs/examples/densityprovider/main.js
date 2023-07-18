09f807e1b08042ee5539072799bb2b10238f24e0
Ravi Lingineni <rlingineni@pinterest.com>
2023-07-17 18:24:36 -0700

    working scoped density provider

--- docs/examples/densityprovider/main.js
+++ docs/examples/densityprovider/main.js
@@ -1,44 +1,56 @@
 // @flow strict
-import { type Node } from 'react';
-import { Box, Button, SegmentedControl, Text, TextField } from 'gestalt';
-
-const SegmentedControlItems = ['News', 'You', 'Messages'];
-
-function Section({ children, title }: {| children: Node, title?: string |}) {
-  return (
-    <Box marginTop={2} marginBottom={2}>
-      <Box marginTop={2} marginBottom={2}>
-        <Text weight="bold" size="200">
-          {title}
-        </Text>
-      </Box>
-      {children}
-    </Box>
-  );
-}
+import { type Node, useState } from 'react';
+import { Box, Button, DensityProvider, SegmentedControl, Text, TextField } from 'gestalt';
 
 export default function Example(): Node {
+  const SegmentedControlItems = ['News', 'You', 'Messages'];
+  const sizes = ['sm', 'md', 'lg'];
+
+  const [selectedIndex, setSelectedIndex] = useState(1);
+
+  // eslint-disable-next-line react/no-unstable-nested-components
+  function Section({ children, title }: {| children: Node, title?: string |}) {
+    return (
+      <Box marginTop={2} marginBottom={2}>
+        <Box marginTop={2} marginBottom={2}>
+          <Text weight="bold" size="200">
+            {title}
+          </Text>
+        </Box>
+        {children}
+      </Box>
+    );
+  }
+
   return (
     <Box display="flex" direction="column" padding={10} height="100%" width="80%" overflow="scroll">
       <Text weight="bold" size="500">
         Gestalt Density Provider
       </Text>
       <Box marginTop={2} />
-      <SegmentedControl items={['Sm', 'Md', 'Lg']} />
+      <SegmentedControl
+        items={sizes}
+        selectedItemIndex={selectedIndex}
+        onChange={({ activeIndex }) => {
+          setSelectedIndex(activeIndex);
+        }}
+      />
 
       <Box marginTop={2} marginBottom={2} />
 
-      <Section title="Button">
-        <Button text="Submit" />
-      </Section>
+      <DensityProvider size={sizes[selectedIndex]}>
+        <Section title="Button">
+          <Button text="Submit" />
+        </Section>
 
-      <Section title="TextField">
-        <TextField id="example" onChange={() => {}} placeholder="First name" />
-      </Section>
+        <Section title="TextField">
+          <TextField id="example" onChange={() => {}} placeholder="First name" />
+        </Section>
 
-      <Section title="Segmented Control">
-        <SegmentedControl items={SegmentedControlItems} />
-      </Section>
+        <Section title="Segmented Control">
+          <SegmentedControl items={SegmentedControlItems} />
+        </Section>
+      </DensityProvider>
     </Box>
   );
 }
--- packages/gestalt/src/DensityProvider.js
+++ packages/gestalt/src/DensityProvider.js
@@ -1,24 +1,76 @@
 // @flow strict
-import { type Node } from 'react';
-import Box from './Box.js';
-import styles from './DensityProvider.css';
+import { type Node, useEffect, useRef } from 'react';
 
 type Props = {|
   /**
-   * Prop description.
+   * Children that density will be applied to.
    */
-  accessibilityLabel?: string,
+  children?: React.Node,
+  /**
+   * Which density to apply to children.
+   */
+  size: 'sm' | 'md' | 'lg',
 |};
+
+// move everything down by 4px, or increment by 4px
+const generateTheme = (size: 'sm' | 'md' | 'lg') => {
+  let baseScale = 4; // 100
+
+  if (size === 'sm') {
+    baseScale = 0;
+  }
+
+  if (size === 'lg') {
+    baseScale = 8;
+  }
+  // generate 16 iterations of the scale
+  const scale = Array.from({ length: 16 }, (_, i) => i * 4 + baseScale);
+
+  // insert a 0 at the start of the scale, so that the first item is 0
+  scale.unshift(0);
+  return scale;
+};
+
+/**
+ * Converts spacing tokens to dynamic variables.
+ */
+const themeToStyles = (size: 'sm' | 'md' | 'lg') => {
+  const theme = generateTheme(size);
+
+  const styles = {};
+
+  theme.forEach((val, i) => {
+    const tokenName = `--space-${i * 100}`;
+    styles[tokenName] = `${val}px`;
+  });
+
+  console.log(styles);
+  return styles;
+};
 
 /**
  * [DensityProvider](https://gestalt.pinterest.systems/web/densityprovider) component should be used for ... on the page.
  * ![DensityProvider light mode](https://raw.githubusercontent.com/pinterest/gestalt/master/playwright/visual-test/DensityProvider.spec.mjs-snapshots/DensityProvider-chromium-darwin.png)
  * ![DensityProvider dark mode](https://raw.githubusercontent.com/pinterest/gestalt/master/playwright/visual-test/DensityProvider-dark.spec.mjs-snapshots/DensityProvider-dark-chromium-darwin.png)
  */
-export default function DensityProvider({ accessibilityLabel }: Props): Node {
+export default function DensityProvider({ children, size = 'md' }: Props): Node {
+  const providerRef = useRef();
+
+  useEffect(() => {
+    if (providerRef.current) {
+      const theme = themeToStyles(size);
+      Object.keys(theme).forEach((key) => {
+        console.log(key, theme[key]);
+        providerRef.current.style.setProperty(key, theme[key]);
+      });
+    }
+  }, [size]);
+
+  // add a css style tag with the theme expanded inline and set as dangerous html
+
   return (
-    <Box aria-label={accessibilityLabel}>
-      <div className={styles.customClass} />
-    </Box>
+    <density-provider size={size} ref={providerRef}>
+      {children}
+    </density-provider>
   );
 }
