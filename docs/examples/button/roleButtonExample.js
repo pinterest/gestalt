b37776d0dfb9fa17b67b85f3073d932937d673ab
eniomoura <erochamoura@pinterest.com>
2023-07-25 18:26:10 -0300

    reverting change to button docs

--- docs/examples/button/roleButtonExample.js
+++ docs/examples/button/roleButtonExample.js
@@ -5,7 +5,7 @@ import { Button, Flex } from 'gestalt';
 export default function Example(): Node {
   return (
     <Flex alignItems="center" height="100%" justifyContent="center" width="100%">
-      <Button accessibilityLabel="Follow" size="lg" text="Follow" />
+      <Button accessibilityLabel="Follow" size="lg" text="Follow" role="button" />
     </Flex>
   );
 }
