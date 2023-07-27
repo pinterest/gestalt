c962ce82c34d52fb0533216343b0934b16c3dd98
acarreras <acarreras@pinterest.com>
2023-07-27 18:03:00 -0400

    change

--- packages/gestalt/src/Caret.js
+++ packages/gestalt/src/Caret.js
@@ -1,12 +1,8 @@
 // @flow strict
 import { type Node } from 'react';
 
-const DOWN =
-  'M0 4c.694 0 1.36-.278 1.846-.773L4.376.66c.806-.819 2.187-.885 3.083-.148.057.047.111.096.163.148l2.526 2.565A2.6 2.6 0 0012 4';
-
 const PATHS = {
-  forceDown: DOWN,
-  down: DOWN,
+  down: 'M0 4c.694 0 1.36-.278 1.846-.773L4.376.66c.806-.819 2.187-.885 3.083-.148.057.047.111.096.163.148l2.526 2.565A2.6 2.6 0 0012 4',
   left: 'M0 0c0 .694.278 1.36.773 1.846l2.567 2.53c.819.806.885 2.187.148 3.083a2.098 2.098 0 01-.148.163L.775 10.148A2.6 2.6 0 000 12',
   right:
     'M4 12c0-.694-.278-1.36-.773-1.847L.66 7.625C-.159 6.818-.225 5.437.512 4.54c.047-.057.096-.111.148-.163l2.565-2.526A2.6 2.6 0 004 0',
@@ -22,7 +18,7 @@ type Props = {|
 export default function Caret(props: Props): Node {
   const { direction, height, width } = props;
 
-  const path = PATHS[direction];
+  const path = PATHS[direction === 'forceDown' ? 'down' : direction];
 
   return (
     <svg height={height} width={width}>
