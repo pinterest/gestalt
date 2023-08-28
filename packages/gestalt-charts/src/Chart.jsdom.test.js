58b0abb1c3f574e263780e9efdc308dced746613
acarreras <acarreras@pinterest.com>
2023-08-28 12:12:48 +0200

    more

--- packages/gestalt-charts/src/Chart.jsdom.test.js
+++ packages/gestalt-charts/src/Chart.jsdom.test.js
@@ -1,11 +1,89 @@
 // @flow strict-local
-import { render, screen } from '@testing-library/react';
-import { Text } from 'gestalt';
+import { create } from 'react-test-renderer';
+import { Flex, Text } from 'gestalt';
+import { Chart } from 'gestalt-charts';
+
+const data = [
+  {
+    name: 'A',
+    'bar_1': 2400,
+    'bar_2': 2400,
+    'bar_3': 2400,
+  },
+  {
+    name: 'B',
+    'bar_1': 2400,
+    'bar_2': 2400,
+    'bar_3': 2400,
+  },
+  {
+    name: 'C',
+    'bar_1': 2400,
+    'bar_2': 9800,
+    'bar_3': 2400,
+  },
+  {
+    name: 'D',
+    'bar_1': 2400,
+    'bar_2': 2400,
+    'bar_3': 2400,
+  },
+  {
+    name: 'E',
+    'bar_1': 2400,
+    'bar_2': 2400,
+    'bar_3': 2400,
+  },
+  {
+    name: 'F',
+    'bar_1': 2400,
+    'bar_2': 2400,
+    'bar_3': 2400,
+  },
+  {
+    name: 'G',
+    'bar_1': 2400,
+    'bar_2': 2400,
+    'bar_3': 2400,
+  },
+];
+
+const { ResizeObserver } = window;
 
 describe('Chart', () => {
-  test('renders correctly', async () => {
-    render(<Text>123</Text>);
+  beforeEach(() => {
+    delete window.ResizeObserver;
+    window.ResizeObserver = jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn(),
+    }));
+  });
 
-    expect(screen.getByText('123')).toBeInTheDocument();
+  afterEach(() => {
+    window.ResizeObserver = ResizeObserver;
+    jest.restoreAllMocks();
+  });
+
+  it('renders', () => {
+    const tree = create(
+      <Flex direction="column" width="100%" height="100%">
+        <Chart
+          type="bar"
+          data={data}
+          renderTooltip={({ label }) => (
+            <Flex direction="column" gap={2}>
+              <Text>{label}</Text>
+            </Flex>
+          )}
+          elements={[
+            { type: 'bar', id: 'bar_01', color: '01', yAxis: 'left' },
+            { type: 'bar', id: 'bar_02', color: '02', yAxis: 'left' },
+            { type: 'bar', id: 'bar_03', color: '03', yAxis: 'left' },
+          ]}
+        />
+      </Flex>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
