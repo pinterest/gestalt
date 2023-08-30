48583be88517a1f85af71a38c52b5a1318e277f6
Diyorbek Sadullaev <dsadullaev@pinterest.com>
2023-08-30 15:25:58 +0200

    WIP

--- docs/examples/iconography/dontTruncateTextInControls.js
+++ docs/examples/iconography/dontTruncateTextInControls.js
@@ -1,9 +1,13 @@
 // @flow strict
-import { type Node } from "react";
-import { Box, Button } from "gestalt";
+import { type Node } from 'react';
+import { Box, Button } from 'gestalt';
 
 export default function Example(): Node {
-    return <Box padding={8} height="100%" display="flex" alignItems="center" justifyContent="center">
-    <Box width={100}><Button color="red" text="Go to..." /></Box>
-    </Box>;
+  return (
+    <Box padding={8} height="100%" display="flex" alignItems="center" justifyContent="center">
+      <Box width={100}>
+        <Button color="red" text="Go to..." />
+      </Box>
+    </Box>
+  );
 }
