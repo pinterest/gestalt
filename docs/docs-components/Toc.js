7f91a7a24b38b6c8d4dc8fa8dd157abeb850b34b
Diyorbek Sadullaev <diyorbek3377@gmail.com>
2023-08-30 19:17:33 +0200

    Docs: Fix ToC overflow (#3176)

--- docs/docs-components/Toc.js
+++ docs/docs-components/Toc.js
@@ -205,7 +205,7 @@ export default function Toc({ cards }: Props): Node {
       mdMarginTop={-6}
       lgMarginTop={-8}
       maxHeight={`calc(100% - ${HEADER_HEIGHT_PX}px - ${FOOTER_HEIGHT_PX}px)`}
-      overflow="visible"
+      overflow="auto"
       paddingX={1}
       paddingY={8} // re-apply just the padding we need
       position="fixed"
