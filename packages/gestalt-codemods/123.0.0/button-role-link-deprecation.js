c4f7bee2c255a72aa1e74fe160c696bd76ad60d3
eniomoura <erochamoura@pinterest.com>
2023-09-13 17:12:01 -0300

    codemod wip more

--- packages/gestalt-codemods/123.0.0/button-role-link-deprecation.js
+++ packages/gestalt-codemods/123.0.0/button-role-link-deprecation.js
@@ -14,86 +14,75 @@
 export default function transformer(file, api) {
   const j = api.jscodeshift;
   const src = j(file.source);
-  let codeHasChanged = false;
+  let codeHasChanged;
 
-  const imports = src.find(j.ImportDeclaration);
+  // Find all import declarations
+  const importDeclarations = j(file.source).find(j.ImportDeclaration);
 
-  // Store component mappings
-  const componentMappings = {
-    Button: 'Button',
-    Callout: 'Callout',
-    ModalAlert: 'ModalAlert',
-    PopoverEducational: 'PopoverEducational',
-    SlimBanner: 'SlimBanner',
-    Toast: 'Toast',
-    Upsell: 'Upsell',
-  };
+  // Map import specifiers to their original names
+  const importSpecifierMap = {};
 
-  imports.forEach((path) => {
-    const importSource = path.value.source.value;
-    const importSpecifiers = path.value.specifiers;
-
-    // Check if the import is being renamed
-    if (importSource && importSpecifiers) {
-      importSpecifiers.forEach((specifier) => {
-        if (specifier.type === 'ImportSpecifier') {
-          const originalName = specifier.imported.name;
-          const renamedName = specifier.local.name;
-          componentMappings[renamedName] = componentMappings[originalName];
-        }
-      });
-    }
+  importDeclarations.forEach((path) => {
+    path.node.specifiers.forEach((specifier) => {
+      if (specifier.type === 'ImportSpecifier') {
+        const originalName = specifier.imported.name;
+        const localName = specifier.local.name;
+        importSpecifierMap[localName] = originalName;
+      }
+    });
   });
 
-  // Helper function to update an attribute's value
-  function updateAttribute(attributes, attributeName, newValue) {
-    return attributes.map((attr) => {
-      if (attr.name.name === attributeName) {
-        return j.jsxAttribute(
-          j.jsxIdentifier(attributeName),
-          j.jsxExpressionContainer(j.literal(newValue)),
-        );
-      }
-      codeHasChanged = true;
-      return attr;
-    });
-  }
+  const componentsToTransform = [
+    'Callout',
+    'ModalAlert',
+    'PopoverEducational',
+    'SlimBanner',
+    'Toast',
+    'Upsell',
+  ];
 
   const transform = src
     .find(j.JSXElement)
+    .filter((path) => {
+      const { openingElement } = path.node;
+      const elementName = openingElement.name.name;
+
+      return (
+        (componentsToTransform.includes(elementName) ||
+          componentsToTransform.includes(importSpecifierMap[elementName])) &&
+        openingElement.attributes.some(
+          (attr) =>
+            attr.name.name === 'primaryAction' &&
+            attr.value &&
+            attr.value.expression &&
+            attr.value.expression.properties.some(
+              (prop) => prop.key.name === 'href' && prop.value.type !== 'JSXExpressionContainer',
+            ),
+        )
+      );
+    })
     .forEach((path) => {
-      const node = path.value;
-      const componentName = node.openingElement.name.name;
+      const primaryActionAttribute = path.node.openingElement.attributes.find(
+        (attr) =>
+          attr.name.name === 'primaryAction' &&
+          attr.value &&
+          attr.value.expression &&
+          attr.value.expression.properties.some(
+            (prop) => prop.key.name === 'href' && prop.value.type !== 'JSXExpressionContainer',
+          ),
+      );
 
-      // Check if the component is in the mappings
-      if (componentMappings[componentName]) {
-        // Get the mapped component name
-        const mappedComponentName = componentMappings[componentName];
+      if (primaryActionAttribute) {
+        // Check if 'role' property is already present
+        const roleProperty = primaryActionAttribute.value.expression.properties.find(
+          (prop) => prop.key.name === 'role',
+        );
 
-        switch (mappedComponentName) {
-          case 'Button':
-            // Update Button role attribute
-            node.openingElement.attributes = updateAttribute(
-              node.openingElement.attributes,
-              'role',
-              undefined,
-            );
-            break;
-          case 'Callout':
-          case 'ModalAlert':
-          case 'PopoverEducational':
-          case 'SlimBanner':
-          case 'Toast':
-          case 'Upsell':
-            // Update primaryAction attribute
-            node.openingElement.attributes = updateAttribute(
-              node.openingElement.attributes,
-              'primaryAction',
-              { role: 'link', ...node.openingElement.attributes[0].value.expression },
-            );
-            break;
-          default:
-            break;
+        if (!roleProperty) {
+          primaryActionAttribute.value.expression.properties.push(
+            j.property('init', j.identifier('role'), j.literal('link')),
+          );
+          codeHasChanged = true;
         }
       }
     })
