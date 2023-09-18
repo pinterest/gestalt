98bc9f54a6ec356ab1fcda1c06721b9e03565e60
laylaleeal <63257116+laylaleeal@users.noreply.github.com>
2023-09-18 11:42:16 -0700

    Update siteIndex.js

--- docs/docs-components/siteIndex.js
+++ docs/docs-components/siteIndex.js
@@ -48,6 +48,7 @@ const siteIndex: $ReadOnlyArray<siteIndexType> = [
       'Overview',
       'Component status',
       'Avatar',
+      'AvatarGroup',
       'Badge',
       'Button',
       'ButtonGroup',
