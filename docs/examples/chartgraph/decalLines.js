21b458c315fdfb6490309e14d3b3c1dec2e7b09c
acarreras <acarreras@pinterest.com>
2023-09-19 22:41:33 +0200

    moew

--- docs/examples/chartgraph/decalLines.js
+++ docs/examples/chartgraph/decalLines.js
@@ -7,49 +7,31 @@ export default function Example(): Node {
 
   const data = [
     {
-      name: 'A',
+      name: new Date(2023, 1, 1).getTime(),
       'Color_01': 50,
       'Color_02': 100,
       'Color_03': 150,
       'Color_04': 200,
       'Color_05': 250,
       'Color_06': 300,
-      'Color_07': 350,
-      'Color_08': 400,
-      'Color_09': 450,
-      'Color_10': 500,
-      'Color_11': 550,
-      'Color_12': 600,
     },
     {
-      name: 'B',
+      name: new Date(2023, 5, 15).getTime(),
       'Color_01': 100,
       'Color_02': 150,
       'Color_03': 200,
       'Color_04': 250,
       'Color_05': 300,
       'Color_06': 350,
-      'Color_07': 400,
-      'Color_08': 450,
-      'Color_09': 500,
-      'Color_10': 550,
-      'Color_11': 600,
-      'Color_12': 650,
     },
     {
-      name: 'C',
+      name: new Date(2023, 10, 1).getTime(),
       'Color_01': 150,
       'Color_02': 200,
       'Color_03': 250,
       'Color_04': 300,
       'Color_05': 350,
       'Color_06': 400,
-      'Color_07': 450,
-      'Color_08': 500,
-      'Color_09': 550,
-      'Color_10': 600,
-      'Color_11': 650,
-      'Color_12': 700,
     },
   ];
 
@@ -61,6 +43,13 @@ export default function Example(): Node {
         setVisualPatternSelected((value) => (value === 'default' ? 'accessible' : 'default'))
       }
       type="line"
+      tickFormatter={{
+        timeseries: (date) =>
+          `${new Intl.DateTimeFormat('en-US', { month: 'short' }).format(
+            date,
+          )}-${new Intl.DateTimeFormat('en-US', { day: '2-digit' }).format(date)}`,
+      }}
+      range={{ xAxisBottom: [new Date(2023, 0, 1).getTime(), new Date(2023, 11, 1).getTime()] }}
       data={data}
       elements={[
         { type: 'line', id: 'Color_01' },
@@ -69,12 +58,6 @@ export default function Example(): Node {
         { type: 'line', id: 'Color_04' },
         { type: 'line', id: 'Color_05' },
         { type: 'line', id: 'Color_06' },
-        { type: 'line', id: 'Color_07' },
-        { type: 'line', id: 'Color_08' },
-        { type: 'line', id: 'Color_09' },
-        { type: 'line', id: 'Color_10' },
-        { type: 'line', id: 'Color_11' },
-        { type: 'line', id: 'Color_12', precision: 'estimate' },
       ]}
     />
   );
