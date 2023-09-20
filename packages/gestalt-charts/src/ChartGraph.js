c2a62a638f851ef94f435061dbd4c725f9d93652
acarreras <acarreras@pinterest.com>
2023-09-20 15:46:34 +0200

    more

--- packages/gestalt-charts/src/ChartGraph.js
+++ packages/gestalt-charts/src/ChartGraph.js
@@ -27,46 +27,54 @@ type Props = {|
   // REQUIRED
   /**
    * Label to provide more context around ChartGraph’s content.
+   *
    * See the [accessibility guidelines on ARIA attributes](https://gestalt.pinterest.systems/web/chartgraph#ARIA-attributes) to learn more.
    */
   accessibilityLabel: string,
   /**
    * The source data, in which each element is an object. Each object must specify a "name" associated to each category (string value) or timestamp (numberic value) in time series charts.
+   *
    * The additional key-values represent one or more series of data presented on ChartGraph for each category or timestamp. A sequence of source data objects generate one or more series of data across categories or timestamps.
-   * See the [controlled component variant](https://gestalt.pinterest.systems/web/chartgraph#Controlled-component) to learn more about implementing the `data` prop.
    */
   data: $ReadOnlyArray<{|
     name: string | number,
     [string]: number,
   |}>,
   /**
-   * The series elements, bars or lines, of the ChartGraph. See the [color variant](https://gestalt.pinterest.systems/web/chartgraph#Layout), [precision variant](https://gestalt.pinterest.systems/web/chartgraph#Precision), and [layout] to learn more about configuring bars and lines.
+   * The series elements, bars or lines, of the ChartGraph.
+   *
+   * See the [combo variants](https://gestalt.pinterest.systems/web/chartgraph#Layout), [color variant](https://gestalt.pinterest.systems/web/chartgraph#Combo), [layout](https://gestalt.pinterest.systems/web/chartgraph#Layout), [color variant](https://gestalt.pinterest.systems/web/chartgraph#Color), [precision in line graphs variant](https://gestalt.pinterest.systems/web/chartgraph#Precision-in-line-graphs) to learn more about configuring bars and lines.
    */
   elements: $ReadOnlyArray<{|
-    type: 'line' | 'bar',
     axis?: 'left' | 'right' | 'bottom' | 'top',
-    id: string,
     color?: '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10' | '11' | '12',
+    id: string,
     precision?: 'exact' | 'estimate',
+    type: 'line' | 'bar',
   |}>,
   /**
    * Callback fired when the Accessibility IconButton in ChartGraph is clicked. ChartGraph's visual patterns is a controlled feature. `onVisualPatternChange` is used to enable/disable visual patterns in ChartGraph.
+   *
    * See the [accessibility guidelines on visual patterns](https://gestalt.pinterest.systems/web/chartgraph#Visual-patterns) to learn more.
    */
   onVisualPatternChange: () => void,
   /**
    * ChartGraph's visual patterns is a controlled feature. `visualPatternSelected` manages visual patterns in ChartGraph. When ChartGraph represents a standalone data series that don't require comparison, the Accessibility IconButton can be hidden with "disabled".
+   *
    * See the [accessibility guidelines on visual patterns](https://gestalt.pinterest.systems/web/chartgraph#Visual-patterns) to learn more.
    */
   visualPatternSelected: 'disabled' | 'default' | 'accessible',
   // OPTIONAL
   /**
    * Description of ChartGraph. Be sure to localize the text.
+   *
    * See the [title variant](https://gestalt.pinterest.systems/web/chartgraph#Title-and-description) to learn more.
    */
   description?: string,
   /**
    * The start and end values of the axis.
+   *
+   * See the [range variant](https://gestalt.pinterest.systems/web/chartgraph#Range) to learn more.
    */
   range?:
     | [
@@ -95,21 +103,25 @@ type Props = {|
       |},
   /**
    * Replaces the labels from `data` in default tooltips, legends, and axis. Use for lacalization.
+   *
    * See the [localizations section](https://gestalt.pinterest.systems/web/chartgraph#Localization) to learn more.
    */
   labelMap?: {| [string]: string |},
   /**
    * Sets the horizontal or vertical layout of bars and lines and the single or double axis in the chart.
+   *
    * See the [layout variant](https://gestalt.pinterest.systems/web/chartgraph#Layout) to learn more.
    */
   layout?: 'horizontal' | 'vertical' | 'horizontalBiaxial' | 'verticalBiaxial',
   /**
    * Displays data about the datasets that are appearing on the chart.
+   *
    * See the [legend variant](https://gestalt.pinterest.systems/web/chartgraph#Legend) to learn more.
    */
   legend?: 'auto' | 'none',
   /**
    * Sets non-data visual areas in ChartGraph.
+   *
    * See the [reference area variant](https://gestalt.pinterest.systems/web/chartgraph#ReferenceArea) to learn more.
    */
   referenceAreas?: $ReadOnlyArray<{|
@@ -124,6 +136,7 @@ type Props = {|
   |}>,
   /**
    * Displays data about the datasets on hover over each data point.
+   *
    * See the [tooltip variant](https://gestalt.pinterest.systems/web/chartgraph#Tooltip) to learn more.
    */
   renderTooltip?:
@@ -132,22 +145,28 @@ type Props = {|
     | (({| active: ?boolean, payload: ?{ ... }, label: string | number |}) => Node),
   /**
    * When set to "true", bars are stacked.
-   * See the [grouped and stacked bars variant](https://gestalt.pinterest.systems/web/chartgraph#Grouped-and-Stacked-bars) to learn more.
+   *
+   * See the [stacked bars variant](https://gestalt.pinterest.systems/web/chartgraph#Stacked-bars) to learn more.
    */
   stacked?: boolean,
   /**
    * Title of ChartGraph. Be sure to localize the text.
-   * See the [title variant](https://gestalt.pinterest.systems/web/chartgraph#Title-and-description) to learn more.
+   *
+   * See the [title and description variant](https://gestalt.pinterest.systems/web/chartgraph#Title-and-description) to learn more.
    */
   title?: string,
   /**
    * ChartGraph is responsive. If your ChartGraph's width is below the 576 px breakpoint, ChartGraph will flick and correct the amount of ticks. To prevent that, set initialTicks to 3. ChartGraph above 576 px, don't require this adjusment.
+   *
+   * See the [color variant](https://gestalt.pinterest.systems/web/chartgraph#Colors) for implementation guidance.
    */
   initialTicks?: 'auto' | 3,
   /**
    * A function for formatting ticks on the axis.
    *
    * Timeseries require the 'timeseries' key for formatting dates in the axis and tooltip. The 'xAxisBottom' overrides 'timeseries' when are both present.
+   *
+   * See the [tick format variant](https://gestalt.pinterest.systems/web/chartgraph#Tick-format) and [time series variant](https://gestalt.pinterest.systems/web/chartgraph#Time-series) to learn more
    */
   tickFormatter?: {|
     timeseries?: (number) => string | number,
@@ -157,7 +176,9 @@ type Props = {|
     yAxisLeft?: (number, number) => string | number,
   |},
   /**
-   * Open slot for TagData or TileData. See the [TagData variant](https://gestalt.pinterest.systems/web/chartgraph#TagData) to learn more.
+   * Open slot for TagData or TileData.
+   *
+   * See the [TagData variant](https://gestalt.pinterest.systems/web/chartgraph#TagData) or the [TileData variant](https://gestalt.pinterest.systems/web/chartgraph#TileData) to learn more.
    */
 
   selectors?:
