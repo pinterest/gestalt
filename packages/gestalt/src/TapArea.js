c4343f2dd1a249687d962d4d5b9d633213c9a335
acarreras <acarreras@pinterest.com>
2023-09-21 23:51:33 +0200

    more

--- packages/gestalt/src/TapArea.js
+++ packages/gestalt/src/TapArea.js
@@ -110,6 +110,10 @@ type Props = {|
     event: SyntheticMouseEvent<HTMLDivElement> | SyntheticKeyboardEvent<HTMLDivElement>,
   |}) => void,
   /**
+   * Ref that is forwarded to the underlying div element.
+   */
+  ref?: HTMLDivElement,
+  /**
    * Select 'button' when TapArea acts like regular buttons and 'switch' when the TapArea represents the states "on" and "off."
    */
   role?: 'button' | 'switch',
