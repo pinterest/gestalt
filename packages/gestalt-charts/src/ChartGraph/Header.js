1eef16e8ec14b998ffdcffa826d7a06586916d92
acarreras <acarreras@pinterest.com>
2023-09-29 02:25:35 +0200

    more

--- packages/gestalt-charts/src/ChartGraph/Header.js
+++ packages/gestalt-charts/src/ChartGraph/Header.js
@@ -46,7 +46,7 @@ export default function Header({
         </Flex.Item>
         <IconButton
           accessibilityLabel=""
-          icon="apps"
+          icon="table"
           iconColor={readyToRender ? 'darkGray' : 'white'}
           bgColor={readyToRender ? undefined : 'transparent'}
           onClick={toggleTabularDataModal}
@@ -57,10 +57,7 @@ export default function Header({
         {showVisualPattern === 'disabled' ? null : (
           <IconButton
             accessibilityLabel=""
-            dangerouslySetSvgPath={{
-              __path:
-                'M16 11h-3v2.667l2.8 3.733a1 1 0 0 1-1.6 1.2L12 15.667 9.8 18.6a1 1 0 1 1-1.6-1.2l2.8-3.733V11H8a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2zm-4-7a2 2 0 1 1-.001 4.001A2 2 0 0 1 12 4zm0-4C5.373 0 0 5.373 0 12c0 6.628 5.373 12 12 12s12-5.372 12-12c0-6.627-5.373-12-12-12z',
-            }}
+            icon="accessinility"
             // We need this hack to tick the eye while chart is not rendered and title/description is repositioning. This prevents from seeing title/description flick.
             iconColor={readyToRender ? 'darkGray' : 'white'}
             bgColor={readyToRender ? undefined : 'transparent'}
