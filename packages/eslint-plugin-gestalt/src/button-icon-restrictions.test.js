c358a570d56f148ee3ddd0d25e6e3b86e2cdd555
acarreras <acarreras@pinterest.com>
2023-11-15 18:55:59 +0100

    more

--- packages/eslint-plugin-gestalt/src/button-icon-restrictions.test.js
+++ packages/eslint-plugin-gestalt/src/button-icon-restrictions.test.js
@@ -8,22 +8,22 @@ import { parserOptions } from './helpers/testHelpers';
 const ruleTester = new RuleTester({ parserOptions });
 
 const validWithSize = readFileSync(
-  path.resolve(__dirname, './__fixtures__/button-icon-restrictions/valid/valid-size'),
+  path.resolve(__dirname, './__fixtures__/button-icon-restrictions/valid/valid-size.js'),
   'utf-8',
 );
 const invalidRenamed = readFileSync(
-  path.resolve(__dirname, './__fixtures__/button-icon-restrictions/invalid/invalid-renamed'),
+  path.resolve(__dirname, './__fixtures__/button-icon-restrictions/invalid/invalid-renamed.js'),
   'utf-8',
 );
 const invalidWrongIcon = readFileSync(
-  path.resolve(__dirname, './__fixtures__/button-icon-restrictions/invalid/invalid-wrong-icon'),
+  path.resolve(__dirname, './__fixtures__/button-icon-restrictions/invalid/invalid-wrong-icon.js'),
   'utf-8',
 );
 
 const invalidWrongIconLinkRole = readFileSync(
   path.resolve(
     __dirname,
-    './__fixtures__/button-icon-restrictions/invalid/invalid-wrong-link-role-icon',
+    './__fixtures__/button-icon-restrictions/invalid/invalid-wrong-link-role-icon.js',
   ),
   'utf-8',
 );
