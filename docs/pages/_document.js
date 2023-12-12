c7372e2482c138f61e285c59dd7127ed14309771
acarreras <acarreras@pinterest.com>
2023-12-12 17:43:53 +0100

    more

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -22,14 +22,7 @@ class GestaltDocument extends Document {
     const { props } = this;
     const cookies = new Cookies(props.cookieHeader);
     const dir = cookies.cookies['gestalt-text-direction'];
-    const gestaltBuildDirectory = path.join(
-      process.cwd(),
-      '..',
-      '..',
-      'node_modules',
-      'gestalt',
-      'dist',
-    );
+    const gestaltBuildDirectory = path.join(process.cwd(), '..', 'node_modules', 'gestalt', 'dist');
 
     const gestaltCssText = readFileSync(path.join(gestaltBuildDirectory, 'gestalt.css'), 'utf8');
     // eslint-disable-next-line no-console
