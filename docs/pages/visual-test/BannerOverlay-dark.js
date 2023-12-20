32d5cf28cab6a36915f80f30872f9a7d5fe8990d
eniomoura <erochamoura@pinterest.com>
2023-12-20 10:59:31 -0300

    adding missing visual test pages

---
+++ docs/pages/visual-test/BannerOverlay-dark.js
@@ -0,0 +1,34 @@
+// @flow strict
+import { type Node as ReactNode } from 'react';
+import { BannerOverlay, ColorSchemeProvider, FixedZIndex, Image, Link, Text } from 'gestalt';
+
+export default function Example(): ReactNode {
+  return (
+    <ColorSchemeProvider colorScheme="dark">
+      <BannerOverlay
+        zIndex={new FixedZIndex(100)}
+        offset={{ top: 130, bottom: 24 }}
+        title="BannerOverlay"
+        message={
+          <Text inline>
+            Saved to{' '}
+            <Link display="inlineBlock" target="blank" href="https://www.pinterest.com/">
+              Sushi time
+            </Link>
+          </Text>
+        }
+        onDismiss={() => {}}
+        thumbnail={{
+          image: (
+            <Image
+              alt="Vegan Teriyaki Sushi Burrito"
+              naturalHeight={1}
+              naturalWidth={1}
+              src="https://i.pinimg.com/564x/ff/ee/52/ffee52eac6cd0f2f5dac8af3899a9f41.jpg"
+            />
+          ),
+        }}
+      />
+    </ColorSchemeProvider>
+  );
+}
---
+++ docs/pages/visual-test/BannerOverlay.js
@@ -0,0 +1,32 @@
+// @flow strict
+import { type Node as ReactNode } from 'react';
+import { BannerOverlay, FixedZIndex, Image, Link, Text } from 'gestalt';
+
+export default function Example(): ReactNode {
+  return (
+    <BannerOverlay
+      zIndex={new FixedZIndex(100)}
+      offset={{ top: 130, bottom: 24 }}
+      title="BannerOverlay"
+      message={
+        <Text inline>
+          Saved to{' '}
+          <Link display="inlineBlock" target="blank" href="https://www.pinterest.com/">
+            Sushi time
+          </Link>
+        </Text>
+      }
+      onDismiss={() => {}}
+      thumbnail={{
+        image: (
+          <Image
+            alt="Vegan Teriyaki Sushi Burrito"
+            naturalHeight={1}
+            naturalWidth={1}
+            src="https://i.pinimg.com/564x/ff/ee/52/ffee52eac6cd0f2f5dac8af3899a9f41.jpg"
+          />
+        ),
+      }}
+    />
+  );
+}
