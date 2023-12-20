4de170142286f712a653623e8ca952f70ae5fee5
erochamoura <erochamoura@pinterest.com>
2023-12-20 11:35:22 +0000

    adding ts file types

--- packages/gestalt/dist/index.d.ts
+++ packages/gestalt/dist/index.d.ts
@@ -403,6 +403,9 @@ interface DefaultLabelProviderProps {
         ActivationCard: {
           accessibilityDismissButtonLabel: string;
         };
+        BannerOverlay: {
+          accessibilityDismissButtonLabel: string;
+        }
         Callout: {
           accessibilityDismissButtonLabel: string;
           iconAccessibilityLabelError: string;
@@ -589,6 +592,48 @@ interface AvatarGroupProps {
     | undefined;
   role?: 'button' | 'link' | undefined;
   size?: 'xs' | 'sm' | 'md' | 'fit' | undefined;
+}
+
+interface BannerOverlayProps {
+  onDismiss: () => void;
+  message: string | React.ReactElement<typeof Text>;
+  offset?: { bottom: number, top: number },
+  helperLink?:
+  | {
+      text: string;
+      accessibilityLabel: string;
+      href: string;
+      onClick?: AbstractEventHandler<
+        React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>,
+        { dangerouslyDisableOnNavigation: () => void }
+      >;
+    }
+  | undefined;
+  primaryAction?:
+  | {
+      accessibilityLabel: string;
+      label: string;
+      href: string | undefined;
+      onClick?: ButtonEventHandlerType | undefined;
+      rel?: RelType | undefined;
+      role: 'link';
+      size?: 'sm' | 'md' | 'lg' | undefined;
+      target?: TargetType | undefined;
+    }
+  | {
+      accessibilityLabel: string;
+      label: string;
+      onClick: ButtonEventHandlerType | undefined;
+      role?: 'button';
+      size?: 'sm' | 'md' | 'lg' | undefined;
+    };
+  thumbnail?:
+  | { image: React.ReactElement<typeof Image> }
+  | { avatar: React.ReactElement<typeof Avatar> }
+  | { icon: React.ReactElement<typeof Icon> }
+  | undefined;
+  title?: string,
+  zIndex?: Indexable,
 }
 
 interface BadgeProps {
@@ -2416,6 +2461,11 @@ export const AvatarGroup: React.FunctionComponent<AvatarGroupProps>;
  * https://gestalt.pinterest.systems/web/badge
  */
 export const Badge: React.FunctionComponent<BadgeProps>;
+
+/**
+ * https://gestalt.pinterest.systems/web/banneroverlay
+ */
+export const BannerOverlay: React.FunctionComponent<BannerOverlayProps>;
 
 /**
  * https://gestalt.pinterest.systems/web/box
