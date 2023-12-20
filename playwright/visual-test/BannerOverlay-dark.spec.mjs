c495fdaa8ad7b7754ffa85892334ac732ab5baf2
eniomoura <erochamoura@pinterest.com>
2023-12-20 11:12:59 -0300

    fixing playwright tests

--- playwright/visual-test/BannerOverlay-dark.spec.mjs
+++ playwright/visual-test/BannerOverlay-dark.spec.mjs
@@ -1,8 +1,11 @@
 // @flow strict
 import { expect, test } from '@playwright/test';
 
-test('BannerOverlay visual dark mode regression check', async ({ page }) => {
-  await page.goto('/visual-test/BannerOverlay-dark');
-  const locator = page.locator('[data-test-id="visual-test"]');
+test('BannerOverlay visual regression check', async ({ page }) => {
+  await page.goto('http://localhost:8888/visual-test/BannerOverlay-dark');
+  const locator = page
+    .locator('[data-test-id="visual-test"] div')
+    .filter({ hasText: 'BannerOverlay' })
+    .nth(1);
   await expect(locator).toHaveScreenshot('BannerOverlay-dark.png');
 });
--- playwright/visual-test/BannerOverlay.spec.mjs
+++ playwright/visual-test/BannerOverlay.spec.mjs
@@ -2,7 +2,10 @@
 import { expect, test } from '@playwright/test';
 
 test('BannerOverlay visual regression check', async ({ page }) => {
-  await page.goto('/visual-test/BannerOverlay');
-  const locator = page.locator('[data-test-id="visual-test"]');
+  await page.goto('http://localhost:8888/visual-test/BannerOverlay');
+  const locator = page
+    .locator('div')
+    .filter({ hasText: 'BannerOverlay' })
+    .nth(2);
   await expect(locator).toHaveScreenshot('BannerOverlay.png');
 });
