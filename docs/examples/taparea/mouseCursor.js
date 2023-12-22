6799df16a2bae5392b3abb4ac9954cdd64d5f7b6
Raviteja Lingineni <rlingineni@pinterest.com>
2023-12-22 11:53:38 -0800

    TapArea: Add cursor doc changes (#3339)

--- docs/examples/taparea/mouseCursor.js
+++ docs/examples/taparea/mouseCursor.js
@@ -2,26 +2,38 @@
 import { type Node as ReactNode } from 'react';
 import { Box, Flex, TapArea, Text } from 'gestalt';
 
+const descriptions = {
+  'copy': 'Use the copy cursor to indicate an element is to be copied.',
+  'default':
+    'Use the default cursor over non-interactive elements. The default cursor should change to a pointer if the element it hovers over is also clickable or indicates functionally',
+  'grab': 'Use the grab cursor to indicate something can be grabbed (dragged to be moved).',
+  'grabbing':
+    'Use the grabbing cursor to indicate something is being grabbed (dragged to be moved).',
+  'move': 'Use the move cursor to indicate something is to be moved.',
+  'noDrop':
+    'Use the noDrop cursor to reinforce that an item may not be dropped at the current location.',
+  'pointer': 'Use the pointer cursor to indicate interactive elements, such as links or actions.',
+  'zoomIn': 'Use the ZoomIn cursor to indicate something can be zoomed (magnified) in.',
+  'zoomOut': 'Use the ZoomIn cursor to indicate something can be zoomed out.',
+};
+
 export default function TapAreaExample(): ReactNode {
   return (
     <Flex wrap gap={2}>
-      {[
-        'copy',
-        'grab',
-        'grabbing',
-        'move',
-        'noDrop',
-        'pointer',
-        'zoomIn',
-        'zoomOut',
-        'default',
-      ].map((cursor) => (
+      {Object.keys(descriptions).map((cursor) => (
         <TapArea key={cursor} mouseCursor={cursor} fullWidth={false}>
-          <Box borderStyle="lg" padding={4} width={250} height={100}>
-            <Text size="200">hover here </Text>
-            <Text size="200" weight="bold">
-              {`mouseCursor="${cursor}"`}
-            </Text>
+          <Box borderStyle="lg" padding={4} width={250} height={250}>
+            <Flex direction="column" justifyContent="between" height="100%">
+              <Box>
+                <Box marginBottom={2}>
+                  <Text size="200" weight="bold">
+                    {`mouseCursor="${cursor}"`}
+                  </Text>
+                </Box>
+                <Text size="200">{descriptions[cursor]}</Text>
+              </Box>
+              <Text size="100">hover here </Text>
+            </Flex>
           </Box>
         </TapArea>
       ))}
