199b3e94e0f10e18002fdc914481fcffb28ec829
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2024-01-24 13:10:53 -0500

    Icon: new icons + update all SVGs files. (#3380)

--- packages/gestalt/src/icons/index.js
+++ packages/gestalt/src/icons/index.js
@@ -1,6 +1,7 @@
 // @flow strict
 import threeD from './3d.svg';
 import threeDMove from './3d-move.svg';
+import threeSixty from './360.svg';
 import accessibility from './accessibility.svg';
 import ad from './ad.svg';
 import adGroup from './ad-group.svg';
@@ -21,6 +22,7 @@ import alignTop from './align-top.svg';
 import alignTopCenter from './align-top-center.svg';
 import alignTopLeft from './align-top-left.svg';
 import alignTopRight from './align-top-right.svg';
+import alphabetical from './alphabetical.svg';
 import androidShare from './android-share.svg';
 import angledPin from './angled-pin.svg';
 import api from './api.svg';
@@ -31,13 +33,16 @@ import arrowCircleBack from './arrow-circle-back.svg';
 import arrowCircleDown from './arrow-circle-down.svg';
 import arrowCircleForward from './arrow-circle-forward.svg';
 import arrowCircleUp from './arrow-circle-up.svg';
+import arrowCounterClockwise from './arrow-counter-clockwise.svg';
 import arrowDown from './arrow-down.svg';
 import arrowEnd from './arrow-end.svg';
 import arrowForward from './arrow-forward.svg';
 import arrowLeftCurved from './arrow-left-curved.svg';
 import arrowStart from './arrow-start.svg';
 import arrowUp from './arrow-up.svg';
+import arrowUpLeft from './arrow-up-left.svg';
 import arrowUpRight from './arrow-up-right.svg';
+import arrowsVertical from './arrows-vertical.svg';
 import bell from './bell.svg';
 import board from './board.svg';
 import briefcase from './briefcase.svg';
@@ -45,6 +50,7 @@ import businessHierarchy from './business-hierarchy.svg';
 import calendar from './calendar.svg';
 import calendarCheck from './calendar-check.svg';
 import camera from './camera.svg';
+import cameraFlip from './camera-flip.svg';
 import cameraRoll from './camera-roll.svg';
 import cancel from './cancel.svg';
 import canonicalPin from './canonical-pin.svg';
@@ -52,13 +58,17 @@ import captions from './captions.svg';
 import captionsOutline from './captions-outline.svg';
 import check from './check.svg';
 import checkCircle from './check-circle.svg';
+import chevronUpCircle from './chevron-up-circle.svg';
 import circleOutline from './circle-outline.svg';
 import clear from './clear.svg';
 import clock from './clock.svg';
 import code from './code.svg';
 import codeChecked from './code-checked.svg';
 import cog from './cog.svg';
+import collapse from './collapse.svg';
 import colorPicker from './color-picker.svg';
+import colorSolid from './color-solid.svg';
+import colorSplit from './color-split.svg';
 import compass from './compass.svg';
 import compose from './compose.svg';
 import conversionTag from './conversion-tag.svg';
@@ -78,13 +88,17 @@ import edit from './edit.svg';
 import ellipsis from './ellipsis.svg';
 import ellipsisCircleOutline from './ellipsis-circle-outline.svg';
 import envelope from './envelope.svg';
+import expand from './expand.svg';
 import eye from './eye.svg';
 import eyeHide from './eye-hide.svg';
 import faceHappy from './face-happy.svg';
 import faceNeutral from './face-neutral.svg';
 import faceSad from './face-sad.svg';
 import faceSmiley from './face-smiley.svg';
+import faceTryon from './face-tryon.svg';
 import facebook from './facebook.svg';
+import fade from './fade.svg';
+import fileBox from './file-box.svg';
 import fileUnknown from './file-unknown.svg';
 import fillOpaque from './fill-opaque.svg';
 import fillTransparent from './fill-transparent.svg';
@@ -96,11 +110,13 @@ import flashlight from './flashlight.svg';
 import flipHorizontal from './flip-horizontal.svg';
 import flipVertical from './flip-vertical.svg';
 import folder from './folder.svg';
+import forward from './forward.svg';
 import gif from './gif.svg';
 import globe from './globe.svg';
 import globeChecked from './globe-checked.svg';
 import gmail from './gmail.svg';
 import graphBar from './graph-bar.svg';
+import graphPie from './graph-pie.svg';
 import handPointing from './hand-pointing.svg';
 import handle from './handle.svg';
 import heart from './heart.svg';
@@ -113,15 +129,18 @@ import ideaPin from './idea-pin.svg';
 import imagePortrait from './image-portrait.svg';
 import impressum from './impressum.svg';
 import infoCircle from './info-circle.svg';
+import information from './information.svg';
 import insightsAudience from './insights-audience.svg';
-import insightsConversions from './insights-conversions.svg';
+import insightsConversion from './insights-conversion.svg';
 import instagram from './instagram.svg';
 import invoice from './invoice.svg';
 import key from './key.svg';
 import knoop from './knoop.svg';
+import layout from './layout.svg';
 import lightbulb from './lightbulb.svg';
 import lightningBoltCircle from './lightning-bolt-circle.svg';
 import link from './link.svg';
+import lips from './lips.svg';
 import live from './live.svg';
 import location from './location.svg';
 import lock from './lock.svg';
@@ -145,6 +164,7 @@ import musicOff from './music-off.svg';
 import musicOn from './music-on.svg';
 import mute from './mute.svg';
 import overlayText from './overlay-text.svg';
+import overlayTextOutline from './overlay-text-outline.svg';
 import overview from './overview.svg';
 import pause from './pause.svg';
 import people from './people.svg';
@@ -163,6 +183,7 @@ import remove from './remove.svg';
 import reorderImages from './reorder-images.svg';
 import replace from './replace.svg';
 import report from './report.svg';
+import rewind from './rewind.svg';
 import rotate from './rotate.svg';
 import saveOutline from './save-outline.svg';
 import saved from './saved.svg';
@@ -171,8 +192,16 @@ import scissors from './scissors.svg';
 import search from './search.svg';
 import security from './security.svg';
 import send from './send.svg';
+import serviceDropbox from './service-dropbox.svg';
+import serviceEtsy from './service-etsy.svg';
+import serviceMessenger from './service-messenger.svg';
+import serviceOutlook from './service-outlook.svg';
+import serviceYahoo from './service-yahoo.svg';
+import serviceYoutube from './service-youtube.svg';
 import share from './share.svg';
 import shoppingBag from './shopping-bag.svg';
+import shrink from './shrink.svg';
+import skintone from './skintone.svg';
 import smiley from './smiley.svg';
 import smileyOutline from './smiley-outline.svg';
 import sortAscending from './sort-ascending.svg';
@@ -183,6 +212,7 @@ import speech from './speech.svg';
 import speechEllipsis from './speech-ellipsis.svg';
 import speechExclamationPoint from './speech-exclamation-point.svg';
 import speechOutline from './speech-outline.svg';
+import spread from './spread.svg';
 import star from './star.svg';
 import starHalf from './star-half.svg';
 import starOutline from './star-outline.svg';
@@ -217,6 +247,7 @@ import videoRewind10Seconds from './video-rewind-10-seconds.svg';
 import viewTypeDefault from './view-type-default.svg';
 import viewTypeDense from './view-type-dense.svg';
 import viewTypeList from './view-type-list.svg';
+import viewTypeSpace from './view-type-space.svg';
 import visit from './visit.svg';
 import whatsApp from './whats-app.svg';
 import wifiNo from './wifi-no.svg';
@@ -229,10 +260,12 @@ import workflowStatusProblem from './workflow-status-problem.svg';
 import workflowStatusQueued from './workflow-status-queued.svg';
 import workflowStatusUnstarted from './workflow-status-unstarted.svg';
 import workflowStatusWarning from './workflow-status-warning.svg';
+import x from './x.svg';
 
 const icons = Object.freeze({
   '3D': threeD,
   '3D-move': threeDMove,
+  '360': threeSixty,
   accessibility,
   ad,
   'ad-group': adGroup,
@@ -243,6 +276,7 @@ const icons = Object.freeze({
   'add-section': addSection,
   'ads-stats': adsStats,
   'ads-overview': adsOverview,
+  alphabetical,
   alert,
   'align-bottom-center': alignBottomCenter,
   'align-bottom-left': alignBottomLeft,
@@ -263,13 +297,16 @@ const icons = Object.freeze({
   'arrow-circle-down': arrowCircleDown,
   'arrow-circle-forward': arrowCircleForward,
   'arrow-circle-up': arrowCircleUp,
+  'arrow-counter-clockwise': arrowCounterClockwise,
   'arrow-down': arrowDown,
   'arrow-end': arrowEnd,
   'arrow-forward': arrowForward,
   'arrow-left-curved': arrowLeftCurved,
   'arrow-start': arrowStart,
   'arrow-up': arrowUp,
+  'arrow-up-left': arrowUpLeft,
   'arrow-up-right': arrowUpRight,
+  'arrows-vertical': arrowsVertical,
   bell,
   board,
   briefcase,
@@ -277,6 +314,7 @@ const icons = Object.freeze({
   calendar,
   'calendar-check': calendarCheck,
   camera,
+  'camera-flip': cameraFlip,
   'camera-roll': cameraRoll,
   cancel,
   'canonical-pin': canonicalPin,
@@ -285,6 +323,7 @@ const icons = Object.freeze({
   'color-picker': colorPicker,
   check,
   'check-circle': checkCircle,
+  'chevron-up-circle': chevronUpCircle,
   'circle-outline': circleOutline,
   clear,
   clock,
@@ -293,6 +332,9 @@ const icons = Object.freeze({
   cog,
   compass,
   compose,
+  collapse,
+  'color-solid': colorSolid,
+  'color-split': colorSplit,
   'copy-to-clipboard': copyToClipboard,
   crop,
   'data-source': dataSource,
@@ -310,13 +352,17 @@ const icons = Object.freeze({
   ellipsis,
   'ellipsis-circle-outline': ellipsisCircleOutline,
   envelope,
+  expand,
   eye,
   'eye-hide': eyeHide,
   facebook,
+  fade,
   'face-happy': faceHappy,
   'face-neutral': faceNeutral,
   'face-sad': faceSad,
   'face-smiley': faceSmiley,
+  'face-tryon': faceTryon,
+  'file-box': fileBox,
   'file-unknown': fileUnknown,
   'fill-opaque': fillOpaque,
   'fill-transparent': fillTransparent,
@@ -328,11 +374,13 @@ const icons = Object.freeze({
   flipHorizontal,
   flipVertical,
   folder,
+  forward,
   gif,
   globe,
   'globe-checked': globeChecked,
   gmail,
   'graph-bar': graphBar,
+  'graph-pie': graphPie,
   handle,
   'hand-pointing': handPointing,
   heart,
@@ -344,16 +392,19 @@ const icons = Object.freeze({
   'idea-pin': ideaPin,
   'image-portrait': imagePortrait,
   impressum,
+  information,
   'insights-audience': insightsAudience,
-  'insights-conversions': insightsConversions,
+  'insights-conversion': insightsConversion,
   instagram,
   'invoice': invoice,
   'info-circle': infoCircle,
   key,
   knoop,
+  layout,
   lightbulb,
   'lightning-bolt-circle': lightningBoltCircle,
   link,
+  lips,
   live,
   location,
   lock,
@@ -377,6 +428,7 @@ const icons = Object.freeze({
   'music-off': musicOff,
   'music-on': musicOn,
   'overlay-text': overlayText,
+  'overlay-text-outline': overlayTextOutline,
   overview,
   pause,
   people,
@@ -395,6 +447,7 @@ const icons = Object.freeze({
   'reorder-images': reorderImages,
   replace,
   report,
+  rewind,
   rotate,
   'save-outline': saveOutline,
   saved,
@@ -402,11 +455,19 @@ const icons = Object.freeze({
   scissors,
   search,
   security,
+  'service-dropbox': serviceDropbox,
+  'service-etsy': serviceEtsy,
+  'service-messenger': serviceMessenger,
+  'service-outlook': serviceOutlook,
+  'service-yahoo': serviceYahoo,
+  'service-youtube': serviceYoutube,
+  share,
   'shopping-bag': shoppingBag,
+  shrink,
+  skintone,
   smiley,
   'smiley-outline': smileyOutline,
   send,
-  share,
   sound,
   'sort-ascending': sortAscending,
   'sort-descending': sortDescending,
@@ -415,6 +476,7 @@ const icons = Object.freeze({
   'speech-ellipsis': speechEllipsis,
   'speech-exclamation-point': speechExclamationPoint,
   'speech-outline': speechOutline,
+  spread,
   'star-outline': starOutline,
   star,
   'star-half': starHalf,
@@ -449,6 +511,7 @@ const icons = Object.freeze({
   'view-type-default': viewTypeDefault,
   'view-type-dense': viewTypeDense,
   'view-type-list': viewTypeList,
+  'view-type-space': viewTypeSpace,
   visit,
   'whats-app': whatsApp,
   'wifi-no': wifiNo,
@@ -461,6 +524,7 @@ const icons = Object.freeze({
   'workflow-status-queued': workflowStatusQueued,
   'workflow-status-unstarted': workflowStatusUnstarted,
   'workflow-status-warning': workflowStatusWarning,
+  x,
 });
 
 export default icons;
