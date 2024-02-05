4bae9aab322aec41121b5a0ad20c08efbf6a2104
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2024-02-05 15:44:20 -0500

    Button: moved Button colors to component tokens (#3400)

---
+++ docs/examples/button/colors.js
@@ -0,0 +1,30 @@
+// @flow strict
+import { type Node as ReactNode } from 'react';
+import { Box, Button, Flex, Text } from 'gestalt';
+
+export default function Example(): ReactNode {
+  return (
+    <Box padding={4}>
+      <Flex height="100%" gap={6} width="100%" wrap>
+        {['gray', 'red', 'blue', 'transparent'].map((color) => (
+          <Flex gap={2} key={color} direction="column">
+            <Box
+              borderStyle="sm"
+              display="flex"
+              width={200}
+              height={200}
+              rounding={4}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Button color={color} size="lg" text="Save" />
+            </Box>
+            <Text size="200" weight="bold">
+              color=&quot;{color}&quot;
+            </Text>
+          </Flex>
+        ))}
+      </Flex>
+    </Box>
+  );
+}
