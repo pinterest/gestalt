66c5015b242cc7f5c60d0305858de7df65857960
acarreras <acarreras@pinterest.com>
2024-02-06 17:24:25 -0500

    more

--- docs/pages/web/pog.js
+++ docs/pages/web/pog.js
@@ -1,6 +1,6 @@
 // @flow strict
 import { type Node as ReactNode } from 'react';
-import { Box, Flex, Pog, Text } from 'gestalt';
+import { Pog } from 'gestalt';
 import AccessibilitySection from '../../docs-components/AccessibilitySection';
 import CombinationNew from '../../docs-components/CombinationNew';
 import docGen, { type DocGen } from '../../docs-components/docgen';
@@ -11,6 +11,8 @@ import PageHeader from '../../docs-components/PageHeader';
 import QualityChecklist from '../../docs-components/QualityChecklist';
 import SandpackExample from '../../docs-components/SandpackExample';
 import main from '../../examples/pog/main';
+import states from '../../examples/pog/states';
+import statesOnBackground from '../../examples/pog/statesOnBackground';
 
 export default function DocsPage({ generatedDocGen }: { generatedDocGen: DocGen }): ReactNode {
   return (
@@ -29,133 +31,63 @@ export default function DocsPage({ generatedDocGen }: { generatedDocGen: DocGen 
 
       <AccessibilitySection name={generatedDocGen?.displayName} />
       <MainSection name="Variants">
-        <MainSection.Subsection title="Colors & states on white backgrounds">
-          <CombinationNew
-            hovered={[false, true]}
-            focused={[false, true]}
-            active={[false, true]}
-            selected={[false, true]}
-          >
-            {({ hovered, focused, active, selected }) => (
-              <Flex gap={2} direction="column">
-                <Flex gap={3}>
-                  <Pog
-                    icon="heart"
-                    hovered={hovered}
-                    focused={focused}
-                    active={active}
-                    bgColor="red"
-                    selected={selected}
-                  />
-                  <Text size="100">red - primary</Text>
-                </Flex>
-                <Flex gap={3}>
-                  <Pog
-                    icon="heart"
-                    hovered={hovered}
-                    focused={focused}
-                    active={active}
-                    bgColor="lightGray"
-                    selected={selected}
-                  />
-                  <Text size="100">lightGray - secondary</Text>
-                </Flex>
-                <Flex gap={3}>
-                  <Pog
-                    icon="heart"
-                    hovered={hovered}
-                    focused={focused}
-                    active={active}
-                    bgColor="transparent"
-                    selected={selected}
-                  />
-                  <Text size="100">transparent - tertiary</Text>
-                </Flex>
-              </Flex>
-            )}
+        <MainSection.Subsection title="Icon colors">
+          <CombinationNew iconColor={['darkGray', 'gray', 'red', 'white', 'brandPrimary']}>
+            {({ iconColor }) => <Pog icon="heart" iconColor={iconColor} />}
           </CombinationNew>
         </MainSection.Subsection>
-        <MainSection.Subsection title="Colors & states on color/image backgrounds">
-          <CombinationNew
-            hovered={[false, true]}
-            focused={[false, true]}
-            active={[false, true]}
-            selected={[false, true]}
-          >
-            {({ hovered, focused, active, selected }) => (
-              <Box
-                borderStyle="sm"
-                display="flex"
-                width={200}
-                height={200}
-                rounding={4}
-                alignItems="center"
-                justifyContent="center"
-                dangerouslySetInlineStyle={{
-                  __style: {
-                    backgroundImage: 'url("https://i.ibb.co/d0pQsJz/stock3.jpg")',
-                  },
-                }}
-                padding={2}
-              >
-                <Flex gap={2} direction="column">
-                  <Flex gap={3}>
-                    <Pog
-                      icon="heart"
-                      hovered={hovered}
-                      focused={focused}
-                      active={active}
-                      bgColor="transparentDarkGray"
-                      selected={selected}
-                    />
-                    <Box color="transparentDarkGray">
-                      <Text color="inverse" weight="bold" size="100">
-                        transparentDarkGray - secondary
-                      </Text>
-                    </Box>
-                  </Flex>
-                  <Flex gap={3}>
-                    <Pog
-                      icon="heart"
-                      hovered={hovered}
-                      focused={focused}
-                      active={active}
-                      bgColor="white"
-                      selected={selected}
-                    />
-                    <Box color="transparentDarkGray">
-                      <Text color="inverse" weight="bold" size="100">
-                        white
-                      </Text>
-                    </Box>
-                  </Flex>
-                  <Flex gap={3}>
-                    <Pog
-                      icon="heart"
-                      hovered={hovered}
-                      focused={focused}
-                      active={active}
-                      bgColor="gray"
-                      selected={selected}
-                    />
-                    <Box color="transparentDarkGray">
-                      <Text color="inverse" weight="bold" size="100">
-                        gray
-                      </Text>
-                    </Box>
-                  </Flex>
-                </Flex>
-              </Box>
-            )}
-          </CombinationNew>
-        </MainSection.Subsection>
+        <MainSection.Subsection
+          title="Background colors on white backgrounds"
+          description={`Pog can be presented in combinations of icon and background colors. In the absence of combinations, for each \`iconColor\` or \`bgColor\` value, a default paired value is assigned.
 
+Follow these guidelines for \`bgColor\`
+
+1. Red ("red"). High emphasis, used for primary actions.
+2. Light Gray ("lightGray"). Medium emphasis, used for secondary actions.
+3. Gray ("gray"). Used for tertiary actions or in cases where the primary "red" is not an option. Medium emphasis when placed on dark backgrounds, used for secondary actions.
+`}
+        >
+          <CombinationNew bgColor={['red', 'lightGray', 'gray']}>
+            {({ bgColor }) => <Pog icon="heart" bgColor={bgColor} />}
+          </CombinationNew>
+        </MainSection.Subsection>
+        <MainSection.Subsection
+          title="Background colors on color/image backgrounds"
+          description={`Pog can be presented in combinations of icon and background colors. In the absence of combinations, for each \`iconColor\` or \`bgColor\` value, a default paired value is assigned.
+
+Follow these guidelines for \`bgColor\`
+
+1. Transparent Dark Gray ("transparentDarkGray"). Medium emphasis, used for secondary actions, usually above a colored background.
+2. White ("white"). Used when there is a need of an IconButton over an image or colored background to provide better contrast and visibility.
+3. Transparent ("transparent"). Used when there is a need to have an IconButton over an image without a background.
+`}
+        >
+          <CombinationNew bgColor={['transparentDarkGray', 'white', 'transparent']}>
+            {({ bgColor }) => <Pog icon="heart" bgColor={bgColor} />}
+          </CombinationNew>
+        </MainSection.Subsection>
+        <MainSection.Subsection title="States">
+          <MainSection.Card
+            cardSize="lg"
+            sandpackExample={<SandpackExample code={states} name="States on white backgrounds" />}
+          />
+        </MainSection.Subsection>
+        <MainSection.Subsection title="States on color/image backgrounds">
+          <MainSection.Card
+            cardSize="lg"
+            sandpackExample={
+              <SandpackExample
+                code={statesOnBackground}
+                name="States states on image/color background"
+              />
+            }
+          />
+        </MainSection.Subsection>
         <MainSection.Subsection title="Sizes with default padding">
           <CombinationNew size={['xs', 'sm', 'md', 'lg', 'xl']} hasCheckerboard={false}>
             {({ size }) => <Pog bgColor="lightGray" icon="heart" size={size} />}
           </CombinationNew>
         </MainSection.Subsection>
-
         <MainSection.Subsection title="Sizes with custom padding">
           <CombinationNew
             size={['xs', 'sm', 'md', 'lg', 'xl']}
@@ -165,27 +97,6 @@ export default function DocsPage({ generatedDocGen }: { generatedDocGen: DocGen 
             {({ size, padding }) => (
               <Pog bgColor="lightGray" icon="heart" size={size} padding={padding} />
             )}
-          </CombinationNew>
-        </MainSection.Subsection>
-
-        <MainSection.Subsection title="Icon Colors">
-          <CombinationNew iconColor={['darkGray', 'gray', 'red', 'white', 'brandPrimary']}>
-            {({ iconColor }) => <Pog icon="heart" iconColor={iconColor} />}
-          </CombinationNew>
-        </MainSection.Subsection>
-
-        <MainSection.Subsection title="Background Colors">
-          <CombinationNew
-            bgColor={[
-              'transparent',
-              'transparentDarkGray',
-              'darkGray',
-              'white',
-              'lightGray',
-              'gray',
-            ]}
-          >
-            {({ bgColor }) => <Pog icon="heart" bgColor={bgColor} />}
           </CombinationNew>
         </MainSection.Subsection>
       </MainSection>
