f0ff0a9973a96705a32805e4760c3c0d1907e992
Ravi Lingineni <rlingineni@pinterest.com>
2024-02-09 14:53:50 -0800

    remove ssr for chartgraph

--- docs/pages/visual-test/ChartGraph-dark.js
+++ docs/pages/visual-test/ChartGraph-dark.js
@@ -1,37 +1,48 @@
 // @flow strict
 import { type Node as ReactNode } from 'react';
+import dynamic from 'next/dynamic';
 import { Box, ColorSchemeProvider } from 'gestalt';
 import { ChartGraph } from 'gestalt-charts';
 
+function NoSsr(props: { children: ReactNode }) {
+  return <div> {props.children} </div>;
+}
+
+const NoSSR = dynamic(() => Promise.resolve(NoSsr), {
+  ssr: false,
+});
+
 export default function Snapshot(): ReactNode {
   return (
-    <ColorSchemeProvider colorScheme="dark">
-      <Box width={200}>
-        <ChartGraph
-          accessibilityLabel="Example of Bar chart"
-          visualPatternSelected="default"
-          onVisualPatternChange={() => {}}
-          type="bar"
-          title="Title"
-          description="Description"
-          legend="auto"
-          data={[
-            {
-              name: 'A',
-              'Series_01': 100,
-            },
-            {
-              name: 'B',
-              'Series_01': 200,
-            },
-            {
-              name: 'C',
-              'Series_01': 300,
-            },
-          ]}
-          elements={[{ type: 'bar', id: 'Series_01' }]}
-        />
-      </Box>
-    </ColorSchemeProvider>
+    <NoSSR>
+      <ColorSchemeProvider colorScheme="dark">
+        <Box width={200}>
+          <ChartGraph
+            accessibilityLabel="Example of Bar chart"
+            visualPatternSelected="default"
+            onVisualPatternChange={() => {}}
+            type="bar"
+            title="Title"
+            description="Description"
+            legend="auto"
+            data={[
+              {
+                name: 'A',
+                'Series_01': 100,
+              },
+              {
+                name: 'B',
+                'Series_01': 200,
+              },
+              {
+                name: 'C',
+                'Series_01': 300,
+              },
+            ]}
+            elements={[{ type: 'bar', id: 'Series_01' }]}
+          />
+        </Box>
+      </ColorSchemeProvider>
+    </NoSSR>
   );
 }
