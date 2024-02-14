1c3ebc33c8788835529c10d71713f3f1a164fc54
acarreras <acarreras@pinterest.com>
2024-02-14 14:01:10 -0500

    more

--- playwright/accessibility/foundations__design_tokens__component_tokens.spec.mjs
+++ playwright/accessibility/foundations__design_tokens__component_tokens.spec.mjs
@@ -2,7 +2,7 @@
 import { test } from '@playwright/test';
 import expectAccessiblePage from './expectAccessiblePage.mjs';
 
-test('Design Tokens Accessibility check', async ({ page }) => {
-  await page.goto('/foundations/design_tokens/overview');
+test('Design Component Tokens Accessibility check', async ({ page }) => {
+  await page.goto('/foundations/design_tokens/component_tokens');
   await expectAccessiblePage({ page });
 });
