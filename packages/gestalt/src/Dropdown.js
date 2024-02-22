72ad220ebf5965d282c441d9f3dd2644dbf8538d
Diyorbek Sadullaev <dsadullaev@pinterest.com>
2024-02-22 11:00:02 +0100

    WIP

--- packages/gestalt/src/Dropdown.js
+++ packages/gestalt/src/Dropdown.js
@@ -288,6 +288,7 @@ export default function Dropdown({
       idealDirection={idealDirection}
       onDismiss={onDismiss}
       positionRelativeToAnchor={isWithinFixedContainer}
+      disablePortal={isInExperiment}
       role="menu"
       shouldFocus
       size="xl"
