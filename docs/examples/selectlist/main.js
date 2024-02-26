97fe6e5d667e483b26802d7660187083aea1bead
acarreras <acarreras@pinterest.com>
2024-02-26 17:32:10 -0500

    more

--- docs/examples/selectlist/main.js
+++ docs/examples/selectlist/main.js
@@ -1,24 +1,22 @@
 // @flow strict
 import { type Node as ReactNode } from 'react';
-import { Box, Flex, SelectList } from 'gestalt';
+import { Flex, SelectList } from 'gestalt';
 
 export default function Example(): ReactNode {
   return (
-    <Box color="successWeak">
-      <Flex alignItems="center" gap={4} height="100%" justifyContent="center" width="100%">
-        <SelectList id="selectlistexample1" label="Country" onChange={() => {}} size="lg">
-          {[
-            { label: 'Algeria', value: 'algeria' },
-            { label: 'Belgium', value: 'belgium' },
-            { label: 'Canada', value: 'canada' },
-            { label: 'Denmark', value: 'denmark' },
-            { label: 'Egypt', value: 'egypt' },
-            { label: 'France', value: 'france' },
-          ].map(({ label, value }) => (
-            <SelectList.Option key={label} label={label} value={value} />
-          ))}
-        </SelectList>
-      </Flex>
-    </Box>
+    <Flex alignItems="center" gap={4} height="100%" justifyContent="center" width="100%">
+      <SelectList id="selectlistexample1" label="Country" onChange={() => {}} size="lg">
+        {[
+          { label: 'Algeria', value: 'algeria' },
+          { label: 'Belgium', value: 'belgium' },
+          { label: 'Canada', value: 'canada' },
+          { label: 'Denmark', value: 'denmark' },
+          { label: 'Egypt', value: 'egypt' },
+          { label: 'France', value: 'france' },
+        ].map(({ label, value }) => (
+          <SelectList.Option key={label} label={label} value={value} />
+        ))}
+      </SelectList>
+    </Flex>
   );
 }
