fa1d1e6ccaab76633594b3d1ff9943ca26cdbed0
Diyorbek Sadullaev <dsadullaev@pinterest.com>
2024-03-25 12:49:36 +0100

    WIP

--- packages/gestalt/dist/index.d.ts
+++ packages/gestalt/dist/index.d.ts
@@ -493,6 +493,9 @@ interface DefaultLabelProviderProps {
         };
         SideNavigation: {
           accessibilityDismissButtonLabel: string;
+          accessibilityCollapseButtonLabel: string;
+          accessibilityExpandButtonLabel: string;
+          accessibilityEllipsisLabel: string;
         };
         BannerSlim: {
           accessibilityDismissButtonLabel: string;
