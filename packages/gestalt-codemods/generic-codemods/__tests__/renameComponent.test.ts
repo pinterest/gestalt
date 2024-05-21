ee028d8773bbb8104d74b0ef3ba64c96a3a765c3
Jack Hsu <jackhsu978@gmail.com>
2024-05-21 22:24:29 +0000

    removed a debugging change in renameComponent.test.ts

--- packages/gestalt-codemods/generic-codemods/__tests__/renameComponent.test.ts
+++ packages/gestalt-codemods/generic-codemods/__tests__/renameComponent.test.ts
@@ -7,19 +7,17 @@ jest.mock('../renameComponent', () =>
 );
 
 describe('renameComponent', () => {
-  ['renameComponent/renameComponent', 'renameComponent/renameComponentAlias']
-    .slice(0, 1) // TODO remove me
-    .forEach((test) => {
-      defineTest(
-        __dirname,
-        'renameComponent',
-        {
-          quote: 'single',
-          componentName: 'ScrollableContainer',
-          nextComponentName: 'ScrollBoundaryContainer',
-        },
-        test,
-        { parser: 'tsx' },
-      );
-    });
+  ['renameComponent/renameComponent', 'renameComponent/renameComponentAlias'].forEach((test) => {
+    defineTest(
+      __dirname,
+      'renameComponent',
+      {
+        quote: 'single',
+        componentName: 'ScrollableContainer',
+        nextComponentName: 'ScrollBoundaryContainer',
+      },
+      test,
+      { parser: 'tsx' },
+    );
+  });
 });
