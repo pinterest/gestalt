41c77e59e324edf5a154bb2c9496fb7712b455f8
Jack Hsu <jackhsu978@gmail.com>
2024-05-21 03:41:08 +0000

    fixup for unwanted suppressions added by yarn ts:migrate

--- packages/gestalt/src/Tag.tsx
+++ packages/gestalt/src/Tag.tsx
@@ -139,7 +139,6 @@ export default function Tag({
   return (
     <Box
       aria-disabled={disabled}
-// @ts-expect-error - TS2322 - Type 'string' is not assignable to type '"selected" | "default" | "shopping" | "inverse" | "light" | "dark" | "darkWash" | "lightWash" | "transparent" | "transparentDarkGray" | "infoBase" | "infoWeak" | "errorBase" | ... 15 more ... | undefined'.
       color={bgColor}
       dangerouslySetInlineStyle={{
         __style:
@@ -161,9 +160,7 @@ export default function Tag({
           <Box marginEnd={1} marginStart={1}>
             <Icon
               accessibilityLabel={accessibilityLabels[type]}
-// @ts-expect-error - TS2322 - Type 'string' is not assignable to type 'IconColor | undefined'.
               color={fgColor}
-// @ts-expect-error - TS2322 - Type 'string' is not assignable to type '"replace" | "search" | "link" | "text" | "dash" | "3D" | "3D-move" | "360" | "accessibility" | "ad" | "ad-group" | "add" | "add-circle" | "add-layout" | "add-pin" | "add-section" | ... 318 more ... | undefined'.
               icon={iconsByType[type]}
               size={iconSize}
             />
@@ -171,7 +168,6 @@ export default function Tag({
         )}
 
         <Text
-// @ts-expect-error - TS2322 - Type 'string' is not assignable to type '"link" | "warning" | "error" | "default" | "subtle" | "success" | "shopping" | "inverse" | "light" | "dark" | undefined'.
           color={fgColor}
           inline
           lineClamp={1}
@@ -199,7 +195,6 @@ export default function Tag({
                   accessibilityLabel={
                     accessibilityRemoveIconLabel ?? accessibilityRemoveIconLabelDefault
                   }
-// @ts-expect-error - TS2322 - Type 'string' is not assignable to type 'IconColor | undefined'.
                   color={fgColor}
                   icon="cancel"
                   size={removeIconSize}
