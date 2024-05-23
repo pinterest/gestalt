b24045e509fb252493949eb0f7dd32e9edd528c4
Diyorbek Sadullaev <dsadullaev@pinterest.com>
2024-05-23 18:48:56 +0200

    WIP

--- playwright/accessibility/foundations__animation__principles.spec.ts
+++ playwright/accessibility/foundations__animation__principles.spec.ts
@@ -1,8 +1,8 @@
 import { test } from '@playwright/test';
 import expectAccessiblePage from './expectAccessiblePage';
 
-test('Animation principles check', async ({ page }) => {
-  await page.goto('/foundations/animation/principles');
+test('Motion principles check', async ({ page }) => {
+  await page.goto('/foundations/motion/principles');
   // @ts-expect-error - TS2345 - Argument of type '{ page: Page; }' is not assignable to parameter of type '{ page: any; rules: any; }'.
   await expectAccessiblePage({ page });
 });
