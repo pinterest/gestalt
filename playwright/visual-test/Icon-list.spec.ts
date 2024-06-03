2e8f45b065be93e7318c5e3ae3ce4b2d166f070d
acarreras <acarreras@pinterest.com>
2024-06-03 19:35:18 -0400

    more

--- playwright/visual-test/Icon-list.spec.ts
+++ playwright/visual-test/Icon-list.spec.ts
@@ -4,7 +4,4 @@ test('Icon list visual regression check', async ({ page }) => {
   await page.goto('/visual-test/Icon-list');
   const locator = page.locator('[data-test-id="visual-test"]');
   await expect(locator).toHaveScreenshot('Icon-list.png');
-  await page.goto('/visual-test/Icon-vr-theme-list');
-  const locatorvr = page.locator('[data-test-id="visual-test"]');
-  await expect(locatorvr).toHaveScreenshot('Icon-vr-theme-list.png');
 });
