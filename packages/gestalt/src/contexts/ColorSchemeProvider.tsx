3650a41c757074eafa6238cb24893898f9672285
acarreras <acarreras@pinterest.com>
2024-06-09 22:36:01 -0400

    more

--- packages/gestalt/src/contexts/ColorSchemeProvider.tsx
+++ packages/gestalt/src/contexts/ColorSchemeProvider.tsx
@@ -41,8 +41,9 @@ const themeToStyles = (
   if (theme.colorSchemeName === 'darkMode') {
     Object.keys(isVisualRefresh ? vrDarkColorDesignTokens : darkColorDesignTokens).forEach(
       (key) => {
+        console.log(key)
         // @ts-expect-error - TS7053 - Element implicitly has an 'any' type because expression of type 'string' can't be used to index type '{ "color-red-pushpin-0": string; "color-red-pushpin-50": string; "color-red-pushpin-100": string; "color-red-pushpin-200": string; "color-red-pushpin-300": string; "color-red-pushpin-400": string; ... 327 more ...; "elevation-datepicker": string; }'.
-        styles += `  --${key}: ${isVisualRefresh ? vrDarkColorDesignTokens :darkColorDesignTokens[key]};\n`;
+        styles += `  --${key}: ${(isVisualRefresh ? vrDarkColorDesignTokens : darkColorDesignTokens)[key]};\n`;
       },
     );
   }
@@ -50,7 +51,7 @@ const themeToStyles = (
     Object.keys(isVisualRefresh ? vrLightColorDesignTokens : lightColorDesignTokens).forEach(
       (key) => {
         // @ts-expect-error - TS7053 - Element implicitly has an 'any' type because expression of type 'string' can't be used to index type '{ "color-red-pushpin-0": string; "color-red-pushpin-50": string; "color-red-pushpin-100": string; "color-red-pushpin-200": string; "color-red-pushpin-300": string; "color-red-pushpin-400": string; ... 327 more ...; "elevation-datepicker": string; }'.
-        styles += `  --${key}: ${isVisualRefresh ? vrLightColorDesignTokens : lightColorDesignTokens[key]};\n`;
+        styles += `  --${key}: ${(isVisualRefresh ? vrLightColorDesignTokens : lightColorDesignTokens)[key]};\n`;
       },
     );
   }
