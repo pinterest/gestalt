76aceb056ab30eee6d6f62a84c86cad528404d5f
eniomoura <eniormoura@gmail.com>
2024-06-12 16:23:18 -0300

    adding visual tests

--- docs/pages/visual-test/ButtonToggle.tsx
+++ docs/pages/visual-test/ButtonToggle.tsx
@@ -11,6 +11,29 @@ export default function Snapshot() {
         <ButtonToggle color="red" iconStart="sparkle" selected={false} text="Save" />
         <ButtonToggle color="red" iconStart="sparkle" selected text="Save" />
       </ButtonGroup>
+
+      <ButtonGroup>
+        <ButtonToggle
+          graphicSrc="https://s.pinimg.com/webapp/protective-8fad3fab.svg"
+          selected={false}
+          size="lg"
+          text="Protective"
+        />
+        <ButtonToggle
+          graphicSrc="https://s.pinimg.com/webapp/protective-8fad3fab.svg"
+          selected
+          size="lg"
+          text="Protective"
+        />
+      </ButtonGroup>
+      <ButtonGroup>
+        <ButtonToggle
+          color={['#F0E3DC', '#F8D7D8', '#F2D7BE', '#F7C3AF']}
+          selected={false}
+          size="lg"
+        />
+        <ButtonToggle color={['#F0E3DC', '#F8D7D8', '#F2D7BE', '#F7C3AF']} selected size="lg" />
+      </ButtonGroup>
     </Box>
   );
 }
