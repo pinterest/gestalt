cceb0d28d01e76668a71a78e0ddc3c31702e648a
Sarah Alli <salli@pinterest.com>
2024-06-17 16:52:21 -0400

    sorted props

--- docs/examples/bannerslim/dismissableExample.tsx
+++ docs/examples/bannerslim/dismissableExample.tsx
@@ -4,9 +4,9 @@ export default function Example() {
   return (
     <Box alignItems="center" display="flex" height="100%" justifyContent="center" padding={8}>
       <BannerSlim
-        onDismiss={() => {}}
         iconAccessibilityLabel="Information"
         message="This ad group is part of a campaign that is using campaign budget optimization. Changes to schedule or budget must be made at the campaign level."
+        onDismiss={() => {}}
         type="info"
       />
     </Box>
