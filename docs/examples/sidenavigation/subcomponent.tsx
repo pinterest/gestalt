cc59a340d9e1ec3aa24ecdbb3ee90d5bd4f34b16
Diyorbek Sadullaev <dsadullaev@pinterest.com>
2024-06-21 11:35:47 +0200

    exmpl

--- docs/examples/sidenavigation/subcomponent.tsx
+++ docs/examples/sidenavigation/subcomponent.tsx
@@ -1,139 +1,141 @@
 import React, { useState } from 'react';
-import { SideNavigation } from 'gestalt';
+import { Box, SideNavigation } from 'gestalt';
 
 export default function Example() {
   const [page, setPage] = useState('');
   const someCondition = true;
 
   return (
-    <SideNavigation accessibilityLabel="Subcomponent composability example">
-      {someCondition && (
-        <SideNavigation.TopItem
-          active={page === 'Trends' ? 'page' : undefined}
-          href="#"
-          icon="ads-stats"
-          label="Trends"
-          onClick={({ event }) => {
-            event.preventDefault();
-            setPage('Trends');
-          }}
-        />
-      )}
-
-      <SideNavigation.Section label="Analytics">
+    <Box width={300}>
+      <SideNavigation accessibilityLabel="Subcomponent composability example">
         {someCondition && (
           <SideNavigation.TopItem
-            active={page === 'Reporting' ? 'page' : undefined}
+            active={page === 'Trends' ? 'page' : undefined}
             href="#"
             icon="ads-stats"
-            label="Reporting"
+            label="Trends"
             onClick={({ event }) => {
               event.preventDefault();
-              setPage('Reporting');
+              setPage('Trends');
             }}
           />
         )}
-        {someCondition && (
-          <SideNavigation.TopItem
-            active={page === 'Conversions' ? 'page' : undefined}
-            href="#"
-            icon="replace"
-            label="Conversions"
-            onClick={({ event }) => {
-              event.preventDefault();
-              setPage('Conversions');
-            }}
-          />
-        )}
-      </SideNavigation.Section>
 
-      <SideNavigation.Section label="Audiences">
-        <SideNavigation.Group
-          active={page === 'Christmas' ? 'page' : undefined}
-          href="#"
-          icon="people"
-          label="Christmas"
-          onClick={({ event }) => {
-            event.preventDefault();
-            setPage('Christmas');
-          }}
-        >
-          <SideNavigation.NestedItem
-            active={page === 'Luxury Christmas' ? 'page' : undefined}
-            href="#"
-            label="Luxury Christmas"
-            onClick={({ event }) => {
-              event.preventDefault();
-              setPage('Luxury Christmas');
-            }}
-          />
-
-          <SideNavigation.NestedGroup
-            active={page === 'Classic Christmas' ? 'page' : undefined}
-            href="#"
-            label="Classic Christmas"
-            onClick={({ event }) => {
-              event.preventDefault();
-              setPage('Classic Christmas');
-            }}
-          >
-            {['West Coast', 'East Coast'].map((x) => (
-              <SideNavigation.NestedItem
-                key={`xmas${x}`}
-                active={page === `Classic ${x}` ? 'page' : undefined}
-                href="#"
-                label={x}
-                onClick={({ event }) => {
-                  event.preventDefault();
-                  setPage(`Classic ${x}`);
-                }}
-              />
-            ))}
-          </SideNavigation.NestedGroup>
-
-          <SideNavigation.NestedGroup label="Alternative Christmas">
-            {['West Coast', 'East Coast'].map((x) => (
-              <SideNavigation.NestedItem
-                key={`xmas${x}`}
-                active={page === `Alternative ${x}` ? 'page' : undefined}
-                href="#"
-                label={x}
-                onClick={({ event }) => {
-                  event.preventDefault();
-                  setPage(`Alternative ${x}`);
-                }}
-              />
-            ))}
-            {['Southern', 'NorthEast'].map((x) => (
-              <SideNavigation.NestedItem
-                key={`xmas${x}`}
-                active={page === `Alternative ${x}` ? 'page' : undefined}
-                href="#"
-                label={x}
-                onClick={({ event }) => {
-                  event.preventDefault();
-                  setPage(`Alternative ${x}`);
-                }}
-              />
-            ))}
-          </SideNavigation.NestedGroup>
-        </SideNavigation.Group>
-
-        <SideNavigation.Group icon="people" label="Halloween">
-          {['West Coast', 'East Coast'].map((x) => (
-            <SideNavigation.NestedItem
-              key={`halloween${x}`}
-              active={page === `Halloween ${x}` ? 'page' : undefined}
+        <SideNavigation.Section label="Analytics">
+          {someCondition && (
+            <SideNavigation.TopItem
+              active={page === 'Reporting' ? 'page' : undefined}
               href="#"
-              label={x}
+              icon="ads-stats"
+              label="Reporting"
               onClick={({ event }) => {
                 event.preventDefault();
-                setPage(`Halloween ${x}`);
+                setPage('Reporting');
               }}
             />
-          ))}
-        </SideNavigation.Group>
-      </SideNavigation.Section>
-    </SideNavigation>
+          )}
+          {someCondition && (
+            <SideNavigation.TopItem
+              active={page === 'Conversions' ? 'page' : undefined}
+              href="#"
+              icon="replace"
+              label="Conversions"
+              onClick={({ event }) => {
+                event.preventDefault();
+                setPage('Conversions');
+              }}
+            />
+          )}
+        </SideNavigation.Section>
+
+        <SideNavigation.Section label="Audiences">
+          <SideNavigation.Group
+            active={page === 'Christmas' ? 'page' : undefined}
+            href="#"
+            icon="people"
+            label="Christmas"
+            onClick={({ event }) => {
+              event.preventDefault();
+              setPage('Christmas');
+            }}
+          >
+            <SideNavigation.NestedItem
+              active={page === 'Luxury Christmas' ? 'page' : undefined}
+              href="#"
+              label="Luxury Christmas"
+              onClick={({ event }) => {
+                event.preventDefault();
+                setPage('Luxury Christmas');
+              }}
+            />
+
+            <SideNavigation.NestedGroup
+              active={page === 'Classic Christmas' ? 'page' : undefined}
+              href="#"
+              label="Classic Christmas"
+              onClick={({ event }) => {
+                event.preventDefault();
+                setPage('Classic Christmas');
+              }}
+            >
+              {['West Coast', 'East Coast'].map((x) => (
+                <SideNavigation.NestedItem
+                  key={`xmas${x}`}
+                  active={page === `Classic ${x}` ? 'page' : undefined}
+                  href="#"
+                  label={x}
+                  onClick={({ event }) => {
+                    event.preventDefault();
+                    setPage(`Classic ${x}`);
+                  }}
+                />
+              ))}
+            </SideNavigation.NestedGroup>
+
+            <SideNavigation.NestedGroup label="Alternative Christmas">
+              {['West Coast', 'East Coast'].map((x) => (
+                <SideNavigation.NestedItem
+                  key={`xmas${x}`}
+                  active={page === `Alternative ${x}` ? 'page' : undefined}
+                  href="#"
+                  label={x}
+                  onClick={({ event }) => {
+                    event.preventDefault();
+                    setPage(`Alternative ${x}`);
+                  }}
+                />
+              ))}
+              {['Southern', 'NorthEast'].map((x) => (
+                <SideNavigation.NestedItem
+                  key={`xmas${x}`}
+                  active={page === `Alternative ${x}` ? 'page' : undefined}
+                  href="#"
+                  label={x}
+                  onClick={({ event }) => {
+                    event.preventDefault();
+                    setPage(`Alternative ${x}`);
+                  }}
+                />
+              ))}
+            </SideNavigation.NestedGroup>
+          </SideNavigation.Group>
+
+          <SideNavigation.Group icon="people" label="Halloween">
+            {['West Coast', 'East Coast'].map((x) => (
+              <SideNavigation.NestedItem
+                key={`halloween${x}`}
+                active={page === `Halloween ${x}` ? 'page' : undefined}
+                href="#"
+                label={x}
+                onClick={({ event }) => {
+                  event.preventDefault();
+                  setPage(`Halloween ${x}`);
+                }}
+              />
+            ))}
+          </SideNavigation.Group>
+        </SideNavigation.Section>
+      </SideNavigation>
+    </Box>
   );
 }
