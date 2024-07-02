pinterest/gestalt

commit 14c16a9287549fb3755333065f35698fb9b83946

SelectList: Added onBlur/onFocus props (#3651)

packages/gestalt/src/SelectList.tsx
@@ -50,9 +50,18 @@ type Props = {
    */
   name?: string;
   /**
+   * Callback triggered when the user blurs the input.
+   */
+  onBlur?: (arg1: { event: React.FocusEvent<HTMLSelectElement>; value: string }) => void;
+  /**
+  /**
    * Callback triggered when the user selects a new option.  See the [controlled component](https://gestalt.pinterest.systems/web/selectlist#Controlled-component) variant to learn more.
    */
   onChange: (arg1: { event: React.ChangeEvent<HTMLSelectElement>; value: string }) => void;
+  /**
+   * Callback triggered when the user focuses the input.
+   */
+  onFocus?: (arg1: { event: React.FocusEvent<HTMLSelectElement>; value: string }) => void;
   /**
    * If not provided, the first item in the list will be shown. Be sure to localize the text. See the [controlled component](https://gestalt.pinterest.systems/web/selectlist#Controlled-component) variant to learn more.
    */
@@ -84,7 +93,9 @@ function SelectList({
   label,
   labelDisplay = 'visible',
   name,
+  onBlur,
   onChange,
+  onFocus,
   placeholder,
   size = 'md',
   value,
@@ -95,6 +106,19 @@ function SelectList({
     if (value !== event.target.value) {
       onChange({ event, value: event.target.value });
     }
+  };
+
+  const handleBlur = (event: React.FocusEvent<HTMLSelectElement>) => {
+    const { value: eventValue } = event.target;
+    onBlur?.({ event, value: eventValue });
+    handleOnChange(event);
+    setFocused(false);
+  };
+
+  const handleFocus = (event: React.FocusEvent<HTMLSelectElement>) => {
+    const { value: eventValue } = event.target;
+    onFocus?.({ event, value: eventValue });
+    setFocused(true);
   };
 
   const classes = classnames(
@@ -160,12 +184,9 @@ function SelectList({
           disabled={disabled}
           id={id}
           name={name}
-          onBlur={(event) => {
-            setFocused(false);
-            handleOnChange(event);
-          }}
+          onBlur={handleBlur}
           onChange={handleOnChange}
-          onFocus={() => setFocused(true)}
+          onFocus={handleFocus}
           // @ts-expect-error - TS2322 - Type 'string | null | undefined' is not assignable to type 'string | number | readonly string[] | undefined'.
           value={showPlaceholder ? placeholder : value}
         >

commit e335b16e3757c520e139be8b17141904f0af09f5

SelectList/TextArea: fix rounding bug introduced in #3651 (#3658)

packages/gestalt/src/SelectList.tsx
@@ -126,6 +126,8 @@ function SelectList({
     formElement.base,
     size === 'md' ? layout.medium : layout.large,
     {
+      [formElement.md]: size === 'md',
+      [formElement.lg]: size === 'lg',
       [formElement.normal]: !errorMessage,
       [formElement.enabledTransparent]: !disabled,
       [formElement.disabled]: disabled,

packages/gestalt/src/TextArea.tsx
@@ -162,7 +162,7 @@ const TextAreaWithForwardRef = forwardRef<HTMLTextAreaElement, Props>(function T
   const classes = classnames(
     styles.textArea,
     formElement.base,
-    formElement.base.lg,
+    formElement.lg,
     disabled ? formElement.disabled : formElement.enabled,
     (hasError || hasErrorMessage) && !focused ? formElement.errored : formElement.normal,
     tags

packages/gestalt/src/__snapshots__/SelectList.test.tsx.snap
@@ -52,7 +52,7 @@ exports[`SelectList renders an option group 1`] = `
     </div>
     <select
       aria-invalid="false"
-      className="select base medium normal enabledTransparent"
+      className="select base medium md normal enabledTransparent"
       disabled={false}
       id="optionGroup"
       onBlur={[Function]}
@@ -145,7 +145,7 @@ exports[`SelectList renders with a hidden label 1`] = `
     </div>
     <select
       aria-invalid="false"
-      className="select base medium normal enabledTransparent"
+      className="select base medium md normal enabledTransparent"
       disabled={false}
       id="test"
       onBlur={[Function]}
@@ -210,7 +210,7 @@ exports[`SelectList renders with typical props 1`] = `
     </div>
     <select
       aria-invalid="false"
-      className="select base medium normal enabledTransparent"
+      className="select base medium md normal enabledTransparent"
       disabled={false}
       id="test"
       onBlur={[Function]}

packages/gestalt/src/__snapshots__/TextArea.test.tsx.snap
@@ -4,7 +4,7 @@ exports[`TextArea TextArea normal 1`] = `
 <span>
   <textarea
     aria-invalid="false"
-    className="textArea base enabled normal"
+    className="textArea base lg enabled normal"
     disabled={false}
     id="test"
     onBlur={[Function]}
@@ -21,7 +21,7 @@ exports[`TextArea TextArea with disabled 1`] = `
 <span>
   <textarea
     aria-invalid="false"
-    className="textArea base disabled normal"
+    className="textArea base lg disabled normal"
     disabled={true}
     id="test"
     onBlur={[Function]}
@@ -38,7 +38,7 @@ exports[`TextArea TextArea with error 1`] = `
 <span>
   <textarea
     aria-invalid="true"
-    className="textArea base enabled errored"
+    className="textArea base lg enabled errored"
     disabled={false}
     id="test"
     onBlur={[Function]}
@@ -99,7 +99,7 @@ exports[`TextArea TextArea with hasError 1`] = `
 <span>
   <textarea
     aria-invalid="true"
-    className="textArea base enabled errored"
+    className="textArea base lg enabled errored"
     disabled={false}
     id="test"
     onBlur={[Function]}
@@ -116,7 +116,7 @@ exports[`TextArea TextArea with readOnly 1`] = `
 <span>
   <textarea
     aria-invalid="false"
-    className="textArea base enabled normal"
+    className="textArea base lg enabled normal"
     disabled={false}
     id="test"
     onBlur={[Function]}
@@ -133,7 +133,7 @@ exports[`TextArea TextArea with rows 1`] = `
 <span>
   <textarea
     aria-invalid="false"
-    className="textArea base enabled normal"
+    className="textArea base lg enabled normal"
     disabled={false}
     id="test"
     onBlur={[Function]}
@@ -150,7 +150,7 @@ exports[`TextArea TextField with maxLength character counter 1`] = `
 <span>
   <textarea
     aria-invalid="false"
-    className="textArea base enabled normal"
+    className="textArea base lg enabled normal"
     disabled={false}
     id="test"
     maxLength={20}
@@ -237,7 +237,7 @@ exports[`TextArea TextField with maxLength character counter 1`] = `
 exports[`TextArea renders tags when supplied 1`] = `
 <span>
   <div
-    className="textArea base enabled normal textAreaWrapper"
+    className="textArea base lg enabled normal textAreaWrapper"
     style={
       Object {
         "maxHeight": 92,