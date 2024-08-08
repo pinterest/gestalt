a1f855bb7cefa3c44a59165b0929136d263112a9
acarreras <acarreras@pinterest.com>
2024-08-08 12:57:30 -0400

    more

--- docs/examples/selectlist/labelHidden.tsx
+++ docs/examples/selectlist/labelHidden.tsx
@@ -18,6 +18,7 @@ export default function Example() {
         <SelectList
           id="selectlist-hidden-label"
           label="Date range"
+          labelDisplay='hidden'
           onChange={({ value }) => setRange(value)}
           placeholder="Select a country"
           value={range}
