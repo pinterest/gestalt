9c016f997e2d469626e4c73815a8ab00fb616411
Ravi Lingineni <rlingineni@pinterest.com>
2024-09-20 15:11:25 -0700

    init add badge-icons

---
+++ packages/gestalt-icons-android/src/vr-theme/components/index.ts
@@ -0,0 +1,17 @@
+import badgeCheckCircleFill from './ic_vr_badge-check-circle-fill_gestalt.svg';
+import badgeInfoCircleFill from './ic_vr_badge-info-circle-fill_gestalt.svg';
+import badgeLock from './ic_vr_badge-lock_gestalt.svg';
+import badgeWorkflowStatusProblem from './ic_vr_badge-workflow-status-problem_gestalt.svg';
+import badgeWorkflowStatusWarning from './ic_vr_badge-workflow-status-warning_gestalt.svg';
+
+
+
+const icons = Object.freeze({
+    'badge-check-circle-fill': badgeCheckCircleFill,
+	'badge-info-circle-fill': badgeInfoCircleFill,
+	'badge-lock': badgeLock,
+	'badge-workflow-status-problem': badgeWorkflowStatusProblem,
+	'badge-workflow-status-warning': badgeWorkflowStatusWarning,
+});
+
+export default icons;
