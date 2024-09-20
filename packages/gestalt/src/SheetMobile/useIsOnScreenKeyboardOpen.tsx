c03e5c02ba11b781b4c2296493dfa389c84dcf57
acarreras <acarreras@pinterest.com>
2024-09-20 02:44:06 +0200

    moe

--- packages/gestalt/src/SheetMobile/useIsOnScreenKeyboardOpen.tsx
+++ packages/gestalt/src/SheetMobile/useIsOnScreenKeyboardOpen.tsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react';
 
-const isKeyboardInput = (elem: HTMLElement) =>
-  (elem.tagName === 'INPUT' &&
-    ['date', 'number', 'email', 'password', 'tel', 'text', 'url'].includes(
-      (elem as HTMLInputElement).type,
-    )) ||
-  elem.hasAttribute('contenteditable');
-
+const isKeyboardInput = (elem: HTMLElement) => {
+  // eslint-disable-next-line no-console
+  console.log(elem);
+  return (
+    (elem.tagName === 'INPUT' &&
+      ['date', 'number', 'email', 'password', 'tel', 'text', 'url'].includes(
+        (elem as HTMLInputElement).type,
+      )) ||
+    elem.hasAttribute('contenteditable')
+  );
+};
 const useIsOnScreenKeyboardOpen = () => {
   const [isOpen, setOpen] = useState(false);
+
   useEffect(() => {
     const handleFocusIn = (e: FocusEvent) => {
       if (!e.target) {
@@ -19,7 +24,9 @@ const useIsOnScreenKeyboardOpen = () => {
         setOpen(true);
       }
     };
+
     document.addEventListener('focusin', handleFocusIn);
+
     const handleFocusOut = (e: FocusEvent) => {
       if (!e.target) {
         return;
@@ -29,6 +36,7 @@ const useIsOnScreenKeyboardOpen = () => {
         setOpen(false);
       }
     };
+
     document.addEventListener('focusout', handleFocusOut);
 
     return () => {
