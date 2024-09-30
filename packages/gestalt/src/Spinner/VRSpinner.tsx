12f75c102a1f43e4892d2827827cf153408cf7c0
Diyorbek Sadullaev <dsadullaev@pinterest.com>
2024-09-30 18:05:39 +0500

    refine

--- packages/gestalt/src/Spinner/VRSpinner.tsx
+++ packages/gestalt/src/Spinner/VRSpinner.tsx
@@ -1,6 +1,13 @@
 import classnames from 'classnames';
 import styles from './VRSpinner.css';
+import Box from '../Box';
 import { useDefaultLabelContext } from '../contexts/DefaultLabelProvider';
+
+const SIZE_NAME_TO_PIXEL = {
+  sm: 32,
+  md: 40,
+  lg: 48,
+} as const;
 
 type Props = {
   accessibilityLabel?: string;
@@ -12,19 +19,26 @@ type Props = {
 export default function Spinner({ accessibilityLabel, delay = true, show, size = 'md' }: Props) {
   const { accessibilityLabel: accessibilityLabelDefault } = useDefaultLabelContext('Spinner');
 
-  if (!show) return <div />;
+  if (!show) return null;
 
   return (
-    <div
-      aria-label={accessibilityLabel ?? accessibilityLabelDefault}
-      className={classnames(styles.spinner, styles[size], { [styles.delay]: delay })}
-    >
-      <div className={styles.spinnerFrame}>
-        <div className={styles.dot1} />
-        <div className={styles.dot2} />
-        <div className={styles.dot3} />
+    <Box display="flex" justifyContent="around">
+      <div
+        aria-label={accessibilityLabel ?? accessibilityLabelDefault}
+        className={classnames(styles.spinner, styles[size], { [styles.delay]: delay })}
+        style={
+          {
+            '--g-size': `${SIZE_NAME_TO_PIXEL[size]}px`,
+          } as React.CSSProperties
+        }
+      >
+        <div className={styles.spinnerFrame}>
+          <div className={styles.dot1} />
+          <div className={styles.dot2} />
+          <div className={styles.dot3} />
+        </div>
       </div>
-    </div>
+    </Box>
   );
 }
 
