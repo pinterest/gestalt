fbf66272d9c9009e58ef2ae0df140745bfc41043
Ravi Lingineni <rlingineni@pinterest.com>
2024-10-16 14:36:38 -0700

    use compact icons

---
+++ packages/gestalt-icons-android/src/classic/compact/index.ts
@@ -0,0 +1,37 @@
+import compactAdd from './ic_compact-add_gestalt.svg';
+import compactArrowUpRight from './ic_compact-arrow-up-right_gestalt.svg';
+import compactCancel from './ic_compact-cancel_gestalt.svg';
+import compactCheck from './ic_compact-check_gestalt.svg';
+import compactCheckCircleFill from './ic_compact-check-circle-fill_gestalt.svg';
+import compactChevronDown from './ic_compact-chevron-down_gestalt.svg';
+import compactChevronLeft from './ic_compact-chevron-left_gestalt.svg';
+import compactChevronRight from './ic_compact-chevron-right_gestalt.svg';
+import compactChevronUp from './ic_compact-chevron-up_gestalt.svg';
+import compactDash from './ic_compact-dash_gestalt.svg';
+import compactLock from './ic_compact-lock_gestalt.svg';
+import compactSparkle from './ic_compact-sparkle_gestalt.svg';
+import compactWorkflowStatusProblem from './ic_compact-workflow-status-problem_gestalt.svg';
+import compactWorkflowStatusWarning from './ic_compact-workflow-status-warning_gestalt.svg';
+import conpactInfoCircleFill from './ic_conpact-info-circle-fill_gestalt.svg';
+
+
+
+const icons = Object.freeze({
+    'compact-add': compactAdd,
+	'compact-arrow-up-right': compactArrowUpRight,
+	'compact-cancel': compactCancel,
+	'compact-check': compactCheck,
+	'compact-check-circle-fill': compactCheckCircleFill,
+	'compact-chevron-down': compactChevronDown,
+	'compact-chevron-left': compactChevronLeft,
+	'compact-chevron-right': compactChevronRight,
+	'compact-chevron-up': compactChevronUp,
+	'compact-dash': compactDash,
+	'compact-lock': compactLock,
+	'compact-sparkle': compactSparkle,
+	'compact-workflow-status-problem': compactWorkflowStatusProblem,
+	'compact-workflow-status-warning': compactWorkflowStatusWarning,
+	'conpact-info-circle-fill': conpactInfoCircleFill,
+});
+
+export default icons;
