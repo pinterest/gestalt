709aaf1f03e0660a3702fd353511343d46cc3bae
Raviteja Lingineni <rlingineni@pinterest.com>
2024-10-22 09:36:27 -0700

    Icon: Add Compact Icons (#3771)

---
+++ docs/examples/icon/compactIcon.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Flex, IconCompact, SelectList } from 'gestalt';
+
+export default function Example() {
+  const compactIconsList = IconCompact.icons;
+  const [selectedValue, setSelectedValue] = useState<string | undefined>(compactIconsList[0]);
+
+  return (
+    <Flex alignItems="center" height="100%" justifyContent="center" width="100%">
+      <Flex alignItems="center" direction="row" gap={4}>
+        <SelectList
+          id="select-list-example"
+          onChange={({ value }) => {
+            setSelectedValue(value);
+          }}
+        >
+          {compactIconsList.map((value: string) => (
+            <SelectList.Option key={value} label={value} value={value} />
+          ))}
+        </SelectList>
+
+        <IconCompact
+          accessibilityLabel="Add a new item"
+          color="default"
+          icon={selectedValue as typeof compactIconsList[0]}
+        />
+      </Flex>
+    </Flex>
+  );
+}
---
+++ packages/gestalt-icons-android/dist/classic/compact/index.d.ts
@@ -0,0 +1,18 @@
+declare const icons: Readonly<{
+    'compact-add': any;
+    'compact-arrow-up-right': any;
+    'compact-cancel': any;
+    'compact-check': any;
+    'compact-check-circle-fill': any;
+    'compact-chevron-down': any;
+    'compact-chevron-left': any;
+    'compact-chevron-right': any;
+    'compact-chevron-up': any;
+    'compact-dash': any;
+    'compact-lock': any;
+    'compact-sparkle': any;
+    'compact-workflow-status-problem': any;
+    'compact-workflow-status-warning': any;
+    'compact-info-circle-fill': any;
+}>;
+export default icons;
