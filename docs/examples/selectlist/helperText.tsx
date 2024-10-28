07893a26e04a83e39675f4e14195cc3e487a55b4
acarreras <acarreras@pinterest.com>
2024-10-28 17:03:14 +0100

    more

--- docs/examples/selectlist/helperText.tsx
+++ docs/examples/selectlist/helperText.tsx
@@ -1,26 +1,48 @@
-import { Box, SelectList } from 'gestalt';
+import { Box, Flex, SelectList } from 'gestalt';
 
 export default function Example() {
   return (
     <Box padding={8} width="100%">
-      <SelectList
-        helperText="Product prices in your data source without an ISO currency code will default to this currency"
-        id="selectlistHelperText"
-        label="Default currency"
-        onChange={() => {}}
-      >
-        {[
-          { label: 'ARS - Argentine peso', value: 'ars' },
-          { label: 'AUD - Australian dollar', value: 'aud' },
-          { label: 'ERN - Eritrean nakfa', value: 'ern' },
-          { label: 'EUR - Euro', value: 'eur' },
-          { label: 'GBP - British pound', value: 'gbp' },
-          { label: 'JPY - Japanese yen', value: 'jpy' },
-          { label: 'USD - United States Dollar', value: 'usd' },
-        ].map(({ label, value }) => (
-          <SelectList.Option key={label} label={label} value={value} />
-        ))}
-      </SelectList>
+      <Flex direction="column" gap={6} width="100%">
+        <SelectList
+          helperText="Product prices in your data source without an ISO currency code will default to this currency"
+          id="selectlistHelperText"
+          label="Default currency"
+          onChange={() => {}}
+          size="md"
+        >
+          {[
+            { label: 'ARS - Argentine peso', value: 'ars' },
+            { label: 'AUD - Australian dollar', value: 'aud' },
+            { label: 'ERN - Eritrean nakfa', value: 'ern' },
+            { label: 'EUR - Euro', value: 'eur' },
+            { label: 'GBP - British pound', value: 'gbp' },
+            { label: 'JPY - Japanese yen', value: 'jpy' },
+            { label: 'USD - United States Dollar', value: 'usd' },
+          ].map(({ label, value }) => (
+            <SelectList.Option key={label} label={label} value={value} />
+          ))}
+        </SelectList>
+        <SelectList
+          helperText="Product prices in your data source without an ISO currency code will default to this currency"
+          id="selectlistHelperText"
+          label="Default currency"
+          onChange={() => {}}
+          size="lg"
+        >
+          {[
+            { label: 'ARS - Argentine peso', value: 'ars' },
+            { label: 'AUD - Australian dollar', value: 'aud' },
+            { label: 'ERN - Eritrean nakfa', value: 'ern' },
+            { label: 'EUR - Euro', value: 'eur' },
+            { label: 'GBP - British pound', value: 'gbp' },
+            { label: 'JPY - Japanese yen', value: 'jpy' },
+            { label: 'USD - United States Dollar', value: 'usd' },
+          ].map(({ label, value }) => (
+            <SelectList.Option key={label} label={label} value={value} />
+          ))}
+        </SelectList>
+      </Flex>
     </Box>
   );
 }
--- docs/examples/selectlist/labelled.tsx
+++ docs/examples/selectlist/labelled.tsx
@@ -3,7 +3,7 @@ import { Box, Flex, IconButton, Label, SelectList, Text } from 'gestalt';
 export default function Example() {
   return (
     <Box padding={8} width="100%">
-      <Flex gap={6}>
+      <Flex direction="column" gap={6} width="100%">
         <Flex direction="column" gap={2}>
           <Label htmlFor="selectList-labelled">
             <Flex alignItems="center" gap={1}>
@@ -24,6 +24,40 @@ export default function Example() {
             label="Date range"
             labelDisplay="hidden"
             onChange={() => {}}
+            size="md"
+          >
+            {[
+              { label: 'Last 5 days', value: '5' },
+              { label: 'Last week', value: '7' },
+              { label: 'Last 30 days', value: '30' },
+              { label: 'Last sixth months', value: '6m' },
+              { label: 'Last year', value: '365' },
+            ].map(({ label, value }) => (
+              <SelectList.Option key={label} label={label} value={value} />
+            ))}
+          </SelectList>
+        </Flex>
+        <Flex direction="column" gap={2}>
+          <Label htmlFor="selectList-labelled">
+            <Flex alignItems="center" gap={1}>
+              <Text size="300">Date range</Text>
+              <IconButton
+                accessibilityLabel="Info"
+                icon="info-circle"
+                size="sm"
+                tooltip={{
+                  text: 'Options available are based on your usage.',
+                  idealDirection: 'right',
+                }}
+              />
+            </Flex>
+          </Label>
+          <SelectList
+            id="selectList-labelled"
+            label="Date range"
+            labelDisplay="hidden"
+            onChange={() => {}}
+            size="lg"
           >
             {[
               { label: 'Last 5 days', value: '5' },
