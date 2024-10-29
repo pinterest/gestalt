410da89c23c0f1747288f8791b44f56c534abc66
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2024-10-29 17:28:48 +0100

    SelectList: fixes VR (#3833)

--- docs/examples/selectlist/disabled.tsx
+++ docs/examples/selectlist/disabled.tsx
@@ -1,30 +1,54 @@
 import { useState } from 'react';
-import { Box, SelectList } from 'gestalt';
+import { Box, Flex, SelectList } from 'gestalt';
 
 export default function Example() {
   const [country, setCountry] = useState('');
   return (
     <Box padding={8} width="100%">
-      <SelectList
-        disabled
-        id="selectlist-disabled"
-        label="Country"
-        name="country"
-        onChange={({ value }) => setCountry(value)}
-        placeholder="Select a country"
-        value={country}
-      >
-        {[
-          { label: 'Algeria', value: 'algeria' },
-          { label: 'Belgium', value: 'belgium' },
-          { label: 'Canada', value: 'canada' },
-          { label: 'Denmark', value: 'denmark' },
-          { label: 'Egypt', value: 'egypt' },
-          { label: 'France', value: 'france' },
-        ].map(({ label, value }) => (
-          <SelectList.Option key={label} label={label} value={value} />
-        ))}
-      </SelectList>
+      <Flex direction="column" gap={6} width="100%">
+        <SelectList
+          disabled
+          id="selectlist-disabled"
+          label="Country"
+          name="country"
+          onChange={({ value }) => setCountry(value)}
+          placeholder="Select a country"
+          size="md"
+          value={country}
+        >
+          {[
+            { label: 'Algeria', value: 'algeria' },
+            { label: 'Belgium', value: 'belgium' },
+            { label: 'Canada', value: 'canada' },
+            { label: 'Denmark', value: 'denmark' },
+            { label: 'Egypt', value: 'egypt' },
+            { label: 'France', value: 'france' },
+          ].map(({ label, value }) => (
+            <SelectList.Option key={label} label={label} value={value} />
+          ))}
+        </SelectList>
+        <SelectList
+          disabled
+          id="selectlist-disabled"
+          label="Country"
+          name="country"
+          onChange={({ value }) => setCountry(value)}
+          placeholder="Select a country"
+          size="lg"
+          value={country}
+        >
+          {[
+            { label: 'Algeria', value: 'algeria' },
+            { label: 'Belgium', value: 'belgium' },
+            { label: 'Canada', value: 'canada' },
+            { label: 'Denmark', value: 'denmark' },
+            { label: 'Egypt', value: 'egypt' },
+            { label: 'France', value: 'france' },
+          ].map(({ label, value }) => (
+            <SelectList.Option key={label} label={label} value={value} />
+          ))}
+        </SelectList>
+      </Flex>
     </Box>
   );
 }
