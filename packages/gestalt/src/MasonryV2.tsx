65868ecb93ee1dc253a4fb16f34c0b065fa2a51b
Bryam Rodriguez <brodriguezaybar@gmail.com>
2024-11-05 14:12:11 -0600

    Fix type

--- packages/gestalt/src/MasonryV2.tsx
+++ packages/gestalt/src/MasonryV2.tsx
@@ -398,7 +398,7 @@ function useLayout<T>({
   _getColumnSpanConfig?: (item: T) => ColumnSpanConfig;
   _loadingStateItems?: ReadonlyArray<LoadingStateItem>;
   _renderLoadingStateItems?: Props<T>['_renderLoadingStateItems'];
-  _earlyBailout?: boolean;
+  _earlyBailout?: (columnSpan: number) => number;
 }): {
   height: number;
   hasPendingMeasurements: boolean;
