5c30b61aaa55cf615cf8fb18ea8a6dedbf0a9f63
acarreras <acarreras@pinterest.com>
2024-11-06 19:09:27 +0100

    more

--- docs/pages/web/iconbutton.tsx
+++ docs/pages/web/iconbutton.tsx
@@ -2,7 +2,7 @@ import disabledStateExample from 'docs/examples/iconbutton/disabled';
 import defaultStateExample from 'docs/examples/iconbutton/enabled';
 import focusOnDarkBackground from 'docs/examples/iconbutton/focusOnDarkBackground';
 import selectedStateExample from 'docs/examples/iconbutton/selectedStateExample';
-import { Box, IconButton, Pog } from 'gestalt';
+import { IconButton, Pog } from 'gestalt';
 import AccessibilitySection from '../../docs-components/AccessibilitySection';
 import CombinationNew from '../../docs-components/CombinationNew';
 import docGen, { DocGen } from '../../docs-components/docgen';
