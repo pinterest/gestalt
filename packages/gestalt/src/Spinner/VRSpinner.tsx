6a72b9a95d42163a744a5f4bbd699326cab889e8
Diyorbek Sadullaev <dsadullaev@pinterest.com>
2024-11-19 01:23:11 +0100

    WIP

--- packages/gestalt/src/Spinner/VRSpinner.tsx
+++ packages/gestalt/src/Spinner/VRSpinner.tsx
@@ -1,4 +1,4 @@
-import { AnimationEventHandler, useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import classnames from 'classnames';
 import styles from './VRSpinner.css';
 import Box from '../Box';
