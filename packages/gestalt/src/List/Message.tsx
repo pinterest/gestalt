1f76b53e5d92135b8b99d8c159697273aebc7e37
acarreras <acarreras@pinterest.com>
2024-12-05 17:04:28 -0500

    more

--- packages/gestalt/src/List/Message.tsx
+++ packages/gestalt/src/List/Message.tsx
@@ -9,7 +9,6 @@ type Props = {
 };
 
 export default function ListText({ size, text }: Props) {
-
   // Flow shuold catch if text is missing. In case Flow is not enabled and text is missing, the errors are not that helpful. This surfaces the problem more explicitly.
   if (!text) {
     throw new Error(`Gestalt List is missing \`label\` prop or a \`text\` prop within List.Item.`);
@@ -21,9 +20,12 @@ export default function ListText({ size, text }: Props) {
 
   // If `text` is a Text component, we need to override any text size within to ensure they all match
   // @ts-expect-error - TS2339
-  if (text && typeof text !== 'string' && Children.only<ReactElement>(text)?.type.displayName === 'Text') {
-
-    return cloneElement(text, { size: size || undefined })
+  if (
+    text &&
+    typeof text !== 'string' &&
+    Children.only<ReactElement>(text)?.type.displayName === 'Text'
+  ) {
+    return cloneElement(text, { size: size || undefined });
   }
 
   throw new Error(
