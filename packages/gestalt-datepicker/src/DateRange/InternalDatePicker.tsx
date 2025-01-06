e2607c7ea8f7471300a1cd863418de9b62c4886b
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2025-01-06 16:44:12 +0100

    DatePicker: upgraded package to latest 7.5.0 (#3920)

--- packages/gestalt-datepicker/src/DateRange/InternalDatePicker.tsx
+++ packages/gestalt-datepicker/src/DateRange/InternalDatePicker.tsx
@@ -1,4 +1,12 @@
-import { forwardRef, ReactElement, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import {
+  ComponentProps,
+  forwardRef,
+  ReactElement,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 import ReactDatePicker, { registerLocale } from 'react-datepicker';
 import { Icon, useDeviceType } from 'gestalt';
 import { Props } from '../DatePicker';
@@ -40,8 +48,10 @@ const InternalDatePickerWithForwardRef = forwardRef<HTMLInputElement, ModifiedPr
     // We keep month in state to trigger a re-render when month changes since height will vary by where days fall
     // in the month and we need to keep the popover pointed at the input correctly
     const [, setMonth] = useState<number | null | undefined>();
-    const [format, setFormat] = useState<string | null | undefined>();
-    const [updatedLocale, setUpdatedLocale] = useState<string | null | undefined>();
+    const [format, setFormat] = useState<string | string[] | undefined>();
+    const [updatedLocale, setUpdatedLocale] = useState<
+      ComponentProps<typeof ReactDatePicker>['locale'] | undefined
+    >();
 
     useEffect(() => {
       if (localeData && localeData.code) {
@@ -96,7 +106,6 @@ const InternalDatePickerWithForwardRef = forwardRef<HTMLInputElement, ModifiedPr
     return (
       <div className="_gestalt">
         <div className={isMobile ? undefined : '_gestalt_daterange'}>
-          {/* @ts-expect-error - TS2769 - No overload matches this call. | TS2786 - 'ReactDatePicker' cannot be used as a JSX component. */}
           <ReactDatePicker
             ref={(refElement) => {
               if (!innerInputRef || !refElement) {
