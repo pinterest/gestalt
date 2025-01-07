e4d76ad8b63a0468ae84a1e75640e8d115f311d7
Ravi Lingineni <rlingineni@pinterest.com>
2025-01-07 16:11:37 -0600

    update tests

--- packages/gestalt-icons-android/dist/vr-theme/index.d.ts
+++ packages/gestalt-icons-android/dist/vr-theme/index.d.ts
@@ -343,12 +343,14 @@ declare const icons: Readonly<{
     timer: any;
     'timer-10': any;
     'timer-3': any;
+    tint: any;
     'trash-can': any;
     trending: any;
     undo: any;
     unlock: any;
     'upload-feed': any;
     viber: any;
+    vibrance: any;
     'video-advance-10-seconds': any;
     'video-camera': any;
     'video-rewind-10-seconds': any;
