56db0a84bd1d426c6448ba603d2160f2837ce810
pabmacias <pabmacias28@gmail.com>
2025-01-29 17:57:22 +0000

    change install

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -9,4 +9,4 @@ yarn config set network-timeout 600000 -g
 (cd packages/gestalt && yarn build)
 (cd packages/gestalt-charts && yarn build)
 (cd packages/gestalt-datepicker && yarn build)
-(cd docs && yarn install && PUBLIC_URL=$DEPLOY_PRIME_URL yarn build)
+(cd docs && PUBLIC_URL=$DEPLOY_PRIME_URL yarn build)
