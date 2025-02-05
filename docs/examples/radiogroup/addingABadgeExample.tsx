cc7fb0f32e7a1d144f33287a5aca9c288392331e
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2025-02-05 23:23:22 +0100

    RadioGroup: VR changes  (#3978)

--- docs/examples/radiogroup/addingABadgeExample.tsx
+++ docs/examples/radiogroup/addingABadgeExample.tsx
@@ -7,54 +7,46 @@ export default function RadioButtonBadgeExample() {
   return (
     <Box alignItems="center" display="flex" height="100%" justifyContent="center" padding={8}>
       <RadioGroup id="badgeExample" legend="What badge would you like?">
-        <Box display="inlineBlock">
-          <RadioGroup.RadioButton
-            badge={{ text: 'badge', type: 'success' }}
-            checked={option === 'success'}
-            id="success"
-            label="I'd like a success badge"
-            onChange={() => {
-              setOption('success');
-            }}
-            value="success"
-          />
-        </Box>
-        <Box display="inlineBlock">
-          <RadioGroup.RadioButton
-            badge={{ text: 'badge', type: 'info' }}
-            checked={option === 'info'}
-            id="info"
-            label="I'd like a info badge"
-            onChange={() => {
-              setOption('info');
-            }}
-            value="info"
-          />
-        </Box>
-        <Box display="inlineBlock">
-          <RadioGroup.RadioButton
-            badge={{ text: 'badge', type: 'warning' }}
-            checked={option === 'warning'}
-            id="warning"
-            label="I'd like a warning badge"
-            onChange={() => {
-              setOption('warning');
-            }}
-            value="warning"
-          />
-        </Box>
-        <Box display="inlineBlock">
-          <RadioGroup.RadioButton
-            badge={{ text: 'badge', type: 'neutral' }}
-            checked={option === 'neutral'}
-            id="neutral"
-            label="I'd like a neutral badge"
-            onChange={() => {
-              setOption('neutral');
-            }}
-            value="neutral"
-          />
-        </Box>
+        <RadioGroup.RadioButton
+          badge={{ text: 'badge', type: 'success' }}
+          checked={option === 'success'}
+          id="success"
+          label="I'd like a success badge"
+          onChange={() => {
+            setOption('success');
+          }}
+          value="success"
+        />
+        <RadioGroup.RadioButton
+          badge={{ text: 'badge', type: 'info' }}
+          checked={option === 'info'}
+          id="info"
+          label="I'd like a info badge"
+          onChange={() => {
+            setOption('info');
+          }}
+          value="info"
+        />
+        <RadioGroup.RadioButton
+          badge={{ text: 'badge', type: 'warning' }}
+          checked={option === 'warning'}
+          id="warning"
+          label="I'd like a warning badge"
+          onChange={() => {
+            setOption('warning');
+          }}
+          value="warning"
+        />
+        <RadioGroup.RadioButton
+          badge={{ text: 'badge', type: 'neutral' }}
+          checked={option === 'neutral'}
+          id="neutral"
+          label="I'd like a neutral badge"
+          onChange={() => {
+            setOption('neutral');
+          }}
+          value="neutral"
+        />
       </RadioGroup>
     </Box>
   );
