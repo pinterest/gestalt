76c4dc162518131b4b58f639ea42b26e3406fa9a
ilopezcruz <ilopezcruz@pinterest.com>
2025-02-06 11:13:04 -0600

    Prettier fix

--- packages/gestalt/src/Masonry/scrollUtils.ts
+++ packages/gestalt/src/Masonry/scrollUtils.ts
@@ -21,7 +21,7 @@ export function getWindowScrollPos(): number {
 }
 
 export function getRelativeScrollTop(element: HTMLElement | Window): number {
-  return (element === window || element instanceof Window)
+  return element === window || element instanceof Window
     ? getWindowScrollPos()
     : element.scrollTop - element.getBoundingClientRect().top;
 }
@@ -34,5 +34,5 @@ export function getScrollHeight(element: HTMLElement): number {
 }
 
 export function getScrollPos(element: HTMLElement | Window): number {
-  return (element === window || element instanceof Window) ? getWindowScrollPos() : element.scrollTop;
+  return element === window || element instanceof Window ? getWindowScrollPos() : element.scrollTop;
 }
