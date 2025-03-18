da016ddc467e86fa6bad65a15b212ed104a4979a
Jose Gomez <josetono612@gmail.com>
2025-03-18 16:32:18 -0600

    fix: scroll position glitch on load

--- packages/gestalt/src/Masonry.tsx
+++ packages/gestalt/src/Masonry.tsx
@@ -296,6 +296,8 @@ export default class Masonry<T> extends ReactComponent<Props<T>, State<T>> {
 
   scrollContainer: ScrollContainer | null | undefined;
 
+  maxHeight: number = 0;
+
   /**
    * Delays resize handling in case the scroll container is still being resized.
    */
@@ -691,8 +693,9 @@ export default class Masonry<T> extends ReactComponent<Props<T>, State<T>> {
       const measuringPositions = getPositions(itemsToMeasure);
       // Math.max() === -Infinity when there are no positions
       const height = positions.length
-        ? Math.max(...positions.map((pos) => pos.top + pos.height))
+        ? Math.max(...positions.map((pos) => pos.top + pos.height), this.maxHeight)
         : 0;
+      this.maxHeight = height;
 
       gridBody = (
         <div ref={this.setGridWrapperRef} style={{ width: '100%' }}>
