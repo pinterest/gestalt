801d17bbce0f8eb0846448462b3ad498b285c473
gabriel <gpinheiro@pinterest.com>
2025-03-20 10:12:42 -0700

    adding test

--- playwright/accessibility/ButtonSocial.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-// @ts-expect-error - the path will be correct once the component is moved to the correct location.
-import expectAccessiblePage from './expectAccessiblePage';
-
-test('About us ButtonSocial check', async ({ page }) => {
-  await page.goto('/buttonsocial');
-  await expectAccessiblePage({ page });
-});
