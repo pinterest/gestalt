e05fa84f94c913165846814dd13f0e020ec58dd0
gabriel <gpinheiro@pinterest.com>
2025-03-20 10:25:06 -0700

    fixing test

--- docs/pages/visual-test/ButtonSocial-dark.tsx
+++ docs/pages/visual-test/ButtonSocial-dark.tsx
@@ -3,7 +3,7 @@ import { ButtonSocial, ColorSchemeProvider } from 'gestalt';
 export default function Snapshot() {
   return (
     <ColorSchemeProvider colorScheme="dark">
-      <ButtonSocial />
+      <ButtonSocial service='apple' type='continue'/>
     </ColorSchemeProvider>
   );
 }
