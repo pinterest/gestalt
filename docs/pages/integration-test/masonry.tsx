8dcc9882814af8679a9a0f45339d25258a2f3a0b
Jose Gomez <josetono612@gmail.com>
2025-04-01 16:31:12 -0600

    fix typo

--- docs/pages/integration-test/masonry.tsx
+++ docs/pages/integration-test/masonry.tsx
@@ -3,7 +3,7 @@ import LazyHydrate from 'react-lazy-hydration';
 import { useRouter } from 'next/router';
 import { ColorSchemeProvider, DesignTokensProvider, Masonry, MasonryV2 } from 'gestalt';
 import generateExampleItems from '../../integration-test-helpers/masonry/items-utils/generateExampleItems';
-import generateFiexedTreeColumnExampleItems from '../../integration-test-helpers/masonry/items-utils/generateFixedTreeColumnExampleItems';
+import generateFixedTreeColumnExampleItems from '../../integration-test-helpers/masonry/items-utils/generateFixedTreeColumnExampleItems';
 import generateMultiColumnExampleItems from '../../integration-test-helpers/masonry/items-utils/generateMultiColumnExampleItems';
 import generateRealisticExampleItems from '../../integration-test-helpers/masonry/items-utils/generateRealisticExampleItems';
 import getRandomNumberGenerator from '../../integration-test-helpers/masonry/items-utils/getRandomNumberGenerator';
@@ -139,7 +139,7 @@ export default function TestPage({
       return generateMultiColumnExampleItems({ name: 'MultiColTest' });
     }
     if (fixedThreeColItemsValue) {
-      return generateFiexedTreeColumnExampleItems({ name: 'FixedThreeColItems' });
+      return generateFixedTreeColumnExampleItems({ name: 'FixedThreeColItems' });
     }
     if (realisticPinHeightsValue) {
       return generateRealisticExampleItems({
