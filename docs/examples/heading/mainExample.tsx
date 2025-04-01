3c9f75851f9b4c3ca66a0fc8d458cbadea3c80f5
acarreras <acarreras@pinterest.com>
2025-04-01 16:30:33 -0400

    more

--- docs/examples/heading/mainExample.tsx
+++ docs/examples/heading/mainExample.tsx
@@ -4,6 +4,6 @@ export default function Example() {
   return (
     <Box alignItems="center" display="flex" height="100%" justifyContent="center" padding={8}>
       <Heading size="600">An H2 Heading example</Heading>;
-          </Box>
+    </Box>
   );
 }
