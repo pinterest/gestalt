2a213777691d300cbe7beeb6a200a6230a7d448a
Alberto Carreras <10593890+AlbertCarreras@users.noreply.github.com>
2025-04-07 13:06:56 -0400

    Docs: remove icons page (#4070)

--- playwright/accessibility/foundations__iconography__library.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-import expectAccessiblePage from './expectAccessiblePage';
-
-test('Iconography and SVGs Accessibility check', async ({ page }) => {
-  await page.goto('/foundations/iconography/library');
-  // @ts-expect-error - TS2345 - Argument of type '{ page: Page; }' is not assignable to parameter of type '{ page: any; rules: any; }'.
-  await expectAccessiblePage({ page });
-});
