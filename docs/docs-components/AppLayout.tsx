c87f0c329fb6861b9e3124fe8f71abb808abc421
kostya-gromov <gromovkostya92@gmail.com>
2025-10-16 19:08:11 +0200

    Changed app layout

--- docs/docs-components/AppLayout.tsx
+++ docs/docs-components/AppLayout.tsx
@@ -1,6 +1,6 @@
 import { Fragment, ReactNode, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { Box, DeviceTypeProvider, Divider, FixedZIndex, Flex } from 'gestalt';
+import { Box, DeviceTypeProvider, Divider, FixedZIndex, Flex, Sticky } from 'gestalt';
 import {
   TOKEN_COLOR_GRAY_ROBOFLOW_700,
   TOKEN_COLOR_ORANGE_FIRETINI_0,
@@ -55,85 +55,95 @@ export default function AppLayout({ children, colorScheme }: Props) {
   }, [setIsSidebarOpen]);
 
   let pageContent = (
-    <Box color="default" minHeight="100vh">
-      <SkipToContent />
-      <Header />
-      {isSidebarOpen && (
-        <Fragment>
-          {/* The <div> element has a child <button> element that allows keyboard interaction */}
-          {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
-          <div
-            onClick={(event) => {
-              if (event.target === event.currentTarget) setIsSidebarOpen(false);
-            }}
-            onKeyPress={(event) => {
-              if (event.target === event.currentTarget) setIsSidebarOpen(false);
-            }}
-            style={{
-              position: 'absolute',
-              top: 0,
-              bottom: 0,
-              left: 0,
-              right: 0,
-            }}
-          />
-          <Box
-            bottom
-            display="block"
-            height="100vh"
-            left
-            mdDisplay="none"
-            overflow="scroll"
-            position="absolute"
-            right
-            top
-            width={MIN_NAV_WIDTH_PX}
-            zIndex={new FixedZIndex(11)}
-          >
-            <DocsConfigProvider isMobile>
-              <DeviceTypeProvider deviceType="mobile">
-                <DocsSideNavigation showBorder />
-              </DeviceTypeProvider>
-            </DocsConfigProvider>
-          </Box>
-        </Fragment>
-      )}
+    <Box
+      color="default"
+      dangerouslySetInlineStyle={{
+        __style: { display: 'grid', gridTemplateColumns: '100%', gridTemplateRows: 'auto 1fr' },
+      }}
+      minHeight="100vh"
+    >
+      <Sticky top={0}>
+        <SkipToContent />
+        <Header />
+        {isSidebarOpen && (
+          <Fragment>
+            {/* The <div> element has a child <button> element that allows keyboard interaction */}
+            {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+            <div
+              onClick={(event) => {
+                if (event.target === event.currentTarget) setIsSidebarOpen(false);
+              }}
+              onKeyPress={(event) => {
+                if (event.target === event.currentTarget) setIsSidebarOpen(false);
+              }}
+              style={{
+                position: 'absolute',
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+              }}
+            />
+            <Box
+              bottom
+              display="block"
+              height="100vh"
+              left
+              mdDisplay="none"
+              overflow="scroll"
+              position="absolute"
+              right
+              top
+              width={MIN_NAV_WIDTH_PX}
+              zIndex={new FixedZIndex(11)}
+            >
+              <DocsConfigProvider isMobile>
+                <DeviceTypeProvider deviceType="mobile">
+                  <DocsSideNavigation showBorder />
+                </DeviceTypeProvider>
+              </DocsConfigProvider>
+            </Box>
+          </Fragment>
+        )}
+      </Sticky>
       <Box mdDisplay="flex">
-        <Box mdDisplay={shouldHideSideNav ? 'none' : 'block'} minWidth={MIN_NAV_WIDTH_PX}>
-          <Box
-            dangerouslySetInlineStyle={{
-              __style: { position: 'sticky', top: HEADER_HEIGHT_WITH_MARGIN },
-            }}
-            display="none"
-            height={`calc(100vh - ${HEADER_HEIGHT_WITH_MARGIN}px)`}
-            marginTop={2}
-            mdDisplay="block"
-          >
-            <DocsSideNavigation showBorder />
+        <Box mdDisplay="flex">
+          <Box mdDisplay={shouldHideSideNav ? 'none' : 'block'} minWidth={MIN_NAV_WIDTH_PX}>
+            <Box
+              dangerouslySetInlineStyle={{
+                __style: { position: 'sticky', top: HEADER_HEIGHT_WITH_MARGIN },
+              }}
+              display="none"
+              height={`calc(100vh - ${HEADER_HEIGHT_WITH_MARGIN}px)`}
+              marginTop={2}
+              mdDisplay="block"
+            >
+              <DocsSideNavigation showBorder />
+            </Box>
           </Box>
         </Box>
-
-        <Box minWidth={0} width="100%">
-          <Box
-            justifyContent="center"
-            marginBottom={12}
-            mdDisplay="flex"
-            mdPadding={8}
-            mdPaddingY={12}
-            minHeight="100%"
-            padding={4}
-            role="main"
-            width="100%"
-          >
-            <Flex
-              alignItems="center"
-              direction="column"
-              flex="none"
-              maxWidth={CONTENT_MAX_WIDTH_PX}
+        <Box direction="column" display="flex" justifyContent="between" width="100%">
+          <Box minWidth={0} width="100%">
+            <Box
+              justifyContent="center"
+              marginBottom={12}
+              mdDisplay="flex"
+              mdPadding={8}
+              mdPaddingY={12}
+              padding={4}
+              role="main"
               width="100%"
             >
-              {children}
-            </Flex>
+              <Flex
+                alignItems="center"
+                direction="column"
+                flex="none"
+                maxWidth={CONTENT_MAX_WIDTH_PX}
+                width="100%"
+              >
+                {children}
+              </Flex>
+            </Box>
           </Box>
           <Box
             color={!isHomePage ? 'default' : undefined}
